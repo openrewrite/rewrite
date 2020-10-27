2e5f9b866074a05e5c9fb10e02c6ed53f220a26a
Sofia Britto Schwartz <sofia.b.schwartz@gmail.com>
2020-10-27 12:44:54 -0700

    Moved @DebugOnly test runners to DebugTest files

--- rewrite-java-11/src/test/kotlin/org/openrewrite/java/Java11RefactorDebugTest.kt
+++ rewrite-java-11/src/test/kotlin/org/openrewrite/java/Java11RefactorDebugTest.kt
@@ -18,7 +18,6 @@ package org.openrewrite.java
 import org.junit.jupiter.api.extension.ExtendWith
 import org.openrewrite.DebugOnly
 import org.openrewrite.java.tree.AutoFormatTest
-import org.openrewrite.java.search.FindTypeTest
 import org.openrewrite.java.utilities.SpansMultipleLinesTest
 import org.openrewrite.java.tree.TreeBuilderTest
 
@@ -137,10 +136,6 @@ class Java11UseStaticImportTest: Java11Test(), UseStaticImportTest
 @DebugOnly
 @ExtendWith(JavaParserResolver::class)
 class Java11TreeBuilderTest: Java11Test(), TreeBuilderTest
-
-@DebugOnly
-@ExtendWith(JavaParserResolver::class)
-class Java11FindTypeTest: Java11Test(), FindTypeTest
 
 @DebugOnly
 @ExtendWith(JavaParserResolver::class)
