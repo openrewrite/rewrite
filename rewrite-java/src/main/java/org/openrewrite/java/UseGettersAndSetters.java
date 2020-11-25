b20f9fa395c679069a5c6a507c737332b9726de4
Greg Adams <gadams@gmail.com>
2020-11-25 09:46:56 -0800

    UseGettersAndSetters visitor
    code review corrections
    - support yaml use of UseGettersAndSetters
    - limit refactoring to target class
    - cleanup

--- rewrite-java/src/main/java/org/openrewrite/java/UseGettersAndSetters.java
+++ rewrite-java/src/main/java/org/openrewrite/java/UseGettersAndSetters.java
@@ -2,7 +2,7 @@ package org.openrewrite.java;
 
 import org.openrewrite.Formatting;
 import org.openrewrite.Tree;
-import org.openrewrite.internal.lang.Nullable;
+import org.openrewrite.Validated;
 import org.openrewrite.java.tree.J;
 import org.openrewrite.java.tree.JavaType;
 import org.openrewrite.java.tree.TypeUtils;
@@ -19,97 +19,129 @@ public class UseGettersAndSetters extends JavaIsoRefactorVisitor {
         setCursoringOn();
     }
 
-    public void setClassType(JavaType.Class classType) {
-        this.classType = classType;
+    public void setClassType(String classType) {
+        this.classType = JavaType.Class.build(classType);
     }
 
     @Override
-    public J.VariableDecls.NamedVar visitVariable(J.VariableDecls.NamedVar variable) {
-        if (variable.isField(getCursor())) {
-            J.VariableDecls variableDecl = getCursor().firstEnclosing(J.VariableDecls.class);
-            if (variableDecl != null && J.Modifier.hasModifier(variableDecl.getModifiers(), "public")) {
-                String fieldName = variable.getSimpleName();
-                andThen(new ScopedPrivateField(variableDecl));
-                andThen(new GenerateGetter.Scoped(enclosingClass(), fieldName));
-                andThen(new GenerateSetter.Scoped(enclosingClass(), fieldName));
-                andThen(new ScopedUpdateReferences(fieldName, classType));
-            }
-        }
-        return super.visitVariable(variable);
+    public Validated validate() {
+        return Validated.required("classType", classType);
     }
 
-    public static class ScopedPrivateField extends JavaIsoRefactorVisitor {
+    @Override
+    public J.ClassDecl visitClassDecl(J.ClassDecl classDecl) {
+        if (classType != null && classDecl.getType() != null
+                && classType.getFullyQualifiedName().equals(classDecl.getType().getFullyQualifiedName())) {
+            andThen(new Scoped(classDecl));
+        }
+        return classDecl;
+    }
 
-        private final J.VariableDecls scope;
+    public static class Scoped extends JavaIsoRefactorVisitor {
 
-        public ScopedPrivateField(J.VariableDecls scope) {
+        private final J.ClassDecl scope;
+
+        public Scoped(J.ClassDecl scope) {
             this.scope = scope;
-        }
-
-        @Override
-        public J.VariableDecls visitMultiVariable(J.VariableDecls multiVariable) {
-            if (scope.isScope(multiVariable)) {
-                return multiVariable.withModifiers(J.Modifier.withModifiers(multiVariable.getModifiers(), "private"));
-            }
-            return super.visitMultiVariable(multiVariable);
-        }
-    }
-
-    public static class ScopedUpdateReferences extends JavaRefactorVisitor {
-
-        private final String fieldName;
-        private final JavaType.Class classType;
-
-        public ScopedUpdateReferences(String fieldName, JavaType.Class classType) {
-            this.fieldName = fieldName;
-            this.classType = classType;
             setCursoringOn();
         }
 
         @Override
-        public J visitFieldAccess(J.FieldAccess fieldAccess) {
-            if (matchesClass(fieldAccess.getTarget().getType()) && fieldAccess.getName().getSimpleName().equals(fieldName)) {
-                Tree t = getCursor().getParentOrThrow().getTree();
-                if (t instanceof J.Assign) {
-                    J.Assign assign = (J.Assign) t;
-                    andThen(new ScopedAssign(assign, fieldName));
-                } else {
-                    List<J> elements = treeBuilder.buildSnippet(getCursor().getParentOrThrow(),
-                fieldAccess.getTarget().printTrimmed() + ".get" + capitalize(fieldName) + "()");
-                    return elements.get(0).withFormatting(Formatting.format(" ", ""));
+        public J.ClassDecl visitClassDecl(J.ClassDecl classDecl) {
+            if (scope.isScope(classDecl)) {
+                return super.visitClassDecl(classDecl);
+            }
+            return classDecl;
+        }
+
+        @Override
+        public J.VariableDecls.NamedVar visitVariable(J.VariableDecls.NamedVar variable) {
+            if (variable.isField(getCursor())) {
+                J.VariableDecls variableDecl = getCursor().firstEnclosing(J.VariableDecls.class);
+                if (variableDecl != null && J.Modifier.hasModifier(variableDecl.getModifiers(), "public")) {
+                    String fieldName = variable.getSimpleName();
+                    andThen(new ChangePublicFieldToPrivate(variableDecl));
+                    andThen(new GenerateGetter.Scoped(enclosingClass(), fieldName));
+                    andThen(new GenerateSetter.Scoped(enclosingClass(), fieldName));
+                    andThen(new UpdateReferences(fieldName, scope));
                 }
             }
-            return super.visitFieldAccess(fieldAccess);
+            return super.visitVariable(variable);
         }
 
+        private static class ChangePublicFieldToPrivate extends JavaIsoRefactorVisitor {
 
-        private boolean matchesClass(@Nullable JavaType test) {
-            JavaType.Class testClassType = TypeUtils.asClass(test);
-            return testClassType != null && testClassType.getFullyQualifiedName().equals(classType.getFullyQualifiedName());
-        }
+            private final J.VariableDecls scope;
 
-        public static class ScopedAssign extends JavaRefactorVisitor {
-
-            private final J.Assign scope;
-            private final String fieldName;
-
-            public ScopedAssign(J.Assign scope, String fieldName) {
+            public ChangePublicFieldToPrivate(J.VariableDecls scope) {
                 this.scope = scope;
+            }
+
+            @Override
+            public J.VariableDecls visitMultiVariable(J.VariableDecls multiVariable) {
+                if (scope.isScope(multiVariable)) {
+                    return multiVariable.withModifiers(J.Modifier.withModifiers(multiVariable.getModifiers(), "private"));
+                }
+                return super.visitMultiVariable(multiVariable);
+            }
+        }
+
+        private static class UpdateReferences extends JavaRefactorVisitor {
+
+            private final String fieldName;
+            private final J.ClassDecl classDecl;
+
+            public UpdateReferences(String fieldName, J.ClassDecl classDecl) {
                 this.fieldName = fieldName;
+                this.classDecl = classDecl;
                 setCursoringOn();
             }
 
             @Override
-            public J visitAssign(J.Assign assign) {
-                if (scope.isScope(assign)) {
-                    J.FieldAccess fieldAccess = (J.FieldAccess) assign.getVariable();
-                    List<J> elements = treeBuilder.buildSnippet(getCursor().getParentOrThrow(),
-                            fieldAccess.getTarget().printTrimmed() + ".set" + capitalize(fieldName) + "(" + assign.getAssignment().printTrimmed() + ")");
-                    return elements.get(0);
+            public J visitFieldAccess(J.FieldAccess fieldAccess) {
+                JavaType.Class targetClassType = TypeUtils.asClass(fieldAccess.getTarget().getType());
+                if (classDecl != null
+                        && classDecl.getType() != null
+                        && targetClassType != null
+                        && targetClassType.getFullyQualifiedName().equals(classDecl.getType().getFullyQualifiedName())
+                        && fieldAccess.getName().getSimpleName().equals(fieldName)) {
+                    Tree t = getCursor().getParentOrThrow().getTree();
+                    if (t instanceof J.Assign) {
+                        J.Assign assign = (J.Assign) t;
+                        andThen(new ChangeAssignmentToSetter(assign, fieldName));
+                    } else {
+                        List<J> elements = treeBuilder.buildSnippet(getCursor().getParentOrThrow(),
+                                fieldAccess.getTarget().printTrimmed() + ".get" + capitalize(fieldName) + "()");
+                        return elements.get(0).withFormatting(Formatting.format(" ", ""));
+                    }
                 }
-                return super.visitAssign(assign);
+                return super.visitFieldAccess(fieldAccess);
+            }
+
+            private static class ChangeAssignmentToSetter extends JavaRefactorVisitor {
+
+                private final J.Assign scope;
+                private final String fieldName;
+
+                public ChangeAssignmentToSetter(J.Assign scope, String fieldName) {
+                    this.scope = scope;
+                    this.fieldName = fieldName;
+                    setCursoringOn();
+                }
+
+                @Override
+                public J visitAssign(J.Assign assign) {
+                    if (scope.isScope(assign)) {
+                        J.FieldAccess fieldAccess = (J.FieldAccess) assign.getVariable();
+                        List<J> elements = treeBuilder.buildSnippet(getCursor().getParentOrThrow(),
+                                fieldAccess.getTarget().printTrimmed() + ".set" + capitalize(fieldName) + "(" + assign.getAssignment().printTrimmed() + ")");
+                        return elements.get(0);
+                    }
+                    return super.visitAssign(assign);
+                }
             }
         }
+
     }
 
 }
--- rewrite-test/src/main/kotlin/org/openrewrite/java/UseGettersAndSettersTest.kt
+++ rewrite-test/src/main/kotlin/org/openrewrite/java/UseGettersAndSettersTest.kt
@@ -6,49 +6,86 @@ import org.openrewrite.RefactorVisitorTest
 interface UseGettersAndSettersTest : RefactorVisitorTest {
 
     @Test
-    fun useGettersAndSetters(jp: JavaParser) = assertRefactored(
+    fun shouldChangePublicFieldIntoGetterAndSetter(jp: JavaParser) = assertRefactored(
             jp,
-            visitorsMapped = listOf { cu -> UseGettersAndSetters().apply { setClassType(cu.classes[0].type)} },
+            visitorsMapped = listOf { cu -> UseGettersAndSetters().apply { setClassType("A") } },
             before =
-                """
-                    public class A {
-                        public String foo;
-                    }
-                    
-                    public class B {
-                        public void useFoo() {
-                            A a = new A();
-                            a.foo = "bar";
-                            String x = a.foo;
-                            
-                            System.out.println("hi" + a.foo);
-                        }
-                    }
-                """
-            ,
+            """
+                public class A {
+                    public String foo;
+                }
+            """,
             after =
-                """
-                    public class A {
-                        private String foo;
+            """
+                public class A {
+                    private String foo;
 
-                        public String getFoo() {
-                            return foo;
-                        }
+                    public String getFoo() {
+                        return foo;
+                    }
 
-                        public void setFoo(String value) {
-                            foo = value;
-                        }
+                    public void setFoo(String value) {
+                        foo = value;
                     }
-                    
-                    public class B {
-                        public void useFoo() {
-                            A a = new A();
-                            a.setFoo("bar");
-                            String x = a.getFoo();
-                            
-                            System.out.println("hi" + a.getFoo());
-                        }
+                }
+            """
+    )
+
+    @Test
+    fun shouldUpdateUsagesInDependentClass(jp: JavaParser) = assertRefactored(
+            jp,
+            visitorsMapped = listOf { cu -> UseGettersAndSetters().apply { setClassType("A") } },
+            before =
+            """
+                public class A {
+                    public String foo;
+                }
+                
+                public class B {
+                    public void useFoo() {
+                        A a = new A();
+                        a.foo = "bar";
+                        String x = a.foo;
+                        
+                        System.out.println("hi" + a.foo);
                     }
-                """
+                }
+            """,
+            after =
+            """
+                public class A {
+                    private String foo;
+
+                    public String getFoo() {
+                        return foo;
+                    }
+
+                    public void setFoo(String value) {
+                        foo = value;
+                    }
+                }
+                
+                public class B {
+                    public void useFoo() {
+                        A a = new A();
+                        a.setFoo("bar");
+                        String x = a.getFoo();
+                        
+                        System.out.println("hi" + a.getFoo());
+                    }
+                }
+            """
+    )
+
+    @Test
+    fun shouldNotAffectClassNotTargeted(jp: JavaParser) = assertUnchanged(
+            jp,
+            visitorsMapped = listOf { cu -> UseGettersAndSetters().apply { setClassType("B") } },
+            before =
+            """
+                public class A {
+                    public String foo;
+                }                    
+            """
     )
 }
