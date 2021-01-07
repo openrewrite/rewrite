e0d3783b009bc7fbb76677d7d61e9ed0d384ec9d
Greg Adams <greg@moderne.io>
2021-01-06 17:35:57 -0800

    functional FindTag test

---
+++ rewrite-xml/src/main/java/org/openrewrite/xml/search/FindTag.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2020 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.xml.search;
+
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.Recipe;
+import org.openrewrite.marker.SearchResult;
+import org.openrewrite.xml.XPathMatcher;
+import org.openrewrite.xml.XmlProcessor;
+import org.openrewrite.xml.tree.Xml;
+
+public class FindTag extends Recipe {
+
+    private XPathMatcher xPathMatcher;
+
+    public FindTag() {
+        this.processor = () -> new FindTagProcessor(xPathMatcher);
+    }
+
+    public void setXPath(String xpath) {
+        this.xPathMatcher = new XPathMatcher(xpath);
+    }
+
+    private static class FindTagProcessor extends XmlProcessor<ExecutionContext> {
+
+        private final XPathMatcher xPathMatcher;
+
+        public FindTagProcessor(XPathMatcher xPathMatcher) {
+            this.xPathMatcher = xPathMatcher;
+            setCursoringOn();
+        }
+
+        @Override
+        public Xml visitTag(Xml.Tag tag, ExecutionContext ctx) {
+            if (xPathMatcher.matches(getCursor())) {
+                return tag.withMarkers(tag.getMarkers().add(new SearchResult()));
+            }
+            return super.visitTag(tag, ctx);
+        }
+    }
+}
