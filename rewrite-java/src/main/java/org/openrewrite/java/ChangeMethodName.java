a1b7dd80bc45a54b70d058fa7f76aa386ee72c36
Tyler Van Gorder <tkvangorder@users.noreply.github.com>
2021-01-20 23:05:41 -0800

    Recipe Immutable Members

--- rewrite-java/src/main/java/org/openrewrite/java/ChangeMethodName.java
+++ rewrite-java/src/main/java/org/openrewrite/java/ChangeMethodName.java
@@ -15,8 +15,6 @@
  */
 package org.openrewrite.java;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import org.openrewrite.ExecutionContext;
 import org.openrewrite.Recipe;
