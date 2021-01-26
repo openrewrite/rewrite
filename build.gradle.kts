134fbefa0a75c0fd0d06938d80013f5d8ab60238
Sam Snyder <sam@moderne.io>
2021-01-25 20:32:44 -0800

    Fix up kotlin standard library exclusion

--- build.gradle.kts
+++ build.gradle.kts
@@ -149,8 +149,9 @@ subprojects {
                                 (dependencyList.item(i) as org.w3c.dom.Element).let { dependency ->
                                     if ((dependency.getElementsByTagName("scope")
                                                     .item(0) as org.w3c.dom.Element).textContent == "provided"
-                                        || (dependency.getElementsByTagName("groupId")
-                                            .item(0) as org.w3c.dom.Element).textContent == "org.jetbrains.kotlin") {
+                                        || (project.name != "rewrite-test"
+                                                && (dependency.getElementsByTagName("groupId")
+                                            .item(0) as org.w3c.dom.Element).textContent == "org.jetbrains.kotlin")) {
                                         dependencies.removeChild(dependency)
                                         i--
                                         length--
