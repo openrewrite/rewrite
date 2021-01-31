f565346b491f4297542dfb2e5d4dfa2a5861ca54
Jonathan Schneider <jkschneider@gmail.com>
2021-01-31 12:11:51 -0800

    Shade jgit

--- rewrite-core/build.gradle.kts
+++ rewrite-core/build.gradle.kts
@@ -1,3 +1,9 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+plugins {
+    id("com.github.johnrengelman.shadow") version "6.1.0"
+}
+
 dependencies {
     implementation("org.eclipse.jgit:org.eclipse.jgit:latest.release")
 
@@ -12,4 +18,52 @@ dependencies {
     implementation("org.slf4j:slf4j-api:1.7.+")
 
     api("com.google.code.findbugs:jsr305:latest.release")
+}
+
+tasks.withType<ShadowJar> {
+    configurations = listOf(project.configurations.compileClasspath.get())
+    archiveClassifier.set(null as String?)
+    dependencies {
+        include(dependency("org.eclipse.jgit:"))
+    }
+    relocate("org.eclipse.jgit", "org.openrewrite.shaded.jgit")
+    metaInf {
+        from("$rootDir/LICENSE")
+        from("$rootDir/NOTICE")
+    }
+}
+
+tasks.withType<Jar> {
+    if(name != "shadowJar") {
+        enabled = false
+        setDependsOn(tasks.withType<ShadowJar>())
+    }
+}
+
+configure<PublishingExtension> {
+    publications {
+        named("nebula", MavenPublication::class.java) {
+            artifact(tasks.withType<ShadowJar>().first())
+
+            pom.withXml {
+                (asElement().getElementsByTagName("dependencies").item(0) as org.w3c.dom.Element).let { dependencies ->
+                    dependencies.getElementsByTagName("dependency").let { dependencyList ->
+                        var i = 0
+                        var length = dependencyList.length
+                        while (i < length) {
+                            (dependencyList.item(i) as org.w3c.dom.Element).let { dependency ->
+                                if (((dependency.getElementsByTagName("groupId").item(0) as org.w3c.dom.Element).textContent ==
+                                            "org.eclipse.jgit")) {
+                                    dependencies.removeChild(dependency)
+                                    i--
+                                    length--
+                                }
+                            }
+                            i++
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
