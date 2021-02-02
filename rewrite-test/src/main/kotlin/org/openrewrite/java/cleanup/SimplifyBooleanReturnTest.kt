38ab7d9a3d4f422784e0089a155a532e958f0981
Aaron Gershman <aegershman@gmail.com>
2021-02-02 16:47:10 -0600

    formatting - fix indentation

--- rewrite-test/src/main/kotlin/org/openrewrite/java/cleanup/SimplifyBooleanReturnTest.kt
+++ rewrite-test/src/main/kotlin/org/openrewrite/java/cleanup/SimplifyBooleanReturnTest.kt
@@ -29,160 +29,160 @@ interface SimplifyBooleanReturnTest : RecipeTest {
     fun simplifyBooleanReturn(jp: JavaParser) = assertChanged(
         jp,
         before = """
-                public class A {
-                    boolean ifNoElse() {
-                        if (isOddMillis()) {
-                            return true;
-                        }
+            public class A {
+                boolean ifNoElse() {
+                    if (isOddMillis()) {
+                        return true;
+                    }
+                    return false;
+                }
+                
+                static boolean isOddMillis() {
+                    boolean even = System.currentTimeMillis() % 2 == 0;
+                    if (even == true) {
                         return false;
                     }
-                    
-                    static boolean isOddMillis() {
-                        boolean even = System.currentTimeMillis() % 2 == 0;
-                        if (even == true) {
-                            return false;
-                        }
-                        else {
-                            return true;
-                        }
+                    else {
+                        return true;
                     }
                 }
-            """,
+            }
+        """,
         after = """
-                public class A {
-                    boolean ifNoElse() {
-                        return isOddMillis();
-                    }
-                    
-                    static boolean isOddMillis() {
-                        boolean even = System.currentTimeMillis() % 2 == 0;
-                        return !(even == true);
-                    }
+            public class A {
+                boolean ifNoElse() {
+                    return isOddMillis();
                 }
-            """
+                
+                static boolean isOddMillis() {
+                    boolean even = System.currentTimeMillis() % 2 == 0;
+                    return !(even == true);
+                }
+            }
+        """
     )
 
     @Test
     fun dontSimplifyToReturnUnlessLastStatement(jp: JavaParser) = assertChanged(
         jp,
         before = """
-                public class A {
-                    public boolean absurdEquals(Object o) {
-                        if(this == o) {
-                            return true;
-                        }
-                        if(this == o) {
-                            return true;
-                        }
-                        return false;
+            public class A {
+                public boolean absurdEquals(Object o) {
+                    if(this == o) {
+                        return true;
                     }
+                    if(this == o) {
+                        return true;
+                    }
+                    return false;
                 }
-            """,
+            }
+        """,
         after = """
-                public class A {
-                    public boolean absurdEquals(Object o) {
-                        if(this == o) {
-                            return true;
-                        }
-                        return this == o;
+            public class A {
+                public boolean absurdEquals(Object o) {
+                    if(this == o) {
+                        return true;
                     }
+                    return this == o;
                 }
-            """
+            }
+        """
     )
 
     @Test
     fun nestedIfsWithNoBlock(jp: JavaParser) = assertUnchanged(
         jp,
         before = """
-                public class A {
-                    public boolean absurdEquals(Object o) {
-                        if(this == o)
-                            if(this == 0) 
-                                return true;
-                        return false;
-                    }
+            public class A {
+                public boolean absurdEquals(Object o) {
+                    if(this == o)
+                        if(this == 0) 
+                            return true;
+                    return false;
                 }
-            """
+            }
+        """
     )
 
     @Test
     fun dontAlterWhenElseIfPresent(jp: JavaParser) = assertUnchanged(
         jp,
         before = """
-                public class A {
-                    public boolean foo(int n) {
-                        if (n == 1) {
-                            return false;
-                        } 
-                        else if (n == 2) {
-                            return true;
-                        } 
-                        else {
-                            return false;
-                        }
+            public class A {
+                public boolean foo(int n) {
+                    if (n == 1) {
+                        return false;
+                    } 
+                    else if (n == 2) {
+                        return true;
+                    } 
+                    else {
+                        return false;
                     }
                 }
-            """
+            }
+        """
     )
 
     @Test
     fun dontAlterWhenElseContainsSomethingOtherThanReturn(jp: JavaParser) = assertUnchanged(
         jp,
         before = """
-                public class A {
-                    public boolean foo(int n) {
-                        if (n == 1) {
-                            return true;
-                        } 
-                        else {
-                            System.out.println("side effect");
-                            return false;
-                        } 
-                    }
+            public class A {
+                public boolean foo(int n) {
+                    if (n == 1) {
+                        return true;
+                    } 
+                    else {
+                        System.out.println("side effect");
+                        return false;
+                    } 
                 }
-            """
+            }
+        """
     )
 
     @Test
     fun onlySimplifyToReturnWhenLastStatement(jp: JavaParser) = assertUnchanged(
         jp,
         before = """
-                import java.util.*;
-                public class A {
-                    public static boolean deepEquals(List<byte[]> l, List<byte[]> r) {
-                        for (int i = 0; i < l.size(); ++i) {
-                            if (!Arrays.equals(l.get(i), r.get(i))) {
-                                return false;
-                            }
+            import java.util.*;
+            public class A {
+                public static boolean deepEquals(List<byte[]> l, List<byte[]> r) {
+                    for (int i = 0; i < l.size(); ++i) {
+                        if (!Arrays.equals(l.get(i), r.get(i))) {
+                            return false;
                         }
-                        return true;
                     }
+                    return true;
                 }
-            """
+            }
+        """
     )
 
     @Test
     fun wrapNotReturnsOfTernaryIfConditionsInParentheses(jp: JavaParser) = assertChanged(
         jp,
         before = """
-                public class A {
-                    Object failure;
-                    public boolean equals(Object o) {
-                        if (failure != null ? !failure.equals(this.failure) : this.failure != null) {
-                            return false;
-                        }
-                        return true;
+            public class A {
+                Object failure;
+                public boolean equals(Object o) {
+                    if (failure != null ? !failure.equals(this.failure) : this.failure != null) {
+                        return false;
                     }
+                    return true;
                 }
-            """,
+            }
+        """,
         after = """
-                public class A {
-                    Object failure;
-                    public boolean equals(Object o) {
-                        return !(failure != null ? !failure.equals(this.failure) : this.failure != null);
-                    }
+            public class A {
+                Object failure;
+                public boolean equals(Object o) {
+                    return !(failure != null ? !failure.equals(this.failure) : this.failure != null);
                 }
-            """
+            }
+        """
     )
 
 }
