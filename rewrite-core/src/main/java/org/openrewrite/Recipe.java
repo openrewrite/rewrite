e7cbd733cc176332a0ca4328c74f85a10147ca0e
Greg Adams <greg@moderne.io>
2021-02-12 13:56:01 -0800

    use watched ExecutionContext when visiting

--- rewrite-core/src/main/java/org/openrewrite/Recipe.java
+++ rewrite-core/src/main/java/org/openrewrite/Recipe.java
@@ -200,7 +200,7 @@ public class Recipe {
 
         WatchForNewMessageExecutionContext ctxWithWatch = new WatchForNewMessageExecutionContext(ctx);
         for (int i = 0; i < maxCycles; i++) {
-            after = visitInternal(before, ctx, forkJoinPool, recipeThatDeletedSourceFile);
+            after = visitInternal(before, ctxWithWatch, forkJoinPool, recipeThatDeletedSourceFile);
             if (after == acc && !ctxWithWatch.needAnotherCycle) {
                 break;
             }
