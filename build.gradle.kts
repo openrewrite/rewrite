ea9f8e2c543a657a647e493c4e65c84267e2c71f
Sam Snyder <sam@moderne.io>
2021-03-14 16:51:45 -0700

    Exclude text files from license checks

--- build.gradle.kts
+++ build.gradle.kts
@@ -145,7 +145,7 @@ subprojects {
     configure<LicenseExtension> {
         ext.set("year", Calendar.getInstance().get(Calendar.YEAR))
         skipExistingHeaders = true
-        excludePatterns.addAll(listOf("**/*.tokens", "**/*.config", "**/*.interp"))
+        excludePatterns.addAll(listOf("**/*.tokens", "**/*.config", "**/*.interp", "**/*.txt"))
         header = project.rootProject.file("gradle/licenseHeader.txt")
         mapping(mapOf("kt" to "SLASHSTAR_STYLE", "java" to "SLASHSTAR_STYLE"))
         strictCheck = true
