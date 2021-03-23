3addc5dcb99b2b776731b749cf25e0ccef14919b
Aaron Gershman <5619476+aegershman@users.noreply.github.com>
2021-03-23 16:43:09 -0500

    ChangeMethodTargetToStatic benchmarking [#392] (#393)

---
+++ rewrite-benchmarks/src/jmh/java/org/openrewrite/benchmarks/java/ChangeMethodTargetToStaticBenchmark.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2020 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.benchmarks.java;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.profile.GCProfiler;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openrewrite.java.ChangeMethodTargetToStatic;
+
+import java.util.concurrent.TimeUnit;
+
+@Fork(1)
+@Measurement(iterations = 2)
+@Warmup(iterations = 2)
+@BenchmarkMode(Mode.SampleTime)
+@OutputTimeUnit(TimeUnit.SECONDS)
+@Threads(4)
+public class ChangeMethodTargetToStaticBenchmark {
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(ChangeMethodTargetToStaticBenchmark.class.getSimpleName())
+                .addProfiler(GCProfiler.class)
+                .build();
+        new Runner(opt).run();
+    }
+
+    @Benchmark
+    public void changeMethodTargetToStatic(JavaCompilationUnitState state) {
+        new ChangeMethodTargetToStatic("org.junit.Assert assertThat(..)", "org.hamcrest.MatcherAssert")
+                .run(state.getSourceFiles());
+    }
+
+}
