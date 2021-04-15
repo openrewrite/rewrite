40d1989dde7410d6119526f6e76eba045015433b
Jonathan Schneider <jkschneider@gmail.com>
2021-04-14 21:00:52 -0700

    ChangeValue YAML recipe (fixes #440)
    - Fix license headers

--- rewrite-yaml/src/main/java/org/openrewrite/yaml/ChangeValue.java
+++ rewrite-yaml/src/main/java/org/openrewrite/yaml/ChangeValue.java
@@ -21,28 +21,31 @@ import org.openrewrite.ExecutionContext;
 import org.openrewrite.Option;
 import org.openrewrite.Recipe;
 import org.openrewrite.TreeVisitor;
+import org.openrewrite.marker.Markers;
 import org.openrewrite.yaml.tree.Yaml;
+
+import static org.openrewrite.Tree.randomId;
 
 @Value
 @EqualsAndHashCode(callSuper = true)
-public class ChangeKey extends Recipe {
-    @Option(displayName = "Old key path",
+public class ChangeValue extends Recipe {
+    @Option(displayName = "Key path",
             description = "An XPath expression to locate a YAML entry.",
             example = "subjects/kind")
     String oldKeyPath;
 
-    @Option(displayName = "New key",
-            example = "kind")
-    String newKey;
+    @Option(displayName = "New value",
+            example = "Deployment")
+    String value;
 
     @Override
     public String getDisplayName() {
-        return "Change key";
+        return "Change value";
     }
 
     @Override
     public String getDescription() {
-        return "Change a YAML mapping entry key leaving the value intact.";
+        return "Change a YAML mapping entry value leaving the key intact.";
     }
 
     @Override
@@ -53,7 +56,10 @@ public class ChangeKey extends Recipe {
             public Yaml visitMappingEntry(Yaml.Mapping.Entry entry, ExecutionContext context) {
                 Yaml.Mapping.Entry e = (Yaml.Mapping.Entry) super.visitMappingEntry(entry, context);
                 if (xPathMatcher.matches(getCursor())) {
-                    e = e.withKey(e.getKey().withValue(newKey));
+                    e = e.withValue(
+                            new Yaml.Scalar(randomId(), e.getValue().getPrefix(), Markers.EMPTY,
+                                    Yaml.Scalar.Style.PLAIN, value)
+                    );
                 }
                 return e;
             }
