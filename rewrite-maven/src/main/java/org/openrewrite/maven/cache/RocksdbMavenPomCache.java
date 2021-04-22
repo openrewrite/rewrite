de82fa68b9a78e3d6a91a1b9283564b2b84b6276
Tyler Van Gorder <tkvangorder@users.noreply.github.com>
2021-04-21 17:52:18 -0700

    Treat maven cache deserialization errors as a cache miss

--- rewrite-maven/src/main/java/org/openrewrite/maven/cache/RocksdbMavenPomCache.java
+++ rewrite-maven/src/main/java/org/openrewrite/maven/cache/RocksdbMavenPomCache.java
@@ -39,7 +39,6 @@ import org.rocksdb.WriteOptions;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URI;
 import java.nio.charset.StandardCharsets;
