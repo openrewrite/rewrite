c0b3b0c80c745951b01fa6fe886014b7a277c4f5
Aaron Gershman <aegershman@gmail.com>
2021-05-26 18:59:00 -0500

    ability to remove maven plugins from maven pom

---
+++ rewrite-maven/src/main/java/org/openrewrite/maven/RemovePlugin.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2020 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.maven;
+
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+import org.openrewrite.*;
+import org.openrewrite.xml.RemoveContentVisitor;
+import org.openrewrite.xml.XPathMatcher;
+import org.openrewrite.xml.tree.Xml;
+
+@Value
+@EqualsAndHashCode(callSuper = true)
+@Incubating(since = "7.7.0")
+public class RemovePlugin extends Recipe {
+
+    private static final XPathMatcher PLUGIN_MATCHER = new XPathMatcher("/project/*/plugins/plugin");
+
+    @Option(displayName = "Group",
+            description = "The first part of a dependency coordinate 'org.openrewrite.maven:rewrite-maven-plugin:VERSION'.",
+            example = "org.openrewrite.maven")
+    String groupId;
+
+    @Option(displayName = "Artifact",
+            description = "The second part of a dependency coordinate 'org.openrewrite.maven:rewrite-maven-plugin:VERSION'.",
+            example = "rewrite-maven-plugin")
+    String artifactId;
+
+    @Override
+    public String getDisplayName() {
+        return "Remove Maven plugin";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Remove the specified Maven plugin from the pom.xml.";
+    }
+
+    @Override
+    protected TreeVisitor<?, ExecutionContext> getVisitor() {
+        return new RemovePluginVisitor();
+    }
+
+    private class RemovePluginVisitor extends MavenVisitor {
+
+        @Override
+        public Xml visitTag(Xml.Tag tag, ExecutionContext ctx) {
+            if (PLUGIN_MATCHER.matches(getCursor())) {
+                if (groupId.equals(tag.getChildValue("groupId").orElse(model.getGroupId())) &&
+                        tag.getChildValue("artifactId")
+                                .map(a -> a.equals(artifactId))
+                                .orElse(artifactId == null)) {
+                    doAfterVisit(new RemoveContentVisitor<>(tag, true));
+                }
+            }
+
+            return super.visitTag(tag, ctx);
+        }
+
+    }
+}
