d5260442d5fb2a5dce103f4b3067b3d64ce471e5
Jonathan Schneider <jkschneider@gmail.com>
2021-06-15 16:34:54 -0700

    Polish

--- rewrite-test/src/main/kotlin/org/openrewrite/java/SameClassNameTest.kt
+++ rewrite-test/src/main/kotlin/org/openrewrite/java/SameClassNameTest.kt
@@ -15,44 +15,29 @@
  */
 package org.openrewrite.java
 
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.openrewrite.ExecutionContext
 import org.openrewrite.Recipe
-import org.openrewrite.SourceFile
 
 interface SameClassNameTest : JavaRecipeTest {
     override val recipe: Recipe
-        get() = ExampleRecipe()
+        get() = object : JavaIsoVisitor<ExecutionContext>() {}.toRecipe()
 
-    @Disabled
     @Test
-    fun canParseTheSameJavaClass(jp: JavaParser) =
-        assertUnchanged(
-            jp,
-            before = """
-                package com.foo;
-                class A {}
-            """,
-            dependsOn = arrayOf("""
+    fun canParseTheSameJavaClass(jp: JavaParser) = assertUnchanged(
+        jp,
+        before = """
+            package com.foo;
+            class A {}
+        """,
+        dependsOn = arrayOf(
+            """
                 package a.b.c;
                 class Bar {}
             """, """
                 package d.e.f;
                 class Bar {}
             """
-            )
         )
-
-    class ExampleRecipe : Recipe() {
-        override fun getDisplayName(): String {
-            return "Do nothing"
-        }
-
-        override fun visit(before: MutableList<SourceFile>, ctx: ExecutionContext): MutableList<SourceFile> {
-            return super.visit(before, ctx)
-        }
-    }
-
-
+    )
 }
