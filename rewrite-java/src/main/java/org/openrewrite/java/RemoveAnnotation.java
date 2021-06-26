5c67c7b58cd81a17a980e6c0eea9c0118f105241
Sam Snyder <sam@moderne.io>
2021-06-25 19:57:04 -0700

    Suparate RemoveAnnotationVisitor into its own class. This is ergonomic/convenient for testing-frameworks' usage

--- rewrite-java/src/main/java/org/openrewrite/java/RemoveAnnotation.java
+++ rewrite-java/src/main/java/org/openrewrite/java/RemoveAnnotation.java
@@ -17,15 +17,8 @@ package org.openrewrite.java;
 
 import lombok.EqualsAndHashCode;
 import lombok.Value;
-import org.openrewrite.ExecutionContext;
 import org.openrewrite.Option;
 import org.openrewrite.Recipe;
-import org.openrewrite.TreeVisitor;
-import org.openrewrite.java.tree.J;
-import org.openrewrite.java.tree.Space;
-import org.openrewrite.java.tree.TypeUtils;
-
-import java.util.List;
 
 @EqualsAndHashCode(callSuper = true)
 @Value
@@ -46,84 +39,7 @@ public class RemoveAnnotation extends Recipe {
     }
 
     @Override
-    public TreeVisitor<?, ExecutionContext> getVisitor() {
-        AnnotationMatcher annotationMatcher = new AnnotationMatcher(annotationPattern);
-        return new JavaIsoVisitor<ExecutionContext>() {
-            @Override
-            public J.ClassDeclaration visitClassDeclaration(J.ClassDeclaration classDecl, ExecutionContext ctx) {
-                J.ClassDeclaration c = super.visitClassDeclaration(classDecl, ctx);
-                J.Annotation annotationRemoved = getCursor().pollMessage("annotationRemoved");
-
-                List<J.Annotation> leadingAnnotations = classDecl.getLeadingAnnotations();
-                if (annotationRemoved != null) {
-                    if (leadingAnnotations.get(0) == annotationRemoved || leadingAnnotations.size() == 1) {
-                        if (!c.getModifiers().isEmpty()) {
-                            c = c.withModifiers(Space.formatFirstPrefix(c.getModifiers(), Space.firstPrefix(c.getModifiers()).withWhitespace("")));
-                        } else if (c.getPadding().getTypeParameters() != null) {
-                            c = c.getPadding().withTypeParameters(c.getPadding().getTypeParameters().withBefore(c.getPadding().getTypeParameters().getBefore().withWhitespace("")));
-                        } else {
-                            c = c.withName(c.getName().withPrefix(c.getName().getPrefix().withWhitespace("")));
-                        }
-                    } else {
-                        c = autoFormat(c, c.getName(), ctx, getCursor().getParentOrThrow());
-                    }
-                }
-                return c;
-            }
-
-            @Override
-            public J.MethodDeclaration visitMethodDeclaration(J.MethodDeclaration method, ExecutionContext ctx) {
-                J.MethodDeclaration m = super.visitMethodDeclaration(method, ctx);
-                J.Annotation annotationRemoved = getCursor().pollMessage("annotationRemoved");
-
-                List<J.Annotation> leadingAnnotations = method.getLeadingAnnotations();
-                if (annotationRemoved != null && !leadingAnnotations.isEmpty()) {
-                    if (leadingAnnotations.get(0) == annotationRemoved || leadingAnnotations.size() == 1) {
-                        if (!m.getModifiers().isEmpty()) {
-                            m = m.withModifiers(Space.formatFirstPrefix(m.getModifiers(), Space.firstPrefix(m.getModifiers()).withWhitespace("")));
-                        } else if (m.getPadding().getTypeParameters() != null) {
-                            m = m.getPadding().withTypeParameters(m.getPadding().getTypeParameters().withPrefix(m.getPadding().getTypeParameters().getPrefix().withWhitespace("")));
-                        } else if (m.getReturnTypeExpression() != null) {
-                            m = m.withReturnTypeExpression(m.getReturnTypeExpression().withPrefix(m.getReturnTypeExpression().getPrefix().withWhitespace("")));
-                        } else {
-                            m = m.withName(m.getName().withPrefix(m.getName().getPrefix().withWhitespace("")));
-                        }
-                    } else {
-                        m = autoFormat(m, m.getName(), ctx, getCursor().getParentOrThrow());
-                    }
-                }
-                return m;
-            }
-
-            @Override
-            public J.VariableDeclarations visitVariableDeclarations(J.VariableDeclarations multiVariable, ExecutionContext ctx) {
-                J.VariableDeclarations v = super.visitVariableDeclarations(multiVariable, ctx);
-                J.Annotation annotationRemoved = getCursor().pollMessage("annotationRemoved");
-
-                List<J.Annotation> leadingAnnotations = multiVariable.getLeadingAnnotations();
-                if (annotationRemoved != null && !leadingAnnotations.isEmpty()) {
-                    if (leadingAnnotations.get(0) == annotationRemoved || leadingAnnotations.size() == 1) {
-                        if (!v.getModifiers().isEmpty()) {
-                            v = v.withModifiers(Space.formatFirstPrefix(v.getModifiers(), Space.firstPrefix(v.getModifiers()).withWhitespace("")));
-                        } else if (v.getTypeExpression() != null) {
-                            v = v.withTypeExpression(v.getTypeExpression().withPrefix(v.getTypeExpression().getPrefix().withWhitespace("")));
-                        }
-                    }
-                }
-
-                return v;
-            }
-
-            @Override
-            public J.Annotation visitAnnotation(J.Annotation annotation, ExecutionContext ctx) {
-                if (annotationMatcher.matches(annotation)) {
-                    getCursor().getParentOrThrow().putMessage("annotationRemoved", annotation);
-                    maybeRemoveImport(TypeUtils.asFullyQualified(annotation.getType()));
-                    //noinspection ConstantConditions
-                    return null;
-                }
-                return super.visitAnnotation(annotation, ctx);
-            }
-        };
+    public RemoveAnnotationVisitor  getVisitor() {
+        return new RemoveAnnotationVisitor(new AnnotationMatcher(annotationPattern));
     }
 }
---
+++ rewrite-java/src/main/java/org/openrewrite/java/RemoveAnnotationVisitor.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2021 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java;
+
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.java.tree.J;
+import org.openrewrite.java.tree.Space;
+import org.openrewrite.java.tree.TypeUtils;
+
+import java.util.List;
+
+@Value
+@EqualsAndHashCode(callSuper = true)
+public class RemoveAnnotationVisitor extends JavaIsoVisitor<ExecutionContext> {
+    AnnotationMatcher annotationMatcher;
+
+    @Override
+    public J.ClassDeclaration visitClassDeclaration(J.ClassDeclaration classDecl, ExecutionContext ctx) {
+        J.ClassDeclaration c = super.visitClassDeclaration(classDecl, ctx);
+        J.Annotation annotationRemoved = getCursor().pollMessage("annotationRemoved");
+
+        List<J.Annotation> leadingAnnotations = classDecl.getLeadingAnnotations();
+        if (annotationRemoved != null) {
+            if (leadingAnnotations.get(0) == annotationRemoved || leadingAnnotations.size() == 1) {
+                if (!c.getModifiers().isEmpty()) {
+                    c = c.withModifiers(Space.formatFirstPrefix(c.getModifiers(), Space.firstPrefix(c.getModifiers()).withWhitespace("")));
+                } else if (c.getPadding().getTypeParameters() != null) {
+                    c = c.getPadding().withTypeParameters(c.getPadding().getTypeParameters().withBefore(c.getPadding().getTypeParameters().getBefore().withWhitespace("")));
+                } else {
+                    c = c.withName(c.getName().withPrefix(c.getName().getPrefix().withWhitespace("")));
+                }
+            } else {
+                c = autoFormat(c, c.getName(), ctx, getCursor().getParentOrThrow());
+            }
+        }
+        return c;
+    }
+
+    @Override
+    public J.MethodDeclaration visitMethodDeclaration(J.MethodDeclaration method, ExecutionContext ctx) {
+        J.MethodDeclaration m = super.visitMethodDeclaration(method, ctx);
+        J.Annotation annotationRemoved = getCursor().pollMessage("annotationRemoved");
+
+        List<J.Annotation> leadingAnnotations = method.getLeadingAnnotations();
+        if (annotationRemoved != null && !leadingAnnotations.isEmpty()) {
+            if (leadingAnnotations.get(0) == annotationRemoved || leadingAnnotations.size() == 1) {
+                if (!m.getModifiers().isEmpty()) {
+                    m = m.withModifiers(Space.formatFirstPrefix(m.getModifiers(), Space.firstPrefix(m.getModifiers()).withWhitespace("")));
+                } else if (m.getPadding().getTypeParameters() != null) {
+                    m = m.getPadding().withTypeParameters(m.getPadding().getTypeParameters().withPrefix(m.getPadding().getTypeParameters().getPrefix().withWhitespace("")));
+                } else if (m.getReturnTypeExpression() != null) {
+                    m = m.withReturnTypeExpression(m.getReturnTypeExpression().withPrefix(m.getReturnTypeExpression().getPrefix().withWhitespace("")));
+                } else {
+                    m = m.withName(m.getName().withPrefix(m.getName().getPrefix().withWhitespace("")));
+                }
+            } else {
+                m = autoFormat(m, m.getName(), ctx, getCursor().getParentOrThrow());
+            }
+        }
+        return m;
+    }
+
+    @Override
+    public J.VariableDeclarations visitVariableDeclarations(J.VariableDeclarations multiVariable, ExecutionContext ctx) {
+        J.VariableDeclarations v = super.visitVariableDeclarations(multiVariable, ctx);
+        J.Annotation annotationRemoved = getCursor().pollMessage("annotationRemoved");
+
+        List<J.Annotation> leadingAnnotations = multiVariable.getLeadingAnnotations();
+        if (annotationRemoved != null && !leadingAnnotations.isEmpty()) {
+            if (leadingAnnotations.get(0) == annotationRemoved || leadingAnnotations.size() == 1) {
+                if (!v.getModifiers().isEmpty()) {
+                    v = v.withModifiers(Space.formatFirstPrefix(v.getModifiers(), Space.firstPrefix(v.getModifiers()).withWhitespace("")));
+                } else if (v.getTypeExpression() != null) {
+                    v = v.withTypeExpression(v.getTypeExpression().withPrefix(v.getTypeExpression().getPrefix().withWhitespace("")));
+                }
+            }
+        }
+
+        return v;
+    }
+
+    @Override
+    public J.Annotation visitAnnotation(J.Annotation annotation, ExecutionContext ctx) {
+        if (annotationMatcher.matches(annotation)) {
+            getCursor().getParentOrThrow().putMessage("annotationRemoved", annotation);
+            maybeRemoveImport(TypeUtils.asFullyQualified(annotation.getType()));
+            //noinspection ConstantConditions
+            return null;
+        }
+        return super.visitAnnotation(annotation, ctx);
+    }
+}
