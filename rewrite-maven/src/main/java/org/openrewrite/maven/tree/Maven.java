a7d1632aae7d4b546e47ca40f55d512a6abeeabd
Greg Adams <greg@moderne.io>
2021-06-28 14:09:24 -0700

    exclude poms in src from maven project parsing

--- rewrite-maven/src/main/java/org/openrewrite/maven/tree/Maven.java
+++ rewrite-maven/src/main/java/org/openrewrite/maven/tree/Maven.java
@@ -74,7 +74,8 @@ public class Maven extends Xml.Document {
 
     public static List<Path> getMavenPoms(Path projectDir, ExecutionContext ctx) {
         return getSources(projectDir, ctx, "pom.xml").stream()
-                .filter(p -> p.getFileName().toString().equals("pom.xml"))
+                .filter(p -> p.getFileName().toString().equals("pom.xml") &&
+                        !p.toString().contains("/src/"))
                 .collect(Collectors.toList());
     }
 
