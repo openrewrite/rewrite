3f28b37fcb3471ef42af3e2ec5220ead8783540f
Sam Snyder <sam@moderne.io>
2021-07-09 14:22:05 -0700

    Finish FinalClassVisitor

--- rewrite-java/src/main/java/org/openrewrite/java/cleanup/FinalClassVisitor.java
+++ rewrite-java/src/main/java/org/openrewrite/java/cleanup/FinalClassVisitor.java
@@ -1,5 +1,21 @@
+/*
+ * Copyright 2021 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.openrewrite.java.cleanup;
 
+import org.openrewrite.Cursor;
 import org.openrewrite.ExecutionContext;
 import org.openrewrite.java.JavaIsoVisitor;
 import org.openrewrite.java.tree.J;
@@ -13,12 +29,16 @@ import java.util.List;
 import static java.util.Collections.emptyList;
 import static org.openrewrite.Tree.randomId;
 import static org.openrewrite.java.cleanup.ModifierOrder.sortModifiers;
+import static org.openrewrite.java.tree.J.ClassDeclaration.Kind.Type.Interface;
 
 public class FinalClassVisitor extends JavaIsoVisitor<ExecutionContext> {
     @Override
     public J.ClassDeclaration visitClassDeclaration(J.ClassDeclaration classDeclaration, ExecutionContext executionContext) {
+        if(classDeclaration.getKind() == Interface) {
+            return classDeclaration;
+        }
         J.ClassDeclaration cd = super.visitClassDeclaration(classDeclaration, executionContext);
-        getCursor().dropParentUntil(it -> it instanceof J.ClassDeclaration && ((J.ClassDeclaration)it))
+
         if(cd.hasModifier(J.Modifier.Type.Final) || cd.getKind() != J.ClassDeclaration.Kind.Type.Class) {
             return cd;
         }
