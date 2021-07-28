6dbc504ad10a76dbf67692085f28f669d6cec121
Aaron Gershman <aegershman@gmail.com>
2021-07-28 16:35:48 -0500

    [#821] remove unused assignments

--- rewrite-java-8/src/test/kotlin/org/openrewrite/java/Java8VisitorDebugTest.kt
+++ rewrite-java-8/src/test/kotlin/org/openrewrite/java/Java8VisitorDebugTest.kt
@@ -288,6 +288,10 @@ class Java8RemoveExtraSemicolonsTest : Java8Test, RemoveExtraSemicolonsTest
 
 @DebugOnly
 @ExtendWith(JavaParserResolver::class)
+class Java8RemoveUnusedAssignmentsTest : Java8Test, RemoveUnusedAssignmentsTest
+
+@DebugOnly
+@ExtendWith(JavaParserResolver::class)
 class Java8RemoveUnusedLocalVariablesTest : Java8Test, RemoveUnusedLocalVariablesTest
 
 @DebugOnly
