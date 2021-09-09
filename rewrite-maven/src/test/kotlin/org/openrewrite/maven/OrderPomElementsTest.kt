cbd2a5df29e0e57e2e9cbe4039411865f6da8642
Patrick <patway99@gmail.com>
2021-09-09 10:38:31 -0700

    Add disabled test for incorrect new-line formatting

--- rewrite-maven/src/test/kotlin/org/openrewrite/maven/OrderPomElementsTest.kt
+++ rewrite-maven/src/test/kotlin/org/openrewrite/maven/OrderPomElementsTest.kt
@@ -15,6 +15,7 @@
  */
 package org.openrewrite.maven
 
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.openrewrite.Recipe
 
@@ -143,6 +144,107 @@ class OrderPomElementsTest : MavenRecipeTest {
                 </repositories>
                 <pluginRepositories>
                 </pluginRepositories>
+                <build>
+                </build>
+            </project>
+        """
+    )
+
+    @Disabled
+    @Test
+    fun updateOrderCorrectNewLines() = assertChanged(
+        before = """
+            <project>
+                <parent>
+                    <groupId>org.example</groupId>
+                    <artifactId>parent-pom-test</artifactId>
+                    <version>1.0-SNAPSHOT</version>
+                </parent>
+                
+                <modelVersion>4.0.0</modelVersion>
+                
+                <artifactId>my-project</artifactId>
+                <groupId>my.org.project</groupId>
+                <version>4.3.0</version>
+                
+                <properties>
+                </properties>
+                
+                <description>Some project desc</description>
+                <name>Some Project</name>
+                
+                <dependencies>
+                    <dependency>
+                        <!-- artifact content
+                            comment -->
+                        <artifactId>my-project</artifactId>
+                        <!-- group content -->
+                        <groupId>my.org.project</groupId>
+                        <!-- version content -->
+                        <version>4.3.0</version>
+                    </dependency>
+                </dependencies>
+                <dependencyManagement>
+                    <dependency>
+                        <version>2</version>
+                        <groupId>my.org.project</groupId>
+                        <artifactId>my-project-thing</artifactId>
+                    </dependency>
+                </dependencyManagement>
+                
+                <repositories>
+                </repositories>
+                <pluginRepositories>
+                </pluginRepositories>
+                
+                <build>
+                </build>
+            </project>
+        """,
+        after = """
+            <project>
+                <modelVersion>4.0.0</modelVersion>
+                
+                <parent>
+                    <groupId>org.example</groupId>
+                    <artifactId>parent-pom-test</artifactId>
+                    <version>1.0-SNAPSHOT</version>
+                </parent>
+                
+                <groupId>my.org.project</groupId>
+                <artifactId>my-project</artifactId>
+                <version>4.3.0</version>
+                
+                <name>Some Project</name>
+                <description>Some project desc</description>
+                
+                <properties>
+                </properties>
+                
+                <dependencyManagement>
+                    <dependency>
+                        <groupId>my.org.project</groupId>
+                        <artifactId>my-project-thing</artifactId>
+                        <version>2</version>
+                    </dependency>
+                </dependencyManagement>
+                <dependencies>
+                    <dependency>
+                        <!-- group content -->
+                        <groupId>my.org.project</groupId>
+                        <!-- artifact content
+                            comment -->
+                        <artifactId>my-project</artifactId>
+                        <!-- version content -->
+                        <version>4.3.0</version>
+                    </dependency>
+                </dependencies>
+                
+                <repositories>
+                </repositories>
+                <pluginRepositories>
+                </pluginRepositories>
+                
                 <build>
                 </build>
             </project>
