f36a064628f0315dd87df3f2265a5d5e1c343290
Aaron Gershman <5619476+aegershman@users.noreply.github.com>
2021-09-30 23:25:20 -0500

    Change dependency groupId and artifactId (#1074)

---
+++ rewrite-maven/src/main/java/org/openrewrite/maven/ChangeDependencyGroupIdAndArtifactId.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2021 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.maven;
+
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.Option;
+import org.openrewrite.Recipe;
+import org.openrewrite.TreeVisitor;
+import org.openrewrite.xml.ChangeTagValueVisitor;
+import org.openrewrite.xml.tree.Xml;
+
+import java.util.Optional;
+
+@Value
+@EqualsAndHashCode(callSuper = true)
+public class ChangeDependencyGroupIdAndArtifactId extends Recipe {
+    @Option(displayName = "Old groupId",
+            description = "The old groupId to replace. The groupId is the first part of a dependency coordinate 'com.google.guava:guava:VERSION'.",
+            example = "org.openrewrite.recipe")
+    String oldGroupId;
+
+    @Option(displayName = "Old artifactId",
+            description = "The old artifactId to replace. The artifactId is the second part of a dependency coordinate 'com.google.guava:guava:VERSION'.",
+            example = "rewrite-testing-frameworks")
+    String oldArtifactId;
+
+    @Option(displayName = "New groupId",
+            description = "The new groupId to use.",
+            example = "corp.internal.openrewrite.recipe")
+    String newGroupId;
+
+    @Option(displayName = "New artifactId",
+            description = "The new artifactId to use.",
+            example = "rewrite-testing-frameworks")
+    String newArtifactId;
+
+    @Override
+    public String getDisplayName() {
+        return "Change Maven dependency groupId and artifactId";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Change the groupId and artifactId of a specified Maven dependency.";
+    }
+
+    @Override
+    protected TreeVisitor<?, ExecutionContext> getVisitor() {
+        return new ChangeDependencyGroupIdAndArtifactIdVisitor();
+    }
+
+    private class ChangeDependencyGroupIdAndArtifactIdVisitor extends MavenVisitor {
+        @Override
+        public Xml visitTag(Xml.Tag tag, ExecutionContext ctx) {
+            if (isDependencyTag(oldGroupId, oldArtifactId)) {
+                Optional<Xml.Tag> groupIdTag = tag.getChild("groupId");
+                if (groupIdTag.isPresent() && !newGroupId.equals(groupIdTag.get().getValue().orElse(null))) {
+                    doAfterVisit(new ChangeTagValueVisitor<>(groupIdTag.get(), newGroupId));
+                }
+                Optional<Xml.Tag> artifactIdTag = tag.getChild("artifactId");
+                if (artifactIdTag.isPresent() && !newArtifactId.equals(artifactIdTag.get().getValue().orElse(null))) {
+                    doAfterVisit(new ChangeTagValueVisitor<>(artifactIdTag.get(), newArtifactId));
+                }
+            }
+
+            return super.visitTag(tag, ctx);
+        }
+    }
+}
