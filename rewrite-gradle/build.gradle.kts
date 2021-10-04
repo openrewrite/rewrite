d24a052553173a2aee741aec39bbc64fd3adf262
Patrick <patway99@gmail.com>
2021-10-04 15:10:38 -0700

    set `gradle-tooling-api` version to latest.release

--- rewrite-gradle/build.gradle.kts
+++ rewrite-gradle/build.gradle.kts
@@ -1,7 +1,7 @@
 dependencies {
     api(project(":rewrite-groovy"))
     implementation("dev.gradleplugins:gradle-api:7.2")
-    implementation("org.gradle:gradle-tooling-api:7.3-20210825160000+0000")
+    implementation("org.gradle:gradle-tooling-api:latest.release")
 
     testImplementation(project(":rewrite-test")) {
         // because gradle-api fatjars this implementation already
