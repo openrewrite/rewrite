c732c9bab697a8123eefe31a7714b8db50d45530
Patrick <patway99@gmail.com>
2021-10-06 13:52:52 -0700

    Only apply NoPrimitiveWrappersForToStringOrCompareTo to java.lang Types.

--- rewrite-java/src/main/java/org/openrewrite/java/cleanup/NoPrimitiveWrappersForToStringOrCompareTo.java
+++ rewrite-java/src/main/java/org/openrewrite/java/cleanup/NoPrimitiveWrappersForToStringOrCompareTo.java
@@ -82,38 +82,41 @@ public class NoPrimitiveWrappersForToStringOrCompareTo extends Recipe {
         @Override
         public J.MethodInvocation visitMethodInvocation(J.MethodInvocation method, ExecutionContext executionContext) {
             J.MethodInvocation mi = super.visitMethodInvocation(method, executionContext);
-            if (NUMBER_TO_STRING_MATCHER.matches(mi) || BOOLEAN_TO_STRING_MATCHER.matches(mi)) {
-                Expression arg = null;
-                if (mi.getSelect() instanceof J.NewClass) {
-                    arg = getSingleArg(((J.NewClass) mi.getSelect()).getArguments());
-                } else if (mi.getSelect() instanceof J.MethodInvocation) {
-                    J.MethodInvocation selectMethod = (J.MethodInvocation) mi.getSelect();
-                    if (VALUE_OF_NUMBER_MATCHER.matches(selectMethod) || VALUE_OF_BOOLEAN_MATCHER.matches(selectMethod)) {
-                        arg = getSingleArg(selectMethod.getArguments());
+            JavaType.Class clazz = mi.getType() != null ? TypeUtils.asClass(mi.getType().getDeclaringType()) : null;
+            if (clazz != null && "java.lang".equals(clazz.getPackageName())) {
+                if (NUMBER_TO_STRING_MATCHER.matches(mi) || BOOLEAN_TO_STRING_MATCHER.matches(mi)) {
+                    Expression arg = null;
+                    if (mi.getSelect() instanceof J.NewClass) {
+                        arg = getSingleArg(((J.NewClass) mi.getSelect()).getArguments());
+                    } else if (mi.getSelect() instanceof J.MethodInvocation) {
+                        J.MethodInvocation selectMethod = (J.MethodInvocation) mi.getSelect();
+                        if (VALUE_OF_NUMBER_MATCHER.matches(selectMethod) || VALUE_OF_BOOLEAN_MATCHER.matches(selectMethod)) {
+                            arg = getSingleArg(selectMethod.getArguments());
+                        }
                     }
-                }
-                if (arg != null && !TypeUtils.isString(arg.getType()) && mi.getType() != null && mi.getSelect() != null) {
-                    JavaType.FullyQualified fq = mi.getType().getDeclaringType();
-                    mi = mi.withSelect(J.Identifier.build(UUID.randomUUID(), mi.getSelect().getPrefix(), Markers.EMPTY, fq.getClassName(), fq));
-                    //noinspection ArraysAsListWithZeroOrOneArgument
-                    mi = mi.withArguments(Arrays.asList(arg));
-                }
-            } else if (NUMBER_COMPARE_TO_MATCHER.matches(mi) || BOOLEAN_COMPARE_TO_MATCHER.matches(mi)) {
-                Expression arg = null;
-                if (mi.getSelect() instanceof J.NewClass) {
-                    arg = getSingleArg(((J.NewClass) mi.getSelect()).getArguments());
-                } else if (mi.getSelect() instanceof J.MethodInvocation) {
-                    J.MethodInvocation selectMethod = (J.MethodInvocation) mi.getSelect();
-                    if (VALUE_OF_NUMBER_MATCHER.matches(selectMethod) || VALUE_OF_BOOLEAN_MATCHER.matches(selectMethod)) {
-                        arg = getSingleArg(selectMethod.getArguments());
+                    if (arg != null && !TypeUtils.isString(arg.getType()) && mi.getSelect() != null) {
+                        JavaType.FullyQualified fq = mi.getType().getDeclaringType();
+                        mi = mi.withSelect(J.Identifier.build(UUID.randomUUID(), mi.getSelect().getPrefix(), Markers.EMPTY, fq.getClassName(), fq));
+                        //noinspection ArraysAsListWithZeroOrOneArgument
+                        mi = mi.withArguments(Arrays.asList(arg));
                     }
-                }
+                } else if (NUMBER_COMPARE_TO_MATCHER.matches(mi) || BOOLEAN_COMPARE_TO_MATCHER.matches(mi)) {
+                    Expression arg = null;
+                    if (mi.getSelect() instanceof J.NewClass) {
+                        arg = getSingleArg(((J.NewClass) mi.getSelect()).getArguments());
+                    } else if (mi.getSelect() instanceof J.MethodInvocation) {
+                        J.MethodInvocation selectMethod = (J.MethodInvocation) mi.getSelect();
+                        if (VALUE_OF_NUMBER_MATCHER.matches(selectMethod) || VALUE_OF_BOOLEAN_MATCHER.matches(selectMethod)) {
+                            arg = getSingleArg(selectMethod.getArguments());
+                        }
+                    }
 
-                if (arg != null && !TypeUtils.isString(arg.getType()) && mi.getType() != null && mi.getSelect() != null) {
-                    JavaType.FullyQualified fq = mi.getType().getDeclaringType();
-                    mi = mi.withSelect(J.Identifier.build(UUID.randomUUID(), mi.getSelect().getPrefix(), Markers.EMPTY, fq.getClassName(), fq));
-                    mi = mi.withArguments(ListUtils.concat(arg, mi.getArguments()));
-                    mi = maybeAutoFormat(mi, mi.withName(mi.getName().withName("compare")), executionContext);
+                    if (arg != null && !TypeUtils.isString(arg.getType()) && mi.getSelect() != null) {
+                        JavaType.FullyQualified fq = mi.getType().getDeclaringType();
+                        mi = mi.withSelect(J.Identifier.build(UUID.randomUUID(), mi.getSelect().getPrefix(), Markers.EMPTY, fq.getClassName(), fq));
+                        mi = mi.withArguments(ListUtils.concat(arg, mi.getArguments()));
+                        mi = maybeAutoFormat(mi, mi.withName(mi.getName().withName("compare")), executionContext);
+                    }
                 }
             }
             return mi;
