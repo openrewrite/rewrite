b18076413ff0d4fe155db92083760225fad7e6a6
Aaron Gershman <5619476+aegershman@users.noreply.github.com>
2021-10-15 14:15:57 -0500

    ChangePropertyKey to handle path expansion by one (#1115)

--- rewrite-yaml/src/main/java/org/openrewrite/yaml/ChangePropertyKey.java
+++ rewrite-yaml/src/main/java/org/openrewrite/yaml/ChangePropertyKey.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 the original author or authors.
+ * Copyright 2021 the original author or authors.
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -106,7 +106,7 @@ public class ChangePropertyKey extends Recipe {
                     Yaml.Mapping.Entry propertyEntry = propertyEntriesLeftToRight.next();
                     String value = propertyEntry.getKey().getValue();
 
-                    if (!propertyToTest.startsWith(value)) {
+                    if (!propertyToTest.startsWith(value) || (propertyToTest.startsWith(value) && !propertyEntriesLeftToRight.hasNext())) {
                         doAfterVisit(new InsertSubpropertyVisitor<>(
                                 propertyEntry,
                                 propertyToTest,
