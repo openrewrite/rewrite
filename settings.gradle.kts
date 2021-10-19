823aaf96783f8485d5cafc441760ebec2d07b1d8
Aaron Gershman <aegershman@gmail.com>
2021-10-19 16:19:47 -0500

    Gradle enterprise for openrewrite

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,3 +21,27 @@ include(
     "rewrite-test",
     "rewrite-benchmarks"
 )
+
+plugins {
+    id("com.gradle.enterprise") version "3.7"
+}
+
+gradleEnterprise {
+    server = "https://ge.openrewrite.org/"
+
+    buildCache {
+        local {
+            isEnabled = true
+        }
+
+        remote(HttpBuildCache::class) {
+            isPush = true
+            url = uri("https://ge.openrewrite.org/cache/")
+        }
+
+    }
+
+    buildScan {
+        publishAlways()
+    }
+}
