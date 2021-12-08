084c9ce01eb9eff5ce2a5d9fb208b672d975ff06
Tyler Van Gorder <1878529+tkvangorder@users.noreply.github.com>
2021-12-07 23:50:09 -0800

    Added a Utility to Print Dependency tree similar to Eclipse pom editor (#1265)
    * Initial checkin
    
    * Adding DependencyInspector
    
    * Adding DependencyInspector
    
    * Adding DependencyInspector
    
    Co-authored-by: Tyler Van Gorder <tkvangorder@users.noreply.github.com>

---
+++ rewrite-maven/src/main/java/org/openrewrite/maven/utilities/DependencyInspector.java
@@ -0,0 +1,171 @@
+/*
+ * Copyright 2021 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.maven.utilities;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.openrewrite.internal.lang.Nullable;
+import org.openrewrite.maven.tree.GroupArtifact;
+import org.openrewrite.maven.tree.Maven;
+import org.openrewrite.maven.tree.Pom;
+import org.openrewrite.maven.tree.Scope;
+
+import java.util.*;
+
+public class DependencyInspector {
+
+    public static DependencyInspectorDependency resolveDependencyTree(Maven maven) {
+        return resolveDependencyTree(maven.getModel());
+    }
+
+    public static DependencyInspectorDependency resolveDependencyTree(@Nullable Pom pom) {
+        if (pom == null) {
+            return null;
+        }
+
+        DependencyInspectorDependency root = new DependencyInspectorDependency(
+                pom.getGroupId(),
+                pom.getArtifactId(),
+                pom.getVersion(),
+                pom.getVersion(),
+                "");
+
+        Map<GroupArtifact, String> seenArtifacts = new HashMap<>();
+        Deque<DependencyTask> workQueue = new ArrayDeque<>();
+
+        workQueue.addFirst(new DependencyTask(root, pom, null, Collections.emptySet()));
+        while (!workQueue.isEmpty()) {
+            DependencyTask task = workQueue.removeFirst();
+            GroupArtifact ga = new GroupArtifact(task.dependency.groupId, task.dependency.artifactId);
+
+            String resolvedVersion = seenArtifacts.get(ga);
+            if (resolvedVersion == null) {
+                resolvedVersion = task.dependency.resolvedVersion;
+                seenArtifacts.putIfAbsent(ga, resolvedVersion);
+
+                List<DependencyInspectorDependency> children = new ArrayList<>(task.pom.getDependencies().size());
+                for (Pom.Dependency dependency : task.pom.getDependencies()) {
+                    GroupArtifact childGav = new GroupArtifact(dependency.getGroupId(), dependency.getArtifactId());
+                    if (task.exclusions.contains(childGav)) {
+                        //Do not add the child if it's in the exclusions of the containing dependency.
+                        continue;
+                    }
+                    Scope childScope = dependency.getScope() == null ? Scope.Compile : dependency.getScope();
+
+                    if (task.scope == null || ((task.scope == Scope.Test || task.scope == Scope.Compile) &&
+                            (dependency.getScope() == Scope.Compile || dependency.getScope() == Scope.Runtime))) {
+                        if (task.scope == Scope.Test) {
+                            childScope = Scope.Test;
+                        }
+                        String childVersion = dependency.getRequestedVersion() != null ? dependency.getRequestedVersion() : dependency.getVersion();
+                        DependencyInspectorDependency child = new DependencyInspectorDependency(
+                                task.pom.getValue(dependency.getGroupId()),
+                                task.pom.getValue(dependency.getArtifactId()),
+                                task.pom.getValue(childVersion),
+                                task.pom.getValue(childVersion),
+                                childScope.name().toLowerCase());
+                        children.add(child);
+                        workQueue.addLast(new DependencyTask(child, dependency.getModel(), childScope, dependency.getExclusions()));
+                    }
+                }
+                if (!children.isEmpty()) {
+                    task.dependency.dependencies = children;
+                }
+            } else {
+                task.dependency.resolvedVersion = resolvedVersion;
+            }
+        }
+        return root;
+    }
+
+    public static String printDependencyTree(Maven maven) {
+        return printDependencyTree(maven.getModel());
+    }
+
+    public static String printDependencyTree(@Nullable Pom pom) {
+
+        DependencyInspectorDependency dependencyTree = resolveDependencyTree(pom);
+
+        StringBuilder buffer = new StringBuilder(1024);
+        printDependencyTree(buffer, dependencyTree, "", true);
+        return buffer.toString();
+    }
+
+    private static void printDependencyTree(StringBuilder buffer, DependencyInspectorDependency dependency, String indent, boolean isLast) {
+        buffer.append(indent);
+        if (!indent.isEmpty()) {
+            buffer.append("|-");
+        }
+        if (!isLast) {
+            indent = indent + "|   ";
+        } else {
+            indent = indent + "    ";
+        }
+
+        buffer/*.append(dependency.getGroupId()).append(':')*/.append(dependency.getArtifactId()).append(':').append(dependency.getOriginalVersion());
+        if (!dependency.getResolvedVersion().equals(dependency.getOriginalVersion())) {
+            buffer.append(" (omitted for conflict with ").append(dependency.getResolvedVersion()).append (')');
+        }
+        if (!dependency.getScope().isEmpty()) {
+            buffer.append(" [").append(dependency.scope).append(']');
+        }
+        buffer.append('\n');
+        int index = 0;
+        for (DependencyInspectorDependency child : dependency.getDependencies()) {
+            printDependencyTree(buffer, child, indent, index == dependency.getDependencies().size() -1);
+            index++;
+        }
+    }
+
+    private static class DependencyTask {
+
+        private DependencyInspectorDependency dependency;
+        private Pom pom;
+        @Nullable
+        private Scope scope;
+        private Set<GroupArtifact> exclusions;
+
+        private DependencyTask(DependencyInspectorDependency dependency, Pom pom, @Nullable Scope scope, Set<GroupArtifact> exclusions) {
+            this.dependency = dependency;
+            this.pom = pom;
+            this.scope = scope;
+            this.exclusions = exclusions;
+        }
+    }
+
+    @Getter
+    public static class DependencyInspectorDependency {
+
+        private final String groupId;
+        private final String artifactId;
+        private String resolvedVersion;
+        private final String originalVersion;
+        private final String scope;
+        private List<DependencyInspectorDependency> dependencies = Collections.emptyList();
+
+        public DependencyInspectorDependency(String groupId, String artifactId, String resolvedVersion, String originalVersion, String scope) {
+            this.groupId = groupId;
+            this.artifactId = artifactId;
+            this.resolvedVersion = resolvedVersion;
+            this.originalVersion = originalVersion;
+            this.scope = scope;
+        }
+    }
+
+    private static String gav(String groupId, String artifactId, String version) {
+        return groupId + ":" + artifactId + ":" + version;
+    }
+}
---
+++ rewrite-maven/src/test/kotlin/org/openrewrite/maven/utilities/DependencyInspectorTest.kt
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2021 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.maven.utilities
+
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+import org.openrewrite.maven.MavenParser
+
+class DependencyInspectorTest {
+
+    @Test
+    @Disabled
+    fun printDependencyTree() {
+        val maven = MavenParser.builder()
+            .build()
+            .parse("""
+                <project xmlns="http://maven.apache.org/POM/4.0.0"
+                         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                         xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+                    <modelVersion>4.0.0</modelVersion>
+                
+                    <groupId>org.openrewrite</groupId>
+                    <artifactId>eclipse-talk</artifactId>
+                    <version>1.0-SNAPSHOT</version>
+                
+                    <properties>
+                        <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
+                        <kotlin.code.style>official</kotlin.code.style>
+                        <kotlin.version>1.5.10</kotlin.version>
+                        <kotlin.compiler.jvmTarget>11</kotlin.compiler.jvmTarget>
+                        <maven-compiler-plugin.version>3.8.1</maven-compiler-plugin.version>
+                        <maven.compiler.showWarnings>true</maven.compiler.showWarnings>
+                        <java.version>11</java.version>
+                        <rewrite.version>7.16.0-SNAPSHOT</rewrite.version>
+                    </properties>
+                
+                    <dependencies>
+                        <dependency>
+                            <groupId>org.openrewrite</groupId>
+                            <artifactId>rewrite-java-11</artifactId>
+                            <version>7.16.0</version>
+                        </dependency>
+                        <dependency>
+                            <groupId>org.projectlombok</groupId>
+                            <artifactId>lombok</artifactId>
+                            <version>1.18.20</version>
+                            <scope>provided</scope>
+                        </dependency>
+                        <dependency>
+                            <groupId>org.junit.jupiter</groupId>
+                            <artifactId>junit-jupiter-api</artifactId>
+                            <version>5.6.0</version>
+                            <scope>test</scope>
+                        </dependency>
+                        <dependency>
+                            <groupId>org.junit.jupiter</groupId>
+                            <artifactId>junit-jupiter-engine</artifactId>
+                            <version>5.6.0</version>
+                            <scope>test</scope>
+                        </dependency>
+                        <dependency>
+                            <groupId>org.openrewrite</groupId>
+                            <artifactId>rewrite-test</artifactId>
+                            <version>7.16.0</version>
+                            <scope>test</scope>
+                            <exclusions>
+                                <exclusion>
+                                    <groupId>org.openrewrite</groupId>
+                                    <artifactId>rewrite-gradle</artifactId>
+                                </exclusion>
+                            </exclusions>
+                        </dependency>
+                        <dependency>
+                            <groupId>org.assertj</groupId>
+                            <artifactId>assertj-core</artifactId>
+                            <version>3.19.0</version>
+                            <scope>test</scope>
+                        </dependency>
+                        <dependency>
+                            <groupId>org.jetbrains.kotlin</groupId>
+                            <artifactId>kotlin-test-junit5</artifactId>
+                            <version>1.5.10</version>
+                            <scope>test</scope>
+                        </dependency>
+                        <dependency>
+                            <groupId>org.jetbrains.kotlin</groupId>
+                            <artifactId>kotlin-stdlib-jdk8</artifactId>
+                            <version>1.5.10</version>
+                            <scope>test</scope>
+                        </dependency>
+                        <dependency>
+                            <groupId>org.jetbrains.kotlin</groupId>
+                            <artifactId>kotlin-reflect</artifactId>
+                            <version>1.5.10</version>
+                            <scope>test</scope>
+                        </dependency>
+                    </dependencies>
+                </project>
+                """)
+        val tree = DependencyInspector.printDependencyTree(maven[0].model)
+        println(tree)
+    }
+}
