5c6f7613635ed80e2a7005d9b7d56b4cea8e60b9
mrbitrary <84456251+mrbitrary@users.noreply.github.com>
2022-01-14 18:57:36 +0700

    fix(rewrite-java): excluding Enum classess from getting serialVersionUid

--- rewrite-java/src/main/java/org/openrewrite/java/cleanup/AddSerialVersionUidToSerializable.java
+++ rewrite-java/src/main/java/org/openrewrite/java/cleanup/AddSerialVersionUidToSerializable.java
@@ -132,6 +132,8 @@ public class AddSerialVersionUidToSerializable extends Recipe {
                     //All other parameterized types fall through
                 } else if (type instanceof JavaType.FullyQualified) {
                     JavaType.FullyQualified fq = (JavaType.FullyQualified) type;
+                    if (fq.getKind() == JavaType.Class.Kind.Enum) return false;
+
                     if (fq.getKind() != JavaType.Class.Kind.Interface &&
                             !fq.isAssignableTo("java.lang.Throwable")) {
                         return fq.isAssignableTo("java.io.Serializable");
