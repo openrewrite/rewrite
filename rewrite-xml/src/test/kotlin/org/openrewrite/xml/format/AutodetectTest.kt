d0b140baa3b3aa6abf602804a4b57bdc57157ccb
Jonathan Schneider <jkschneider@gmail.com>
2022-03-31 10:36:43 -0400

    Polish

--- rewrite-xml/src/test/kotlin/org/openrewrite/xml/format/AutodetectTest.kt
+++ rewrite-xml/src/test/kotlin/org/openrewrite/xml/format/AutodetectTest.kt
@@ -28,51 +28,51 @@ class AutodetectTest {
     @Test
     fun autodetectSimple() = hasIndentation(
         """
-                <project>
-                  <dependencies>
-                    <dependency>
-                      <groupId>org.springframework.boot</groupId>
-                      <artifactId>spring-boot-starter-test</artifactId>
-                      <exclusions>
-                        <exclusion>
-                          <groupId>org.junit.vintage</groupId>
-                        </exclusion>
-                      </exclusions>
-                    </dependency>
-                  </dependencies>
-                </project>
-            """, indentSize = 2
+            <project>
+              <dependencies>
+                <dependency>
+                  <groupId>org.springframework.boot</groupId>
+                  <artifactId>spring-boot-starter-test</artifactId>
+                  <exclusions>
+                    <exclusion>
+                      <groupId>org.junit.vintage</groupId>
+                    </exclusion>
+                  </exclusions>
+                </dependency>
+              </dependencies>
+            </project>
+        """, indentSize = 2
     )
 
     @Issue("https://github.com/openrewrite/rewrite/issues/1498")
     @Test
     fun autodetectQuarkus() = hasIndentation(
         """
-                <project xmlns="http://maven.apache.org/POM/4.0.0"
-                    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-                    xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd">
-                    <modelVersion>4.0.0</modelVersion>
-                    <parent>
-                        <groupId>io.quarkus</groupId>
-                        <artifactId>quarkus-bootstrap-parent</artifactId>
-                        <version>999-SNAPSHOT</version>
-                    </parent>
-                    <artifactId>quarkus-bootstrap-bom</artifactId>
-                </project>
-            """, indentSize = 4, continuationIndentSize = 4
+            <project xmlns="http://maven.apache.org/POM/4.0.0"
+                xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd">
+                <modelVersion>4.0.0</modelVersion>
+                <parent>
+                    <groupId>io.quarkus</groupId>
+                    <artifactId>quarkus-bootstrap-parent</artifactId>
+                    <version>999-SNAPSHOT</version>
+                </parent>
+                <artifactId>quarkus-bootstrap-bom</artifactId>
+            </project>
+        """, indentSize = 4, continuationIndentSize = 4
     )
 
     @Test
     fun continuationIndents() = hasIndentation(
         """
-        <project
-                 xmlns="http://maven.apache.org/POM/4.0.0"
-                 xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-                 xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd">
-            <modelVersion>4.0.0</modelVersion>
-            <artifactId>quarkus-bootstrap-bom</artifactId>
-        </project>
-    """, indentSize = 4, continuationIndentSize = 9
+            <project
+                     xmlns="http://maven.apache.org/POM/4.0.0"
+                     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                     xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd">
+                <modelVersion>4.0.0</modelVersion>
+                <artifactId>quarkus-bootstrap-bom</artifactId>
+            </project>
+        """, indentSize = 4, continuationIndentSize = 9
     )
 
     private fun hasIndentation(
