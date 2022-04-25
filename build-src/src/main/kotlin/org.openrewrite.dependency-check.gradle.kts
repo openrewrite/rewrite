fe1503619648bf3680c84f0dd5f7aceb3303fc88
Patrick <patway99@gmail.com>
2022-04-25 13:34:48 -0700

    Update dependency-check configuration with specific list of projects to scan.

--- build-src/src/main/kotlin/org.openrewrite.dependency-check.gradle.kts
+++ build-src/src/main/kotlin/org.openrewrite.dependency-check.gradle.kts
@@ -6,4 +6,20 @@ configure<org.owasp.dependencycheck.gradle.extension.DependencyCheckExtension> {
     skipConfigurations = listOf("integTestImplementationDependenciesMetadata")
     analyzers.assemblyEnabled = false
     failBuildOnCVSS = 9.0F
+    scanProjects = listOf("rewrite-core",
+                              "rewrite-gradle",
+                              "rewrite-groovy",
+                              "rewrite-hcl",
+                              "rewrite-java",
+                              "rewrite-java-8",
+                              "rewrite-java-11",
+                              "rewrite-json",
+                              "rewrite-maven",
+                              "rewrite-properties",
+                              "rewrite-protobuf",
+                              "rewrite-xml",
+                              "rewrite-yaml",
+                              "rewrite-test",
+                              "rewrite-bom",
+                              "rewrite-benchmarks")
 }
