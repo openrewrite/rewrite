e86ccdee054d00f49020f019f0b8bc7ed52d808d
Sam Snyder <sam@moderne.io>
2022-04-25 20:48:50 -0700

    Fix #1712, JavaSourceSet missing some JRE provided types, including java.lang.Object

--- rewrite-java/src/main/java/org/openrewrite/java/marker/JavaSourceSet.java
+++ rewrite-java/src/main/java/org/openrewrite/java/marker/JavaSourceSet.java
@@ -32,6 +32,7 @@ import org.openrewrite.marker.Marker;
 
 import java.nio.file.Path;
 import java.util.*;
+import java.util.stream.Collectors;
 
 import static org.openrewrite.Tree.randomId;
 
@@ -62,7 +63,11 @@ public class JavaSourceSet implements Marker {
                 .acceptPackages("java")
                 .ignoreClassVisibility()
                 .scan()) {
-            types = typesFrom(packagesToTypeDeclarations(scanResult), typeCache, Collections.emptyList(), fullTypeInformation);
+            Map<String, List<String>> packagesToTypes = packagesToTypeDeclarations(scanResult);
+            // Peculiarly, Classgraph will not return a ClassInfo for java.lang.Object, although it does for all other java.lang types
+            packagesToTypes.computeIfAbsent("java.lang", p -> new ArrayList<>())
+                    .add("java.lang.Object");
+            types = typesFrom(packagesToTypes, typeCache, Collections.emptyList(), fullTypeInformation);
         }
         if (classpath.iterator().hasNext()) {
             // Load types from the classpath
@@ -141,7 +146,7 @@ public class JavaSourceSet implements Marker {
         } else {
             for (Map.Entry<String, List<String>> packageToTypes : packagesToTypes.entrySet()) {
                 for (String className : packageToTypes.getValue()) {
-                    types.add(JavaType.ShallowClass.build(packageToTypes.getKey() + "." + className));
+                    types.add(JavaType.ShallowClass.build(className));
                 }
             }
         }
@@ -155,7 +160,14 @@ public class JavaSourceSet implements Marker {
         String[] result = new String[packagesToTypeNames.size()];
         int i = 0;
         for (Map.Entry<String, List<String>> packageToTypes : packagesToTypeNames.entrySet()) {
-            StringBuilder sb = new StringBuilder("package ").append(packageToTypes.getKey()).append(";\n")
+            boolean isJreType = packageToTypes.getKey().startsWith("java.");
+            StringBuilder sb = new StringBuilder("package ");
+            if(isJreType) {
+                // Avoid java compiler complaints that we're redefining java.lang, etc.
+                // Only apply to java provided types because this limits our ability to get type information from package-private classes
+                sb.append("rewrite.");
+            }
+            sb.append(packageToTypes.getKey()).append(";\n")
                     .append("abstract class $RewriteTypeStub {\n");
 
             List<String> value = packageToTypes.getValue();
@@ -184,13 +196,17 @@ public class JavaSourceSet implements Marker {
     @Nullable
     private static String typeDeclarationFor(ClassInfo classInfo) {
         String name;
+        if(classInfo.getName().startsWith("java.") && !classInfo.isPublic()) {
+            // Because we put java-supplied types into another package, we cannot access package-private types
+            return null;
+        }
         if (classInfo.isInnerClass()) {
             // Java allows "$" in class names, and also uses "$" as part of the names of inner classes. e.g.: OuterClass$InnerClass
             // So if you only look at the textual representation of a class name, you can't tell if "A$B" means "class A$B {}" or "class A { class B {}}"
             // The declarable name of "class A$B {}" is "A$B"
             // The declarable name of class B in "class A { class B {}}" is "A.B"
+            // ClassInfo.getPackageName() does not understand this and will always replace "$" in names with "."
             StringBuilder sb = new StringBuilder();
-            String packageName = classInfo.getPackageName();
             ClassInfoList outerClasses = classInfo.getOuterClasses();
             // Classgraph orders this collection innermost -> outermost, but type names are declared outermost -> innermost
             for (int i = outerClasses.size() - 1; i >= 0; i--) {
@@ -198,18 +214,13 @@ public class JavaSourceSet implements Marker {
                 if (outerClass.isPrivate() || outerClass.isAnonymousInnerClass() || outerClass.isSynthetic()) {
                     return null;
                 }
-                String typeName;
-                if (outerClass.getName().contains(packageName)) {
-                    typeName = outerClass.getName();
+                if(i == outerClasses.size() - 1) {
+                    sb.append(outerClass.getName()).append(".");
                 } else {
-                    if (!classInfo.getName().contains(outerClass.getSimpleName())) {
-                        return null;
-                    }
-                    typeName = classInfo.getName().substring(0, classInfo.getName().indexOf(outerClass.getSimpleName()) + outerClass.getSimpleName().length());
+                    sb.append(outerClass.getName().substring(sb.length())).append(".");
                 }
-                sb.append(typeName.substring(packageName.length() + 1 + sb.length())).append(".");
             }
-            String nameFragment = classInfo.getName().substring(packageName.length() + 1 + sb.length());
+            String nameFragment = classInfo.getName().substring(sb.length());
 
             if (isUndeclarable(nameFragment)) {
                 return null;
@@ -217,9 +228,7 @@ public class JavaSourceSet implements Marker {
             sb.append(nameFragment);
             name = sb.toString();
         } else {
-            name = classInfo.getPackageName().length() == 0 ?
-                    classInfo.getName() :
-                    classInfo.getName().substring(classInfo.getPackageName().length() + 1);
+            name = classInfo.getName();
             if (isUndeclarable(name)) {
                 return null;
             }
