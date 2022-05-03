ab4f057462ebeebd1e07cf640df61c7813bbb619
Jonathan Schneider <jkschneider@gmail.com>
2022-05-03 12:09:12 -0400

    Turn on predictive test selection

--- build-src/src/main/kotlin/org.openrewrite.test.gradle.kts
+++ build-src/src/main/kotlin/org.openrewrite.test.gradle.kts
@@ -1,12 +1,12 @@
 import org.gradle.api.tasks.testing.Test
-//import org.gradle.kotlin.dsl.predictiveSelection
+import org.gradle.kotlin.dsl.predictiveSelection
 import org.gradle.kotlin.dsl.withType
 
 tasks.withType<Test>().configureEach {
     // recently failed tests will get selected, so let's DISABLE for the nightly
     // scheduled builds and releases
 
-//    predictiveSelection {
-//        enabled.set(providers.gradleProperty("enablePredictiveTestSelection").map(String::toBoolean).orElse(true))
-//    }
+    predictiveSelection {
+        enabled.set(providers.gradleProperty("enablePredictiveTestSelection").map(String::toBoolean).orElse(true))
+    }
 }
