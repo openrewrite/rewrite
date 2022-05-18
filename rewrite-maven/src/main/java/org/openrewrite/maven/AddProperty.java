c828ecd20ca130cf964b92edd7d346adc69639c2
Aurélien Mino <aurelien.mino@gmail.com>
2022-05-18 22:15:00 +0200

    Introduce AddProperty maven recipe (#1799)

--- rewrite-maven/src/main/java/org/openrewrite/maven/AddProperty.java
+++ rewrite-maven/src/main/java/org/openrewrite/maven/AddProperty.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 the original author or authors.
+ * Copyright 2022 the original author or authors.
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,40 +25,39 @@ import org.openrewrite.internal.lang.Nullable;
 import org.openrewrite.xml.AddToTagVisitor;
 import org.openrewrite.xml.ChangeTagValueVisitor;
 import org.openrewrite.xml.TagNameComparator;
-import org.openrewrite.xml.format.AutoFormatVisitor;
 import org.openrewrite.xml.tree.Xml;
 
 import java.util.Optional;
 
 @Value
 @EqualsAndHashCode(callSuper = true)
-public class ChangePropertyValue extends Recipe {
+public class AddProperty extends Recipe {
 
     @Option(displayName = "Key",
-            description = "The name of the property key whose value is to be changed.",
+            description = "The name of the property key to be added.",
             example = "junit.version")
     String key;
 
     @Option(displayName = "Value",
-            description = "Value to apply to the matching property.",
+            description = "The value of property to be added.",
             example = "4.13")
-    String newValue;
+    String value;
 
-    @Option(displayName = "Add if missing",
-            description = "Add the property if it is missing from the pom file.",
+    @Option(displayName = "Preserve value if property exists",
+            description = "Preserve previous value if the property already exists in the pom file.",
             required = false,
             example = "false")
     @Nullable
-    Boolean addIfMissing;
+    Boolean preserveExistingValue;
 
     @Override
     public String getDisplayName() {
-        return "Change Maven project property value";
+        return "Add Maven project property";
     }
 
     @Override
     public String getDescription() {
-        return "Changes the specified Maven project property value leaving the key intact.";
+        return "Add a new property to the Maven project property.";
     }
 
     @Override
@@ -69,17 +68,24 @@ public class ChangePropertyValue extends Recipe {
             @Override
             public Xml.Document visitDocument(Xml.Document document, ExecutionContext ctx) {
                 Xml.Document d = super.visitDocument(document, ctx);
-                if (Boolean.TRUE.equals(addIfMissing)) {
-                    doAfterVisit(new AddProperty(key, newValue, true));
+                Xml.Tag root = d.getRoot();
+                Optional<Xml.Tag> properties = root.getChild("properties");
+                if (!properties.isPresent()) {
+                    Xml.Tag propertiesTag = Xml.Tag.build("<properties>\n<" + propertyName + ">" + value + "</" + propertyName + ">\n</properties>");
+                    d = (Xml.Document) new AddToTagVisitor<ExecutionContext>(root, propertiesTag, new MavenTagInsertionComparator(root.getChildren())).visitNonNull(d, ctx);
+                } else if (!properties.get().getChildValue(propertyName).isPresent()) {
+                    Xml.Tag propertyTag = Xml.Tag.build("<" + propertyName + ">" + value + "</" + propertyName + ">");
+                    d = (Xml.Document) new AddToTagVisitor<>(properties.get(), propertyTag, new TagNameComparator()).visitNonNull(d, ctx);
                 }
                 return d;
             }
 
             @Override
             public Xml.Tag visitTag(Xml.Tag tag, ExecutionContext ctx) {
-                if (isPropertyTag() && propertyName.equals(tag.getName()) &&
-                        !newValue.equals(tag.getValue().orElse(null))) {
-                    doAfterVisit(new ChangeTagValueVisitor<>(tag, newValue));
+                if (!Boolean.TRUE.equals(preserveExistingValue)
+                        && isPropertyTag() && propertyName.equals(tag.getName())
+                        && !value.equals(tag.getValue().orElse(null))) {
+                    doAfterVisit(new ChangeTagValueVisitor<>(tag, value));
                 }
                 return super.visitTag(tag, ctx);
             }
