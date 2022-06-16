24d5d024071e3f3ec279c23edb3e42bfae2bdff0
Shyam Mehta <shyam.mehta@hotmail.com>
2022-06-16 10:59:53 -0400

    Annotated erroneous (switch statement) test case

--- rewrite-test/src/main/kotlin/org/openrewrite/java/dataflow/FindLocalFlowPathsStringTest.kt
+++ rewrite-test/src/main/kotlin/org/openrewrite/java/dataflow/FindLocalFlowPathsStringTest.kt
@@ -1091,6 +1091,7 @@ interface FindLocalFlowPathsStringTest : RewriteTest {
         )
     )
 
+    // currently giving 'java.lang.AssertionError: The recipe must make changes'
     @Test
     fun `switch with multiple cases`() = rewriteRun(
         java(
@@ -1265,30 +1266,36 @@ interface FindLocalFlowPathsStringTest : RewriteTest {
         )
     )
 
-    @Disabled
+
     @Test
     fun `lambda expression`() = rewriteRun(
         java(
             """
-            import java.util.*;
-
-            abstract class Test {
+            import java.util.ArrayList; abstract class Test {
                 abstract boolean guard();
 
                 void test() {
                     String n = "42";
-                    int [] x = new int[5];
-                    Arrays.sort(x, (Integer a, Integer b) -> {return a.compareTo(b);});
+                    ArrayList<Integer> numbers = new ArrayList<Integer>();
+                    numbers.add(5);
+                    numbers.add(9);
+                    numbers.add(8);
+                    numbers.add(1);
+                    numbers.forEach( (i) -> { System.out.println(i); } );
                 }
             }
             """, """
-            abstract class Test {
+            import java.util.ArrayList; abstract class Test {
                 abstract boolean guard();
 
                 void test() {
                     String n = /*~~>*/"42";
-                    int [] x = new int[5];
-                    Arrays.sort(x, (Integer a, Integer b) -> {return a.compareTo(b);});
+                    ArrayList<Integer> numbers = new ArrayList<Integer>();
+                    numbers.add(5);
+                    numbers.add(9);
+                    numbers.add(8);
+                    numbers.add(1);
+                    numbers.forEach( (i) -> { System.out.println(i); } );
                 }
             }
             """
