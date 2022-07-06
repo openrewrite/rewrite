3ddc194dc3c8009c97eb0ee449b29dfe951b8790
Jonathan Schneider <jkschneider@gmail.com>
2022-07-05 21:29:53 -0400

    Remove rewrite plugin from root

--- build-src/src/main/kotlin/org.openrewrite.root-project.gradle.kts
+++ build-src/src/main/kotlin/org.openrewrite.root-project.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("org.openrewrite.base")
-    id("org.openrewrite.rewrite")
     id("nebula.release")
     id("io.github.gradle-nexus.publish-plugin")
 }
@@ -13,10 +12,6 @@ nexusPublishing {
 
 configure<nebula.plugin.release.git.base.ReleasePluginExtension> {
     defaultVersionStrategy = nebula.plugin.release.NetflixOssStrategies.SNAPSHOT(project)
-}
-
-configure<org.openrewrite.gradle.RewriteExtension> {
-    activeRecipes = listOf("org.openrewrite.java.format.AutoFormat")
 }
 
 defaultTasks("build")
