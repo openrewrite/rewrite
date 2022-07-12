6c486556b2f39cf7ea18a4c2ad47c15e9ee72790
Jonathan Leitschuh <Jonathan.Leitschuh@gmail.com>
2022-07-12 19:25:03 -0400

    Fix rendering bug in ControlFlowJavaPrinter

--- rewrite-java/src/main/java/org/openrewrite/java/controlflow/ControlFlowJavaPrinter.java
+++ rewrite-java/src/main/java/org/openrewrite/java/controlflow/ControlFlowJavaPrinter.java
@@ -34,7 +34,6 @@ public class ControlFlowJavaPrinter<P> extends JavaPrinter<P> {
     public J visitAssert(J.Assert azzert, PrintOutputCapture<P> p) {
         maybeEnableOrDisable(azzert, p);
         J j = super.visitAssert(azzert, p);
-        castPrint(p).disable();
         return j;
     }
 
@@ -42,7 +41,6 @@ public class ControlFlowJavaPrinter<P> extends JavaPrinter<P> {
     public J visitAssignment(J.Assignment assignment, PrintOutputCapture<P> p) {
         maybeEnableOrDisable(assignment, p);
         J j = super.visitAssignment(assignment, p);
-        castPrint(p).disable();
         return j;
     }
 
@@ -50,7 +48,6 @@ public class ControlFlowJavaPrinter<P> extends JavaPrinter<P> {
     public J visitAssignmentOperation(J.AssignmentOperation assignOp, PrintOutputCapture<P> p) {
         maybeEnableOrDisable(assignOp, p);
         J j = super.visitAssignmentOperation(assignOp, p);
-        castPrint(p).disable();
         return j;
     }
 
@@ -58,7 +55,6 @@ public class ControlFlowJavaPrinter<P> extends JavaPrinter<P> {
     public J visitBinary(J.Binary binary, PrintOutputCapture<P> p) {
         maybeEnableOrDisable(binary, p);
         J j = super.visitBinary(binary, p);
-        castPrint(p).disable();
         return j;
     }
 
@@ -66,7 +62,6 @@ public class ControlFlowJavaPrinter<P> extends JavaPrinter<P> {
     public J visitBlock(J.Block block, PrintOutputCapture<P> p) {
         maybeEnableOrDisable(block, p);
         J j = super.visitBlock(block, p);
-        castPrint(p).disable();
         return j;
     }
 
@@ -77,14 +72,12 @@ public class ControlFlowJavaPrinter<P> extends JavaPrinter<P> {
         }
         maybeEnableOrDisable(paddedStat.getElement(), p);
         super.visitStatement(paddedStat, location, p);
-        castPrint(p).disable();
     }
 
     @Override
     public J visitBreak(J.Break breakStatement, PrintOutputCapture<P> p) {
         maybeEnableOrDisable(breakStatement, p);
         J j = super.visitBreak(breakStatement, p);
-        castPrint(p).disable();
         return j;
     }
 
@@ -92,7 +85,6 @@ public class ControlFlowJavaPrinter<P> extends JavaPrinter<P> {
     public J visitCase(J.Case caze, PrintOutputCapture<P> p) {
         maybeEnableOrDisable(caze, p);
         J j = super.visitCase(caze, p);
-        castPrint(p).disable();
         return j;
     }
 
@@ -100,7 +92,6 @@ public class ControlFlowJavaPrinter<P> extends JavaPrinter<P> {
     public J visitCatch(J.Try.Catch catzh, PrintOutputCapture<P> p) {
         maybeEnableOrDisable(catzh, p);
         J j = super.visitCatch(catzh, p);
-        castPrint(p).disable();
         return j;
     }
 
@@ -108,7 +99,6 @@ public class ControlFlowJavaPrinter<P> extends JavaPrinter<P> {
     public J visitClassDeclaration(J.ClassDeclaration classDecl, PrintOutputCapture<P> p) {
         maybeEnableOrDisable(classDecl, p);
         J j = super.visitClassDeclaration(classDecl, p);
-        castPrint(p).disable();
         return j;
     }
 
@@ -116,15 +106,17 @@ public class ControlFlowJavaPrinter<P> extends JavaPrinter<P> {
     public J visitContinue(J.Continue continueStatement, PrintOutputCapture<P> p) {
         maybeEnableOrDisable(continueStatement, p);
         J j = super.visitContinue(continueStatement, p);
-        castPrint(p).disable();
         return j;
     }
 
     @Override
     public <T extends J> J visitControlParentheses(J.ControlParentheses<T> controlParens, PrintOutputCapture<P> p) {
+        J.If enclosing = getCursor().firstEnclosing(J.If.class);
+        if (enclosing != null && enclosing.getIfCondition() == controlParens) {
+            return super.visitControlParentheses(controlParens, p);
+        }
         maybeEnableOrDisable(controlParens, p);
         J j = super.visitControlParentheses(controlParens, p);
-        castPrint(p).disable();
         return j;
     }
 
@@ -132,7 +124,6 @@ public class ControlFlowJavaPrinter<P> extends JavaPrinter<P> {
     public J visitDoWhileLoop(J.DoWhileLoop doWhileLoop, PrintOutputCapture<P> p) {
         maybeEnableOrDisable(doWhileLoop, p);
         J j = super.visitDoWhileLoop(doWhileLoop, p);
-        castPrint(p).disable();
         return j;
     }
 
@@ -140,7 +131,6 @@ public class ControlFlowJavaPrinter<P> extends JavaPrinter<P> {
     public J visitEnumValue(J.EnumValue enoom, PrintOutputCapture<P> p) {
         maybeEnableOrDisable(enoom, p);
         J j = super.visitEnumValue(enoom, p);
-        castPrint(p).disable();
         return j;
     }
 
@@ -148,7 +138,6 @@ public class ControlFlowJavaPrinter<P> extends JavaPrinter<P> {
     public J visitEnumValueSet(J.EnumValueSet enums, PrintOutputCapture<P> p) {
         maybeEnableOrDisable(enums, p);
         J j = super.visitEnumValueSet(enums, p);
-        castPrint(p).disable();
         return j;
     }
 
@@ -156,7 +145,6 @@ public class ControlFlowJavaPrinter<P> extends JavaPrinter<P> {
     public J visitFieldAccess(J.FieldAccess fieldAccess, PrintOutputCapture<P> p) {
         maybeEnableOrDisable(fieldAccess, p);
         J j = super.visitFieldAccess(fieldAccess, p);
-        castPrint(p).disable();
         return j;
     }
 
@@ -164,7 +152,6 @@ public class ControlFlowJavaPrinter<P> extends JavaPrinter<P> {
     public J visitForLoop(J.ForLoop forLoop, PrintOutputCapture<P> p) {
         maybeEnableOrDisable(forLoop, p);
         J j = super.visitForLoop(forLoop, p);
-        castPrint(p).disable();
         return j;
     }
 
@@ -172,7 +159,6 @@ public class ControlFlowJavaPrinter<P> extends JavaPrinter<P> {
     public J visitForEachLoop(J.ForEachLoop forEachLoop, PrintOutputCapture<P> p) {
         maybeEnableOrDisable(forEachLoop, p);
         J j = super.visitForEachLoop(forEachLoop, p);
-        castPrint(p).disable();
         return j;
     }
 
@@ -195,7 +181,6 @@ public class ControlFlowJavaPrinter<P> extends JavaPrinter<P> {
         }
         maybeEnableOrDisable(ident, p);
         J j = super.visitIdentifier(ident, p);
-        castPrint(p).disable();
         return j;
     }
 
@@ -203,7 +188,6 @@ public class ControlFlowJavaPrinter<P> extends JavaPrinter<P> {
     public J visitIf(J.If iff, PrintOutputCapture<P> p) {
         maybeEnableOrDisable(iff, p);
         J j = super.visitIf(iff, p);
-        castPrint(p).disable();
         return j;
     }
 
@@ -211,7 +195,6 @@ public class ControlFlowJavaPrinter<P> extends JavaPrinter<P> {
     public J visitInstanceOf(J.InstanceOf instanceOf, PrintOutputCapture<P> p) {
         maybeEnableOrDisable(instanceOf, p);
         J j = super.visitInstanceOf(instanceOf, p);
-        castPrint(p).disable();
         return j;
     }
 
@@ -219,7 +202,6 @@ public class ControlFlowJavaPrinter<P> extends JavaPrinter<P> {
     public J visitLambda(J.Lambda lambda, PrintOutputCapture<P> p) {
         maybeEnableOrDisable(lambda, p);
         J j = super.visitLambda(lambda, p);
-        castPrint(p).disable();
         return j;
     }
 
@@ -227,7 +209,6 @@ public class ControlFlowJavaPrinter<P> extends JavaPrinter<P> {
     public J visitLiteral(J.Literal literal, PrintOutputCapture<P> p) {
         maybeEnableOrDisable(literal, p);
         J j = super.visitLiteral(literal, p);
-        castPrint(p).disable();
         return j;
     }
 
@@ -235,7 +216,6 @@ public class ControlFlowJavaPrinter<P> extends JavaPrinter<P> {
     public J visitMemberReference(J.MemberReference memberRef, PrintOutputCapture<P> p) {
         maybeEnableOrDisable(memberRef, p);
         J j = super.visitMemberReference(memberRef, p);
-        castPrint(p).disable();
         return j;
     }
 
@@ -243,7 +223,6 @@ public class ControlFlowJavaPrinter<P> extends JavaPrinter<P> {
     public J visitMethodDeclaration(J.MethodDeclaration method, PrintOutputCapture<P> p) {
         maybeEnableOrDisable(method, p);
         J j = super.visitMethodDeclaration(method, p);
-        castPrint(p).disable();
         return j;
     }
 
@@ -251,7 +230,6 @@ public class ControlFlowJavaPrinter<P> extends JavaPrinter<P> {
     public J visitMethodInvocation(J.MethodInvocation method, PrintOutputCapture<P> p) {
         maybeEnableOrDisable(method, p);
         J j = super.visitMethodInvocation(method, p);
-        castPrint(p).disable();
         return j;
     }
 
@@ -259,7 +237,6 @@ public class ControlFlowJavaPrinter<P> extends JavaPrinter<P> {
     public J visitMultiCatch(J.MultiCatch multiCatch, PrintOutputCapture<P> p) {
         maybeEnableOrDisable(multiCatch, p);
         J j = super.visitMultiCatch(multiCatch, p);
-        castPrint(p).disable();
         return j;
     }
 
@@ -267,7 +244,6 @@ public class ControlFlowJavaPrinter<P> extends JavaPrinter<P> {
     public J visitVariableDeclarations(J.VariableDeclarations multiVariable, PrintOutputCapture<P> p) {
         maybeEnableOrDisable(multiVariable, p);
         J j = super.visitVariableDeclarations(multiVariable, p);
-        castPrint(p).disable();
         return j;
     }
 
@@ -275,7 +251,6 @@ public class ControlFlowJavaPrinter<P> extends JavaPrinter<P> {
     public J visitNewArray(J.NewArray newArray, PrintOutputCapture<P> p) {
         maybeEnableOrDisable(newArray, p);
         J j = super.visitNewArray(newArray, p);
-        castPrint(p).disable();
         return j;
     }
 
@@ -283,7 +258,6 @@ public class ControlFlowJavaPrinter<P> extends JavaPrinter<P> {
     public <T extends J> J visitParentheses(J.Parentheses<T> parens, PrintOutputCapture<P> p) {
         maybeEnableOrDisable(parens, p);
         J j = super.visitParentheses(parens, p);
-        castPrint(p).disable();
         return j;
     }
 
@@ -291,7 +265,6 @@ public class ControlFlowJavaPrinter<P> extends JavaPrinter<P> {
     public J visitSwitch(J.Switch switzh, PrintOutputCapture<P> p) {
         maybeEnableOrDisable(switzh, p);
         J j = super.visitSwitch(switzh, p);
-        castPrint(p).disable();
         return j;
     }
 
@@ -299,7 +272,6 @@ public class ControlFlowJavaPrinter<P> extends JavaPrinter<P> {
     public J visitSynchronized(J.Synchronized synch, PrintOutputCapture<P> p) {
         maybeEnableOrDisable(synch, p);
         J j = super.visitSynchronized(synch, p);
-        castPrint(p).disable();
         return j;
     }
 
@@ -307,7 +279,6 @@ public class ControlFlowJavaPrinter<P> extends JavaPrinter<P> {
     public J visitTernary(J.Ternary ternary, PrintOutputCapture<P> p) {
         maybeEnableOrDisable(ternary, p);
         J j = super.visitTernary(ternary, p);
-        castPrint(p).disable();
         return j;
     }
 
@@ -315,7 +286,6 @@ public class ControlFlowJavaPrinter<P> extends JavaPrinter<P> {
     public J visitThrow(J.Throw thrown, PrintOutputCapture<P> p) {
         maybeEnableOrDisable(thrown, p);
         J j = super.visitThrow(thrown, p);
-        castPrint(p).disable();
         return j;
     }
 
@@ -323,7 +293,6 @@ public class ControlFlowJavaPrinter<P> extends JavaPrinter<P> {
     public J visitTry(J.Try tryable, PrintOutputCapture<P> p) {
         maybeEnableOrDisable(tryable, p);
         J j = super.visitTry(tryable, p);
-        castPrint(p).disable();
         return j;
     }
 
@@ -331,7 +300,6 @@ public class ControlFlowJavaPrinter<P> extends JavaPrinter<P> {
     public J visitUnary(J.Unary unary, PrintOutputCapture<P> p) {
         maybeEnableOrDisable(unary, p);
         J j = super.visitUnary(unary, p);
-        castPrint(p).disable();
         return j;
     }
 
@@ -339,7 +307,6 @@ public class ControlFlowJavaPrinter<P> extends JavaPrinter<P> {
     public J visitVariable(J.VariableDeclarations.NamedVariable variable, PrintOutputCapture<P> p) {
         maybeEnableOrDisable(variable, p);
         J j = super.visitVariable(variable, p);
-        castPrint(p).disable();
         return j;
     }
 
@@ -347,7 +314,6 @@ public class ControlFlowJavaPrinter<P> extends JavaPrinter<P> {
     public J visitWhileLoop(J.WhileLoop whileLoop, PrintOutputCapture<P> p) {
         maybeEnableOrDisable(whileLoop, p);
         J j = super.visitWhileLoop(whileLoop, p);
-        castPrint(p).disable();
         return j;
     }
 
