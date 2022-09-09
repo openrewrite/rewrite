40ca714c7630b6d89c5d69d9b90e1e7d2b67ff25
Sam Snyder <sam@moderne.io>
2022-09-08 20:51:06 -0700

    Cleanup unused imports

--- rewrite-java/src/main/java/org/openrewrite/java/search/FindDeprecatedUses.java
+++ rewrite-java/src/main/java/org/openrewrite/java/search/FindDeprecatedUses.java
@@ -16,14 +16,9 @@
 package org.openrewrite.java.search;
 
 import lombok.Getter;
-import org.openrewrite.ExecutionContext;
 import org.openrewrite.Option;
 import org.openrewrite.Recipe;
-import org.openrewrite.TreeVisitor;
 import org.openrewrite.internal.lang.Nullable;
-import org.openrewrite.java.JavaIsoVisitor;
-import org.openrewrite.java.tree.J;
-import org.openrewrite.java.tree.JavaSourceFile;
 
 @Getter
 public class FindDeprecatedUses extends Recipe {
