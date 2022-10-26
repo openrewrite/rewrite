409634fba79f35217f76c58409bf16aadeaa07fc
Jonathan Schneider <jkschneider@gmail.com>
2022-10-26 11:12:32 -0700

    Relax  target type to include methods

--- rewrite-core/src/main/java/org/openrewrite/Option.java
+++ rewrite-core/src/main/java/org/openrewrite/Option.java
@@ -22,7 +22,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ElementType.FIELD})
+@Target({ElementType.FIELD, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Option {
     @Language("markdown") String displayName() default "";
