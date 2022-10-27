f63a6b39e9928e8809d36525acf4008c5fd49245
Patrick Way <pway99@users.noreply.github.com>
2022-10-27 11:53:03 -0700

    Add UpdateOwaspSuppressionDate recipe. (#2369)

---
+++ rewrite-xml/src/main/java/org/openrewrite/xml/security/UpdateOwaspSuppressionDate.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright 2022 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.xml.security;
+
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+import org.openrewrite.*;
+import org.openrewrite.internal.ListUtils;
+import org.openrewrite.internal.lang.Nullable;
+import org.openrewrite.xml.XPathMatcher;
+import org.openrewrite.xml.XmlIsoVisitor;
+import org.openrewrite.xml.tree.Xml;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+@Value
+@EqualsAndHashCode(callSuper = true)
+public class UpdateOwaspSuppressionDate extends Recipe {
+
+    @Override
+    public String getDisplayName() {
+        return "Update OWASP suppression date bounds";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Updates the expiration date for OWASP suppressions having a matching cve tag. " +
+                "For use with the OWASP `dependency-check` tool. " +
+                "More details: https://jeremylong.github.io/DependencyCheck/general/suppression.html";
+    }
+
+    @Option(displayName = "CVE",
+            description = "Update suppressions having the specified CVE tag.",
+            example = "CVE-2022-1234")
+    String cve;
+
+    @Option(displayName = "Until date",
+            required = false,
+            description = "Optional. The date to add to the suppression. Default will be 30 days from today.",
+            example = "2023-01-01")
+    @Nullable
+    String untilDate;
+
+    @Override
+    protected TreeVisitor<?, ExecutionContext> getVisitor() {
+        return new UpdateDateBoundedSuppressionVisitor();
+    }
+
+    @Override
+    protected TreeVisitor<?, ExecutionContext> getSingleSourceApplicableTest() {
+        return new IsOwaspSuppressionsFile();
+    }
+
+    @Override
+    public Validated validate() {
+        return super.validate().and(Validated.test("untilDate", "Must be empty or a valid date of format yyyy-MM-dd", untilDate, date -> {
+            if (date != null && !date.isEmpty()) {
+                try {
+                    LocalDate.parse(date);
+                } catch (Exception e) {
+                    return false;
+                }
+            }
+            return true;
+        }));
+    }
+
+    private class UpdateDateBoundedSuppressionVisitor extends XmlIsoVisitor<ExecutionContext> {
+        @Override
+        public Xml.Tag visitTag(Xml.Tag tag, ExecutionContext ctx) {
+            Xml.Tag t = super.visitTag(tag, ctx);
+            if (new XPathMatcher("/suppressions/suppress").matches(getCursor())) {
+                boolean hasCve = false;
+                List<Xml.Tag> cveList = t.getChildren("cve");
+                for (Xml.Tag xml : cveList) {
+                    String cveNum = xml.getValue().orElse("");
+                    if (cve.equals(cveNum)) {
+                        hasCve = true;
+                        break;
+                    }
+                }
+                if (hasCve) {
+                    String date = (untilDate != null && !untilDate.isEmpty()) ? untilDate : LocalDate.now().plus(30, ChronoUnit.DAYS).toString();
+                    final String zuluDate = date + "Z";
+                    t = t.withAttributes(ListUtils.map(t.getAttributes(), attr -> {
+                        if ("until".equals(attr.getKeyAsString())) {
+                            if (!zuluDate.equals(attr.getValueAsString())) {
+                                attr = attr.withValue(attr.getValue().withValue(zuluDate));
+                            }
+                        }
+                        return attr;
+                    }));
+                }
+            }
+            return t;
+        }
+    }
+}
---
+++ rewrite-xml/src/test/java/org/openrewrite/xml/security/UpdateOwaspSuppressionDateTest.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright 2022 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.xml.security;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.openrewrite.test.RewriteTest;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.openrewrite.xml.Assertions.xml;
+
+public class UpdateOwaspSuppressionDateTest implements RewriteTest {
+
+    @Test
+    void noUpdateIfCveDoesNotExist() {
+        rewriteRun(
+                spec -> spec.recipe(new UpdateOwaspSuppressionDate("CVE-2022-5678", "2020-02-01")),
+                xml("""
+                    <?xml version="1.0" encoding="UTF-8" ?>
+                    <suppressions xmlns="https://jeremylong.github.io/DependencyCheck/dependency-suppression.1.3.xsd">
+                        <suppress until="2020-01-01Z">
+                            <notes>
+                            </notes>
+                            <cve>CVE-2019-10321</cve>
+                            <cve>CVE-2022-1234</cve>
+                        </suppress>
+                    </suppressions>""",
+                spec -> spec.path("suppressions.xml"))
+        );
+    }
+
+    @Test
+    void updatesUntilIfCveExists() {
+        rewriteRun(
+                spec -> spec.recipe(new UpdateOwaspSuppressionDate("CVE-2022-1234", "2020-02-01")),
+                xml("""
+                    <?xml version="1.0" encoding="UTF-8" ?>
+                    <suppressions xmlns="https://jeremylong.github.io/DependencyCheck/dependency-suppression.1.3.xsd">
+                        <suppress until="2020-01-01Z">
+                            <notes>
+                            </notes>
+                            <cve>CVE-2019-5678</cve>
+                            <cve>CVE-2022-1234</cve>
+                        </suppress>
+                    </suppressions>""", """
+                    <?xml version="1.0" encoding="UTF-8" ?>
+                    <suppressions xmlns="https://jeremylong.github.io/DependencyCheck/dependency-suppression.1.3.xsd">
+                        <suppress until="2020-02-01Z">
+                            <notes>
+                            </notes>
+                            <cve>CVE-2019-5678</cve>
+                            <cve>CVE-2022-1234</cve>
+                        </suppress>
+                    </suppressions>""",
+                spec -> spec.path("suppressions.xml"))
+        );
+    }
+
+    @Test
+    void defaultIs30DaysInFuture() {
+        LocalDate today = LocalDate.now();
+        LocalDate thirtyDaysFromNow = today.plusDays(30);
+        String thirtyDaysFromNowString = thirtyDaysFromNow + "Z";
+        rewriteRun(
+                spec -> spec.recipe(new UpdateOwaspSuppressionDate("CVE-2022-1234", null)),
+                xml("""
+                    <?xml version="1.0" encoding="UTF-8" ?>
+                    <suppressions xmlns="https://jeremylong.github.io/DependencyCheck/dependency-suppression.1.3.xsd">
+                        <suppress until="2020-01-01Z">
+                            <notes>
+                            </notes>
+                            <cve>CVE-2019-10321</cve>
+                            <cve>CVE-2022-1234</cve>
+                        </suppress>
+                    </suppressions>""", """
+                    <?xml version="1.0" encoding="UTF-8" ?>
+                    <suppressions xmlns="https://jeremylong.github.io/DependencyCheck/dependency-suppression.1.3.xsd">
+                        <suppress until="%s">
+                            <notes>
+                            </notes>
+                            <cve>CVE-2019-10321</cve>
+                            <cve>CVE-2022-1234</cve>
+                        </suppress>
+                    </suppressions>""".formatted(thirtyDaysFromNowString),
+                spec -> spec.path("suppressions.xml"))
+        );
+    }
+
+    @ParameterizedTest
+    @CsvSource({"abcd,false",
+            "2022,false",
+            "2022-01-01,true"})
+    void valid(String untilDate, boolean valid) {
+        assertThat(new DateBoundSuppressions(untilDate).validate().isValid()).isEqualTo(valid);
+    }
+}
