7138a5201c03398a86a2dfae180f483d827826a2
Knut Wannheden <knut.wannheden@mobi.ch>
2022-11-04 09:50:31 +0100

    New Maven `RemoveDuplicateDependencies` recipe
    New Maven recipe to remove duplicates from the dependencies sections (but managed and actual).
    
    Also optimizes `MavenVisitor#findDependency()`.
    
    Fixes #2381

---
+++ rewrite-maven/src/test/java/org/openrewrite/maven/RemoveDuplicateDependenciesTest.java
@@ -0,0 +1,430 @@
+/*
+ * Copyright 2022 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.maven;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.maven.Assertions.pomXml;
+
+class RemoveDuplicateDependenciesTest implements RewriteTest {
+
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.expectedCyclesThatMakeChanges(1).recipe(new RemoveDuplicateDependencies());
+    }
+
+    @Test
+    void notApplicable() {
+        rewriteRun(
+          spec -> spec.expectedCyclesThatMakeChanges(0),
+          pomXml(
+            """
+                  <project>
+                    <modelVersion>4.0.0</modelVersion>
+                    
+                    <groupId>com.mycompany.app</groupId>
+                    <artifactId>my-app</artifactId>
+                    <version>1</version>
+                  </project>
+              """
+          )
+        );
+    }
+
+    @Test
+    void removeSingleDuplicate() {
+        rewriteRun(
+          pomXml(
+            """
+                  <project>
+                    <modelVersion>4.0.0</modelVersion>
+                    
+                    <groupId>com.mycompany.app</groupId>
+                    <artifactId>my-app</artifactId>
+                    <version>1</version>
+                    
+                    <dependencies>
+                      <dependency>
+                        <groupId>com.google.guava</groupId>
+                        <artifactId>guava</artifactId>
+                        <version>29.0-jre</version>
+                      </dependency>
+                      <dependency>
+                        <groupId>junit</groupId>
+                        <artifactId>junit</artifactId>
+                        <version>4.13.1</version>
+                        <scope>test</scope>
+                      </dependency>
+                      <dependency>
+                        <groupId>com.google.guava</groupId>
+                        <artifactId>guava</artifactId>
+                        <version>29.0-jre</version>
+                      </dependency>
+                    </dependencies>
+                  </project>
+              """,
+            """
+                  <project>
+                    <modelVersion>4.0.0</modelVersion>
+                    
+                    <groupId>com.mycompany.app</groupId>
+                    <artifactId>my-app</artifactId>
+                    <version>1</version>
+                    
+                    <dependencies>
+                      <dependency>
+                        <groupId>com.google.guava</groupId>
+                        <artifactId>guava</artifactId>
+                        <version>29.0-jre</version>
+                      </dependency>
+                      <dependency>
+                        <groupId>junit</groupId>
+                        <artifactId>junit</artifactId>
+                        <version>4.13.1</version>
+                        <scope>test</scope>
+                      </dependency>
+                    </dependencies>
+                  </project>
+              """
+          )
+        );
+    }
+
+    @Test
+    void preservesComments() {
+        rewriteRun(
+          pomXml(
+            """
+                  <project>
+                    <modelVersion>4.0.0</modelVersion>
+                    
+                    <groupId>com.mycompany.app</groupId>
+                    <artifactId>my-app</artifactId>
+                    <version>1</version>
+                    
+                    <dependencies>
+                      <!-- comment 1 -->
+                      <dependency>
+                        <groupId>com.google.guava</groupId>
+                        <artifactId>guava</artifactId>
+                        <version>29.0-jre</version>
+                      </dependency>
+                      <!-- comment 2 -->
+                      <dependency>
+                        <groupId>junit</groupId>
+                        <artifactId>junit</artifactId>
+                        <version>4.13.1</version>
+                        <scope>test</scope>
+                      </dependency>
+                      <!-- comment 3 -->
+                      <dependency>
+                        <groupId>com.google.guava</groupId>
+                        <artifactId>guava</artifactId>
+                        <version>29.0-jre</version>
+                      </dependency>
+                    </dependencies>
+                  </project>
+              """,
+            """
+                  <project>
+                    <modelVersion>4.0.0</modelVersion>
+                    
+                    <groupId>com.mycompany.app</groupId>
+                    <artifactId>my-app</artifactId>
+                    <version>1</version>
+                    
+                    <dependencies>
+                      <!-- comment 1 -->
+                      <dependency>
+                        <groupId>com.google.guava</groupId>
+                        <artifactId>guava</artifactId>
+                        <version>29.0-jre</version>
+                      </dependency>
+                      <!-- comment 2 -->
+                      <dependency>
+                        <groupId>junit</groupId>
+                        <artifactId>junit</artifactId>
+                        <version>4.13.1</version>
+                        <scope>test</scope>
+                      </dependency>
+                      <!-- comment 3 -->
+                    </dependencies>
+                  </project>
+              """
+          )
+        );
+    }
+
+    @Test
+    void removeMultipleDuplicates() {
+        rewriteRun(
+          pomXml(
+            """
+                  <project>
+                    <modelVersion>4.0.0</modelVersion>
+                    
+                    <groupId>com.mycompany.app</groupId>
+                    <artifactId>my-app</artifactId>
+                    <version>1</version>
+                    
+                    <dependencies>
+                      <dependency>
+                        <groupId>com.google.guava</groupId>
+                        <artifactId>guava</artifactId>
+                        <version>29.0-jre</version>
+                      </dependency>
+                      <dependency>
+                        <groupId>com.google.guava</groupId>
+                        <artifactId>guava</artifactId>
+                        <version>29.0-jre</version>
+                      </dependency>
+                      <dependency>
+                        <groupId>junit</groupId>
+                        <artifactId>junit</artifactId>
+                        <version>4.13.1</version>
+                        <scope>test</scope>
+                      </dependency>
+                      <dependency>
+                        <groupId>com.google.guava</groupId>
+                        <artifactId>guava</artifactId>
+                        <version>29.0-jre</version>
+                      </dependency>
+                    </dependencies>
+                  </project>
+              """,
+            """
+                  <project>
+                    <modelVersion>4.0.0</modelVersion>
+                    
+                    <groupId>com.mycompany.app</groupId>
+                    <artifactId>my-app</artifactId>
+                    <version>1</version>
+                    
+                    <dependencies>
+                      <dependency>
+                        <groupId>com.google.guava</groupId>
+                        <artifactId>guava</artifactId>
+                        <version>29.0-jre</version>
+                      </dependency>
+                      <dependency>
+                        <groupId>junit</groupId>
+                        <artifactId>junit</artifactId>
+                        <version>4.13.1</version>
+                        <scope>test</scope>
+                      </dependency>
+                    </dependencies>
+                  </project>
+              """
+          )
+        );
+    }
+
+    @Test
+    void removeDependencyWithDifferentVersion() {
+        rewriteRun(
+          pomXml(
+            """
+                  <project>
+                    <modelVersion>4.0.0</modelVersion>
+                    
+                    <groupId>com.mycompany.app</groupId>
+                    <artifactId>my-app</artifactId>
+                    <version>1</version>
+                    
+                    <dependencies>
+                      <dependency>
+                        <groupId>com.google.inject</groupId>
+                        <artifactId>guice</artifactId>
+                        <version>4.2.1</version>
+                      </dependency>
+                      <dependency>
+                        <groupId>com.google.inject</groupId>
+                        <artifactId>guice</artifactId>
+                        <version>4.2.2</version>
+                      </dependency>
+                    </dependencies>
+                  </project>
+              """, """
+                  <project>
+                    <modelVersion>4.0.0</modelVersion>
+                    
+                    <groupId>com.mycompany.app</groupId>
+                    <artifactId>my-app</artifactId>
+                    <version>1</version>
+                    
+                    <dependencies>
+                      <dependency>
+                        <groupId>com.google.inject</groupId>
+                        <artifactId>guice</artifactId>
+                        <version>4.2.1</version>
+                      </dependency>
+                    </dependencies>
+                  </project>
+              """
+          )
+        );
+    }
+
+    @Test
+    void keepDependencyWithClassifier() {
+        rewriteRun(
+          spec -> spec.expectedCyclesThatMakeChanges(0),
+          pomXml(
+            """
+                  <project>
+                    <modelVersion>4.0.0</modelVersion>
+                    
+                    <groupId>com.mycompany.app</groupId>
+                    <artifactId>my-app</artifactId>
+                    <version>1</version>
+                    
+                    <dependencies>
+                      <dependency>
+                        <groupId>com.google.inject</groupId>
+                        <artifactId>guice</artifactId>
+                        <version>4.2.2</version>
+                      </dependency>
+                      <dependency>
+                        <groupId>com.google.inject</groupId>
+                        <artifactId>guice</artifactId>
+                        <version>4.2.2</version>
+                        <classifier>no_aop</classifier>
+                      </dependency>
+                    </dependencies>
+                  </project>
+              """
+          )
+        );
+    }
+
+    @Disabled("Unsure if this is a valid use case or not")
+    @Test
+    void keepDependencyWithType() {
+        rewriteRun(
+          spec -> spec.expectedCyclesThatMakeChanges(0),
+          pomXml(
+            """
+                  <project>
+                    <modelVersion>4.0.0</modelVersion>
+                    
+                    <groupId>com.mycompany.app</groupId>
+                    <artifactId>my-app</artifactId>
+                    <version>1</version>
+                    
+                    <dependencies>
+                      <dependency>
+                        <groupId>com.google.guava</groupId>
+                        <artifactId>guava</artifactId>
+                        <version>29.0-jre</version>
+                      </dependency>
+                      <dependency>
+                        <groupId>com.google.guava</groupId>
+                        <artifactId>guava</artifactId>
+                        <version>29.0-jre</version>
+                        <type>war</type>
+                      </dependency>
+                    </dependencies>
+                  </project>
+              """
+          )
+        );
+    }
+
+    @Test
+    void keepDependencyWithDifferentScope() {
+        rewriteRun(
+          spec -> spec.expectedCyclesThatMakeChanges(0),
+          pomXml(
+            """
+                  <project>
+                    <modelVersion>4.0.0</modelVersion>
+                    
+                    <groupId>com.mycompany.app</groupId>
+                    <artifactId>my-app</artifactId>
+                    <version>1</version>
+                    
+                    <dependencies>
+                      <dependency>
+                        <groupId>com.google.guava</groupId>
+                        <artifactId>guava</artifactId>
+                        <version>29.0-jre</version>
+                        <scope>compile</scope>
+                      </dependency>
+                      <dependency>
+                        <groupId>com.google.guava</groupId>
+                        <artifactId>guava</artifactId>
+                        <version>29.0-jre</version>
+                        <scope>test</scope>
+                      </dependency>
+                    </dependencies>
+                  </project>
+              """
+          )
+        );
+    }
+
+    @Test
+    void removeDependencyWithDefaultType() {
+        rewriteRun(
+          pomXml(
+            """
+                  <project>
+                    <modelVersion>4.0.0</modelVersion>
+                    
+                    <groupId>com.mycompany.app</groupId>
+                    <artifactId>my-app</artifactId>
+                    <version>1</version>
+                    
+                    <dependencies>
+                      <dependency>
+                        <groupId>com.google.guava</groupId>
+                        <artifactId>guava</artifactId>
+                        <version>29.0-jre</version>
+                      </dependency>
+                      <dependency>
+                        <groupId>com.google.guava</groupId>
+                        <artifactId>guava</artifactId>
+                        <version>29.0-jre</version>
+                        <type>jar</type>
+                      </dependency>
+                    </dependencies>
+                  </project>
+              """,
+            """
+                  <project>
+                    <modelVersion>4.0.0</modelVersion>
+                    
+                    <groupId>com.mycompany.app</groupId>
+                    <artifactId>my-app</artifactId>
+                    <version>1</version>
+                    
+                    <dependencies>
+                      <dependency>
+                        <groupId>com.google.guava</groupId>
+                        <artifactId>guava</artifactId>
+                        <version>29.0-jre</version>
+                      </dependency>
+                    </dependencies>
+                  </project>
+              """
+          )
+        );
+    }
+
+}
