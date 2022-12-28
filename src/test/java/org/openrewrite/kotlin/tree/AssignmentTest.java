4560539a84dd2c3241f43ca6ef28e70e7399f91d
traceyyoshima <tracey.yoshima@gmail.com>
2022-12-27 17:12:08 -0800

    More parsing tests.

---
+++ src/test/java/org/openrewrite/kotlin/tree/AssignmentTest.java
@@ -0,0 +1,26 @@
+package org.openrewrite.kotlin.tree;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.kotlin.Assertions.kotlin;
+import static org.openrewrite.kotlin.tree.ParserAsserts.isFullyParsed;
+
+public class AssignmentTest implements RewriteTest {
+
+    @Disabled("Requires function call and Convert PSI.")
+    @Test
+    void unaryMinus() {
+        rewriteRun(
+          kotlin("""
+              val i = -1
+              val l = -2L
+              val f = -3.0f
+              val d = -4.0
+              """,
+            isFullyParsed()
+          )
+        );
+    }
+}
---
+++ src/test/java/org/openrewrite/kotlin/tree/BinaryTest.java
@@ -0,0 +1,40 @@
+package org.openrewrite.kotlin.tree;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.kotlin.Assertions.kotlin;
+import static org.openrewrite.kotlin.tree.ParserAsserts.isFullyParsed;
+
+public class BinaryTest implements RewriteTest {
+
+    @Test
+    void equals() {
+        rewriteRun(
+          kotlin("""
+              fun method() {
+                val n = 0
+                val b = n == 0
+              }
+              """,
+            isFullyParsed()
+          )
+        );
+    }
+
+    @Disabled("Requires function call and Convert PSI.")
+    @Test
+    void minusEquals() {
+        rewriteRun(
+          kotlin(
+            """
+              fun method() {
+                var n = 0
+                n -= 5
+              }
+              """
+          )
+        );
+    }
+}
