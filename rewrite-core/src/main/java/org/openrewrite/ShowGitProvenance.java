bb33750e937de54a39b0ad9a97551d8e86319bf5
Kevin McCarpenter™️ <kevin@moderne.io>
2023-01-12 22:37:56 -0600

    Recipe to print out git provenance (#2640)

---
+++ rewrite-core/src/main/java/org/openrewrite/ShowGitProvenance.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2022 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite;
+
+import org.openrewrite.internal.ListUtils;
+import org.openrewrite.marker.GitProvenance;
+import org.openrewrite.marker.Markup;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class ShowGitProvenance extends Recipe {
+
+    @Override
+    public String getDisplayName() {
+        return "Show Git source control metadata";
+    }
+
+    @Override
+    public String getDescription() {
+        return "List out the contents of each unique `GitProvenance` marker in the set of source files. " +
+               "When everything is working correctly, exactly one such marker should be printed as all source files are " +
+               "expected to come from the same repository / branch / commit ID.";
+    }
+
+    @Override
+    protected List<SourceFile> visit(List<SourceFile> before, ExecutionContext ctx) {
+        Set<GitProvenance> provenances = new HashSet<>();
+
+        return ListUtils.map(before, sourceFile -> {
+            GitProvenance provenance = sourceFile.getMarkers().findFirst(GitProvenance.class).orElse(null);
+            if (provenance == null || !provenances.add(provenance)) {
+                return sourceFile;
+            }
+            return Markup.info(sourceFile, String.format("GitProvenance:\n" +
+                                                         "    origin: %s\n" +
+                                                         "    branch: %s\n" +
+                                                         "    changeset: %s\n" +
+                                                         "    autocrlf: %s\n" +
+                                                         "    eol: %s",
+                    provenance.getOrigin(),
+                    provenance.getBranch(),
+                    provenance.getChange(),
+                    provenance.getAutocrlf() != null ? provenance.getAutocrlf().toString() : "null",
+                    provenance.getEol() != null ? provenance.getEol().toString() : "null"));
+        });
+    }
+}
