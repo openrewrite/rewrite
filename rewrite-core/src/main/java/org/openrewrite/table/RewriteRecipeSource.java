e49084855a2b9249d5b32546ce2abdee10546478
Jonathan Schneider <jkschneider@gmail.com>
2023-01-17 00:11:12 -0500

    FindRecipes / fixes #2661

---
+++ rewrite-core/src/main/java/org/openrewrite/table/RewriteRecipeSource.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2022 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreType;
+import lombok.Value;
+import org.openrewrite.DataTable;
+import org.openrewrite.Recipe;
+
+@JsonIgnoreType
+public class RewriteRecipeSource extends DataTable<RewriteRecipeSource.Row> {
+
+    public RewriteRecipeSource(Recipe recipe) {
+        super(recipe, Row.class,
+                RewriteRecipeSource.class.getName(),
+                "Rewrite recipe source code",
+                "This table contains the source code of recipes along with their metadata " +
+                "for use in an experiment fine-tuning large language models to produce more recipes."
+        );
+    }
+
+    @Value
+    public static class Row {
+        String displayName;
+        String description;
+        RecipeType recipeType;
+        String sourceCode;
+    }
+
+    public enum RecipeType {
+        Java,
+        Yaml
+    }
+}
