f05fe6b93c4d33bca69cc960d2b181fa4c39cdc4
Nick McKinney <mckinneynicholas@gmail.com>
2023-01-23 09:47:07 -0500

    renaming groupId in test case to avoid cached results from other test cases in class (!)

--- rewrite-maven/src/test/java/org/openrewrite/maven/MavenParserTest.java
+++ rewrite-maven/src/test/java/org/openrewrite/maven/MavenParserTest.java
@@ -2074,7 +2074,7 @@ class MavenParserTest implements RewriteTest {
                 <?xml version="1.0" encoding="UTF-8"?>
                 <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
                   <modelVersion>4.0.0</modelVersion>
-                  <groupId>org.sample</groupId>
+                  <groupId>org.sample.optional</groupId>
                   <artifactId>a</artifactId>
                   <version>1.0.0</version>
                 </project>
@@ -2084,12 +2084,12 @@ class MavenParserTest implements RewriteTest {
                 <?xml version="1.0" encoding="UTF-8"?>
                 <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
                   <modelVersion>4.0.0</modelVersion>
-                  <groupId>org.sample</groupId>
+                  <groupId>org.sample.optional</groupId>
                   <artifactId>b</artifactId>
                   <version>1.0.0</version>
                   <dependencies>
                     <dependency>
-                      <groupId>org.sample</groupId>
+                      <groupId>org.sample.optional</groupId>
                       <artifactId>a</artifactId>
                       <version>1.0.0</version>
                       <optional>true</optional>
@@ -2107,12 +2107,12 @@ class MavenParserTest implements RewriteTest {
                 <?xml version="1.0" encoding="UTF-8"?>
                 <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
                   <modelVersion>4.0.0</modelVersion>
-                  <groupId>org.sample</groupId>
+                  <groupId>org.sample.optional</groupId>
                   <artifactId>c</artifactId>
                   <version>1.0.0</version>
                   <dependencies>
                     <dependency>
-                      <groupId>org.sample</groupId>
+                      <groupId>org.sample.optional</groupId>
                       <artifactId>b</artifactId>
                       <version>1.0.0</version>
                     </dependency>
