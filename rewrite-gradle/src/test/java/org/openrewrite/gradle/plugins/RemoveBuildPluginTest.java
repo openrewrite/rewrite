f15af11d3ba54c8202b269397121dc68d6bbd540
Shannon Pamperl <shanman190@gmail.com>
2023-01-24 20:04:51 -0600

    Add remove plugin support and Gradle Enterprise migration recipe (#2701)

---
+++ rewrite-gradle/src/test/java/org/openrewrite/gradle/plugins/RemoveBuildPluginTest.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2023 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.gradle.plugins;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.gradle.Assertions.buildGradle;
+import static org.openrewrite.gradle.Assertions.settingsGradle;
+
+class RemoveBuildPluginTest implements RewriteTest {
+    @Test
+    void removePlugin() {
+        rewriteRun(
+          spec -> spec.recipe(new RemoveBuildPlugin("com.jfrog.bintray")),
+          buildGradle(
+            """
+              plugins {
+                  id "com.jfrog.bintray" version "1.0"
+              }
+              """,
+            ""
+          )
+        );
+    }
+
+    @Test
+    void removePluginWithoutVersion() {
+        rewriteRun(
+          spec -> spec.recipe(new RemoveBuildPlugin("java")),
+          buildGradle(
+            """
+              plugins {
+                  id "java"
+              }
+              """,
+            ""
+          )
+        );
+    }
+
+    @Test
+    void removeUnappliedPlugin() {
+        rewriteRun(
+          spec -> spec.recipe(new RemoveBuildPlugin("com.jfrog.bintray")),
+          buildGradle(
+            """
+              plugins {
+                  id "com.jfrog.bintray" version "1.0" apply false
+              }
+              """,
+            ""
+          )
+        );
+    }
+
+    @Disabled("Temporarily disabled until the tooling model introduces fully formed project support")
+    @Test
+    void removeUnappliedPluginWithoutVersion() {
+        rewriteRun(
+          spec -> spec.recipe(new RemoveBuildPlugin("com.jfrog.bintray")),
+          buildGradle(
+            """
+              plugins {
+                  id "com.jfrog.bintray" apply false
+              }
+              """,
+            ""
+          ),
+          settingsGradle(
+            """
+              pluginManagement {
+                  plugins {
+                      id "com.jfrog.bintray" version "1.0"
+                  }
+              }
+              
+              rootProject.name = "example"
+              """
+          )
+        );
+    }
+}
---
+++ rewrite-gradle/src/test/java/org/openrewrite/gradle/plugins/RemoveSettingsPluginTest.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2023 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.gradle.plugins;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.gradle.Assertions.settingsGradle;
+
+class RemoveSettingsPluginTest implements RewriteTest {
+    @Test
+    void removePlugin() {
+        rewriteRun(
+          spec -> spec.recipe(new RemoveSettingsPlugin("com.gradle.enterprise")),
+          settingsGradle(
+            """
+              plugins {
+                  id "com.gradle.enterprise" version "3.12.0"
+              }
+              """,
+            ""
+          )
+        );
+    }
+}
