4db0aeb9dcdc150765d89d89a79358f0bea474d8
Tim te Beek <tim@moderne.io>
2023-02-02 19:17:10 +0100

    Print stacktrace on IllegalArgumentException in JGit
    Exceptions where hidden, making it tough to debug in Graal where an IllegalArgumentException wraps a NoSuchMethodException.

--- rewrite-core/src/main/java/org/openrewrite/marker/GitProvenance.java
+++ rewrite-core/src/main/java/org/openrewrite/marker/GitProvenance.java
@@ -184,6 +184,7 @@ public class GitProvenance implements Marker {
         } catch (IOException e) {
             throw new UncheckedIOException(e);
         } catch (IllegalArgumentException | GitAPIException e) {
+            e.printStackTrace();
             return null;
         }
     }
