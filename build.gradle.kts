0b601629bd1718cca46982fa29cdbc06c758d3f6
Sam Snyder <sam@moderne.io>
2023-02-03 14:15:39 -0800

    Standardize build configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,56 +1,8 @@
-import nebula.plugin.contacts.Contact
-import nebula.plugin.contacts.ContactsExtension
-
 plugins {
-    `java-library`
-
-    id("nebula.release") version "16.0.0"
-
-    id("nebula.maven-manifest") version "18.4.0"
-    id("nebula.maven-nebula-publish") version "18.4.0"
-    id("nebula.maven-resolved-dependencies") version "18.4.0"
-
-    id("nebula.contacts") version "6.0.0"
-    id("nebula.info") version "11.3.3"
-
-    id("nebula.javadoc-jar") version "18.4.0"
-    id("nebula.source-jar") version "18.4.0"
+    id("org.openrewrite.build.recipe-library") version "latest.release"
 }
-
-apply(plugin = "nebula.publish-verification")
-
-configure<nebula.plugin.release.git.base.ReleasePluginExtension> {
-    defaultVersionStrategy = nebula.plugin.release.NetflixOssStrategies.SNAPSHOT(project)
-}
-
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
-    }
-}
-
-// Set as appropriate for your organization
 group = "org.openrewrite"
 description = "Rewrite Kotlin"
-
-repositories {
-    mavenLocal()
-    maven {
-        url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
-    }
-    maven {
-        url = uri("https://repo.gradle.org/gradle/libs-releases-local/")
-    }
-    mavenCentral()
-    gradlePluginPortal()
-}
-
-configurations.all {
-    resolutionStrategy {
-        cacheChangingModulesFor(0, TimeUnit.SECONDS)
-        cacheDynamicVersionsFor(0, TimeUnit.SECONDS)
-    }
-}
 
 // Temporary pin to latest.release to enable a release of kotlin without a release of rewrite.
 val latest = "latest.release"
@@ -84,41 +36,4 @@ dependencies {
     testImplementation("org.openrewrite:rewrite-test")
 
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:latest.release")
-}
-
-tasks.named<Test>("test") {
-    useJUnitPlatform()
-    jvmArgs = listOf("-XX:+UnlockDiagnosticVMOptions", "-XX:+ShowHiddenFrames")
-}
-
-tasks.withType<JavaCompile>().configureEach {
-    options.encoding = "UTF-8"
-    options.compilerArgs.add("-parameters")
-}
-
-tasks.named<JavaCompile>("compileJava") {
-    options.release.set(8)
-}
-
-configure<ContactsExtension> {
-    val j = Contact("team@moderne.io")
-    j.moniker("Team Moderne")
-    people["team@moderne.io"] = j
-}
-
-configure<PublishingExtension> {
-    publications {
-        named("nebula", MavenPublication::class.java) {
-            suppressPomMetadataWarningsFor("runtimeElements")
-        }
-    }
-}
-
-publishing {
-  repositories {
-      maven {
-          name = "moderne"
-          url = uri("https://us-west1-maven.pkg.dev/moderne-dev/moderne-recipe")
-      }
-  }
 }
