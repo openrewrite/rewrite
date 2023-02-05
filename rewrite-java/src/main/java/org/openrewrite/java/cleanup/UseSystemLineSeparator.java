4535e89f917c27a8349c120ef9a2c0e74dcb931a
yeikel <email@yeikel.com>
2023-02-04 21:48:37 -0500

    Replace `System.getProperty("line.separator")` with `System.lineSeparator()`

---
+++ rewrite-java/src/main/java/org/openrewrite/java/cleanup/UseSystemLineSeparator.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2023 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.cleanup;
+
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.Recipe;
+import org.openrewrite.TreeVisitor;
+import org.openrewrite.internal.ListUtils;
+import org.openrewrite.internal.lang.Nullable;
+import org.openrewrite.java.JavaIsoVisitor;
+import org.openrewrite.java.JavaTemplate;
+import org.openrewrite.java.MethodMatcher;
+import org.openrewrite.java.search.UsesMethod;
+import org.openrewrite.java.tree.J;
+
+import java.time.Duration;
+import java.util.ArrayList;
+
+public class UseSystemLineSeparator extends Recipe {
+
+    private static final MethodMatcher GET_PROPERTY = new MethodMatcher("java.lang.System getProperty(..)");
+
+    @Override
+    public String getDisplayName() {
+        return "Use `System.lineSeparator()`";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Replaces calls to `System.getProperty(\"line.separator\")` with `System.lineSeparator()`.";
+    }
+    @Override
+    public Duration getEstimatedEffortPerOccurrence() {
+        return Duration.ofMinutes(1);
+    }
+
+    @Override
+    protected @Nullable TreeVisitor<?, ExecutionContext> getSingleSourceApplicableTest() {
+        return new UsesMethod<>(GET_PROPERTY);
+    }
+
+    @Override
+    public TreeVisitor<?, ExecutionContext> getVisitor() {
+        return new JavaIsoVisitor<ExecutionContext>() {
+            @Override
+            public J.MethodInvocation visitMethodInvocation(J.MethodInvocation method, ExecutionContext context) {
+                J.MethodInvocation invocation = super.visitMethodInvocation(method, context);
+
+                if ("System.getProperty(\"line.separator\")".equalsIgnoreCase(invocation.toString())){
+
+                    return invocation
+                            .withName(invocation.getName().withSimpleName("lineSeparator"))
+                            .withArguments(new ArrayList<>());
+                }
+
+                return invocation;
+            }
+        };
+    }
+}
