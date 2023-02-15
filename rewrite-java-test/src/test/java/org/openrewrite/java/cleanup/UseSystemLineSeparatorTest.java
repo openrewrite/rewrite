3161fde63a9fcd6391d7ea28491aa927a468de61
Yeikel <email@yeikel.com>
2023-02-14 22:05:54 -0500

    Replace `System.getProperty("line.separator")` with `System.lineSeparator()` (#2778)
    * Replace `System.getProperty("line.separator")` with `System.lineSeparator()`
    
    * Use template
    
    * Update UseSystemLineSeparatorTest.java
    
    * update
    
    * update

---
+++ rewrite-java-test/src/test/java/org/openrewrite/java/cleanup/UseSystemLineSeparatorTest.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2023 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.cleanup;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.Issue;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.java.Assertions.java;
+
+class UseSystemLineSeparatorTest implements RewriteTest {
+
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.recipe(new UseSystemLineSeparator());
+    }
+
+    @Issue("https://github.com/openrewrite/rewrite/issues/2363")
+    @Test
+    void useSystemLineSeparator() {
+        rewriteRun(
+          java(
+            """
+              class A {
+                  String s = System.getProperty("line.separator");
+                  var a = System.getProperty("line.separator");
+              }
+              """,
+            """
+              class A {
+                  String s = System.lineSeparator();
+                  var a = System.lineSeparator();
+              }
+              """
+          )
+        );
+    }
+
+    @Issue("https://github.com/openrewrite/rewrite/issues/2363")
+    @Test
+    void useSystemLineSeparatorStaticImport() {
+        rewriteRun(
+          java(
+            """
+              import static java.lang.System.getProperty;
+              
+              class A {
+                  String s = getProperty("line.separator");
+                  var a = getProperty("line.separator");
+              }
+              """,
+            """
+              import static java.lang.System.lineSeparator;
+              
+              class A {
+                  String s = lineSeparator();
+                  var a = lineSeparator();
+              }
+              """
+          )
+        );
+    }
+}
