b2faf5ec971427e56061d869c9fe20ccad44d1e8
Tim te Beek <tim@moderne.io>
2023-02-18 12:23:00 +0100

    Change order of applied recipes
    As seen in https://github.com/openrewrite/rewrite-migrate-java/blob/e9aaf04dd5a90509baee77e9cc215a2b084a09b4/src/main/resources/META-INF/rewrite/no-guava.yml#L104

--- rewrite-java-test/src/test/java/org/openrewrite/java/ReplaceApacheCommonsLang3ValidateNotNullWithObjectsRequireNonNullTest.java
+++ rewrite-java-test/src/test/java/org/openrewrite/java/ReplaceApacheCommonsLang3ValidateNotNullWithObjectsRequireNonNullTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2023 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.openrewrite.java;
 
 import org.junit.jupiter.api.Test;
@@ -15,17 +30,18 @@ class ReplaceApacheCommonsLang3ValidateNotNullWithObjectsRequireNonNullTest impl
             .logCompilationWarningsAndErrors(true)
             .classpath("commons-lang3"))
           .recipe(Environment.builder()
-          .scanRuntimeClasspath("org.openrewrite.java")
-          .build()
-          .activateRecipes("org.openrewrite.java.cleanup.ReplaceApacheCommonsLang3ValidateNotNullWithObjectsRequireNonNull"));
+            .scanRuntimeClasspath("org.openrewrite.java")
+            .build()
+            .activateRecipes("org.openrewrite.java.cleanup.ReplaceApacheCommonsLang3ValidateNotNullWithObjectsRequireNonNull"));
     }
 
     @Test
-    void replaceMethodsWithOneArg(){
+    void replaceWithOneArgument() {
         rewriteRun(
           java(
             """
               import org.apache.commons.lang3.Validate;
+              
               class Test {
                   void test(Object obj) {
                         Validate.notNull(obj);
@@ -34,7 +50,7 @@ class ReplaceApacheCommonsLang3ValidateNotNullWithObjectsRequireNonNullTest impl
               """,
             """
               import java.util.Objects;
-
+              
               class Test {
                   void test(Object obj) {
                         Objects.requireNonNull(obj);
@@ -62,17 +78,18 @@ class ReplaceApacheCommonsLang3ValidateNotNullWithObjectsRequireNonNullTest impl
     }
 
     @Test
-    void doNothingOnMethodsWithThreeArg(){
+    void doNothingWithThreeArguments() {
         rewriteRun(
           java(
             """
-                     import org.apache.commons.lang3.Validate;
-                     class Test {
-                         void test(Object obj) {
-                               Validate.notNull(obj,"Object in %s should not be null","request xyz");
-                         }
-                     }
-                  """
+              import org.apache.commons.lang3.Validate;
+              
+              class Test {
+                  void test(Object obj) {
+                        Validate.notNull(obj, "Object in %s should not be null", "request xyz");
+                  }
+              }
+              """
           )
         );
     }
