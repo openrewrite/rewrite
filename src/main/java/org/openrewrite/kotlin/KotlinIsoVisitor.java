a3252f7db65cf8a84ca180bcd1c323bdc111ff76
traceyyoshima <tracey.yoshima@gmail.com>
2023-02-22 10:19:27 -0800

    Added visits to ISO visitor. fixes #19

--- src/main/java/org/openrewrite/kotlin/KotlinIsoVisitor.java
+++ src/main/java/org/openrewrite/kotlin/KotlinIsoVisitor.java
@@ -15,5 +15,52 @@
  */
 package org.openrewrite.kotlin;
 
+import org.openrewrite.kotlin.tree.K;
+
 public class KotlinIsoVisitor<P> extends KotlinVisitor<P> {
+
+    @Override
+    public K.CompilationUnit visitCompilationUnit(K.CompilationUnit cu, P p) {
+        return (K.CompilationUnit) super.visitCompilationUnit(cu, p);
+    }
+
+    @Override
+    public K.Binary visitBinary(K.Binary binary, P p) {
+        return (K.Binary) super.visitBinary(binary, p);
+    }
+
+    @Override
+    public K.FunctionType visitFunctionType(K.FunctionType functionType, P p) {
+        return (K.FunctionType) super.visitFunctionType(functionType, p);
+    }
+
+    @Override
+    public K.KReturn visitKReturn(K.KReturn kReturn, P p) {
+        return (K.KReturn) super.visitKReturn(kReturn, p);
+    }
+
+    @Override
+    public K.KString visitKString(K.KString kString, P p) {
+        return (K.KString) super.visitKString(kString, p);
+    }
+
+    @Override
+    public K.KString.Value visitKStringValue(K.KString.Value value, P p) {
+        return (K.KString.Value) super.visitKStringValue(value, p);
+    }
+
+    @Override
+    public K.ListLiteral visitListLiteral(K.ListLiteral listLiteral, P p) {
+        return (K.ListLiteral) super.visitListLiteral(listLiteral, p);
+    }
+
+    @Override
+    public K.When visitWhen(K.When when, P p) {
+        return (K.When) super.visitWhen(when, p);
+    }
+
+    @Override
+    public K.WhenBranch visitWhenBranch(K.WhenBranch whenBranch, P p) {
+        return (K.WhenBranch) super.visitWhenBranch(whenBranch, p);
+    }
 }
