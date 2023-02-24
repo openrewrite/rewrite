598ae9f8d540bc401b16bdb8e233d82cc6ac02fa
Shannon Pamperl <shanman190@gmail.com>
2023-02-23 20:21:43 -0600

    Remove the gradle-api from being shaded into rewrite-gradle.
    This enables better alignment in terms of type attribution by utilizing the Gradle, Groovy, and (future) Kotlin versions as provided by the executing Gradle wrapper.

--- rewrite-gradle/build.gradle.kts
+++ rewrite-gradle/build.gradle.kts
@@ -1,8 +1,5 @@
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-
 plugins {
     id("org.openrewrite.build.language-library")
-    id("org.openrewrite.build.shadow")
     id("groovy")
 }
 
@@ -17,25 +14,17 @@ repositories {
 
 dependencies {
     api(project(":rewrite-core"))
-    api(project(":rewrite-groovy"))
+    api(project(":rewrite-groovy")) {
+        exclude("org.codehaus.groovy", "groovy")
+    }
     api(project(":rewrite-maven"))
     api("org.jetbrains:annotations:latest.release")
     compileOnly(project(":rewrite-test"))
     implementation(project(":rewrite-properties"))
-    implementation("org.codehaus.groovy:groovy:latest.release")
     implementation("org.openrewrite.gradle.tooling:model:latest.release")
 
-    compileOnly("org.gradle:gradle-base-services:latest.release")
-    compileOnly("org.gradle:gradle-core-api:latest.release")
-    compileOnly("org.gradle:gradle-language-groovy:latest.release")
-    compileOnly("org.gradle:gradle-language-java:latest.release")
-    compileOnly("org.gradle:gradle-logging:latest.release")
-    compileOnly("org.gradle:gradle-messaging:latest.release")
-    compileOnly("org.gradle:gradle-native:latest.release")
-    compileOnly("org.gradle:gradle-process-services:latest.release")
-    compileOnly("org.gradle:gradle-resources:latest.release")
-    compileOnly("org.gradle:gradle-testing-base:latest.release")
-    compileOnly("org.gradle:gradle-testing-jvm:latest.release")
+    compileOnly("org.codehaus.groovy:groovy:latest.release")
+    compileOnly(gradleApi())
 
     compileOnly("com.gradle:gradle-enterprise-gradle-plugin:latest.release")
 
@@ -47,33 +36,14 @@ dependencies {
         // because gradle-api fatjars this implementation already
         exclude("ch.qos.logback", "logback-classic")
     }
-    testImplementation(project(":rewrite-maven"))
     testImplementation("com.squareup.okhttp3:mockwebserver:4.+")
 
-    testRuntimeOnly("org.gradle:gradle-base-services:latest.release")
-    testRuntimeOnly("org.gradle:gradle-core-api:latest.release")
-    testRuntimeOnly("org.gradle:gradle-language-groovy:latest.release")
-    testRuntimeOnly("org.gradle:gradle-language-java:latest.release")
-    testRuntimeOnly("org.gradle:gradle-logging:latest.release")
-    testRuntimeOnly("org.gradle:gradle-messaging:latest.release")
-    testRuntimeOnly("org.gradle:gradle-native:latest.release")
-    testRuntimeOnly("org.gradle:gradle-process-services:latest.release")
-    testRuntimeOnly("org.gradle:gradle-resources:latest.release")
-    testRuntimeOnly("org.gradle:gradle-testing-base:latest.release")
-    testRuntimeOnly("org.gradle:gradle-testing-jvm:latest.release")
+    testRuntimeOnly("org.codehaus.groovy:groovy:latest.release")
+    testRuntimeOnly(gradleApi())
     testRuntimeOnly("com.gradle:gradle-enterprise-gradle-plugin:latest.release")
     testRuntimeOnly("com.google.guava:guava:latest.release")
     testRuntimeOnly(project(":rewrite-java-17"))
     testRuntimeOnly("org.projectlombok:lombok:latest.release")
-}
-
-tasks.withType<ShadowJar> {
-    exclude("org/slf4j/impl/**.class")
-    exclude("org/gradle/internal/logging/slf4j/**.class")
-    dependencies {
-        include(dependency("org.gradle:"))
-        include(dependency("com.gradle:"))
-    }
 }
 
 // This seems to be the only way to get the groovy compiler to emit java-8 compatible bytecode
