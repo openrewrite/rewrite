d40cfc34c88e63fbc54201be552c054ca15229bd
Shannon Pamperl <shanman190@gmail.com>
2023-02-27 16:30:56 -0600

    Remove the gradle-api from being shaded into rewrite-gradle. (#2890)
    * Remove the gradle-api from being shaded into rewrite-gradle.
    
    This enables better alignment in terms of type attribution by utilizing the Gradle, Groovy, and (future) Kotlin versions as provided by the executing Gradle wrapper.
    
    * Switch to initial expression for default value as  is only available on Groovy 3+
    
    * Minimize public API

--- rewrite-gradle/src/main/java/org/openrewrite/gradle/GradleParser.java
+++ rewrite-gradle/src/main/java/org/openrewrite/gradle/GradleParser.java
@@ -15,17 +15,16 @@
  */
 package org.openrewrite.gradle;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.experimental.Accessors;
 import org.openrewrite.ExecutionContext;
 import org.openrewrite.Parser;
 import org.openrewrite.gradle.internal.DefaultImportsCustomizer;
 import org.openrewrite.groovy.GroovyParser;
 import org.openrewrite.groovy.tree.G;
 import org.openrewrite.internal.lang.Nullable;
+import org.openrewrite.java.JavaParser;
 
 import java.nio.file.Path;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -35,14 +34,9 @@ public class GradleParser implements Parser<G.CompilationUnit> {
     private final GroovyParser buildParser;
     private final GroovyParser settingsParser;
 
+    @Deprecated//(since = "7.37.0", forRemoval = true)
     public GradleParser(GroovyParser.Builder groovyParser) {
         GroovyParser.Builder base = groovyParser;
-        try {
-            base = groovyParser.classpath("gradle-core-api", "gradle-language-groovy", "gradle-language-java", "gradle-resources",
-                    "gradle-testing-base", "gradle-testing-jvm", "gradle-enterprise-gradle-plugin");
-        } catch (IllegalArgumentException e) {
-            // when gradle API has been fatjared into the rewrite-gradle distribution
-        }
         this.buildParser = GroovyParser.builder(base)
                 .compilerCustomizers(
                         new DefaultImportsCustomizer(),
@@ -50,6 +44,24 @@ public class GradleParser implements Parser<G.CompilationUnit> {
                 )
                 .build();
         this.settingsParser = GroovyParser.builder(base)
+                .compilerCustomizers(
+                        new DefaultImportsCustomizer(),
+                        config -> config.setScriptBaseClass("RewriteSettings")
+                )
+                .build();
+    }
+
+    private GradleParser(Builder builder) {
+        GroovyParser.Builder base = builder.groovyParser;
+        this.buildParser = GroovyParser.builder(base)
+                .classpath(builder.buildscriptClasspath)
+                .compilerCustomizers(
+                        new DefaultImportsCustomizer(),
+                        config -> config.setScriptBaseClass("RewriteGradleProject")
+                )
+                .build();
+        this.settingsParser = GroovyParser.builder(base)
+                .classpath(builder.settingsClasspath)
                 .compilerCustomizers(
                         new DefaultImportsCustomizer(),
                         config -> config.setScriptBaseClass("RewriteSettings")
@@ -83,18 +95,59 @@ public class GradleParser implements Parser<G.CompilationUnit> {
         return new Builder();
     }
 
-    @Accessors(chain=true)
-    @Setter
-    @Getter
     public static class Builder extends Parser.Builder {
         protected GroovyParser.Builder groovyParser = GroovyParser.builder();
+
+        @Nullable
+        private Collection<Path> buildscriptClasspath = JavaParser.runtimeClasspath();
+
+        @Nullable
+        private Collection<Path> settingsClasspath = JavaParser.runtimeClasspath();
 
         public Builder() {
             super(G.CompilationUnit.class);
         }
 
+        public Builder groovyParser(GroovyParser.Builder groovyParser) {
+            this.groovyParser = groovyParser;
+            return this;
+        }
+
+        /**
+         * @deprecated Use {@code groovyParser(GroovyParser.Builder)} instead.
+         */
+        @Deprecated//(since = "7.37.0", forRemoval = true)
+        public Builder setGroovyParser(GroovyParser.Builder groovyParser) {
+            return groovyParser(groovyParser);
+        }
+
+        @Deprecated//(since = "7.37.0", forRemoval = true)
+        public GroovyParser.Builder getGroovyParser() {
+            return groovyParser;
+        }
+
+        public Builder buildscriptClasspath(Collection<Path> classpath) {
+            this.buildscriptClasspath = classpath;
+            return this;
+        }
+
+        public Builder buildscriptClasspath(String... classpath) {
+            this.buildscriptClasspath = JavaParser.dependenciesFromClasspath(classpath);
+            return this;
+        }
+
+        public Builder settingsClasspath(Collection<Path> classpath) {
+            this.settingsClasspath = classpath;
+            return this;
+        }
+
+        public Builder settingsClasspath(String... classpath) {
+            this.settingsClasspath = JavaParser.dependenciesFromClasspath(classpath);
+            return this;
+        }
+
         public GradleParser build() {
-            return new GradleParser(groovyParser);
+            return new GradleParser(this);
         }
 
         @Override
