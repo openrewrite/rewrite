1101d0872003dbe31286a2054676922366f436cc
Kun Li <kun@moderne.io>
2023-03-14 18:18:03 -0700

    support transitive

---
+++ rewrite-java-test/src/test/java/org/openrewrite/java/search/FindImplementsOrExtendsTest.java
@@ -0,0 +1,239 @@
+/*
+ * Copyright 2023 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.search;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.java.Assertions.java;
+
+class FindImplementsOrExtendsTest implements RewriteTest {
+
+    @Test
+    void found() {
+        rewriteRun(
+          spec -> spec.recipe(new FindImplementsOrExtends("java.lang.Runnable")),
+          java(
+            """
+              class Test implements Runnable {
+                  @Override
+                  public void run() {
+                  }
+              }
+              """,
+            """
+              /*~~>*/class Test implements Runnable {
+                  @Override
+                  public void run() {
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void notFound() {
+        rewriteRun(
+          spec -> spec.recipe(new FindImplementsOrExtends("java.lang.Runnable")),
+          java(
+            """
+              class Test  {
+                  public void run() {
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void genericType() {
+        rewriteRun(
+          spec -> spec.recipe(new FindImplementsOrExtends("java.lang.Comparable<java.lang.String>")),
+          java(
+            """
+              class Test implements Comparable<String> {
+                  @Override
+                  public int compareTo(String o) {
+                      return 0;
+                  }
+              }
+              """,
+            """
+              /*~~>*/class Test implements Comparable<String> {
+                  @Override
+                  public int compareTo(String o) {
+                      return 0;
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void genericType2() {
+        rewriteRun(
+          spec -> spec.recipe(new FindImplementsOrExtends("java.lang.Comparable")),
+          java(
+            """
+              class Test implements Comparable<String> {
+                  @Override
+                  public int compareTo(String o) {
+                      return 0;
+                  }
+              }
+              """,
+            """
+              /*~~>*/class Test implements Comparable<String> {
+                  @Override
+                  public int compareTo(String o) {
+                      return 0;
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void unmatchedGenericType() {
+        rewriteRun(
+          spec -> spec.recipe(new FindImplementsOrExtends("java.lang.Comparable<java.lang.Runnable>")),
+          java(
+            """
+              class Test implements Comparable<String> {
+                  @Override
+                  public int compareTo(String o) {
+                      return 0;
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void transitiveImplementsExtends() {
+        rewriteRun(
+          spec -> spec.recipe(new FindImplementsOrExtends("org.x.A")),
+          java(
+            """
+              package org.x;
+              interface A {
+                  void foo();
+              }
+              """
+          ),
+          java(
+            """
+              package org.x;
+              interface B extends A {
+                  void bar();
+              }
+              """
+            ,
+            """
+              package org.x;
+              /*~~>*/interface B extends A {
+                  void bar();
+              }
+              """
+          ),
+          java(
+            """
+              package org.x;
+              public class C implements B {
+                  @Override
+                  public void foo() {
+                  }
+
+                  @Override
+                  public void bar() {
+                  }
+              }
+              """
+            ,
+            """
+              package org.x;
+              /*~~>*/public class C implements B {
+                  @Override
+                  public void foo() {
+                  }
+
+                  @Override
+                  public void bar() {
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void transitiveExtendsImplements() {
+        rewriteRun(
+          spec -> spec.recipe(new FindImplementsOrExtends("org.x.A")),
+          java(
+            """
+              package org.x;
+              interface A {
+                  void foo();
+              }
+              """
+          ),
+          java(
+            """
+              package org.x;
+              class B implements A {
+                  @Override
+                  void foo() {
+                  }
+              }
+              """
+            ,
+            """
+              package org.x;
+              /*~~>*/class B implements A {
+                  @Override
+                  void foo() {
+                  }
+              }
+              """
+          ),
+          java(
+            """
+              package org.x;
+              public class C extends B {
+                  @Override
+                  public void foo() {
+                  }
+              }
+              """
+            ,
+            """
+              package org.x;
+              /*~~>*/public class C extends B {
+                  @Override
+                  public void foo() {
+                  }
+              }
+              """
+          )
+        );
+    }
+}
--- rewrite-java/src/main/java/org/openrewrite/java/search/FindImplementsOrExtends.java
+++ rewrite-java/src/main/java/org/openrewrite/java/search/FindImplementsOrExtends.java
@@ -18,17 +18,17 @@ package org.openrewrite.java.search;
 import org.openrewrite.ExecutionContext;
 import org.openrewrite.Recipe;
 import org.openrewrite.TreeVisitor;
+import org.openrewrite.internal.lang.Nullable;
 import org.openrewrite.java.JavaIsoVisitor;
 import org.openrewrite.java.tree.J;
+import org.openrewrite.java.tree.JavaType;
 import org.openrewrite.java.tree.TypeTree;
 import org.openrewrite.marker.SearchResult;
 
-import java.util.Objects;
-
-public class FindImplements extends Recipe {
+public class FindImplementsOrExtends extends Recipe {
     private final String interfaceFullyQualifiedName;
 
-    public FindImplements(String interfaceFullyQualifiedName) {
+    public FindImplementsOrExtends(String interfaceFullyQualifiedName) {
         this.interfaceFullyQualifiedName = interfaceFullyQualifiedName;
     }
 
@@ -49,15 +49,55 @@ public class FindImplements extends Recipe {
             public J.ClassDeclaration visitClassDeclaration(J.ClassDeclaration classDecl,
                                                             ExecutionContext ctx) {
                 classDecl = super.visitClassDeclaration(classDecl, ctx);
+
                 if (classDecl.getImplements() != null) {
                     for (TypeTree impl : classDecl.getImplements()) {
-                        if (interfaceFullyQualifiedName.equals(Objects.requireNonNull(impl.getType()).toString())) {
+                        if (findInterface(impl.getType())) {
                             return SearchResult.found(classDecl);
                         }
+                    }
+                }
+
+                TypeTree ext = classDecl.getExtends();
+                if (ext != null) {
+                    JavaType.Class jc = (JavaType.Class) ext.getType();
+                    if (findInterface(jc)) {
+                        return SearchResult.found(classDecl);
                     }
                 }
                 return classDecl;
             }
         };
+    }
+
+    private boolean findInterface(@Nullable JavaType javaType) {
+        if (javaType == null) {
+            return false;
+        }
+
+        if (javaType instanceof JavaType.Class) {
+            JavaType.Class jc = (JavaType.Class) javaType;
+            if (jc.toString().equals(interfaceFullyQualifiedName)) {
+                return true;
+            }
+
+            for (JavaType.FullyQualified it : jc.getInterfaces()) {
+                if (it.toString().equals(interfaceFullyQualifiedName)) {
+                    return true;
+                }
+
+                if (findInterface(it)) {
+                    return true;
+                }
+            }
+            return false;
+        } else if (javaType instanceof JavaType.Parameterized) {
+            JavaType.Parameterized jp = (JavaType.Parameterized) javaType;
+            if (jp.toString().equals(interfaceFullyQualifiedName)) {
+                return true;
+            }
+            return findInterface(jp.getType());
+        }
+        return false;
     }
 }
