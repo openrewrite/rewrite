8b0c05fbfb393d71e2f609e70837a7b78c641bab
Knut Wannheden <knut@moderne.io>
2023-03-24 10:17:48 +0100

    Return defensive copy in `Properties.File#getContent()`
    The LST elements need to be immutable and thus `Properties.File#getContent()` needs to return a copy of the `List`.
    
    Fixes: #3026

--- rewrite-properties/src/main/java/org/openrewrite/properties/tree/Properties.java
+++ rewrite-properties/src/main/java/org/openrewrite/properties/tree/Properties.java
@@ -25,6 +25,7 @@ import org.openrewrite.properties.internal.PropertiesPrinter;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -52,27 +53,34 @@ public interface Properties extends Tree {
 
     @lombok.Value
     @EqualsAndHashCode(callSuper = false, onlyExplicitlyIncluded = true)
-    @With
     class File implements Properties, SourceFile {
+        @With
         @EqualsAndHashCode.Include
         UUID id;
 
+        @With
         String prefix;
+        @With
         Markers markers;
+        @With
         Path sourcePath;
 
         List<Content> content;
+        @With
         String eof;
 
         @Nullable // for backwards compatibility
         @With(AccessLevel.PRIVATE)
         String charsetName;
 
+        @With
         boolean charsetBomMarked;
 
+        @With
         @Nullable
         FileAttributes fileAttributes;
 
+        @With
         @Nullable
         Checksum checksum;
 
@@ -81,9 +89,29 @@ public interface Properties extends Tree {
             return charsetName == null ? StandardCharsets.UTF_8 : Charset.forName(charsetName);
         }
 
+        @SuppressWarnings("unchecked")
         @Override
         public SourceFile withCharset(Charset charset) {
             return withCharsetName(charset.name());
+        }
+
+        public List<Content> getContent() {
+            return new ArrayList<>(content);
+        }
+
+        public File withContent(List<Content> content) {
+            int size = content.size();
+            if (size == this.content.size()) {
+                for (int i = 0; i < size; i++) {
+                    if (content.get(i) != this.content.get(i)) {
+                        break;
+                    } else if (i == size - 1) {
+                        return this;
+                    }
+                }
+            }
+
+            return new File(id, prefix, markers, sourcePath, new ArrayList<>(content), eof, charsetName, charsetBomMarked, fileAttributes, checksum);
         }
 
         @Override
---
+++ rewrite-properties/src/test/java/org/openrewrite/properties/Issue3026Test.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2023 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.properties;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.Issue;
+import org.openrewrite.marker.Markers;
+import org.openrewrite.properties.tree.Properties;
+import org.openrewrite.test.RewriteTest;
+
+import java.util.List;
+
+import static org.openrewrite.Tree.randomId;
+import static org.openrewrite.properties.Assertions.properties;
+import static org.openrewrite.test.RewriteTest.toRecipe;
+
+@SuppressWarnings("UnusedProperty")
+class Issue3026Test implements RewriteTest {
+
+    @Test
+    @Issue("https://github.com/openrewrite/rewrite/issues/3026")
+    void modifyContents() {
+        rewriteRun(
+          spec -> spec.recipe(toRecipe(() -> new PropertiesVisitor<>() {
+              @Override
+              public Properties visitFile(Properties.File file, ExecutionContext ctx) {
+                  List<Properties.Content> content = file.getContent();
+                  if (content.size() != 1) {
+                      return file;
+                  }
+                  content.add(0, new Properties.Comment(randomId(), "", Markers.EMPTY, Properties.Comment.Delimiter.HASH_TAG, "foo\n"));
+                  return file.withContent(content);
+              }
+          })),
+          properties(
+            """
+              foo=bar
+              """,
+            """
+              #foo
+              foo=bar
+              """
+          )
+        );
+    }
+}
