e6e3587e8d7d6cbc4831202a6c82347e8be892d5
George Gastaldi <gegastaldi@gmail.com>
2023-03-28 14:14:47 -0300

    Introduce `org.openrewrite.maven.ChangePluginGroupIdAndArtifactId` (#3048)
    - Fixes #3047

---
+++ rewrite-maven/src/main/java/org/openrewrite/maven/ChangePluginGroupIdAndArtifactId.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2023 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.maven;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+import org.openrewrite.*;
+import org.openrewrite.internal.lang.Nullable;
+import org.openrewrite.maven.table.MavenMetadataFailures;
+import org.openrewrite.xml.ChangeTagValueVisitor;
+import org.openrewrite.xml.tree.Xml;
+
+import java.util.*;
+
+@Value
+@EqualsAndHashCode(callSuper = true)
+public class ChangePluginGroupIdAndArtifactId extends Recipe {
+    @EqualsAndHashCode.Exclude
+    MavenMetadataFailures metadataFailures = new MavenMetadataFailures(this);
+
+    @Option(displayName = "Old groupId",
+            description = "The old groupId to replace. The groupId is the first part of a plugin coordinate 'com.google.guava:guava:VERSION'. Supports glob expressions.",
+            example = "org.openrewrite.recipe")
+    String oldGroupId;
+
+    @Option(displayName = "Old artifactId",
+            description = "The old artifactId to replace. The artifactId is the second part of a plugin coordinate 'com.google.guava:guava:VERSION'. Supports glob expressions.",
+            example = "my-deprecated-maven-plugin")
+    String oldArtifactId;
+
+    @Option(displayName = "New groupId",
+            description = "The new groupId to use. Defaults to the existing group id.",
+            example = "corp.internal.openrewrite.recipe",
+            required = false)
+    @Nullable
+    String newGroupId;
+
+    @Option(displayName = "New artifactId",
+            description = "The new artifactId to use. Defaults to the existing artifact id.",
+            example = "my-new-maven-plugin",
+            required = false)
+    @Nullable
+    String newArtifactId;
+
+    @JsonCreator
+    public ChangePluginGroupIdAndArtifactId(String oldGroupId, String oldArtifactId, @Nullable String newGroupId, @Nullable String newArtifactId) {
+        this.oldGroupId = oldGroupId;
+        this.oldArtifactId = oldArtifactId;
+        this.newGroupId = newGroupId;
+        this.newArtifactId = newArtifactId;
+    }
+
+    @Override
+    public String getDisplayName() {
+        return "Change Maven plugin groupId and artifactId";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Change the groupId and/or the artifactId of a specified Maven plugin.";
+    }
+
+    @Override
+    public TreeVisitor<?, ExecutionContext> getVisitor() {
+
+        return new MavenVisitor<ExecutionContext>() {
+            @Override
+            public Xml visitTag(Xml.Tag tag, ExecutionContext ctx) {
+                Xml.Tag t = (Xml.Tag) super.visitTag(tag, ctx);
+                if (isPluginTag(oldGroupId, oldArtifactId)) {
+                    if (newGroupId != null) {
+                        t = changeChildTagValue(t, "groupId", newGroupId, ctx);
+                    }
+                    if (newArtifactId != null) {
+                        t = changeChildTagValue(t, "artifactId", newArtifactId, ctx);
+                    }
+                    if (t != tag) {
+                        maybeUpdateModel();
+                    }
+                }
+                //noinspection ConstantConditions
+                return t;
+            }
+
+            private Xml.Tag changeChildTagValue(Xml.Tag tag, String childTagName, String newValue, ExecutionContext ctx) {
+                Optional<Xml.Tag> childTag = tag.getChild(childTagName);
+                if (childTag.isPresent() && !newValue.equals(childTag.get().getValue().orElse(null))) {
+                    tag = (Xml.Tag) new ChangeTagValueVisitor<>(childTag.get(), newValue).visitNonNull(tag, ctx);
+                }
+                return tag;
+            }
+        };
+    }
+}
