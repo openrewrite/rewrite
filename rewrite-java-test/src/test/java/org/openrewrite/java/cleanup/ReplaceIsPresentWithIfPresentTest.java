7ba1474d692b63edf3e2a29a9eb89eaa2c3f440c
Tim te Beek <tim@moderne.io>
2023-04-03 13:03:01 +0200

    Apply formatter to tests

--- rewrite-java-test/src/test/java/org/openrewrite/java/cleanup/ReplaceIsPresentWithIfPresentTest.java
+++ rewrite-java-test/src/test/java/org/openrewrite/java/cleanup/ReplaceIsPresentWithIfPresentTest.java
@@ -26,186 +26,186 @@ public class ReplaceIsPresentWithIfPresentTest implements RewriteTest {
     @Override
     public void defaults(RecipeSpec spec) {
         spec
-            .parser(JavaParser.fromJavaVersion()
-                .logCompilationWarningsAndErrors(true))
-            .recipe(new ReplaceIsPresentWithIfPresent());
+          .parser(JavaParser.fromJavaVersion()
+            .logCompilationWarningsAndErrors(true))
+          .recipe(new ReplaceIsPresentWithIfPresent());
     }
 
     @Test
     void doNothingIfIsPresentNotFound() {
         rewriteRun(
-            java(
-                """
-                    package com.foobar;
-                    import java.util.Optional;
-                    import java.util.ArrayList;
-                    import java.util.List;
-                    public class A {
-                        void method() {
-                            List<Integer> list = new ArrayList<>();
-                            Optional<Integer> o = Optional.of(2);
-                        }
-                    }
-                    """
-            )
+          java(
+            """
+              package com.foobar;
+              import java.util.Optional;
+              import java.util.ArrayList;
+              import java.util.List;
+              public class A {
+                  void method() {
+                      List<Integer> list = new ArrayList<>();
+                      Optional<Integer> o = Optional.of(2);
+                  }
+              }
+              """
+          )
         );
     }
 
     @Test
     void DoNothingIfPresentPartOfElseIf() {
         rewriteRun(
-            java(
-                """
-                    package com.foobar;
-                    import java.util.Optional;
-                    import java.util.ArrayList;
-                    import java.util.List;
-                    public class A {
-                        void method() {
-                            boolean x=false;
-                            List<Integer> list = new ArrayList<>();
-                            Optional<Integer> o = Optional.of(2);
-                            if(x){
-                                System.out.println("hello");
-                            }else if(o.isPresent()){
-                                list.add(o.get());
-                            }
-                        }
-                    }
-                    """
-            )
+          java(
+            """
+              package com.foobar;
+              import java.util.Optional;
+              import java.util.ArrayList;
+              import java.util.List;
+              public class A {
+                  void method() {
+                      boolean x=false;
+                      List<Integer> list = new ArrayList<>();
+                      Optional<Integer> o = Optional.of(2);
+                      if(x){
+                          System.out.println("hello");
+                      }else if(o.isPresent()){
+                          list.add(o.get());
+                      }
+                  }
+              }
+              """
+          )
         );
     }
 
     @Test
     void DoNothingIfElsePartPresent() {
         rewriteRun(
-            java(
-                """
-                    package com.foobar;
-                    import java.util.Optional;
-                    import java.util.ArrayList;
-                    import java.util.List;
-                    public class A {
-                        void method() {
-                            List<Integer> list = new ArrayList<>();
-                            Optional<Integer> o = Optional.of(2);
-                            if(o.isPresent()){
-                                list.add(o.get());
-                            }else{
-                                System.out.println("else part");
-                            }
-                        }
-                    }
-                    """
-            )
+          java(
+            """
+              package com.foobar;
+              import java.util.Optional;
+              import java.util.ArrayList;
+              import java.util.List;
+              public class A {
+                  void method() {
+                      List<Integer> list = new ArrayList<>();
+                      Optional<Integer> o = Optional.of(2);
+                      if(o.isPresent()){
+                          list.add(o.get());
+                      }else{
+                          System.out.println("else part");
+                      }
+                  }
+              }
+              """
+          )
         );
     }
 
     @Test
     void DoNothingIfNotLambdaCompatible() {
         rewriteRun(
-            java(
-                """
-                    package com.foobar;
-                    import java.util.Optional;
-                    import java.util.ArrayList;
-                    import java.util.List;
-                    public class A {
-                        void method() {
-                            int x;
-                            List<Integer> list = new ArrayList<>();
-                            Optional<Integer> o = Optional.of(2);
-                            if(o.isPresent()){
-                                x=o.get();
-                                list.add(o.get());
-                            }
-                        }
-                    }
-                    """
-            )
+          java(
+            """
+              package com.foobar;
+              import java.util.Optional;
+              import java.util.ArrayList;
+              import java.util.List;
+              public class A {
+                  void method() {
+                      int x;
+                      List<Integer> list = new ArrayList<>();
+                      Optional<Integer> o = Optional.of(2);
+                      if(o.isPresent()){
+                          x=o.get();
+                          list.add(o.get());
+                      }
+                  }
+              }
+              """
+          )
         );
     }
 
     @Test
     void replace() {
         rewriteRun(
-            java(
-                """
-                    package com.foobar;
-                    import java.util.Optional;
-                    import java.util.ArrayList;
-                    import java.util.List;
-                    public class A {
-                        void method() {
-                            List<Integer> list = new ArrayList<>();
-                            Optional<Integer> o = Optional.of(2);
-                            if(o.isPresent()){
-                                list.add(o.get());
-                            }
-                        }
-                    }
-                    """,
-                """
-                    package com.foobar;
-                    import java.util.Optional;
-                    import java.util.ArrayList;
-                    import java.util.List;
-                    public class A {
-                        void method() {
-                            List<Integer> list = new ArrayList<>();
-                            Optional<Integer> o = Optional.of(2);
-                            o.ifPresent((obj) -> {
-                                list.add(obj);
-                            });
-                        }
-                    }
-                    """
-            )
+          java(
+            """
+              package com.foobar;
+              import java.util.Optional;
+              import java.util.ArrayList;
+              import java.util.List;
+              public class A {
+                  void method() {
+                      List<Integer> list = new ArrayList<>();
+                      Optional<Integer> o = Optional.of(2);
+                      if(o.isPresent()){
+                          list.add(o.get());
+                      }
+                  }
+              }
+              """,
+            """
+              package com.foobar;
+              import java.util.Optional;
+              import java.util.ArrayList;
+              import java.util.List;
+              public class A {
+                  void method() {
+                      List<Integer> list = new ArrayList<>();
+                      Optional<Integer> o = Optional.of(2);
+                      o.ifPresent((obj) -> {
+                          list.add(obj);
+                      });
+                  }
+              }
+              """
+          )
         );
     }
 
     @Test
     void replaceNestedIf() {
         rewriteRun(
-            java(
-                """
-                    package com.foobar;
-                    import java.util.Optional;
-                    import java.util.ArrayList;
-                    import java.util.List;
-                    public class A {
-                        void method() {
-                            boolean x = true;
-                            List<Integer> list = new ArrayList<>();
-                            Optional<Integer> o = Optional.of(2);
-                            if(x){
-                                if(o.isPresent()){
-                                    list.add(o.get());
-                                }
-                            }
-                        }
-                    }
-                    """,
-                """
-                    package com.foobar;
-                    import java.util.Optional;
-                    import java.util.ArrayList;
-                    import java.util.List;
-                    public class A {
-                        void method() {
-                            boolean x = true;
-                            List<Integer> list = new ArrayList<>();
-                            Optional<Integer> o = Optional.of(2);
-                            if(x){
-                                o.ifPresent((obj) -> {
-                                    list.add(obj);
-                                });
-                            }
-                        }
-                    }
-                    """
-            )
+          java(
+            """
+              package com.foobar;
+              import java.util.Optional;
+              import java.util.ArrayList;
+              import java.util.List;
+              public class A {
+                  void method() {
+                      boolean x = true;
+                      List<Integer> list = new ArrayList<>();
+                      Optional<Integer> o = Optional.of(2);
+                      if(x){
+                          if(o.isPresent()){
+                              list.add(o.get());
+                          }
+                      }
+                  }
+              }
+              """,
+            """
+              package com.foobar;
+              import java.util.Optional;
+              import java.util.ArrayList;
+              import java.util.List;
+              public class A {
+                  void method() {
+                      boolean x = true;
+                      List<Integer> list = new ArrayList<>();
+                      Optional<Integer> o = Optional.of(2);
+                      if(x){
+                          o.ifPresent((obj) -> {
+                              list.add(obj);
+                          });
+                      }
+                  }
+              }
+              """
+          )
         );
     }
 }
