164ca7224e1a0fedd8ce299c08708c9e6602562c
traceyyoshima <tracey.yoshima@gmail.com>
2023-04-06 14:32:39 -0600

    Removed duplicate test.

--- src/test/java/org/openrewrite/kotlin/tree/ClassDeclarationTest.java
+++ src/test/java/org/openrewrite/kotlin/tree/ClassDeclarationTest.java
@@ -16,6 +16,7 @@
 package org.openrewrite.kotlin.tree;
 
 import org.junit.jupiter.api.Test;
+import org.junitpioneer.jupiter.ExpectedToFail;
 import org.openrewrite.Issue;
 import org.openrewrite.test.RewriteTest;
 
@@ -299,22 +300,6 @@ class ClassDeclarationTest implements RewriteTest {
             
                 fun root ( ) : R {
                     return root
-                }
-            }
-            """
-          )
-        );
-    }
-
-    @Issue("https://github.com/openrewrite/rewrite-kotlin/issues/68")
-    @Test
-    void classWithInit() {
-        rewriteRun(
-          kotlin(
-            """
-            class Test {
-                init {
-                    println("Hello, world!")
                 }
             }
             """
