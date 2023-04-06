430a2e4fd2a4433fc1aad66cd5fbcff963c7561f
Sean C. Sullivan <github@seansullivan.com>
2023-04-06 12:43:40 -0700

    fix AppendToSequenceVisitor logic

--- rewrite-yaml/src/main/java/org/openrewrite/yaml/AppendToSequenceVisitor.java
+++ rewrite-yaml/src/main/java/org/openrewrite/yaml/AppendToSequenceVisitor.java
@@ -14,34 +14,31 @@ public class AppendToSequenceVisitor extends YamlIsoVisitor<org.openrewrite.Exec
     private final String value;
 
     public AppendToSequenceVisitor(JsonPathMatcher matcher, String value) {
-        System.out.println("xxx AppendToSequenceVisitor constructor");
         this.matcher = matcher;
         this.value = value;
     }
 
     @Override
     public Yaml.Sequence visitSequence(Yaml.Sequence sequence, ExecutionContext executionContext) {
-        System.out.println("xxx visitSequence called, " + sequence.toString());
         Yaml.Sequence s = super.visitSequence(sequence, executionContext);
             Cursor parent = getCursor().getParent();
             if (!matcher.matches(parent)) {
                 return s;
             }
             if (alreadyVisited(s, executionContext)) {
-                System.out.println("alreadyVisited: true");
                 return s;
             }
-            System.out.println("alreadyVisited: false");
             setVisited(s, executionContext);
 
             List<Yaml.Sequence.Entry> entries = sequence.getEntries();
-            boolean hasDash = true;
+            boolean hasDash = false;
             Yaml.Scalar.Style style = Yaml.Scalar.Style.PLAIN;
             String entryPrefix = "";
-            String entryTrailingCommaPrefix = "";
+            String entryTrailingCommaPrefix = null;
             String itemPrefix = "";
             if (!entries.isEmpty()) {
-                Yaml.Sequence.Entry existingEntry = entries.get(entries.size() - 1);
+                final int lastEntryIndex = entries.size() - 1;
+                Yaml.Sequence.Entry existingEntry = entries.get(lastEntryIndex);
                 hasDash = existingEntry.isDash();
                 entryPrefix = existingEntry.getPrefix();
                 entryTrailingCommaPrefix = existingEntry.getTrailingCommaPrefix();
@@ -50,11 +47,13 @@ public class AppendToSequenceVisitor extends YamlIsoVisitor<org.openrewrite.Exec
                 if (block instanceof Yaml.Sequence.Scalar) {
                     style = ((Yaml.Sequence.Scalar) block).getStyle();
                 }
+                if (!existingEntry.isDash()) {
+                    entries.set(lastEntryIndex, existingEntry.withTrailingCommaPrefix(""));
+                }
             }
             Yaml.Scalar newItem = new Yaml.Scalar(randomId(), itemPrefix, Markers.EMPTY, style, null, this.value);
             Yaml.Sequence.Entry newEntry = new Yaml.Sequence.Entry(randomId(), entryPrefix, Markers.EMPTY, newItem, hasDash, entryTrailingCommaPrefix);
             entries.add(newEntry);
-            // System.out.println("xxx entries size " + entries.size());
             // return maybeAutoFormat(sequence, s.withEntries(entries), ec);
             return s.withEntries(entries);
     }
