14d3a474b05ef904a2f831bcf22d42eecdcd74a1
aboyko <aboyko@vmware.com>
2023-04-06 16:44:44 -0400

    Revert unnecessary change

--- rewrite-maven/src/test/java/org/openrewrite/maven/search/DoesNotIncludeDependencyTest.java
+++ rewrite-maven/src/test/java/org/openrewrite/maven/search/DoesNotIncludeDependencyTest.java
@@ -54,7 +54,7 @@ class DoesNotIncludeDependencyTest implements RewriteTest {
         void dependencyPresentTransitivelyFailsApplicability() {
             rewriteRun(
               spec -> spec.recipe(new AddProperty("foo", "bar", null, null)
-                .addApplicableTest(new DoesNotIncludeDependency("org.springframework", "spring-beans", null, "Compile"))),
+                .addApplicableTest(new DoesNotIncludeDependency("org.springframework", "spring-beans", null, null))),
               pomXml("""
                 <?xml version="1.0" encoding="UTF-8"?>
                 <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
