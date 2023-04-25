27099fb27260cd0aa31dbc36be3c7cefc0874a45
Knut Wannheden <knut@moderne.io>
2023-04-25 16:36:54 +0200

    Implement prerelease versions according to Maven rules (#3163)
    The version qualifiers in Maven versions are case-insensitive. Adjusting the code accordingly and also matching `CR` as an alias for `RC` (this is for example used by the Quarkus project).
    
    Fixes: #3143

--- rewrite-core/src/main/java/org/openrewrite/semver/VersionComparator.java
+++ rewrite-core/src/main/java/org/openrewrite/semver/VersionComparator.java
@@ -24,7 +24,8 @@ import java.util.regex.Pattern;
 
 public interface VersionComparator extends Comparator<String> {
     Pattern RELEASE_PATTERN = Pattern.compile("(\\d+)(?:\\.(\\d+))?(?:\\.(\\d+))?(?:\\.(\\d+))?(?:\\.(\\d+))?([-+].*)?.*");
-    Pattern PRE_RELEASE_ENDING = Pattern.compile("[.-](SNAPSHOT|RC|rc|M|m|milestone|beta|alpha)[.-]?\\d*$");
+    String[] RELEASE_SUFFIXES = new String[]{".final", ".ga", ".release"};
+    Pattern PRE_RELEASE_ENDING = Pattern.compile("[.-](alpha|a|beta|b|milestone|m|rc|cr|snapshot)[.-]?\\d*$", Pattern.CASE_INSENSITIVE);
 
     @Deprecated
     default boolean isValid(String version) {
