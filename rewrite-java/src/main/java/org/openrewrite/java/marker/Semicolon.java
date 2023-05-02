843306352c8398141adfb4c686bcb13a2caede74
traceyyoshima <tracey.yoshima@gmail.com>
2023-05-02 12:35:38 -0600

    Added Semicolon marker to java to use in groovy, kotlin and javascript.

--- rewrite-java/src/main/java/org/openrewrite/java/marker/Semicolon.java
+++ rewrite-java/src/main/java/org/openrewrite/java/marker/Semicolon.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 the original author or authors.
+ * Copyright 2023 the original author or authors.
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.openrewrite.groovy.marker;
+package org.openrewrite.java.marker;
 
 import lombok.Value;
 import lombok.With;
@@ -21,7 +21,9 @@ import org.openrewrite.marker.Marker;
 
 import java.util.UUID;
 
-@Deprecated
+/**
+ * Represents optional semi-colons on java like languages.
+ */
 @Value
 @With
 public class Semicolon implements Marker {
