056ff18a1aef95b1be9b67b3ad6cd3b53d391238
Kun Li <122563761+kunli2@users.noreply.github.com>
2023-05-09 09:32:23 -0700

    Add guard for loading yaml source, gracefully handling the loading error. (#3224)
    * Add guard for loading yaml source, gracefully handling the loading error.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("org.openrewrite.build.root") version("latest.release")
-    id("org.openrewrite.build.java-base") version("latest.release")
+    id("org.openrewrite.build.root") version("1.11.6")
+    id("org.openrewrite.build.java-base") version("1.11.6")
     id("org.openrewrite.rewrite") version("latest.release")
 }
 
