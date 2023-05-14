bf5e477d51dd5f888ecd70427d09bacda9c77efb
Tim te Beek <tim@moderne.io>
2023-05-14 18:26:27 +0100

    Fix NoClassDefFoundError in RewriteTest on jgit diff()
    Fixes https://github.com/openrewrite/rewrite/issues/3243

--- rewrite-test/build.gradle.kts
+++ rewrite-test/build.gradle.kts
@@ -11,4 +11,7 @@ dependencies {
     implementation("org.assertj:assertj-core:latest.release")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-csv")
     implementation("org.slf4j:slf4j-api:1.7.36")
+
+    // NoClassDefFoundError jgit FileMode in RewriteTest diff() https://github.com/openrewrite/rewrite/issues/3243
+    implementation("org.eclipse.jgit:org.eclipse.jgit:5.13.+")
 }
