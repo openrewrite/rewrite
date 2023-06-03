ca982c70e4a7c5113c80e466b8ebc49e47d132d4
Jonathan Schneider <jkschneider@gmail.com>
2023-06-03 13:44:50 -0400

    Remove unnecessary test from RecipeMarkupDemonstrationTest

--- rewrite-java-test/src/test/java/org/openrewrite/java/RecipeMarkupDemonstrationTest.java
+++ rewrite-java-test/src/test/java/org/openrewrite/java/RecipeMarkupDemonstrationTest.java
@@ -15,19 +15,11 @@
  */
 package org.openrewrite.java;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.openrewrite.ExecutionContext;
-import org.openrewrite.Tree;
-import org.openrewrite.internal.lang.Nullable;
-import org.openrewrite.java.tree.J;
-import org.openrewrite.java.tree.JavaSourceFile;
-import org.openrewrite.marker.Markup;
 import org.openrewrite.test.RewriteTest;
 
 import static org.openrewrite.java.Assertions.java;
-import static org.openrewrite.test.RewriteTest.toRecipe;
 
 class RecipeMarkupDemonstrationTest implements RewriteTest {
 
@@ -46,38 +38,6 @@ class RecipeMarkupDemonstrationTest implements RewriteTest {
               }
               """, level.equals("error") || level.equals("info") ? "n" : "", level)
           )
-        );
-    }
-
-    @Test
-    void exceptionsWithoutCircularReferences() {
-        rewriteRun(
-          spec -> spec
-            .recipe(toRecipe(r -> new JavaIsoVisitor<>() {
-                @Override
-                public @Nullable J visit(@Nullable Tree tree, ExecutionContext ctx) {
-                    if (tree instanceof JavaSourceFile) {
-                        Exception exception = new RuntimeException("this the parent of a circular exception");
-                        Exception exception2 = new RuntimeException("this the child of a circular exception", exception);
-                        exception.initCause(exception2);
-                        JavaSourceFile sf = (JavaSourceFile) Markup.error(tree, exception);
-                        Markup.Error marker = sf.getMarkers().findFirst(Markup.Error.class).get();
-                        assert marker.getException().getCause() == null;
-                        //Otherwise, there is an error if the exception is serialized.
-                        return sf;
-                    }
-                    return (J) tree;
-                }
-            })),
-          java(
-            """
-              class Test {
-              }
-              """,
-            """
-              /*~~(this the parent of a circular exception)~~>*/class Test {
-              }
-              """)
         );
     }
 }
