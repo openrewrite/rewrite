e0b1f3bc1e8cc371b0b68abdeada4654ca296c8b
Kun Li <kun@moderne.io>
2023-06-05 16:35:14 -0700

    Add pluginManagement to settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,10 @@
+pluginManagement {
+    repositories {
+        mavenLocal()
+        gradlePluginPortal()
+    }
+}
+
 rootProject.name = "rewrite-kotlin"
 
 enableFeaturePreview("VERSION_ORDERING_V2")
