0813fad9b9597bd5ce046cc7cfad7db00eeb9d6e
Tim te Beek <tim@moderne.io>
2023-06-12 09:29:04 +0200

    Reformat ArraysAsListToImmutableRecipeTest to minimize indentation

--- rewrite-java/src/test/java/org/openrewrite/java/recipes/ArraysAsListToImmutableRecipeTest.java
+++ rewrite-java/src/test/java/org/openrewrite/java/recipes/ArraysAsListToImmutableRecipeTest.java
@@ -16,119 +16,111 @@
 package org.openrewrite.java.recipes;
 
 import org.junit.jupiter.api.Test;
+import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
 
 import static org.openrewrite.java.Assertions.java;
 
 class ArraysAsListToImmutableRecipeTest implements RewriteTest {
-	@Test
-	void wrapArrayListToBeUnmodifiableWhenStrings() {
-		rewriteRun(
-				recipeSpec -> recipeSpec.recipe(new ArraysAsListToImmutableRecipe()),
-				java(
-						"""
-								package my.test;
-										
-								import java.util.Arrays;
-											
-								class A
-								{
-								   public static final List<String> entries = Arrays.asList("A", "B");
-								}
-								""",
-						"""
-								package my.test;
-											
-								import java.util.Arrays;
-								import java.util.Collections;
-           								   
-								class A
-								{
-								   public static final List<String> entries = Collections.unmodifiableList(Arrays.asList("A", "B"));
-								}
-								"""
-				)
-		);
-	}
 
-	@Test
-	void wrapArrayListToBeUnmodifiableWhenNulls() {
-		rewriteRun(
-				recipeSpec -> recipeSpec.recipe(new ArraysAsListToImmutableRecipe()),
-				java(
-						"""
-								package my.test;
-										
-								import java.util.Arrays;
-											
-								class A
-								{
-								   public static final List<String> entries = Arrays.asList("A", null);
-								}
-								""",
-						"""
-								package my.test;
-											
-								import java.util.Arrays;
-								import java.util.Collections;
-           								   
-								class A
-								{
-								   public static final List<String> entries = Collections.unmodifiableList(Arrays.asList("A", null));
-								}
-								"""
-				)
-		);
-	}
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.recipe(new ArraysAsListToImmutableRecipe());
+    }
+
+    @Test
+    void wrapArrayListToBeUnmodifiableWhenStrings() {
+        rewriteRun(
+          java("""
+              package my.test;
+              
+              import java.util.Arrays;
+              
+              class A {
+                 public static final List<String> entries = Arrays.asList("A", "B");
+              }
+              """,
+            """
+              package my.test;
+              
+              import java.util.Arrays;
+              import java.util.Collections;
+              
+              class A {
+                 public static final List<String> entries = Collections.unmodifiableList(Arrays.asList("A", "B"));
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void wrapArrayListToBeUnmodifiableWhenNulls() {
+        rewriteRun(
+          java("""
+              package my.test;
+              
+              import java.util.Arrays;
+              
+              class A {
+                 public static final List<String> entries = Arrays.asList("A", null);
+              }
+              """,
+            """
+              package my.test;
+              
+              import java.util.Arrays;
+              import java.util.Collections;
+              
+              class A {
+                 public static final List<String> entries = Collections.unmodifiableList(Arrays.asList("A", null));
+              }
+              """
+          )
+        );
+    }
 
 
-	@Test
-	void wrapArrayListToBeUnmodifiableWhenIntegers() {
-		rewriteRun(
-				recipeSpec -> recipeSpec.recipe(new ArraysAsListToImmutableRecipe()),
-				java(
-						"""
-								package my.test;
-										
-								import java.util.Arrays;
-											
-								class A
-								{
-								   public static final List<Integer> entries = Arrays.asList(1, 2);
-								}
-								""",
-						"""
-								package my.test;
-											
-								import java.util.Arrays;
-								import java.util.Collections;
-           								   
-								class A
-								{
-								   public static final List<Integer> entries = Collections.unmodifiableList(Arrays.asList(1, 2));
-								}
-								"""
-				)
-		);
-	}
+    @Test
+    void wrapArrayListToBeUnmodifiableWhenIntegers() {
+        rewriteRun(
+          java("""
+              package my.test;
+              
+              import java.util.Arrays;
+              
+              class A {
+                 public static final List<Integer> entries = Arrays.asList(1, 2);
+              }
+              """,
+            """
+              package my.test;
+              
+              import java.util.Arrays;
+              import java.util.Collections;
+              
+              class A {
+                 public static final List<Integer> entries = Collections.unmodifiableList(Arrays.asList(1, 2));
+              }
+              """
+          )
+        );
+    }
 
-	@Test
-	void doNotWrapIfItIsAlreadyUnmodifiable() {
-		rewriteRun(
-				recipeSpec -> recipeSpec.expectedCyclesThatMakeChanges(0).recipe(new ArraysAsListToImmutableRecipe()),
-				java(
-						"""
-                                package my.test;
-                                		
-                                import java.util.Arrays;
-                                import java.util.Collections;
-                                			
-                                class A
-                                {
-                                   public static final List<String> entries = Collections.unmodifiableList(Arrays.asList("A", "B"));
-                                }
-                                """
-				)
-		);
-	}
+    @Test
+    void doNotWrapIfItIsAlreadyUnmodifiable() {
+        rewriteRun(
+          java("""
+            package my.test;
+            
+            import java.util.Arrays;
+            import java.util.Collections;
+            
+            class A {
+               public static final List<String> entries = Collections.unmodifiableList(Arrays.asList("A", "B"));
+            }
+            """
+          )
+        );
+    }
 }
