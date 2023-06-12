9629b71a60cdec69ef39acd0c556e1334dfcc010
Sebastian Konieczek <sebastian.konieczek@openvalue.de>
2023-06-12 09:44:25 +0200

    Adjust code style and add license header

--- rewrite-java-test/src/test/java/org/openrewrite/java/UsePrecompiledRegExpForReplaceAllTest.java
+++ rewrite-java-test/src/test/java/org/openrewrite/java/UsePrecompiledRegExpForReplaceAllTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2023 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.openrewrite.java;
 
 import org.junit.jupiter.api.Test;
@@ -9,26 +24,21 @@ class UsePrecompiledRegExpForReplaceAllTest implements RewriteTest {
 
     @Test
     void replaceSimpleVar() {
-        rewriteRun(recipeSpec -> recipeSpec.recipe(new UsePrecompiledRegExpForReplaceAll()),
-          java(
-            """
-             class A {
-                 public void replace(){
-                     String init = "Bob is a Bird... Bob is a Plane... Bob is Superman!";
-                     String changed = init.replaceAll("/[@]/g,", "_");
-                 }
-             }
-             """,
-            """
-              class A {
-                  private static final java.util.regex.Pattern openRewriteReplaceAllPatternVar = Pattern.compile("/[@]/g,");
-                  public void replace(){
-                      String init = "Bob is a Bird... Bob is a Plane... Bob is Superman!";
-                      String changed = openRewriteReplaceAllPatternVar.matcher(init).replaceAll("_");
-                  }
+        rewriteRun(recipeSpec -> recipeSpec.recipe(new UsePrecompiledRegExpForReplaceAll()), java("""
+          class A {
+              public void replace(){
+                  String init = "Bob is a Bird... Bob is a Plane... Bob is Superman!";
+                  String changed = init.replaceAll("/[@]/g,", "_");
               }
-              """
-          )
-        );
+          }
+          """, """
+          class A {
+              private static final java.util.regex.Pattern openRewriteReplaceAllPatternVar = Pattern.compile("/[@]/g,");
+              public void replace(){
+                  String init = "Bob is a Bird... Bob is a Plane... Bob is Superman!";
+                  String changed = openRewriteReplaceAllPatternVar.matcher(init).replaceAll("_");
+              }
+          }
+          """));
     }
 }
--- rewrite-java/src/main/java/org/openrewrite/java/UsePrecompiledRegExpForReplaceAll.java
+++ rewrite-java/src/main/java/org/openrewrite/java/UsePrecompiledRegExpForReplaceAll.java
@@ -1,8 +1,20 @@
+/*
+ * Copyright 2023 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.openrewrite.java;
 
-import static org.openrewrite.Tree.randomId;
-
-import java.util.regex.Pattern;
 import org.openrewrite.Cursor;
 import org.openrewrite.ExecutionContext;
 import org.openrewrite.Recipe;
@@ -12,88 +24,84 @@ import org.openrewrite.java.tree.JavaType;
 import org.openrewrite.java.tree.Space;
 import org.openrewrite.marker.Markers;
 
-public class UsePrecompiledRegExpForReplaceAll
-   extends Recipe
-{
+import java.util.regex.Pattern;
 
-   public static final String REG_EXP_KEY = "regExp";
+import static org.openrewrite.Tree.randomId;
 
-   @Override
-   public String getDisplayName()
-   {
-      return getClass().getSimpleName();
-   }
+public class UsePrecompiledRegExpForReplaceAll extends Recipe {
 
-   @Override
-   public String getDescription()
-   {
-      return getClass().getSimpleName() + ".";
-   }
+    public static final String REG_EXP_KEY = "regExp";
 
-   @Override
-   public TreeVisitor< ?, ExecutionContext > getVisitor()
-   {
-      return new ReplaceVisitor();
-   }
+    @Override
+    public String getDisplayName() {
+        return getClass().getSimpleName();
+    }
 
-   public static class ReplaceVisitor
-      extends JavaIsoVisitor< ExecutionContext >
-   {
-      private static final String PATTERN_VAR = "openRewriteReplaceAllPatternVar";
-      JavaTemplate matcherTemplate =
-         JavaTemplate.builder( "#{any()}.matcher( #{any(java.lang.CharSequence)}).replaceAll( #{any(java.lang.String)})" )
-                     .contextSensitive()
-                     .imports( "java.util.regex.Pattern" )
-                     .build();
-      JavaTemplate compilePatternTemplate =
-         JavaTemplate.builder( "private static final java.util.regex.Pattern " + PATTERN_VAR + "= Pattern.compile(#{});" )
-                     .contextSensitive()
-                     .imports( "java.util.regex.Pattern" )
-                     .build();
+    @Override
+    public String getDescription() {
+        return getClass().getSimpleName() + ".";
+    }
 
-      @Override
-      public J.Block visitBlock(
-         final J.Block block,
-         final ExecutionContext executionContext )
-      {
-         final J.Block block1 = super.visitBlock( block, executionContext );
-         final Cursor parentOrThrow = getCursor().getParentOrThrow();
-         if( parentOrThrow.getValue() instanceof J.ClassDeclaration ) {
-            Object regExp = parentOrThrow.pollNearestMessage( REG_EXP_KEY );
-            if( regExp == null ) {
-               return block1;
+    @Override
+    public TreeVisitor<?, ExecutionContext> getVisitor() {
+        return new ReplaceVisitor();
+    }
+
+    public static class ReplaceVisitor extends JavaIsoVisitor<ExecutionContext> {
+        private static final String PATTERN_VAR = "openRewriteReplaceAllPatternVar";
+        JavaTemplate matcherTemplate = JavaTemplate
+                .builder("#{any()}.matcher( #{any(java.lang.CharSequence)}).replaceAll( #{any(java.lang.String)})")
+                .contextSensitive()
+                .imports("java.util.regex.Pattern")
+                .build();
+        JavaTemplate compilePatternTemplate = JavaTemplate
+                .builder("private static final java.util.regex.Pattern " + PATTERN_VAR + "= Pattern.compile(#{});")
+                .contextSensitive()
+                .imports("java.util.regex.Pattern")
+                .build();
+
+        @Override
+        public J.Block visitBlock(final J.Block block, final ExecutionContext executionContext) {
+            final J.Block processedBlock = super.visitBlock(block, executionContext);
+            final Cursor parentOrThrow = getCursor().getParentOrThrow();
+            if (parentOrThrow.getValue() instanceof J.ClassDeclaration) {
+                Object regExp = parentOrThrow.pollNearestMessage(REG_EXP_KEY);
+                if (regExp == null) {
+                    return processedBlock;
+                }
+                return super.visitBlock(
+                        compilePatternTemplate.apply(
+                                getCursor(),
+                                processedBlock.getCoordinates().firstStatement(),
+                                regExp),
+                        executionContext);
             }
-            return super.visitBlock( compilePatternTemplate.apply( getCursor(),
-                                                                   block1.getCoordinates().firstStatement(),
-                                                                   regExp ), executionContext );
-         }
-         return block1;
-      }
+            return processedBlock;
+        }
 
-      @Override
-      public J.MethodInvocation visitMethodInvocation(
-         J.MethodInvocation method,
-         ExecutionContext executionContext )
-      {
-         J.MethodInvocation invocation = super.visitMethodInvocation( method, executionContext );
-         JavaType.Method methodType = invocation.getMethodType();
-         if( methodType == null
-             || !"java.lang.String".equals( methodType.getDeclaringType().getFullyQualifiedName() )
-             || !"replaceAll".equals( invocation.getName().getSimpleName() ) ) {
-            return invocation;
-         }
-         Object varIdentifier = new J.Identifier( randomId(),
-                                                  Space.SINGLE_SPACE,
-                                                  Markers.EMPTY,
-                                                  PATTERN_VAR,
-                                                  JavaType.buildType( Pattern.class.getName() ),
-                                                  null );
-         getCursor().putMessageOnFirstEnclosing( J.ClassDeclaration.class, REG_EXP_KEY, invocation.getArguments().get( 0 ) );
-         return matcherTemplate.apply( getCursor(),
-                                       invocation.getCoordinates().replace(),
-                                       varIdentifier,
-                                       invocation.getSelect(),
-                                       invocation.getArguments().get( 1 ) );
-      }
-   }
+        @Override
+        public J.MethodInvocation visitMethodInvocation(J.MethodInvocation method, ExecutionContext executionContext) {
+            J.MethodInvocation invocation = super.visitMethodInvocation(method, executionContext);
+            JavaType.Method methodType = invocation.getMethodType();
+            if (methodType == null
+                    || !"java.lang.String".equals(methodType.getDeclaringType().getFullyQualifiedName())
+                    || !"replaceAll".equals(invocation.getName().getSimpleName())) {
+                return invocation;
+            }
+            Object varIdentifier = new J.Identifier(
+                    randomId(),
+                    Space.SINGLE_SPACE,
+                    Markers.EMPTY,
+                    PATTERN_VAR,
+                    JavaType.buildType(Pattern.class.getName()),
+                    null);
+
+            getCursor().putMessageOnFirstEnclosing(J.ClassDeclaration.class, REG_EXP_KEY, invocation.getArguments().get(0));
+            return matcherTemplate.apply(
+                    getCursor(),
+                    invocation.getCoordinates().replace(),
+                    varIdentifier, invocation.getSelect(),
+                    invocation.getArguments().get(1));
+        }
+    }
 }
