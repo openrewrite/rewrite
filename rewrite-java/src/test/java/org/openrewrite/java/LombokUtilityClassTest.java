6d87f63a3c3c38ad848899553c62e72868ccf10b
Alexei Bratuhin <alexei.bratuhin@googlemail.com>
2023-06-12 09:48:34 +0200

    #3156 = add additional suggestion for a test case

--- rewrite-java/src/test/java/org/openrewrite/java/LombokUtilityClassTest.java
+++ rewrite-java/src/test/java/org/openrewrite/java/LombokUtilityClassTest.java
@@ -14,6 +14,7 @@ import static org.openrewrite.java.Assertions.java;
  * - abstract classes
  * - instantiations of changed classes
  * - constructor
+ * - public static void main
  */
 class LombokUtilityClassTest implements RewriteTest {
 
