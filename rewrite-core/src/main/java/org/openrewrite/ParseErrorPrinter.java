e4e58c05119eaa84d3900169e1741337d56fe881
Sam Snyder <sam@moderne.io>
2023-06-21 11:17:13 -0700

    Print markers in parse errors

--- rewrite-core/src/main/java/org/openrewrite/ParseErrorPrinter.java
+++ rewrite-core/src/main/java/org/openrewrite/ParseErrorPrinter.java
@@ -1,9 +1,21 @@
 package org.openrewrite;
 
+import org.openrewrite.marker.Marker;
+
 public class ParseErrorPrinter<P> extends ParseErrorVisitor<PrintOutputCapture<P>> {
     @Override
     public ParseError visitParseError(ParseError e, PrintOutputCapture<P> p) {
+        for (Marker marker : e.getMarkers().getMarkers()) {
+            p.append(p.getMarkerPrinter().beforePrefix(marker, new Cursor(getCursor(), marker), it -> it));
+        }
+        visitMarkers(e.getMarkers(), p);
+        for (Marker marker : e.getMarkers().getMarkers()) {
+            p.append(p.getMarkerPrinter().beforeSyntax(marker, new Cursor(getCursor(), marker), it -> it));
+        }
         p.append(e.getText());
+        for (Marker marker : e.getMarkers().getMarkers()) {
+            p.append(p.getMarkerPrinter().afterSyntax(marker, new Cursor(getCursor(), marker), it -> it));
+        }
         return e;
     }
 }
