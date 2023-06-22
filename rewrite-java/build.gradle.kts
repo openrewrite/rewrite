3e02ecfab2c872a62ea9b3bf711e00cbd03ccf77
Kun Li <kun@moderne.io>
2023-06-21 17:33:52 -0700

    Bump snappy-java version to latest to suppress dependency vulnerability

--- rewrite-java/build.gradle.kts
+++ rewrite-java/build.gradle.kts
@@ -33,7 +33,7 @@ dependencies {
     implementation("org.apache.commons:commons-text:latest.release")
     implementation("io.github.classgraph:classgraph:latest.release")
 
-    implementation("org.xerial.snappy:snappy-java:1.1.8.4")
+    implementation("org.xerial.snappy:snappy-java:1.1.10.1")
 
     api("com.fasterxml.jackson.core:jackson-annotations")
 
