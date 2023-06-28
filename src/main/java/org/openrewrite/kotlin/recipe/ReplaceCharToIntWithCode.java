ec856a542d450b552a102c63eee0bf3e192e930a
Kun Li <kun@moderne.io>
2023-06-28 14:52:25 -0700

    add license header

--- src/main/java/org/openrewrite/kotlin/recipe/ReplaceCharToIntWithCode.java
+++ src/main/java/org/openrewrite/kotlin/recipe/ReplaceCharToIntWithCode.java
@@ -1,5 +1,22 @@
+/*
+ * Copyright 2021 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.openrewrite.kotlin.recipe;
 
+import lombok.EqualsAndHashCode;
+import lombok.Value;
 import org.openrewrite.ExecutionContext;
 import org.openrewrite.Recipe;
 import org.openrewrite.TreeVisitor;
@@ -10,6 +27,8 @@ import org.openrewrite.kotlin.KotlinVisitor;
 import org.openrewrite.kotlin.tree.K;
 
 
+@Value
+@EqualsAndHashCode(callSuper = true)
 public class ReplaceCharToIntWithCode extends Recipe {
     private static final MethodMatcher CHAR_TO_INT_METHOD_MATCHER = new MethodMatcher("kotlin.Char toInt()");
     private static J.FieldAccess charCodeTemplate = null;
@@ -37,7 +56,7 @@ public class ReplaceCharToIntWithCode extends Recipe {
             @Override
             public J visitMethodInvocation(J.MethodInvocation method,
                                                             ExecutionContext executionContext) {
-                if (CHAR_TO_INT_METHOD_MATCHER.matches(method)) {
+                if (CHAR_TO_INT_METHOD_MATCHER.matches(method) && method.getSelect() != null) {
                     J.FieldAccess codeTemplate = getCharCodeTemplate();
                     return codeTemplate.withTarget(method.getSelect()).withPrefix(method.getPrefix());
                 }
