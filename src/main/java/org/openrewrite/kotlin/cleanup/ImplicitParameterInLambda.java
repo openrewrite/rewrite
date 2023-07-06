66a95c459600d9fc2f133be75213ff0f48431b42
traceyyoshima <tracey.yoshima@gmail.com>
2023-07-06 10:08:46 -0600

    Polish.

--- src/main/java/org/openrewrite/kotlin/cleanup/ImplicitParameterInLambda.java
+++ src/main/java/org/openrewrite/kotlin/cleanup/ImplicitParameterInLambda.java
@@ -42,7 +42,7 @@ public class ImplicitParameterInLambda extends Recipe {
     public String getDescription() {
         return "\"it\" is a special identifier that allows you to refer to the current parameter being passed to a " +
                "lambda expression without explicitly naming the parameter." +
-               "Lambda expressions are a concise way of writing anonymous functions. Many lambda expressions have " +
+               " Lambda expressions are a concise way of writing anonymous functions. Many lambda expressions have " +
                "only one parameter, when this is true the compiler can determine the parameter type by context. Thus " +
                "when using it with single parameter lambda expressions, you do not need to declare the type.";
     }
