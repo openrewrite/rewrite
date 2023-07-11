2b79cfaac1715c6b9bab7d9ed6f931000c908612
Kun Li <kun@moderne.io>
2023-07-11 11:18:35 -0700

    fix destructuring declaration case

--- src/main/java/org/openrewrite/kotlin/internal/KotlinParserVisitor.java
+++ src/main/java/org/openrewrite/kotlin/internal/KotlinParserVisitor.java
@@ -1655,6 +1655,10 @@ public class KotlinParserVisitor extends FirDefaultVisitor<J, ExecutionContext> 
         List<J.Modifier> modifiers = emptyList();
         PsiElement currentNode = getCurrentPsiNode();
         PsiElement propertyNode = (currentNode instanceof KtProperty) ? currentNode : PsiTreeUtil.getParentOfType(currentNode, KtProperty.class);
+        property.getName();
+        if ("<destruct>".equals(property.getName().toString())) {
+            propertyNode = (currentNode instanceof KtDestructuringDeclaration) ? currentNode : PsiTreeUtil.getParentOfType(currentNode, KtDestructuringDeclaration.class);
+        }
 
         if (currentNode instanceof KtAnnotationEntry) {
             currentNode = PsiTreeUtil.getParentOfType(currentNode, KtModifierList.class);
