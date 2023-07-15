7ab734e54feb091f766bdbe12cee5e822ec3c6b0
Tim te Beek <tim@moderne.io>
2023-07-15 12:22:40 +0200

    Replicate issue with Yaml Unicode suffix parsing
    https://github.com/openrewrite/rewrite/issues/2062

---
+++ rewrite-yaml/src/test/java/org/openrewrite/yaml/YamlParserTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2023 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.yaml;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.openrewrite.SourceFile;
+import org.openrewrite.yaml.tree.Yaml;
+
+import java.util.stream.Stream;
+
+class YamlParserTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+      "Breaking Changes",
+      "Breaking Changes 🛠"
+    })
+    void parseYamlWithUnicode(String input) {
+        Stream<SourceFile> yamlSources = YamlParser.builder().build().parse("title: %s\n".formatted(input));
+        SourceFile sourceFile = yamlSources.findFirst().get();
+        Yaml.Documents documents = (Yaml.Documents) sourceFile;
+        Yaml.Document document = documents.getDocuments().get(0);
+
+        // Assert that end is parsed correctly
+        Assertions.assertThat(document.getEnd().getPrefix()).isEqualTo("\n");
+
+        // Assert that the title is parsed correctly
+        Yaml.Mapping mapping = (Yaml.Mapping) document.getBlock();
+        Yaml.Mapping.Entry entry = mapping.getEntries().get(0);
+        Yaml.Scalar title = (Yaml.Scalar) entry.getValue();
+        Assertions.assertThat(title.getValue()).isEqualTo(input);
+    }
+
+}
