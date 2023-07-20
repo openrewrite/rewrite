237c4dde2e68e5321c66fcbda65daf7d8bed41e3
Tim te Beek <tim@moderne.io>
2023-07-20 16:07:41 +0200

    Turn YAML files with unicode into ParseErrors

--- rewrite-yaml/src/test/java/org/openrewrite/yaml/YamlParserTest.java
+++ rewrite-yaml/src/test/java/org/openrewrite/yaml/YamlParserTest.java
@@ -15,31 +15,26 @@
  */
 package org.openrewrite.yaml;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.openrewrite.SourceFile;
 import org.openrewrite.tree.ParseError;
 import org.openrewrite.yaml.tree.Yaml;
 
+import java.util.List;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class YamlParserTest {
 
-    @ParameterizedTest
-    @ValueSource(strings = {
-      "b",
-      "🛠",
-      "🛠🛠",
-      "🛠 🛠"
-    })
-    void parseYamlWithUnicode(String input) {
-        Stream<SourceFile> yamlSources = YamlParser.builder().build().parse("a: %s\n".formatted(input));
-        SourceFile sourceFile = yamlSources.findFirst().get();
-        assertThat(sourceFile).isNotInstanceOf(ParseError.class);
+    @Test
+    void ascii() {
+        List<SourceFile> yamlSources = YamlParser.builder().build().parse("a: b\n").toList();
+        assertThat(yamlSources).singleElement().isInstanceOf(Yaml.Documents.class);
 
-        Yaml.Documents documents = (Yaml.Documents) sourceFile;
+        Yaml.Documents documents = (Yaml.Documents) yamlSources.get(0);
         Yaml.Document document = documents.getDocuments().get(0);
 
         // Assert that end is parsed correctly
@@ -49,7 +44,18 @@ class YamlParserTest {
         Yaml.Mapping mapping = (Yaml.Mapping) document.getBlock();
         Yaml.Mapping.Entry entry = mapping.getEntries().get(0);
         Yaml.Scalar title = (Yaml.Scalar) entry.getValue();
-        assertThat(title.getValue()).isEqualTo(input);
+        assertThat(title.getValue()).isEqualTo("b");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+      "🛠",
+      "🛠🛠",
+      "🛠 🛠"
+    })
+    void unicodeParseError(String input ) {
+        Stream<SourceFile> yamlSources = YamlParser.builder().build().parse("a: %s\n".formatted(input));
+        assertThat(yamlSources).singleElement().isInstanceOf(ParseError.class);
     }
 
 }
