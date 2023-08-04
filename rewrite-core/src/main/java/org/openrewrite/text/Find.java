78fa25c3e17303a3746df34aafb5f9d306c97537
Sam Snyder <sam@moderne.io>
2023-08-04 09:10:59 -0400

    File pattern not a required field for text search

--- rewrite-core/src/main/java/org/openrewrite/text/Find.java
+++ rewrite-core/src/main/java/org/openrewrite/text/Find.java
@@ -84,6 +84,7 @@ public class Find extends Recipe {
                           "Multiple patterns may be specified, separated by a semicolon `;`. " +
                           "If multiple patterns are supplied any of the patterns matching will be interpreted as a match. " +
                           "When not set, all source files are searched. ",
+            required = false,
             example = "**/*.java")
     @Nullable
     String filePattern;
