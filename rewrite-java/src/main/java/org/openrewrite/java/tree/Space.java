83b75bc2432fd4319e79184d00830649696daa10
Knut Wannheden <knut@moderne.io>
2023-08-24 21:56:16 +0200

    Make `Space#flyweights` synchronized once again (#3499)
    Using a `HashMap` (or `WeakHashMap`) concurrently can cause infinite loops.
    
    At the same time also move the string allocation to the value provider for the `computeIfAbsent()` call to save some object allocations.

--- rewrite-java/src/main/java/org/openrewrite/java/tree/Space.java
+++ rewrite-java/src/main/java/org/openrewrite/java/tree/Space.java
@@ -20,10 +20,7 @@ import lombok.EqualsAndHashCode;
 import org.openrewrite.internal.lang.Nullable;
 import org.openrewrite.marker.Markers;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.WeakHashMap;
+import java.util.*;
 
 import static java.util.Collections.emptyList;
 
@@ -46,7 +43,7 @@ public class Space {
      * e.g.: a single space between keywords, or the common indentation of every line in a block.
      * So use flyweights to avoid storing many instances of functionally identical spaces
      */
-    private static final Map<String, Space> flyweights = new WeakHashMap<>();
+    private static final Map<String, Space> flyweights = Collections.synchronizedMap(new WeakHashMap<>());
 
     static {
         flyweights.put(" ", SINGLE_SPACE);
