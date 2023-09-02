fb0a7854399052b9758487a510ec73745997e2d4
Jonathan Schneider <jkschneider@gmail.com>
2023-09-02 19:41:06 -0400

    SSHD

--- rewrite-core/build.gradle.kts
+++ rewrite-core/build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
 dependencies {
     compileOnly("org.eclipse.jgit:org.eclipse.jgit:5.13.+")
     compileOnly("org.eclipse.jgit:org.eclipse.jgit.ssh.jsch:5.13.+")
+    compileOnly("org.eclipse.jgit:org.eclipse.jgit.ssh.apache.agent:latest.release")
 
     implementation("org.openrewrite.tools:java-object-diff:latest.release")
 
