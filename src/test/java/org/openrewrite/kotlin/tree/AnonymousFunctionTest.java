6de841a36bcd7413c61481b499023fee2002bce6
Knut Wannheden <knut@moderne.io>
2023-09-07 10:19:58 +0200

    Add some more assertions to `AnonymousFunctionTest`

--- src/test/java/org/openrewrite/kotlin/tree/AnonymousFunctionTest.java
+++ src/test/java/org/openrewrite/kotlin/tree/AnonymousFunctionTest.java
@@ -17,15 +17,44 @@ package org.openrewrite.kotlin.tree;
 
 import org.junit.jupiter.api.Test;
 import org.openrewrite.Issue;
+import org.openrewrite.java.tree.J;
+import org.openrewrite.java.tree.JavaType;
+import org.openrewrite.kotlin.marker.AnonymousFunction;
 import org.openrewrite.test.RewriteTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.openrewrite.kotlin.Assertions.kotlin;
 
 class AnonymousFunctionTest implements RewriteTest {
 
     @Test
     @Issue("https://github.com/openrewrite/rewrite-kotlin/issues/287")
-    void notInitialized() {
+    void noArgs() {
+        rewriteRun(
+          kotlin(
+            """
+              val alwaysTrue = fun() = true
+              """,
+            spec -> spec.afterRecipe(cu -> {
+                assertThat(cu.getStatements()).hasSize(1);
+                J.VariableDeclarations alwaysTrue = (J.VariableDeclarations) cu.getStatements().get(0);
+                assertThat(alwaysTrue.getVariables()).hasSize(1);
+                J.VariableDeclarations.NamedVariable alwaysTrueVar = alwaysTrue.getVariables().get(0);
+                assertThat(((JavaType.FullyQualified) alwaysTrueVar.getType()).getFullyQualifiedName()).isEqualTo("kotlin.Function0");
+                assertThat(alwaysTrueVar.getInitializer()).isInstanceOf(J.Lambda.class);
+                J.Lambda lambda = (J.Lambda) alwaysTrueVar.getInitializer();
+                assertThat(lambda.getMarkers().findFirst(AnonymousFunction.class)).isPresent();
+                assertThat(lambda.getParameters().getParameters()).satisfiesExactly(
+                    empty -> assertThat(empty).isInstanceOf(J.Empty.class)
+                );
+            })
+          )
+        );
+    }
+
+    @Test
+    @Issue("https://github.com/openrewrite/rewrite-kotlin/issues/287")
+    void singleArg() {
         rewriteRun(
           kotlin(
             """
