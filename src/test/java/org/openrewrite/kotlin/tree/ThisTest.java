a8ef4bf03f7baec4e51edc93fc9cb98ea4ad69a6
Knut Wannheden <knut@moderne.io>
2023-09-11 09:21:30 +0200

    Add failing test case for #302

---
+++ src/test/java/org/openrewrite/kotlin/tree/ThisTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2023 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.kotlin.tree;
+
+import org.junit.jupiter.api.Test;
+import org.junitpioneer.jupiter.ExpectedToFail;
+import org.openrewrite.Issue;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.kotlin.Assertions.kotlin;
+
+public class ThisTest implements RewriteTest {
+
+    @Test
+    @Issue("https://github.com/openrewrite/rewrite-kotlin/issues/302")
+    @ExpectedToFail
+    void qualifiedThis() {
+        rewriteRun(
+          kotlin(
+            """
+              import kotlin.collections.MutableMap.MutableEntry
+
+              abstract class LinkedHashTreeMap<K, V> : AbstractMutableMap<K, V>() {
+                override var size = 0
+              
+                abstract inner class EntrySet : AbstractMutableSet<MutableEntry<K, V>>() {
+                  override val size: Int
+                    get() = this@LinkedHashTreeMap.size
+              
+                  override fun iterator(): MutableIterator<MutableEntry<K, V>> {
+                    return null!!
+                  }
+                }
+              }
+              """
+          )
+        );
+    }
+}
