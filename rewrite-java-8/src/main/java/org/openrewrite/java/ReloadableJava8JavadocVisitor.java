b0eddd06010bfc98056cc6636e87a2773504449b
Knut Wannheden <knut@moderne.io>
2023-10-03 12:47:16 +0200

    Add missing `visitArrayType()` to Javadoc parsers for Java 8 and 11
    Issue: #3575

--- rewrite-java-8/src/main/java/org/openrewrite/java/ReloadableJava8JavadocVisitor.java
+++ rewrite-java-8/src/main/java/org/openrewrite/java/ReloadableJava8JavadocVisitor.java
@@ -16,7 +16,7 @@
 package org.openrewrite.java;
 
 import com.sun.source.doctree.*;
-import com.sun.source.tree.IdentifierTree;
+import com.sun.source.tree.ArrayTypeTree;
 import com.sun.source.tree.MemberSelectTree;
 import com.sun.source.tree.ParameterizedTypeTree;
 import com.sun.source.tree.PrimitiveTypeTree;
@@ -87,8 +87,6 @@ public class ReloadableJava8JavadocVisitor extends DocTreeScanner<Tree, List<Jav
     }
 
     private void init() {
-        char[] sourceArr = source.toCharArray();
-
         StringBuilder firstPrefixBuilder = new StringBuilder();
         StringBuilder javadocContent = new StringBuilder();
         StringBuilder marginBuilder = null;
@@ -97,12 +95,12 @@ public class ReloadableJava8JavadocVisitor extends DocTreeScanner<Tree, List<Jav
 
         // skip past the opening '/**'
         int i = 3;
-        for (; i < sourceArr.length; i++) {
-            char c = sourceArr[i];
+        for (; i < source.length(); i++) {
+            char c = source.charAt(i);
             if (inFirstPrefix) {
                 // '*' characters are considered a part of the margin until a non '*' is parsed.
                 if (Character.isWhitespace(c) || c == '*' && isPrefixAsterisk) {
-                    if (isPrefixAsterisk && i + 1 <= sourceArr.length - 1 && sourceArr[i + 1] != '*') {
+                    if (isPrefixAsterisk && i + 1 <= source.length() - 1 && source.charAt(i + 1) != '*') {
                         isPrefixAsterisk = false;
                     }
                     firstPrefixBuilder.append(c);
@@ -117,30 +115,31 @@ public class ReloadableJava8JavadocVisitor extends DocTreeScanner<Tree, List<Jav
             }
 
             if (c == '\n') {
+                char prev = source.charAt(i - 1);
                 if (inFirstPrefix) {
                     firstPrefix = firstPrefixBuilder.toString();
                     inFirstPrefix = false;
                 } else {
                     // Handle consecutive new lines.
-                    if ((sourceArr[i - 1] == '\n' ||
-                            sourceArr[i - 1] == '\r' && sourceArr[i -2] == '\n')) {
-                        String prevLineLine = sourceArr[i - 1] == '\n' ? "\n" : "\r\n";
+                    if ((prev == '\n' ||
+                            prev == '\r' && source.charAt(i - 2) == '\n')) {
+                        String prevLineLine = prev == '\n' ? "\n" : "\r\n";
                         lineBreaks.put(javadocContent.length(), new Javadoc.LineBreak(randomId(), prevLineLine, Markers.EMPTY));
                     } else if (marginBuilder != null) { // A new line with no '*' that only contains whitespace.
-                        String newLine = sourceArr[i - 1] == '\r' ? "\r\n" : "\n";
+                        String newLine = prev == '\r' ? "\r\n" : "\n";
                         lineBreaks.put(javadocContent.length(), new Javadoc.LineBreak(randomId(), newLine, Markers.EMPTY));
                         javadocContent.append(marginBuilder.substring(marginBuilder.indexOf("\n") + 1));
                     }
                     javadocContent.append(c);
                 }
-                String newLine = sourceArr[i - 1] == '\r' ? "\r\n" : "\n";
+                String newLine = prev == '\r' ? "\r\n" : "\n";
                 marginBuilder = new StringBuilder(newLine);
             } else if (marginBuilder != null) {
                 if (!Character.isWhitespace(c)) {
                     if (c == '*') {
                         // '*' characters are considered a part of the margin until a non '*' is parsed.
                         marginBuilder.append(c);
-                        if (i + 1 <= sourceArr.length - 1 && sourceArr[i + 1] != '*') {
+                        if (i + 1 <= source.length() - 1 && source.charAt(i + 1) != '*') {
                             lineBreaks.put(javadocContent.length(), new Javadoc.LineBreak(randomId(),
                                     marginBuilder.toString(), Markers.EMPTY));
                             marginBuilder = null;
@@ -151,7 +150,7 @@ public class ReloadableJava8JavadocVisitor extends DocTreeScanner<Tree, List<Jav
                                     marginBuilder.toString(), Markers.EMPTY));
                             javadocContent.append(c);
                         } else {
-                            String newLine = marginBuilder.toString().startsWith("\r") ? "\r\n" : "\n";
+                            String newLine = marginBuilder.charAt(0) == '\r' ? "\r\n" : "\n";
                             lineBreaks.put(javadocContent.length(), new Javadoc.LineBreak(randomId(),
                                     newLine, Markers.EMPTY));
                             String margin = marginBuilder.toString();
@@ -788,18 +787,16 @@ public class ReloadableJava8JavadocVisitor extends DocTreeScanner<Tree, List<Jav
 
         if (!node.isEmpty() && Character.isWhitespace(node.charAt(0)) &&
                 !Character.isWhitespace(source.charAt(cursor))) {
-            char[] charArray = node.toCharArray();
-
             int i = 0;
-            for (; i < charArray.length && Character.isWhitespace(charArray[i]); i++) {
+            for (; i < node.length() && Character.isWhitespace(node.charAt(i)); i++) {
             }
 
             node = node.substring(i);
         }
 
-        char[] textArr = node.toCharArray();
         StringBuilder text = new StringBuilder();
-        for (char c : textArr) {
+        for (int i = 0; i < node.length(); i++) {
+            char c = node.charAt(i);
             cursor++;
             if (c == '\n') {
                 if (text.length() > 0) {
@@ -1039,7 +1036,7 @@ public class ReloadableJava8JavadocVisitor extends DocTreeScanner<Tree, List<Jav
         }
 
         @Override
-        public J visitIdentifier(IdentifierTree node, Space fmt) {
+        public J visitIdentifier(com.sun.source.tree.IdentifierTree node, Space fmt) {
             String name = node.getName().toString();
             cursor += name.length();
             JavaType type = typeMapping.type(node);
@@ -1052,6 +1049,43 @@ public class ReloadableJava8JavadocVisitor extends DocTreeScanner<Tree, List<Jav
             String name = primitiveType.toString();
             cursor += name.length();
             return new J.Identifier(randomId(), fmt, Markers.EMPTY, emptyList(), name, typeMapping.primitive(primitiveType.typetag), null);
+        }
+
+        @Override
+        public J visitArrayType(ArrayTypeTree node, Space fmt) {
+            com.sun.source.tree.Tree typeIdent = node.getType();
+            int dimCount = 1;
+
+            while (typeIdent instanceof ArrayTypeTree) {
+                dimCount++;
+                typeIdent = ((ArrayTypeTree) typeIdent).getType();
+            }
+
+            TypeTree elemType = (TypeTree) scan(typeIdent, fmt);
+
+            List<JRightPadded<Space>> dimensions = emptyList();
+            if (dimCount > 0) {
+                dimensions = new ArrayList<>(dimCount);
+                for (int n = 0; n < dimCount; n++) {
+                    if (!source.substring(cursor).startsWith("...")) {
+                        dimensions.add(padRight(
+                                Space.build(sourceBeforeAsString("["), emptyList()),
+                                Space.build(sourceBeforeAsString("]"), emptyList())));
+                    }
+                }
+            }
+
+            return new J.ArrayType(
+                    randomId(),
+                    Space.EMPTY,
+                    Markers.EMPTY,
+                    elemType,
+                    dimensions
+            );
+        }
+
+        private <T> JRightPadded<T> padRight(T tree, Space right) {
+            return new JRightPadded<>(tree, right, Markers.EMPTY);
         }
 
         @Override
