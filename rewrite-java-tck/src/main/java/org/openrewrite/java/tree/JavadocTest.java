1670c0144964c419f6ff42b72d5f278eac5a87db
Knut Wannheden <knut@moderne.io>
2023-10-04 21:10:42 +0200

    Fix printing of false Javadoc attributes
    This to ensure parse-to-print idempotency.

--- rewrite-java-tck/src/main/java/org/openrewrite/java/tree/JavadocTest.java
+++ rewrite-java-tck/src/main/java/org/openrewrite/java/tree/JavadocTest.java
@@ -1707,4 +1707,23 @@ class JavadocTest implements RewriteTest {
           )
         );
     }
+
+    @Test
+    void emptyAttributes() {
+        rewriteRun(
+          java(
+            """
+              /**
+               * DEFINE TENANCY TenantB AS <TenantB OCID>
+               * ENDORSE GROUP <TenantA user group name> TO {OBJECTSTORAGE_NAMESPACE_READ} IN TENANCY TenantB
+               *
+               * DEFINE TENANCY TenantA AS <TenantA OCID>
+               * DEFINE GROUP TenantAGroup AS <TenantA user group OCID>
+               * ADMIT GROUP TenantAGroup OF TENANCY TenantA TO {OBJECTSTORAGE_NAMESPACE_READ} IN TENANCY
+               */
+              class Test {}
+              """
+          )
+        );
+    }
 }
