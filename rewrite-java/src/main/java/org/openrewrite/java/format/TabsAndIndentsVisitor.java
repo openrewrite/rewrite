openrewrite/rewrite

commit d7a38b2c1657e9fa05c5a54add11c735baa0f84f

Align method parameters (#1920)
* Added support for alignment of multiline method parameters.

* Added auto-detection for alignment of multiline method params to the first argument.

rewrite-java/src/main/java/org/openrewrite/java/format/TabsAndIndentsVisitor.java
@@ -21,15 +21,11 @@ import org.openrewrite.internal.ListUtils;
 import org.openrewrite.internal.StringUtils;
 import org.openrewrite.internal.lang.Nullable;
 import org.openrewrite.java.JavaIsoVisitor;
-import org.openrewrite.java.JavaVisitor;
-import org.openrewrite.java.JavadocVisitor;
 import org.openrewrite.java.style.TabsAndIndentsStyle;
 import org.openrewrite.java.tree.*;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 public class TabsAndIndentsVisitor<P> extends JavaIsoVisitor<P> {
     @Nullable
@@ -211,19 +207,25 @@ public class TabsAndIndentsVisitor<P> extends JavaIsoVisitor<P> {
                     case METHOD_DECLARATION_PARAMETER: {
                         JContainer<J> container = getCursor().getParentOrThrow().getValue();
                         J firstArg = container.getElements().iterator().next();
-                        if (firstArg.getPrefix().getWhitespace().isEmpty()) {
-                            after = right.getAfter();
-                        } else if (firstArg.getPrefix().getLastWhitespace().contains("\n")) {
-                            // if the first argument is on its own line, align all arguments to be continuation indented
-                            elem = visitAndCast(elem, p);
-                            after = indentTo(right.getAfter(), indent, loc.getAfterLocation());
+                        if (style.getMethodDeclarationParameters().getAlignWhenMultiple()) {
+                            J.MethodDeclaration method = getCursor().firstEnclosing(J.MethodDeclaration.class);
+                            if (method != null) {
+                                int alignTo;
+                                if (firstArg.getPrefix().getLastWhitespace().contains("\n")) {
+                                    alignTo = firstArg.getPrefix().getLastWhitespace().length() - 1;
+                                } else {
+                                    String source = method.print(getCursor());
+                                    alignTo = source.indexOf(firstArg.print(getCursor())) - 1;
+                                }
+                                getCursor().getParentOrThrow().putMessage("lastIndent", alignTo - style.getContinuationIndent());
+                                elem = visitAndCast(elem, p);
+                                after = indentTo(right.getAfter(), alignTo, loc.getAfterLocation());
+                            } else {
+                                after = right.getAfter();
+                            }
                         } else {
-                            // align to first argument when the first argument isn't on its own line
-                            int firstArgIndent = findIndent(firstArg.getPrefix());
-                            getCursor().getParentOrThrow().putMessage("lastIndent", firstArgIndent);
                             elem = visitAndCast(elem, p);
-                            getCursor().getParentOrThrow().putMessage("lastIndent", indent);
-                            after = indentTo(right.getAfter(), firstArgIndent, loc.getAfterLocation());
+                            after = indentTo(right.getAfter(), style.getContinuationIndent(), loc.getAfterLocation());
                         }
                         break;
                     }

rewrite-java/src/main/java/org/openrewrite/java/style/Autodetect.java
@@ -113,6 +113,9 @@ public class Autodetect extends NamedStyles {
         private int linesWithSpaceIndents = 0;
         private int linesWithTabIndents = 0;
 
+        private int multilineAlignedToFirstArgument = 0;
+        private int multilineNotAlignedToFirstArgument = 0;
+
         public boolean isIndentedWithSpaces() {
             return linesWithSpaceIndents >= linesWithTabIndents;
         }
@@ -145,7 +148,9 @@ public class Autodetect extends NamedStyles {
                     useTabs ? indent : 1,
                     useTabs ? 1 : indent,
                     continuationIndent,
-                    false
+                    false,
+                    new TabsAndIndentsStyle.MethodDeclarationParameters(
+                            multilineAlignedToFirstArgument >= multilineNotAlignedToFirstArgument)
             );
         }
     }
@@ -173,6 +178,31 @@ public class Autodetect extends NamedStyles {
     }
 
     private static class FindIndentJavaVisitor extends JavaIsoVisitor<IndentStatistics> {
+        @Override
+        public J.MethodDeclaration visitMethodDeclaration(J.MethodDeclaration method, IndentStatistics stats) {
+            if (method.getParameters().size() > 1) {
+                int alignTo;
+                if (method.getParameters().get(0).getPrefix().getLastWhitespace().contains("\n")) {
+                    // Compare to the prefix of the first arg.
+                    alignTo = method.getParameters().get(0).getPrefix().getLastWhitespace().length() - 1;
+                } else {
+                    String source = method.print(getCursor());
+                    alignTo = source.indexOf(method.getParameters().get(0).print(getCursor())) - 1;
+                }
+                List<Statement> parameters = method.getParameters();
+                for (int i = 1; i < parameters.size(); i++) {
+                    if (parameters.get(i).getPrefix().getLastWhitespace().contains("\n")) {
+                        if (alignTo == parameters.get(i).getPrefix().getLastWhitespace().length()) {
+                            stats.multilineAlignedToFirstArgument++;
+                        } else {
+                            stats.multilineNotAlignedToFirstArgument++;
+                        }
+                    }
+                }
+            }
+            return super.visitMethodDeclaration(method, stats);
+        }
+
         @Override
         public Space visitSpace(Space space, Space.Location loc, IndentStatistics stats) {
             Integer lastIndent = getCursor().getNearestMessage("lastIndent");

rewrite-java/src/main/java/org/openrewrite/java/style/IntelliJ.java
@@ -66,7 +66,8 @@ public class IntelliJ extends NamedStyles {
     }
 
     public static TabsAndIndentsStyle tabsAndIndents() {
-        return new TabsAndIndentsStyle(false, 4, 4, 8, false);
+        return new TabsAndIndentsStyle(false, 4, 4, 8, false,
+                new TabsAndIndentsStyle.MethodDeclarationParameters(true));
     }
 
     public static BlankLinesStyle blankLines() {

rewrite-java/src/main/java/org/openrewrite/java/style/TabsAndIndentsStyle.java
@@ -33,6 +33,14 @@ public class TabsAndIndentsStyle implements JavaStyle {
     Integer continuationIndent;
     Boolean indentsRelativeToExpressionStart;
 
+    MethodDeclarationParameters methodDeclarationParameters;
+
+    @Value
+    @With
+    public static class MethodDeclarationParameters {
+        Boolean alignWhenMultiple;
+    }
+
     @Override
     public Style applyDefaults() {
         return StyleHelper.merge(IntelliJ.tabsAndIndents(), this);

rewrite-java/src/test/kotlin/org/openrewrite/java/style/StyleHelperTest.kt
@@ -23,11 +23,12 @@ class StyleHelperTest {
 
     @Test
     fun mergeTabsAndIndentsStyles() {
-        val merged = StyleHelper.merge(IntelliJ.tabsAndIndents(), TabsAndIndentsStyle(true, 1, 1, 2, true))
+        val merged = StyleHelper.merge(IntelliJ.tabsAndIndents(), TabsAndIndentsStyle(true, 1, 1, 2, true, TabsAndIndentsStyle.MethodDeclarationParameters(true)))
         assertThat(merged.useTabCharacter).isTrue
         assertThat(merged.tabSize).isEqualTo(1)
         assertThat(merged.indentSize).isEqualTo(1)
         assertThat(merged.continuationIndent).isEqualTo(2)
+        assertThat(merged.methodDeclarationParameters.alignWhenMultiple).isTrue
         assertThat(merged.indentsRelativeToExpressionStart).isTrue
     }
 

rewrite-test/src/main/kotlin/org/openrewrite/java/format/TabsAndIndentsTest.kt
@@ -42,6 +42,71 @@ interface TabsAndIndentsTest : JavaRecipeTest {
         )
     )
 
+    @Issue("https://github.com/openrewrite/rewrite/issues/1913")
+    @Test
+    fun alignMethodDeclarationParamsWhenMultiple(jp: JavaParser) = assertChanged(
+        jp,
+        before = """
+            class Test {
+                private void firstArgNoPrefix(String first,
+                                              int times,
+                     String third) {
+                }
+                private void firstArgOnNewLine(
+                        String first,
+                        int times,
+                     String third) {
+                }
+            }
+        """,
+        after = """
+            class Test {
+                private void firstArgNoPrefix(String first,
+                                              int times,
+                                              String third) {
+                }
+                private void firstArgOnNewLine(
+                        String first,
+                        int times,
+                        String third) {
+                }
+            }
+        """
+    )
+
+    @Issue("https://github.com/openrewrite/rewrite/issues/1913")
+    @Test
+    fun alignMethodDeclarationParamsWhenContinuationIndent(jp: JavaParser.Builder<*, *>) = assertChanged(
+        jp.styles(tabsAndIndents { withMethodDeclarationParameters(
+            TabsAndIndentsStyle.MethodDeclarationParameters(false)) }).build(),
+        before = """
+            class Test {
+                private void firstArgNoPrefix(String first,
+                                              int times,
+                                              String third) {
+                }
+                private void firstArgOnNewLine(
+                                               String first,
+                                               int times,
+                                               String third) {
+                }
+            }
+        """,
+        after = """
+            class Test {
+                private void firstArgNoPrefix(String first,
+                        int times,
+                        String third) {
+                }
+                private void firstArgOnNewLine(
+                        String first,
+                        int times,
+                        String third) {
+                }
+            }
+        """
+    )
+
     // https://rules.sonarsource.com/java/tag/confusing/RSPEC-3973
     @Test
     fun rspec3973(jp: JavaParser) = assertChanged(

commit a81258fd35b87ff8b3f8cc8affb408be9e9daa5b

Formatter: Fix method parameter alignment bug
The multi-line method parameter alignment which was introduced in #1920 caused excessive indentation when the declaring method had annotations.

rewrite-java/src/main/java/org/openrewrite/java/format/TabsAndIndentsVisitor.java
@@ -227,7 +227,9 @@ public class TabsAndIndentsVisitor<P> extends JavaIsoVisitor<P> {
                                     alignTo = firstArg.getPrefix().getLastWhitespace().length() - 1;
                                 } else {
                                     String source = method.print(getCursor());
-                                    alignTo = source.indexOf(firstArg.print(getCursor())) - 1;
+                                    int firstArgIndex = source.indexOf(firstArg.print(getCursor()));
+                                    int lineBreakIndex = source.lastIndexOf('\n', firstArgIndex);
+                                    alignTo = (firstArgIndex - (lineBreakIndex == -1 ? 0 : lineBreakIndex)) - 1;
                                 }
                                 getCursor().getParentOrThrow().putMessage("lastIndent", alignTo - style.getContinuationIndent());
                                 elem = visitAndCast(elem, p);

rewrite-java-test/src/test/java/org/openrewrite/java/format/TabsAndIndentsTest.java
@@ -103,9 +103,10 @@ class TabsAndIndentsTest implements RewriteTest {
           java(
             """
               class Test {
+                  @SuppressWarnings
                   private void firstArgNoPrefix(String first,
-                                                int times,
-                                                String third
+                          int times,
+                          String third
                        ) {
                   }
                   private void firstArgOnNewLine(
@@ -118,6 +119,7 @@ class TabsAndIndentsTest implements RewriteTest {
               """,
             """
               class Test {
+                  @SuppressWarnings
                   private void firstArgNoPrefix(String first,
                                                 int times,
                                                 String third