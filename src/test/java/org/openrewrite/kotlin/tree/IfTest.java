abf41983593ce26f5e346f71db085bd5dfc4e6f1
Knut Wannheden <knut@moderne.io>
2023-10-17 14:07:29 +0200

    Issue #298 is not yet fixed

--- src/test/java/org/openrewrite/kotlin/tree/IfTest.java
+++ src/test/java/org/openrewrite/kotlin/tree/IfTest.java
@@ -16,6 +16,7 @@
 package org.openrewrite.kotlin.tree;
 
 import org.junit.jupiter.api.Test;
+import org.junitpioneer.jupiter.ExpectedToFail;
 import org.openrewrite.Issue;
 import org.openrewrite.test.RewriteTest;
 
@@ -154,6 +155,7 @@ class IfTest implements RewriteTest {
         );
     }
 
+    @ExpectedToFail
     @Test
     @Issue("https://github.com/openrewrite/rewrite-kotlin/issues/298")
     void functionCallCondition() {
