d7dbdc70c1fbb50581a8599b13561811f5de3abc
Kun Li <kun@moderne.io>
2023-10-19 16:49:20 -0700

    deprecate Reified marker

--- src/main/java/org/openrewrite/kotlin/internal/KotlinPrinter.java
+++ src/main/java/org/openrewrite/kotlin/internal/KotlinPrinter.java
@@ -1207,8 +1207,6 @@ public class KotlinPrinter<P> extends KotlinVisitor<PrintOutputCapture<P>> {
         public <M extends Marker> M visitMarker(Marker marker, PrintOutputCapture<P> p) {
             if (marker instanceof Semicolon) {
                 p.append(';');
-            } else if (marker instanceof Reified) {
-                p.append("reified");
             } else if (marker instanceof TrailingComma) {
                 // TODO consider adding cursor message to only print for last element in list
                 // TODO the space should then probably be printed anyway (could contain a comment)
--- src/main/java/org/openrewrite/kotlin/marker/Reified.java
+++ src/main/java/org/openrewrite/kotlin/marker/Reified.java
@@ -21,6 +21,7 @@ import org.openrewrite.marker.Marker;
 
 import java.util.UUID;
 
+@Deprecated
 @Value
 @With
 public class Reified implements Marker {
