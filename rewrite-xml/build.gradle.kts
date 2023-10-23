1de3b129f4b3a4536e0b009ed028a7d4a076b2c0
Kushagra Nigam <93257324+TheMarvelFan@users.noreply.github.com>
2023-10-23 13:28:22 +0530

    Restored build.gradle.kts in rewrite-xml

--- rewrite-xml/build.gradle.kts
+++ rewrite-xml/build.gradle.kts
@@ -2,10 +2,6 @@ plugins {
     id("org.openrewrite.build.language-library")
 }
 
-tasks.named<JavaCompile>("compileJava") {
-    options.release.set(8)
-}
-
 tasks.register<JavaExec>("generateAntlrSources") {
     mainClass.set("org.antlr.v4.Tool")
 
@@ -25,29 +21,8 @@ dependencies {
 
     compileOnly(project(":rewrite-test"))
 
-    // import Rewrite's bill of materials.
-    implementation(platform("org.openrewrite.recipe:rewrite-recipe-bom:2.3.1"))
-
-    // rewrite-java dependencies only necessary for Java Recipe development
-    implementation("org.openrewrite:rewrite-java")
-
-    runtimeOnly("org.openrewrite:rewrite-java-8")
-    runtimeOnly("org.openrewrite:rewrite-java-11")
-    runtimeOnly("org.openrewrite:rewrite-java-17")
-
-    implementation("org.openrewrite:rewrite-xml")
-
-    annotationProcessor("org.projectlombok:lombok:latest.release")
-
-    // For authoring tests for any kind of Recipe
-    testImplementation("org.openrewrite:rewrite-test")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:latest.release")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:latest.release")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:latest.release")
-
     implementation("org.antlr:antlr4:4.11.1")
     implementation("io.micrometer:micrometer-core:1.9.+")
 
     testImplementation(project(":rewrite-test"))
     testImplementation(project(":rewrite-maven"))
-}
