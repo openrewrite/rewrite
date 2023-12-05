df5932ebbc25a62168d6678aee7e73a6e7a4fdb6
Tim te Beek <tim@moderne.io>
2023-12-05 11:47:58 +0100

    Detect teamcity build environment (#3762)
    Fixes #3751

--- rewrite-core/src/main/java/org/openrewrite/marker/ci/BitbucketBuildEnvironment.java
+++ rewrite-core/src/main/java/org/openrewrite/marker/ci/BitbucketBuildEnvironment.java
@@ -29,7 +29,7 @@ import static org.openrewrite.Tree.randomId;
 
 @Value
 @EqualsAndHashCode(callSuper = false)
-public class BitbucketBuildEnvironment implements BuildEnvironment{
+public class BitbucketBuildEnvironment implements BuildEnvironment {
     @With
     UUID id;
     String httpOrigin;
@@ -47,8 +47,8 @@ public class BitbucketBuildEnvironment implements BuildEnvironment{
     @Override
     public GitProvenance buildGitProvenance() throws IncompleteGitConfigException {
         if (StringUtils.isBlank(httpOrigin)
-                || StringUtils.isBlank(branch)
-                || StringUtils.isBlank(sha)) {
+            || StringUtils.isBlank(branch)
+            || StringUtils.isBlank(sha)) {
             throw new IncompleteGitConfigException();
         } else {
             return new GitProvenance(UUID.randomUUID(), httpOrigin, branch, sha,
