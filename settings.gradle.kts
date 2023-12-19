d38c9b8e52922425283fa9b4cb305542cd2a8e9a
Knut Wannheden <knut@moderne.io>
2023-12-19 09:32:18 +0100

    Add Gradle Enterprise

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,3 +6,34 @@ pluginManagement {
 }
 
 rootProject.name = "rewrite-kotlin"
+
+plugins {
+    id("com.gradle.enterprise") version "latest.release"
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "latest.release"
+}
+
+gradleEnterprise {
+    val isCiServer = System.getenv("CI")?.equals("true") ?: false
+    server = "https://ge.openrewrite.org/"
+
+    buildCache {
+        remote(HttpBuildCache::class) {
+            url = uri("https://ge.openrewrite.org/cache/")
+            // Check access key presence to avoid build cache errors on PR builds when access key is not present
+            val accessKey = System.getenv("GRADLE_ENTERPRISE_ACCESS_KEY")
+            isPush = isCiServer && !accessKey.isNullOrEmpty()
+        }
+    }
+
+    buildScan {
+        capture {
+            isTaskInputFiles = true
+        }
+
+        isUploadInBackground = !isCiServer
+
+        publishAlways()
+        this as com.gradle.enterprise.gradleplugin.internal.extension.BuildScanExtensionWithHiddenFeatures
+        publishIfAuthenticated()
+    }
+}
