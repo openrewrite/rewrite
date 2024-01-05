f706de431bfae116e5536f902b7e76f023766515
Knut Wannheden <tim@moderne.io>
2024-01-05 17:45:01 +0100

    Set copyright year to 2024

--- rewrite-java/src/main/java/org/openrewrite/java/RemoveAnnotationAttribute.java
+++ rewrite-java/src/main/java/org/openrewrite/java/RemoveAnnotationAttribute.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 the original author or authors.
+ * Copyright 2024 the original author or authors.
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,7 +27,6 @@ import java.util.concurrent.atomic.AtomicBoolean;
 @Value
 @EqualsAndHashCode(callSuper = true)
 public class RemoveAnnotationAttribute extends Recipe {
-
 
     @Option(displayName = "Annotation Type",
             description = "The fully qualified name of the annotation.",
