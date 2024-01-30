b822f87a63999aad5dc2ca537e3cefa01bb066b5
Tim te Beek <timtebeek@gmail.com>
2024-01-30 15:33:38 +0100

    Update rewrite-java/src/main/java/org/openrewrite/java/tree/TypeUtils.java

--- rewrite-java/src/main/java/org/openrewrite/java/tree/TypeUtils.java
+++ rewrite-java/src/main/java/org/openrewrite/java/tree/TypeUtils.java
@@ -39,7 +39,7 @@ public class TypeUtils {
     }
 
     public static String toFullyQualifiedName(String fqn) {
-        /// replace every $ that is not prefixed by a dot in an iterative way to handle inner classes wich name $
+        // replace every $ that is not prefixed by a dot in an iterative way to handle inner classes whose name is $
         while (true) {
             String nxt = fqn.replaceFirst("(?<!\\.)\\$", ".");
             if (nxt.equals(fqn)) break;
