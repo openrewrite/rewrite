919a4ead1b5dff7543bc24d715f243afbd8c3464
Dawn James <dawnj@spotify.com>
2024-04-29 11:33:02 +0100

    Format code via IntelliJ IDEA.

--- rewrite-maven/src/main/java/org/openrewrite/maven/ChangePluginGroupIdAndArtifactId.java
+++ rewrite-maven/src/main/java/org/openrewrite/maven/ChangePluginGroupIdAndArtifactId.java
@@ -32,37 +32,38 @@ import java.util.Optional;
 @Value
 @EqualsAndHashCode(callSuper = false)
 public class ChangePluginGroupIdAndArtifactId extends Recipe {
+
     @EqualsAndHashCode.Exclude
     MavenMetadataFailures metadataFailures = new MavenMetadataFailures(this);
 
     @Option(displayName = "Old group ID",
-            description = "The old group ID to replace. The group ID is the first part of a plugin coordinate 'com.google.guava:guava:VERSION'. Supports glob expressions.",
-            example = "org.openrewrite.recipe")
+        description = "The old group ID to replace. The group ID is the first part of a plugin coordinate 'com.google.guava:guava:VERSION'. Supports glob expressions.",
+        example = "org.openrewrite.recipe")
     String oldGroupId;
 
     @Option(displayName = "Old artifact ID",
-            description = "The old artifactId to replace. The artifact ID is the second part of a plugin coordinate 'com.google.guava:guava:VERSION'. Supports glob expressions.",
-            example = "my-deprecated-maven-plugin")
+        description = "The old artifactId to replace. The artifact ID is the second part of a plugin coordinate 'com.google.guava:guava:VERSION'. Supports glob expressions.",
+        example = "my-deprecated-maven-plugin")
     String oldArtifactId;
 
     @Option(displayName = "New group ID",
-            description = "The new group ID to use. Defaults to the existing group ID.",
-            example = "corp.internal.openrewrite.recipe",
-            required = false)
+        description = "The new group ID to use. Defaults to the existing group ID.",
+        example = "corp.internal.openrewrite.recipe",
+        required = false)
     @Nullable
     String newGroupId;
 
     @Option(displayName = "New artifact ID",
-            description = "The new artifact ID to use. Defaults to the existing artifact ID.",
-            example = "my-new-maven-plugin",
-            required = false)
+        description = "The new artifact ID to use. Defaults to the existing artifact ID.",
+        example = "my-new-maven-plugin",
+        required = false)
     @Nullable
     String newArtifact;
 
     @Option(displayName = "Look everywhere",
-            description = "If true, will look for plugin tags everywhere, including pluginManagement in and out of profiles. Default `false`.",
-            example = "my-new-maven-plugin",
-            required = false)
+        description = "If true, will look for plugin tags everywhere, including pluginManagement in and out of profiles. Default `false`.",
+        example = "my-new-maven-plugin",
+        required = false)
     @Nullable
     Boolean lookEverywhere;
 
@@ -86,7 +87,8 @@ public class ChangePluginGroupIdAndArtifactId extends Recipe {
 
         return new MavenVisitor<ExecutionContext>() {
 
-            final BiFunction<String, String, Boolean> isPluginTag = Boolean.TRUE.equals(lookEverywhere) ? this::isAnyPluginTag : this::isPluginTag;
+            final BiFunction<String, String, Boolean> isPluginTag =
+                Boolean.TRUE.equals(lookEverywhere) ? this::isAnyPluginTag : this::isPluginTag;
 
             @Override
             public Xml visitTag(Xml.Tag tag, ExecutionContext ctx) {
@@ -106,10 +108,13 @@ public class ChangePluginGroupIdAndArtifactId extends Recipe {
                 return t;
             }
 
-            private Xml.Tag changeChildTagValue(Xml.Tag tag, String childTagName, String newValue, ExecutionContext ctx) {
+            private Xml.Tag changeChildTagValue(Xml.Tag tag, String childTagName, String newValue,
+                ExecutionContext ctx) {
                 Optional<Xml.Tag> childTag = tag.getChild(childTagName);
-                if (childTag.isPresent() && !newValue.equals(childTag.get().getValue().orElse(null))) {
-                    tag = (Xml.Tag) new ChangeTagValueVisitor<>(childTag.get(), newValue).visitNonNull(tag, ctx);
+                if (childTag.isPresent() && !newValue.equals(
+                    childTag.get().getValue().orElse(null))) {
+                    tag = (Xml.Tag) new ChangeTagValueVisitor<>(childTag.get(),
+                        newValue).visitNonNull(tag, ctx);
                 }
                 return tag;
             }
