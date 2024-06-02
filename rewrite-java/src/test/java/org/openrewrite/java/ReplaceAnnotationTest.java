c2e1279de34505d4835c260ea22e56a0bc0007c5
Jonathan Schneider <jkschneider@gmail.com>
2024-06-01 22:41:47 -0400

    Fix ReplaceAnnotationTest

--- rewrite-java/src/test/java/org/openrewrite/java/ReplaceAnnotationTest.java
+++ rewrite-java/src/test/java/org/openrewrite/java/ReplaceAnnotationTest.java
@@ -18,16 +18,11 @@ package org.openrewrite.java;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.openrewrite.DocumentExample;
-import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
 
 import static org.openrewrite.java.Assertions.java;
 
 class ReplaceAnnotationTest implements RewriteTest {
-    @Override
-    public void defaults(RecipeSpec spec) {
-        spec.parser(JavaParser.fromJavaVersion().classpath("lombok", "annotations"));
-    }
 
     @Nested
     class OnMatch {
@@ -35,21 +30,25 @@ class ReplaceAnnotationTest implements RewriteTest {
         void matchNoPrams() {
             rewriteRun(
               spec -> spec.recipe(new ReplaceAnnotation("@org.jetbrains.annotations.NotNull", "@lombok.NonNull", null)),
-              java("""
-                import org.jetbrains.annotations.NotNull;
-                
-                class A {
-                    @NotNull
-                    String testMethod() {}
-                }
-                """, """
-                import lombok.NonNull;
-                
-                class A {
-                    @NonNull
-                    String testMethod() {}
-                }
-                """));
+              java(
+                """
+                  import org.jetbrains.annotations.NotNull;
+
+                  class A {
+                      @NotNull
+                      String testMethod() {}
+                  }
+                  """,
+                  """
+                  import lombok.NonNull;
+
+                  class A {
+                      @NonNull
+                      String testMethod() {}
+                  }
+                  """
+              )
+            );
         }
 
         @Test
@@ -57,42 +56,50 @@ class ReplaceAnnotationTest implements RewriteTest {
         void matchWithPrams() {
             rewriteRun(
               spec -> spec.recipe(new ReplaceAnnotation("@org.jetbrains.annotations.NotNull(\"Test\")", "@lombok.NonNull", null)),
-              java("""
-                import org.jetbrains.annotations.NotNull;
-                
-                class A {
-                    @NotNull("Test")
-                    String testMethod() {}
-                }
-                """, """
-                import lombok.NonNull;
-                
-                class A {
-                    @NonNull
-                    String testMethod() {}
-                }
-                """));
+              java(
+                """
+                  import org.jetbrains.annotations.NotNull;
+
+                  class A {
+                      @NotNull("Test")
+                      String testMethod() {}
+                  }
+                  """,
+                  """
+                  import lombok.NonNull;
+
+                  class A {
+                      @NonNull
+                      String testMethod() {}
+                  }
+                  """
+              )
+            );
         }
 
         @Test
         void insertWithParams() {
             rewriteRun(
               spec -> spec.recipe(new ReplaceAnnotation("@lombok.NonNull", "@org.jetbrains.annotations.NotNull(\"Test\")", null)),
-              java("""
-                import lombok.NonNull;
-                
-                class A {
-                    @NonNull
-                    String testMethod() {}
-                }
-                """, """
-                import org.jetbrains.annotations.NotNull;
-                
-                class A {
-                    @NotNull("Test")
-                    String testMethod() {}
-                }
-                """));
+              java(
+                """
+                  import lombok.NonNull;
+
+                  class A {
+                      @NonNull
+                      String testMethod() {}
+                  }
+                  """,
+                """
+                  import org.jetbrains.annotations.NotNull;
+
+                  class A {
+                      @NotNull("Test")
+                      String testMethod() {}
+                  }
+                  """
+              )
+            );
         }
     }
 
@@ -102,28 +109,33 @@ class ReplaceAnnotationTest implements RewriteTest {
         void noMatchOtherType() {
             rewriteRun(
               spec -> spec.recipe(new ReplaceAnnotation("@org.jetbrains.annotations.NotNull", "@lombok.NonNull", null)),
-              java("""
-                import org.jetbrains.annotations.Nullable;
-                
-                class A {
-                    @Nullable("Test")
-                    String testMethod() {}
-                }
-                """));
+              java(
+                """
+                  import org.jetbrains.annotations.Nullable;
+
+                  class A {
+                      @Nullable("Test")
+                      String testMethod() {}
+                  }
+                  """
+              )
+            );
         }
 
         @Test
         void noMatchParameter() {
             rewriteRun(
               spec -> spec.recipe(new ReplaceAnnotation("@org.jetbrains.annotations.NotNull(\"Test\")", "@lombok.NonNull", null)),
-              java("""
-                import org.jetbrains.annotations.Nullable;
-                
-                class A {
-                    @Nullable("Other")
-                    String testMethod() {}
-                }
-                """)
+              java(
+                """
+                  import org.jetbrains.annotations.Nullable;
+
+                  class A {
+                      @Nullable("Other")
+                      String testMethod() {}
+                  }
+                  """
+              )
             );
         }
     }
