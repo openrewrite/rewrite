bc6b3e8f4c1da5a8eeb727ba873d67f2540042aa
Andreas Skoog <andreas.skoog@avanza.se>
2024-06-27 14:02:21 +0200

    Preserve multiple dependencyManagement with different types
    It is a valid case to specify different dependencyManagement dependencies for the same artifact but with different types (used for example if you create a test-jar of a dependency and manage its version).
    
    Find managedDependency() did not take type into account

--- rewrite-maven/src/main/java/org/openrewrite/maven/MavenVisitor.java
+++ rewrite-maven/src/main/java/org/openrewrite/maven/MavenVisitor.java
@@ -270,8 +270,9 @@ public class MavenVisitor<P> extends XmlVisitor<P> {
                 .orElse(getResolutionResult().getPom().getGroupId()));
         String artifactId = getResolutionResult().getPom().getValue(tag.getChildValue("artifactId").orElse(""));
         String classifier = getResolutionResult().getPom().getValue(tag.getChildValue("classifier").orElse(null));
+        String type = getResolutionResult().getPom().getValue(tag.getChildValue("type").orElse(null));
         if (groupId != null && artifactId != null) {
-            return findManagedDependency(groupId, artifactId, classifier);
+            return findManagedDependency(groupId, artifactId, classifier, type);
         }
         return null;
     }
@@ -283,10 +284,16 @@ public class MavenVisitor<P> extends XmlVisitor<P> {
 
     @Nullable
     private ResolvedManagedDependency findManagedDependency(String groupId, String artifactId, @Nullable String classifier) {
+        return findManagedDependency(groupId, artifactId, classifier, null);
+    }
+
+    @Nullable
+    private ResolvedManagedDependency findManagedDependency(String groupId, String artifactId, @Nullable String classifier, @Nullable String type) {
         for (ResolvedManagedDependency d : getResolutionResult().getPom().getDependencyManagement()) {
             if (groupId.equals(d.getGroupId()) &&
                 artifactId.equals(d.getArtifactId()) &&
-                (classifier == null || classifier.equals(d.getClassifier()))) {
+                (classifier == null || classifier.equals(d.getClassifier())) &&
+                (type == null || type.equals(d.getType()))) {
                 return d;
             }
         }
