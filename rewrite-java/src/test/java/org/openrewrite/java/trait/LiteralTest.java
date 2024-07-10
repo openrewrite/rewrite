5562f24ccb9d4d192f7055e7f77ed400acabc6d3
Jonathan Schnéider <jkschneider@gmail.com>
2024-07-10 13:30:55 -0400

    Update rewrite-java/src/test/java/org/openrewrite/java/trait/LiteralTest.java
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- rewrite-java/src/test/java/org/openrewrite/java/trait/LiteralTest.java
+++ rewrite-java/src/test/java/org/openrewrite/java/trait/LiteralTest.java
@@ -26,7 +26,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.openrewrite.java.Assertions.java;
 import static org.openrewrite.java.trait.Traits.literal;
 
-public class LiteralTest implements RewriteTest {
+class LiteralTest implements RewriteTest {
 
     @Test
     void numericLiteral() {
