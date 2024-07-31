0ed38dc6c5759fab604afd322f9eb5aa1e191019
Sam Snyder <sam@moderne.io>
2024-07-31 11:47:20 -0700

    Link JsonPath parameter descriptions to our documentation

--- rewrite-yaml/src/main/java/org/openrewrite/yaml/MergeYaml.java
+++ rewrite-yaml/src/main/java/org/openrewrite/yaml/MergeYaml.java
@@ -27,7 +27,7 @@ import org.openrewrite.yaml.tree.Yaml;
 @EqualsAndHashCode(callSuper = false)
 public class MergeYaml extends Recipe {
     @Option(displayName = "Key path",
-            description = "A [JsonPath](https://github.com/json-path/JsonPath) expression used to find matching keys.",
+            description = "A [JsonPath](https://docs.openrewrite.org/reference/jsonpath-and-jsonpathmatcher-reference) expression used to find matching keys.",
             example = "$.metadata")
     String key;
 
