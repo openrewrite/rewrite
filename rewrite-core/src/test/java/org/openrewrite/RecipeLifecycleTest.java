71e1425339ff486e542b4c2eb2e709e45fbba10d
Tim te Beek <tim@moderne.io>
2024-08-04 01:58:48 +0200

    Use star import for Assertions

--- rewrite-core/src/test/java/org/openrewrite/RecipeLifecycleTest.java
+++ rewrite-core/src/test/java/org/openrewrite/RecipeLifecycleTest.java
@@ -41,9 +41,7 @@ import java.util.Properties;
 import java.util.UUID;
 
 import static java.util.Objects.requireNonNull;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.fail;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.*;
 import static org.openrewrite.Recipe.noop;
 import static org.openrewrite.test.RewriteTest.toRecipe;
 import static org.openrewrite.test.SourceSpecs.text;
