7bb2cebec8ea0ca53d3e7fb73242afcf2e269734
Samuel Cox <sacox@paypal.com>
2024-08-05 08:00:50 -0500

    [4382] Add test showing issue with ChangeStaticFieldToMethod recipe..
    and nested code.
    
     #4382

--- rewrite-java-test/src/test/java/org/openrewrite/java/ChangeStaticFieldToMethodTest.java
+++ rewrite-java-test/src/test/java/org/openrewrite/java/ChangeStaticFieldToMethodTest.java
@@ -166,6 +166,34 @@ class ChangeStaticFieldToMethodTest implements RewriteTest {
     }
 
     @Test
+    @Issue("https://github.com/openrewrite/rewrite/issues/4382")
+    void migratesNestedFieldInitializer() {
+        rewriteRun(
+          java(acmeLists),
+          java(
+            """
+              import java.util.Collections;
+
+              class Foo {
+                  void bar() {
+                      final Object collection = java.util.Arrays.asList(Collections.EMPTY_LIST);
+                  }
+              }
+              """,
+            """
+              import com.acme.Lists;
+
+              class Foo {
+                  void bar() {
+                      final Object collection = java.util.Arrays.asList(Lists.of());
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
     void ignoresUnrelatedFields() {
         rewriteRun(
           java(
