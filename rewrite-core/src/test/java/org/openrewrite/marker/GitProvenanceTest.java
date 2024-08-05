a0314ce7dbcd3a3848b50f167bfbd2aec7ef7519
Peter Streef <peter@moderne.io>
2024-08-05 20:24:15 +0200

    feat: Add `GitRemote` detection logic (#4381)
    * feat: Determine SCM in GitProvenance for more accurate path estimation
    
    Determine the SCM from a list of known urls and any manually registered SCMs. The SCM is then used to determine ScmUrlComponents which in turn give information about a repository.
    
    * Apply suggestions from code review
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * rename gitlab scm
    
    * Comments
    
    * fix tests
    
    * fix boolean
    
    * fix git provenance tests
    
    * Rename to `ScmUrlComponents` to `CloneUrl` and move logic into that
    
    * Add full cloneUrl
    
    * fully parse on construction
    
    * Update rewrite-core/src/test/java/org/openrewrite/scm/GitLabScmTest.java
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update rewrite-core/src/main/java/org/openrewrite/scm/AzureDevopsCloneUrl.java
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Add missing newlines at the end of files
    
    * Add missing newline at the end of GitLabScmTest
    
    * refactor: Azure devops refactoring (#4378)
    
    * refactor: Azure DevOps refactoring.
    
    * review changes
    
    * review changes
    
    ---------
    
    Co-authored-by: Peter Streef <p.streef@gmail.com>
    
    * new line?
    
    * Lazy initialization
    
    * Lombok compiler issue
    
    * Update rewrite-core/src/main/java/org/openrewrite/internal/Lazy.java
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Replace Lazy with `@NonFinal` field
    
    * fix message, use getters
    
    * feat: Add `GitRemote` detection logic
    
    * Cleanup and add new tests
    
    * update comment
    
    * Don't try to fix the `getOrganizationName(String baseUrl)` to avoid unexpected results (not using the baseUrl)
    
    * replace `Optional`
    
    * Simplify
    
    * Simplify
    
    * remove deprecation and fix whitespace
    
    * pass null and let `#fromProjectDirectory(Path, BuildEnvironment, GitRemote.Parser)` init instead
    
    * add `getRepositoryPath()` instance method
    
    * do not update to unknown
    
    * no nullable
    
    * extract `GitRemote`
    
    * license
    
    * review comments
    
    * add url to message
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Tim te Beek <tim@moderne.io>
    Co-authored-by: bryceatmoderne <167870666+bryceatmoderne@users.noreply.github.com>

--- rewrite-core/src/test/java/org/openrewrite/marker/GitProvenanceTest.java
+++ rewrite-core/src/test/java/org/openrewrite/marker/GitProvenanceTest.java
@@ -19,8 +19,10 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.openrewrite.GitRemote;
 import org.openrewrite.jgit.api.Git;
 import org.openrewrite.jgit.api.errors.GitAPIException;
 import org.openrewrite.jgit.lib.Constants;
@@ -38,6 +40,7 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
@@ -51,40 +54,63 @@ import static org.openrewrite.jgit.lib.ConfigConstants.CONFIG_BRANCH_SECTION;
 @SuppressWarnings({"ConstantConditions", "HttpUrlsUsage"})
 class GitProvenanceTest {
 
-    private static Stream<String> remotes() {
+    private static Stream<Arguments> remotes() {
         return Stream.of(
-          "ssh://git@github.com/openrewrite/rewrite.git",
-          "https://github.com/openrewrite/rewrite.git",
-          "file:///openrewrite/rewrite.git",
-          "http://localhost:7990/scm/openrewrite/rewrite.git",
-          "http://localhost:7990/scm/some/openrewrite/rewrite.git",
-          "git@github.com:openrewrite/rewrite.git",
-          "org-12345678@github.com:openrewrite/rewrite.git"
+          Arguments.of("ssh://git@github.com/openrewrite/rewrite.git", "openrewrite", "rewrite"),
+          Arguments.of("https://github.com/openrewrite/rewrite.git", "openrewrite", "rewrite"),
+          Arguments.of("file:///openrewrite/rewrite.git", "openrewrite", "rewrite"),
+          Arguments.of("http://localhost:7990/scm/openrewrite/rewrite.git", "openrewrite", "rewrite"),
+          Arguments.of("http://localhost:7990/scm/some/openrewrite/rewrite.git", "openrewrite", "rewrite"),
+          Arguments.of("git@github.com:openrewrite/rewrite.git", "openrewrite", "rewrite"),
+          Arguments.of("org-12345678@github.com:openrewrite/rewrite.git", "openrewrite", "rewrite"),
+          Arguments.of("https://dev.azure.com/openrewrite/rewrite/_git/rewrite", "openrewrite/rewrite", "rewrite"),
+          Arguments.of("https://openrewrite@dev.azure.com/openrewrite/rewrite/_git/rewrite",
+            "openrewrite/rewrite",
+            "rewrite"),
+          Arguments.of("git@ssh.dev.azure.com:v3/openrewrite/rewrite/rewrite", "openrewrite/rewrite", "rewrite"),
+          Arguments.of("ssh://git@ssh.dev.azure.com/v3/openrewrite/rewrite/rewrite", "openrewrite/rewrite", "rewrite")
         );
     }
 
-    @SuppressWarnings("deprecation")
     @ParameterizedTest
     @MethodSource("remotes")
-    void getOrganizationName(String remote) {
-        assertThat(new GitProvenance(randomId(), remote, "main", "123", null, null, emptyList()).getOrganizationName())
-          .isEqualTo("openrewrite");
+    void getRepositoryPath(String origin, String expectedOrg, String expectedRepo) {
+        GitProvenance gitProvenance = new GitProvenance(randomId(), origin, "main", "123", null, null, List.of());
+        assertThat(gitProvenance.getOrganizationName()).isEqualTo(expectedOrg);
+        assertThat(gitProvenance.getRepositoryName()).isEqualTo(expectedRepo);
+        String expectedPath = expectedOrg + '/' + expectedRepo;
+        assertThat(gitProvenance.getRepositoryPath()).isEqualTo(expectedPath);
+    }
+
+    @ParameterizedTest
+    @MethodSource("remotes")
+    void getRepositoryOrigin(String origin, String expectedOrg, String expectedRepo) {
+        GitProvenance gitProvenance = new GitProvenance(randomId(), origin, "main", "123", null, null, List.of());
+        assertThat(gitProvenance.getOrganizationName()).isEqualTo(expectedOrg);
+        assertThat(gitProvenance.getRepositoryName()).isEqualTo(expectedRepo);
+        assertThat(gitProvenance.getOrigin()).isEqualTo(origin);
     }
 
     @ParameterizedTest
     @CsvSource({
-      "git@gitlab.acme.com:organization/subgroup/repository.git, https://gitlab.acme.com, organization/subgroup",
-      "git@gitlab.acme.com:organization/subgroup/repository.git, git@gitlab.acme.com, organization/subgroup"
+      "git@gitlab.acme.com:organization/subgroup/repository.git, https://gitlab.acme.com, GitLab, organization/subgroup",
+      "git@gitlab.acme.com:organization/subgroup/repository.git, git@gitlab.acme.com, GitLab, organization/subgroup",
+      "git@gitlab.acme.com:organization/subgroup/repository.git, git@gitlab.acme.com, GitLab, organization/subgroup",
+      "https://dev.azure.com/organization/project/_git/repository, https://dev.azure.com, AzureDevOps, organization/project",
+      "https://organization@dev.azure.com/organization/project/_git/repository, https://dev.azure.com, AzureDevOps, organization/project",
+      "git@ssh.dev.azure.com:v3/organization/project/repository, git@ssh.dev.azure.com, AzureDevOps, organization/project"
     })
-    void getOrganizationNameWithBaseUrl(String gitOrigin, String baseUrl, String organizationName) {
-        assertThat(new GitProvenance(randomId(), gitOrigin, "main", "123", null, null, emptyList()).getOrganizationName(baseUrl))
+    void getOrganizationName(String gitOrigin, String baseUrl, GitRemote.Service service, String organizationName) {
+        GitRemote.Parser parser = new GitRemote.Parser();
+        parser.registerRemote(service, baseUrl);
+        assertThat(new GitProvenance(randomId(), gitOrigin, "main", "123", null, null, emptyList(), parser.parse(gitOrigin)).getOrganizationName())
           .isEqualTo(organizationName);
     }
 
     @ParameterizedTest
     @MethodSource("remotes")
     void getRepositoryName(String remote) {
-        assertThat(new GitProvenance(randomId(), remote, "main", "123", null, null, emptyList()).getRepositoryName())
+        assertThat(new GitProvenance(randomId(), remote, "main", "123", null, null, emptyList(), null).getRepositoryName())
           .isEqualTo("rewrite");
     }
 
@@ -196,6 +222,7 @@ class GitProvenanceTest {
         );
     }
 
+    @SuppressWarnings("deprecation")
     @ParameterizedTest
     @MethodSource("baseUrls")
     void multiplePathSegments(String baseUrl) {
@@ -205,7 +232,8 @@ class GitProvenanceTest {
           "1234567890abcdef1234567890abcdef12345678",
           null,
           null,
-          emptyList());
+          emptyList(),
+          null);
 
         assertThat(provenance.getOrganizationName(baseUrl)).isEqualTo("group/subgroup1/subgroup2");
         assertThat(provenance.getRepositoryName()).isEqualTo("repo");
