2726a54cd456b2e93274b13b5df962398471c02c
Tim te Beek <tim@moderne.io>
2024-08-05 18:09:32 +0200

    Fix indentation issues

--- rewrite-maven/src/test/java/org/openrewrite/maven/AddDependencyTest.java
+++ rewrite-maven/src/test/java/org/openrewrite/maven/AddDependencyTest.java
@@ -399,19 +399,19 @@ class AddDependencyTest implements RewriteTest {
             ),
             pomXml(
               """
-                    <project>
-                        <groupId>com.mycompany.app</groupId>
-                        <artifactId>my-app</artifactId>
-                        <version>1</version>
-                        <dependencies>
-                            <dependency>
-                                <groupId>org.junit.jupiter</groupId>
-                                <artifactId>junit-jupiter-engine</artifactId>
-                                <version>5.7.1</version>
-                                <scope>test</scope>
-                            </dependency>
-                        </dependencies>
-                    </project>
+                <project>
+                    <groupId>com.mycompany.app</groupId>
+                    <artifactId>my-app</artifactId>
+                    <version>1</version>
+                    <dependencies>
+                        <dependency>
+                            <groupId>org.junit.jupiter</groupId>
+                            <artifactId>junit-jupiter-engine</artifactId>
+                            <version>5.7.1</version>
+                            <scope>test</scope>
+                        </dependency>
+                    </dependencies>
+                </project>
                 """
             )
           )
@@ -446,39 +446,39 @@ class AddDependencyTest implements RewriteTest {
             ),
             pomXml(
               """
-                    <project>
-                        <groupId>com.mycompany.app</groupId>
-                        <artifactId>my-app</artifactId>
-                        <version>1</version>
-                        <dependencies>
-                            <dependency>
-                                <groupId>org.junit.jupiter</groupId>
-                                <artifactId>junit-jupiter-engine</artifactId>
-                                <version>5.7.1</version>
-                                <scope>test</scope>
-                            </dependency>
-                        </dependencies>
-                    </project>
+                <project>
+                    <groupId>com.mycompany.app</groupId>
+                    <artifactId>my-app</artifactId>
+                    <version>1</version>
+                    <dependencies>
+                        <dependency>
+                            <groupId>org.junit.jupiter</groupId>
+                            <artifactId>junit-jupiter-engine</artifactId>
+                            <version>5.7.1</version>
+                            <scope>test</scope>
+                        </dependency>
+                    </dependencies>
+                </project>
                 """,
               """
-                    <project>
-                        <groupId>com.mycompany.app</groupId>
-                        <artifactId>my-app</artifactId>
-                        <version>1</version>
-                        <dependencies>
-                            <dependency>
-                                <groupId>org.junit.jupiter</groupId>
-                                <artifactId>junit-jupiter-api</artifactId>
-                                <version>5.10.3</version>
-                            </dependency>
-                            <dependency>
-                                <groupId>org.junit.jupiter</groupId>
-                                <artifactId>junit-jupiter-engine</artifactId>
-                                <version>5.7.1</version>
-                                <scope>test</scope>
-                            </dependency>
-                        </dependencies>
-                    </project>
+                <project>
+                    <groupId>com.mycompany.app</groupId>
+                    <artifactId>my-app</artifactId>
+                    <version>1</version>
+                    <dependencies>
+                        <dependency>
+                            <groupId>org.junit.jupiter</groupId>
+                            <artifactId>junit-jupiter-api</artifactId>
+                            <version>5.10.3</version>
+                        </dependency>
+                        <dependency>
+                            <groupId>org.junit.jupiter</groupId>
+                            <artifactId>junit-jupiter-engine</artifactId>
+                            <version>5.7.1</version>
+                            <scope>test</scope>
+                        </dependency>
+                    </dependencies>
+                </project>
                 """
             )
           )
@@ -1148,35 +1148,35 @@ class AddDependencyTest implements RewriteTest {
                   <groupId>org.springframework.samples</groupId>
                   <artifactId>spring-petclinic</artifactId>
                   <version>2.7.3</version>
-                                
+
                   <parent>
                     <groupId>org.springframework.boot</groupId>
                     <artifactId>spring-boot-starter-parent</artifactId>
                     <version>3.0.5</version>
                   </parent>
                   <name>petclinic</name>
-                                
+
                   <properties>
                     <jakarta-servlet.version>5.0.0</jakarta-servlet.version>
-                                
+
                     <java.version>17</java.version>
                     <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
                     <project.reporting.outputEncoding>UTF-8</project.reporting.outputEncoding>
-                                
+
                     <webjars-bootstrap.version>5.1.3</webjars-bootstrap.version>
                     <webjars-font-awesome.version>4.7.0</webjars-font-awesome.version>
-                                
+
                     <jacoco.version>0.8.8</jacoco.version>
-                                
+
                   </properties>
-                                
+
                   <dependencies>
                     <dependency>
                       <groupId>org.springframework.boot</groupId>
                       <artifactId>spring-boot-starter-data-jpa</artifactId>
                     </dependency>
                   </dependencies>
-                                
+
                 </project>
                 """,
               """
@@ -1185,28 +1185,28 @@ class AddDependencyTest implements RewriteTest {
                   <groupId>org.springframework.samples</groupId>
                   <artifactId>spring-petclinic</artifactId>
                   <version>2.7.3</version>
-                                
+
                   <parent>
                     <groupId>org.springframework.boot</groupId>
                     <artifactId>spring-boot-starter-parent</artifactId>
                     <version>3.0.5</version>
                   </parent>
                   <name>petclinic</name>
-                                
+
                   <properties>
                     <jakarta-servlet.version>5.0.0</jakarta-servlet.version>
-                                
+
                     <java.version>17</java.version>
                     <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
                     <project.reporting.outputEncoding>UTF-8</project.reporting.outputEncoding>
-                                
+
                     <webjars-bootstrap.version>5.1.3</webjars-bootstrap.version>
                     <webjars-font-awesome.version>4.7.0</webjars-font-awesome.version>
-                                
+
                     <jacoco.version>0.8.8</jacoco.version>
-                                
+
                   </properties>
-                                
+
                   <dependencies>
                     <dependency>
                       <groupId>jakarta.xml.bind</groupId>
@@ -1217,7 +1217,7 @@ class AddDependencyTest implements RewriteTest {
                       <artifactId>spring-boot-starter-data-jpa</artifactId>
                     </dependency>
                   </dependencies>
-                                
+
                 </project>
                 """
             )
