750f38ba3faca69839bd2aa459b62963556dadc8
Scott Jungling <scott@moderne.io>
2024-08-12 14:39:14 -0700

    chore: add suppressions for owasp dependency check
    add initial set that were previously reported / false positives
    
    re: https://github.com/moderneinc/dependency-vulnerability-reports/issues/709

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,17 @@
 plugins {
     id("org.openrewrite.build.root") version("latest.release")
     id("org.openrewrite.build.java-base") version("latest.release")
+    id("org.owasp.dependencycheck") version("latest.release")
+}
+
+configure<org.owasp.dependencycheck.gradle.extension.DependencyCheckExtension> {
+    analyzers.assemblyEnabled = false
+    analyzers.nodeAuditEnabled = false
+    analyzers.nodeEnabled = false
+    failBuildOnCVSS = System.getenv("FAIL_BUILD_ON_CVSS")?.toFloatOrNull() ?: 9.0F
+    format = System.getenv("DEPENDENCY_CHECK_FORMAT") ?: "HTML"
+    nvd.apiKey = System.getenv("NVD_API_KEY")
+    suppressionFile = "suppressions.xml"
 }
 
 repositories {
