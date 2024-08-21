dc09e6c9d495afa3b4bf8e70d7ef0a397bb58645
Laurens Westerlaken <laurens.westerlaken@jdriven.com>
2024-08-21 16:45:41 +0200

    Refactor tests after clarifying actual problem

--- rewrite-maven/src/test/java/org/openrewrite/maven/AddDependencyTest.java
+++ rewrite-maven/src/test/java/org/openrewrite/maven/AddDependencyTest.java
@@ -1285,7 +1285,7 @@ class AddDependencyTest implements RewriteTest {
     }
 
     @Test
-    void childThatOverridesParentScopeShouldDropScope() {
+    void dependencyThatIsTransitivelyProvidedWithWrongScopeShouldBeAdded() {
         rewriteRun(
           spec -> spec
             .parser(JavaParser.fromJavaVersion()
@@ -1365,6 +1365,97 @@ class AddDependencyTest implements RewriteTest {
                           <relativePath>../pom.xml</relativePath>
                       </parent>
                       <artifactId>child</artifactId>
+                      <dependencies>
+                          <dependency>
+                              <groupId>org.checkerframework</groupId>
+                              <artifactId>checker-qual</artifactId>
+                              <version>3.44.0</version>
+                          </dependency>
+                          <dependency>
+                              <groupId>com.google.guava</groupId>
+                              <artifactId>guava</artifactId>
+                              <version>31.1-jre</version>
+                              <scope>test</scope>
+                          </dependency>
+                      </dependencies>
+                  </project>
+                  """
+              )
+            )
+          )
+        );
+    }
+
+    @Test
+    void dependencyThatIsTransitivelyProvidedWithCorrectScopeShouldNotBeAdded() {
+        rewriteRun(
+          spec -> spec
+            .parser(JavaParser.fromJavaVersion()
+              .dependsOn(
+                """
+                  package main.java.checkerframework.checker.nullness.qual;
+                  public @interface NonNull {}
+                  """
+              )
+            )
+            .recipes(
+              new AddDependency("org.checkerframework", "checker-qual", "3.44.0",
+                null, null, null, "main.java.checkerframework..*", null, null, null, null,
+                true),
+              new ChangePackage("main.java.checkerframework", "org.checkerframework", true)
+            ),
+          mavenProject("parent",
+            pomXml(
+              """
+                <project>
+                    <groupId>com.mycompany.app</groupId>
+                    <artifactId>parent</artifactId>
+                    <version>1</version>
+                    <modules>
+                        <module>child</module>
+                    </modules>
+                    <dependencies>
+                        <!-- Has transitive dependency on org.checkerframework:checker-qual -->
+                        <dependency>
+                            <groupId>com.google.guava</groupId>
+                            <artifactId>guava</artifactId>
+                            <version>31.1-jre</version>
+                        </dependency>
+                    </dependencies>
+                </project>
+                """
+            ),
+            mavenProject("child",
+              srcTestJava(
+                java(
+                  """
+                    import main.java.checkerframework.checker.nullness.qual.NonNull;
+                    class Foo {}
+                    """,
+                  """
+                    import org.checkerframework.checker.nullness.qual.NonNull;
+                    class Foo {}
+                    """
+                )
+              ),
+              pomXml(
+                """
+                  <project>
+                      <parent>
+                          <groupId>com.mycompany.app</groupId>
+                          <artifactId>parent</artifactId>
+                          <version>1</version>
+                          <relativePath>../pom.xml</relativePath>
+                      </parent>
+                      <artifactId>child</artifactId>
+                      <dependencies>
+                          <dependency>
+                              <groupId>com.google.guava</groupId>
+                              <artifactId>guava</artifactId>
+                              <version>31.1-jre</version>
+                              <scope>test</scope>
+                          </dependency>
+                      </dependencies>
                   </project>
                   """
               )
