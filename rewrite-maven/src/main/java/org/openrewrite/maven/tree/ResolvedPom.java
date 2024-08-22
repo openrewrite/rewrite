a87527a659044d45bac90c65a652d9d7da5955fb
Laurens Westerlaken <laurens.w@live.nl>
2024-08-22 12:45:26 +0200

    Do not merge in parent dependency where child overrides the version or scope (#4434)
    * Add proposed testcase showcasing the problem
    
    * Refactor tests after clarifying actual problem
    
    * Add testcase to maven parser test
    
    * Child dependency takes precedence over parent
    
    * Add comments and remove commented out line
    
    * De-stream
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Laurens Westerlaken <laurens.westerlaken@jdriven.com>
    Co-authored-by: Tim te Beek <tim@moderne.io>

--- rewrite-maven/src/main/java/org/openrewrite/maven/tree/ResolvedPom.java
+++ rewrite-maven/src/main/java/org/openrewrite/maven/tree/ResolvedPom.java
@@ -508,7 +508,21 @@ public class ResolvedPom {
                     //If it's empty, we ensure to create a mutable list.
                     requestedDependencies = new ArrayList<>(incomingRequestedDependencies);
                 } else {
-                    requestedDependencies.addAll(incomingRequestedDependencies);
+                    // When a child dependency has overriden a parent dependency (either version or scope)
+                    // We shouldn't add the parent definition when requested; the child takes precedence
+                    for (Dependency incReqDep : incomingRequestedDependencies) {
+                        boolean found = false;
+                        for (Dependency reqDep : requestedDependencies) {
+                            if (reqDep.getGav().getGroupId().equals(incReqDep.getGav().getGroupId())
+                                && reqDep.getArtifactId().equals(incReqDep.getArtifactId())) {
+                                found = true;
+                                break;
+                            }
+                        }
+                        if (!found) {
+                            requestedDependencies.add(incReqDep);
+                        }
+                    }
                 }
             }
         }
