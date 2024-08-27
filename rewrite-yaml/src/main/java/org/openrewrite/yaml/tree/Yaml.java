a8be9935d7e5aef802b0d8f2787a5146a46892d1
Sam Snyder <sam@moderne.io>
2024-08-27 16:47:39 -0700

    If the snippet begins with a comment, preserve it in the output

--- rewrite-yaml/src/main/java/org/openrewrite/yaml/tree/Yaml.java
+++ rewrite-yaml/src/main/java/org/openrewrite/yaml/tree/Yaml.java
@@ -19,6 +19,7 @@ import lombok.*;
 import lombok.experimental.FieldDefaults;
 import org.jspecify.annotations.Nullable;
 import org.openrewrite.*;
+import org.openrewrite.internal.ListUtils;
 import org.openrewrite.marker.Markers;
 import org.openrewrite.yaml.YamlVisitor;
 import org.openrewrite.yaml.internal.YamlPrinter;
@@ -283,12 +284,16 @@ public interface Yaml extends Tree {
             return "";
         }
 
+        /**
+         * Mappings do not have a prefix of their own, it instead accrues to the first entry.
+         * This is a shortcut for changing the prefix of the first entry in the list of entries.
+         */
         @Override
         public Mapping withPrefix(String prefix) {
-            if (!prefix.isEmpty()) {
-                throw new UnsupportedOperationException("Yaml.Mapping may not have a non-empty prefix");
+            if (entries.isEmpty()) {
+                return this;
             }
-            return this;
+            return withEntries(ListUtils.mapFirst(entries, e -> e.withPrefix(prefix)));
         }
 
         @Value
