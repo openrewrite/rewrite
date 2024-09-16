bc10c0de22967606d87751bfddd9e07e051a987e
Tim te Beek <tim@moderne.io>
2024-09-16 16:34:32 +0200

    Apply formatter and extract common target to variable

--- rewrite-java-test/src/test/java/org/openrewrite/java/OrderImportsTest.java
+++ rewrite-java-test/src/test/java/org/openrewrite/java/OrderImportsTest.java
@@ -27,10 +27,7 @@ import org.openrewrite.test.SourceSpec;
 import static java.util.Collections.emptySet;
 import static java.util.Collections.singletonList;
 import static org.openrewrite.Tree.randomId;
-import static org.openrewrite.java.Assertions.addTypesToSourceSet;
-import static org.openrewrite.java.Assertions.java;
-import static org.openrewrite.java.Assertions.srcMainJava;
-import static org.openrewrite.java.Assertions.version;
+import static org.openrewrite.java.Assertions.*;
 
 class OrderImportsTest implements RewriteTest {
 
@@ -54,7 +51,7 @@ class OrderImportsTest implements RewriteTest {
             """
               import java.awt.List;
               import java.util.List;
-                            
+              
               class Test {}
               """
           )
@@ -88,9 +85,9 @@ class OrderImportsTest implements RewriteTest {
           java(
             """
               import java.util.List;
-                            
+              
               import static java.util.Collections.*;
-                            
+              
               class A {}
               """
           )
@@ -134,7 +131,7 @@ class OrderImportsTest implements RewriteTest {
           java(
             """
               import java.util.*;
-                            
+              
               class A {
                   List<Integer> list;
                   List<Integer> list2;
@@ -142,7 +139,7 @@ class OrderImportsTest implements RewriteTest {
               """,
             """
               import java.util.List;
-                            
+              
               class A {
                   List<Integer> list;
                   List<Integer> list2;
@@ -159,7 +156,7 @@ class OrderImportsTest implements RewriteTest {
           java(
             """
               import java.util.*;
-                            
+              
               class A {
                   List<Integer> list;
                   Map<Integer, Integer> map;
@@ -168,7 +165,7 @@ class OrderImportsTest implements RewriteTest {
             """
               import java.util.List;
               import java.util.Map;
-                            
+              
               class A {
                   List<Integer> list;
                   Map<Integer, Integer> map;
@@ -197,18 +194,18 @@ class OrderImportsTest implements RewriteTest {
           java(
             """
               import java.util.List;
-                            
+              
               import static java.util.Collections.*;
-                            
+              
               class A {
                   List<Integer> list = emptyList();
               }
               """,
             """
               import java.util.List;
-                            
+              
               import static java.util.Collections.emptyList;
-                            
+              
               class A {
                   List<Integer> list = emptyList();
               }
@@ -271,25 +268,25 @@ class OrderImportsTest implements RewriteTest {
               import java.nio.charset.StandardCharsets;
               import java.util.Collections;
               import java.util.zip.GZIPOutputStream;
-                            
+              
               import javax.servlet.ReadListener;
               import javax.servlet.ServletInputStream;
               import javax.servlet.ServletOutputStream;
-                            
+              
               import com.fasterxml.jackson.databind.ObjectMapper;
               import org.apache.commons.logging.Log;
               import reactor.core.publisher.Mono;
-                            
+              
               import org.springframework.core.io.buffer.DataBuffer;
               import org.springframework.core.io.buffer.DataBufferFactory;
               import org.springframework.http.HttpHeaders;
               import org.springframework.util.MultiValueMap;
               import org.springframework.web.bind.annotation.PathVariable;
               import org.springframework.web.server.ServerWebExchange;
-                            
+              
               import static java.util.Arrays.stream;
               import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.toAsyncPredicate;
-                            
+              
               class A {}
               """
           )
@@ -341,7 +338,7 @@ class OrderImportsTest implements RewriteTest {
           java(
             """
               import static java.util.Collections.*;
-                            
+              
               class Test {
                   Object[] o = new Object[] { emptyList(), emptyMap(), emptySet() };
               }
@@ -363,7 +360,7 @@ class OrderImportsTest implements RewriteTest {
           java(
             """
               import static my.MyCollections.*;
-                            
+              
               class Test {
                   Object[] o = new Object[] { emptyList(), emptyMap(), emptySet() };
               }
@@ -379,7 +376,7 @@ class OrderImportsTest implements RewriteTest {
           java(
             """
               import static java.util.Collections.*;
-                            
+              
               class Test {
                   Object[] o = new Object[] { emptyList(), emptyMap(), emptySet() };
               }
@@ -471,9 +468,9 @@ class OrderImportsTest implements RewriteTest {
               """,
             """
               import static java.util.Collections.singletonList;
-                            
+              
               import java.util.List;
-                            
+              
               class Test {
               }
               """
@@ -511,10 +508,10 @@ class OrderImportsTest implements RewriteTest {
               // org.slf4j should be detected as a block pattern, and not be moved to all other imports.
               import org.slf4j.Logger;
               import org.slf4j.LoggerFactory;
-                            
+              
               import java.util.Arrays;
               import java.util.List;
-                            
+              
               public class C {
               }
               """
@@ -555,14 +552,14 @@ class OrderImportsTest implements RewriteTest {
               import java.util.HashSet;
               import java.util.List;
               import java.util.Set;
-                            
+              
               import javax.persistence.Entity;
               import javax.persistence.FetchType;
               import javax.persistence.JoinColumn;
               import javax.persistence.JoinTable;
               import javax.persistence.ManyToMany;
               import javax.persistence.Table;
-                            
+              
               public class C {
               }
               """
@@ -669,12 +666,12 @@ class OrderImportsTest implements RewriteTest {
             singletonList(
               new NamedStyles(
                 randomId(), "test", "test", "test", emptySet(), singletonList(
-                  ImportLayoutStyle.builder()
-                    .packageToFold("java.util.*", false)
-                    .importAllOthers()
-                    .importStaticAllOthers()
-                    .build()
-                )
+                ImportLayoutStyle.builder()
+                  .packageToFold("java.util.*", false)
+                  .importAllOthers()
+                  .importStaticAllOthers()
+                  .build()
+              )
               )
             ))),
           java(
@@ -695,35 +692,35 @@ class OrderImportsTest implements RewriteTest {
           spec -> spec.recipe(new OrderImports(true)),
           java(
             """
-            import org.openrewrite.java.tree.JContainer;
-            import org.openrewrite.java.tree.JLeftPadded;
-            import org.openrewrite.java.tree.JRightPadded;
-            import org.openrewrite.java.tree.JavaType;
-            import org.openrewrite.java.tree.JavaType.Variable;
-            import org.openrewrite.java.tree.Space;
-
-            public class Foo {
-                Variable myVariable = null;
-                JLeftPadded<JavaType> myLeftPadded = null;
-                JRightPadded<JavaType> myRightPadded = null;
-                JContainer<JavaType> myContainer = null;
-                Space mySpace = null;
-                JavaType.Variable myVariable = null;
-            }
-            """,
+              import org.openrewrite.java.tree.JContainer;
+              import org.openrewrite.java.tree.JLeftPadded;
+              import org.openrewrite.java.tree.JRightPadded;
+              import org.openrewrite.java.tree.JavaType;
+              import org.openrewrite.java.tree.JavaType.Variable;
+              import org.openrewrite.java.tree.Space;
+              
+              public class Foo {
+                  Variable myVariable = null;
+                  JLeftPadded<JavaType> myLeftPadded = null;
+                  JRightPadded<JavaType> myRightPadded = null;
+                  JContainer<JavaType> myContainer = null;
+                  Space mySpace = null;
+                  JavaType.Variable myVariable = null;
+              }
+              """,
             """
-            import org.openrewrite.java.tree.*;
-            import org.openrewrite.java.tree.JavaType.Variable;
-
-            public class Foo {
-                Variable myVariable = null;
-                JLeftPadded<JavaType> myLeftPadded = null;
-                JRightPadded<JavaType> myRightPadded = null;
-                JContainer<JavaType> myContainer = null;
-                Space mySpace = null;
-                JavaType.Variable myVariable = null;
-            }
-            """
+              import org.openrewrite.java.tree.*;
+              import org.openrewrite.java.tree.JavaType.Variable;
+              
+              public class Foo {
+                  Variable myVariable = null;
+                  JLeftPadded<JavaType> myLeftPadded = null;
+                  JRightPadded<JavaType> myRightPadded = null;
+                  JContainer<JavaType> myContainer = null;
+                  Space mySpace = null;
+                  JavaType.Variable myVariable = null;
+              }
+              """
           )
         );
     }
@@ -735,29 +732,29 @@ class OrderImportsTest implements RewriteTest {
           spec -> spec.recipe(new OrderImports(true)),
           java(
             """
-            package a;
-
-            public class A {
-                public final class B {}
-                public static class C {}
-            }
-            """,
+              package a;
+              
+              public class A {
+                  public final class B {}
+                  public static class C {}
+              }
+              """,
             SourceSpec::skip),
           java(
             """
-            import a.*;
-
-            public class Foo {
-                A method(A.B ab, A.C ac) {}
-            }
-            """,
+              import a.*;
+              
+              public class Foo {
+                  A method(A.B ab, A.C ac) {}
+              }
+              """,
             """
-            import a.A;
-
-            public class Foo {
-                A method(A.B ab, A.C ac) {}
-            }
-            """
+              import a.A;
+              
+              public class Foo {
+                  A method(A.B ab, A.C ac) {}
+              }
+              """
           )
         );
     }
