8d7bfecb95c4b9cf5e37e936b145796f7ff7f871
Peter Streef <p.streef@gmail.com>
2024-10-02 11:36:24 +0200

    use toRecipe instead

--- rewrite-java/src/test/java/org/openrewrite/java/JavadocPrinterTest.java
+++ rewrite-java/src/test/java/org/openrewrite/java/JavadocPrinterTest.java
@@ -1,10 +1,7 @@
 package org.openrewrite.java;
 
-import lombok.EqualsAndHashCode;
-import lombok.Value;
 import org.junit.jupiter.api.Test;
 import org.openrewrite.ExecutionContext;
-import org.openrewrite.Recipe;
 import org.openrewrite.internal.ListUtils;
 import org.openrewrite.java.tree.Javadoc;
 import org.openrewrite.java.tree.Space;
@@ -13,13 +10,25 @@ import org.openrewrite.test.RewriteTest;
 
 import static org.openrewrite.Tree.randomId;
 import static org.openrewrite.java.Assertions.java;
+import static org.openrewrite.test.RewriteTest.toRecipe;
 
 class JavadocPrinterTest implements RewriteTest {
 
     @Test
     void findInJavadoc() {
         rewriteRun(
-          spec -> spec.recipe(new FindDocComment()),
+          spec -> spec.recipe(toRecipe(() -> new JavaVisitor<>() {
+              @Override
+              public Space visitSpace(Space space, Space.Location loc, ExecutionContext ctx) {
+                  return space.withComments(ListUtils.map(space.getComments(), comment -> {
+                      if (comment instanceof Javadoc.DocComment) {
+                          return comment.withMarkers(comment.getMarkers().
+                            computeByType(new SearchResult(randomId(), null), (s1, s2) -> s1 == null ? s2 : s1));
+                      }
+                      return comment;
+                  }));
+              }
+          })),
           java(
             """
               /** this is a doc comment*/
@@ -34,37 +43,4 @@ class JavadocPrinterTest implements RewriteTest {
           )
         );
     }
-
-    @EqualsAndHashCode(callSuper = false)
-    @Value
-    private static class FindDocComment extends Recipe {
-
-        @Override
-        public String getDisplayName() {
-            return "Find Javadoc comments";
-        }
-
-        @Override
-        public String getDescription() {
-            return "Find Javadoc comments.";
-        }
-
-        @Override
-        public JavaVisitor<ExecutionContext> getVisitor() {
-            return new JavaVisitor<>() {
-
-                @Override
-                public Space visitSpace(Space space, Space.Location loc, ExecutionContext ctx) {
-                    return space.withComments(ListUtils.map(space.getComments(), comment -> {
-                        if (comment instanceof Javadoc.DocComment) {
-                            return comment.withMarkers(comment.getMarkers().
-                              computeByType(new SearchResult(randomId(), null), (s1, s2) -> s1 == null ? s2 : s1));
-                        }
-                        return comment;
-                    }));
-                }
-            };
-        }
-    }
-
 }
