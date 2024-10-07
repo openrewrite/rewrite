7ff9ce55c1878b06b7fa6253e84f8cb572191037
Julius van Dis <zzave@duck.com>
2024-10-07 10:14:17 +0200

    Allow RecipeSerializer to (de)serialize java.time.Duration for estimatedEffortPerOccurrence field (#4556)
    * Allow RecipeSerializer to (de)serialize java.time.Duration for estimatedEffortPerOccurrence field
    
    * Update rewrite-core/src/test/java/org/openrewrite/RecipeSerializerTest.java
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Peter Streef <p.streef@gmail.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ rewrite-core/src/test/java/org/openrewrite/RecipeSerializerTest.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2024 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite;
+
+import java.time.Duration;
+import org.jspecify.annotations.NullMarked;
+import org.jspecify.annotations.Nullable;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RecipeSerializerTest {
+
+    @Test
+    void shouldBeAbleToSerializeAndDeserializeEstimatedEffortPerOccurrence() {
+        RecipeSerializer serializer = new RecipeSerializer();
+        final var recipe = new RecipeWithEstimatedEffortPerOccurrence(Duration.ofDays(1));
+        final Recipe read = serializer.read(serializer.write(recipe));
+        assertThat(read.getEstimatedEffortPerOccurrence()).isEqualTo(Duration.ofDays(1));
+    }
+
+    @NullMarked
+    static class RecipeWithEstimatedEffortPerOccurrence extends Recipe {
+        @Nullable
+        private Duration estimatedEffortPerOccurrence;
+
+        public RecipeWithEstimatedEffortPerOccurrence() {
+            this(null);
+        }
+
+        public RecipeWithEstimatedEffortPerOccurrence(@Nullable Duration estimatedEffortPerOccurrence) {
+            this.estimatedEffortPerOccurrence = estimatedEffortPerOccurrence;
+        }
+
+        @Override
+        public String getDisplayName() {
+            return "Recipe with estimatedEffortPerOccurrence";
+        }
+
+        @Override
+        public String getDescription() {
+            return "A fancy description.";
+        }
+
+        @Override
+        public @Nullable Duration getEstimatedEffortPerOccurrence() {
+            return this.estimatedEffortPerOccurrence;
+        }
+
+        public void setEstimatedEffortPerOccurrence(@Nullable Duration estimatedEffortPerOccurrence) {
+            this.estimatedEffortPerOccurrence = estimatedEffortPerOccurrence;
+        }
+    }
+}
