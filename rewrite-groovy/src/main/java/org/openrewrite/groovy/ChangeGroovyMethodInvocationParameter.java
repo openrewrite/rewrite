3ec001b78f046dbbcfc159a4070caf475bc0bb67
Simon Gartner <simon.gartner2003@gmail.com>
2024-10-11 11:52:21 +0200

    added license header

--- rewrite-groovy/src/main/java/org/openrewrite/groovy/ChangeGroovyMethodInvocationParameter.java
+++ rewrite-groovy/src/main/java/org/openrewrite/groovy/ChangeGroovyMethodInvocationParameter.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2024 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.openrewrite.groovy;
 
 
@@ -37,7 +52,7 @@ public class ChangeGroovyMethodInvocationParameter extends Recipe {
 
     @Override
     public TreeVisitor<?, ExecutionContext> getVisitor() {
-        return new GroovyIsoVisitor<>() {
+        return new GroovyIsoVisitor<ExecutionContext>() {
             @Override
             public J visitMapEntry(G.MapEntry mapEntry, ExecutionContext executionContext) {
                 mapEntry = (G.MapEntry) super.visitMapEntry(mapEntry, executionContext);
