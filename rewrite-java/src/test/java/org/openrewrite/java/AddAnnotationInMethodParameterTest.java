d3d26f86e0080547b4c4686e48e4e3bd48de8840
Tim te Beek <timtebeek@gmail.com>
2024-10-30 14:44:04 +0100

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- rewrite-java/src/test/java/org/openrewrite/java/AddAnnotationInMethodParameterTest.java
+++ rewrite-java/src/test/java/org/openrewrite/java/AddAnnotationInMethodParameterTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2024 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.openrewrite.java;
 
 import lombok.EqualsAndHashCode;
@@ -29,7 +44,7 @@ class AddAnnotationInMethodParameterTest implements RewriteTest {
 
     }
     @Value
-    @EqualsAndHashCode(callSuper = true)
+    @EqualsAndHashCode(callSuper = false)
     static class ExampleRecipe extends Recipe {
 
         @Override
@@ -49,7 +64,7 @@ class AddAnnotationInMethodParameterTest implements RewriteTest {
                 private final AnnotationMatcher annotationMatcher = new AnnotationMatcher("@org.openrewrite.java.DeprecatedMethodParameter");
 
                 @Override
-                public J.VariableDeclarations visitVariableDeclarations(J.VariableDeclarations multiVariable, ExecutionContext executionContext) {
+                public J.VariableDeclarations visitVariableDeclarations(J.VariableDeclarations multiVariable, ExecutionContext ctx) {
                     boolean isMatched = multiVariable.getLeadingAnnotations()
                       .stream()
                       .filter(c -> c.getSimpleName().equals("DeprecatedMethodParameter"))
@@ -67,7 +82,7 @@ class AddAnnotationInMethodParameterTest implements RewriteTest {
                         .contextSensitive()
                         .build()
                         .apply(getCursor(), multiVariable.getCoordinates().addAnnotation((o1, o2) -> -1)),
-                      executionContext
+                      ctx
                     );
                 }
             };
@@ -80,7 +95,7 @@ class AddAnnotationInMethodParameterTest implements RewriteTest {
     }
 
     @Test
-    void testAddAnnotationFormatting() {
+    void addAnnotationFormatting() {
 
         rewriteRun(
           java(
