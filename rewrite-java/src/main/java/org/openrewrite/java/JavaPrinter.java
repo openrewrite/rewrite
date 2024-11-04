c31ebfccfc3df469c8e8016efba6078f8a52ef1f
Laurens Westerlaken <laurens.westerlaken@jdriven.com>
2024-11-04 13:26:39 +0100

    WIP

--- rewrite-java/src/main/java/org/openrewrite/java/JavaPrinter.java
+++ rewrite-java/src/main/java/org/openrewrite/java/JavaPrinter.java
@@ -852,10 +852,13 @@ public class JavaPrinter<P> extends JavaVisitor<PrintOutputCapture<P>> {
                 // The `visit(multiVariable.getTypeExpression(), p);` statement above does not know the
                 // enclosing VariableDeclarations is a vararg therefore we have to remove unnecessary dimensions here
                 if (p.out.charAt(p.out.length() - 1) == ']') {
-                    if (p.out.charAt(p.out.length() - 2) == '[') {
-                        p.out.delete(p.out.length() - 2, p.out.length());
-                    } else {
-                        throw new IllegalStateException("Vararg was interpreted with non empty dimensions");
+                    int posToCheck = p.out.length() - 2;
+                    while (p.out.charAt(posToCheck) != '[') {
+                        if (Character.isWhitespace(p.out.charAt(posToCheck))) {
+                            posToCheck--;
+                        } else {
+                            throw new IllegalStateException("Vararg was interpreted with non empty dimensions");
+                        }
                     }
                 }
             }
