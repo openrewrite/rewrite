ea3bf6c3ee08a14707d70b6e676bff2c8e07e25a
Tim te Beek <timtebeek@gmail.com>
2024-11-12 00:08:15 +0100

    Update rewrite-java/src/test/java/org/openrewrite/java/search/FindVariableEscapeLocationsTest.java
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- rewrite-java/src/test/java/org/openrewrite/java/search/FindVariableEscapeLocationsTest.java
+++ rewrite-java/src/test/java/org/openrewrite/java/search/FindVariableEscapeLocationsTest.java
@@ -23,7 +23,7 @@ import org.openrewrite.test.RewriteTest;
 
 import static org.openrewrite.java.Assertions.java;
 
-public class FindVariableEscapeLocationsTest implements RewriteTest {
+class FindVariableEscapeLocationsTest implements RewriteTest {
 
     @Override
     public void defaults(RecipeSpec spec) {
