cd4d47a9420ea405410929f91cc2a979dcef343e
Laurens Westerlaken <laurens.westerlaken@jdriven.com>
2024-11-20 19:31:33 +0100

    Add `AddAnnotationProcessor` recipe

---
+++ rewrite-maven/src/main/java/org/openrewrite/maven/AddAnnotationProcessor.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2024 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.maven;
+
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.Option;
+import org.openrewrite.Recipe;
+import org.openrewrite.TreeVisitor;
+import org.openrewrite.internal.ListUtils;
+import org.openrewrite.xml.XPathMatcher;
+import org.openrewrite.xml.XmlIsoVisitor;
+import org.openrewrite.xml.tree.Xml;
+
+import java.util.Optional;
+
+import static org.openrewrite.xml.AddOrUpdateChild.addOrUpdateChild;
+
+@Value
+@EqualsAndHashCode(callSuper = false)
+public class AddAnnotationProcessor extends Recipe {
+    private static final XPathMatcher PLUGINS_MATCHER = new XPathMatcher("/project/build/plugins");
+    private static final String MAVEN_COMPILER_PLUGIN_GROUP_ID = "org.apache.maven.plugins";
+    private static final String MAVEN_COMPILER_PLUGIN_ARTIFACT_ID = "maven-compiler-plugin";
+
+    @Option(displayName = "Group",
+            description = "The first part of the coordinate 'org.openrewrite.maven:rewrite-maven-plugin:VERSION' of the processor to add.",
+            example = "org.projectlombok")
+    String groupId;
+
+    @Option(displayName = "Artifact",
+            description = "The second part of a coordinate 'org.openrewrite.maven:rewrite-maven-plugin:VERSION' of the processor to add.",
+            example = "lombok-mapstruct-binding")
+    String artifactId;
+
+    @Override
+    public String getDisplayName() {
+        return "Change Maven plugin configuration";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Apply the specified configuration to a Maven plugin. Will not add the plugin if it does not already exist in the pom.";
+    }
+
+    @Override
+    public TreeVisitor<?, ExecutionContext> getVisitor() {
+        return new MavenVisitor<ExecutionContext>() {
+            @Override
+            public Xml visitTag(Xml.Tag tag, ExecutionContext ctx) {
+                Xml.Tag plugins = (Xml.Tag) super.visitTag(tag, ctx);
+                if (PLUGINS_MATCHER.matches(getCursor())) {
+                    Optional<Xml.Tag> maybePlugin = plugins.getChildren().stream()
+                            .filter(plugin ->
+                                    "plugin".equals(plugin.getName()) &&
+                                    MAVEN_COMPILER_PLUGIN_GROUP_ID.equals(plugin.getChildValue("groupId").orElse(null)) &&
+                                    MAVEN_COMPILER_PLUGIN_ARTIFACT_ID.equals(plugin.getChildValue("artifactId").orElse(null))
+                            )
+                            .findAny();
+                    if (maybePlugin.isPresent()) {
+                        Xml.Tag plugin = maybePlugin.get();
+                        plugin = new XmlIsoVisitor<ExecutionContext>() {
+                            @Override
+                            public Xml.Tag visitTag(Xml.Tag tag, ExecutionContext ctx) {
+                                Xml.Tag tg = super.visitTag(tag, ctx);
+                                if (tg.getName().equals("annotationProcessorPaths")) {
+                                    boolean found = false;
+                                    for (Xml.Tag child : tg.getChildren()) {
+                                        if (groupId.equals(child.getChildValue("groupId").orElse(null))
+                                            && artifactId.equals(child.getChildValue("artifactId").orElse(null))) {
+                                            found = true;
+                                        }
+                                    }
+                                    if (!found) {
+                                        return tg.withContent(
+                                                ListUtils.concat(tg.getChildren(),
+                                                        Xml.Tag.build(String.format("<path>\n<groupId>%s</groupId>\n<artifactId>%s</artifactId>\n<version>${version.mapstruct-lombok}</version>\n</path>", groupId, artifactId))));
+                                    }
+                                }
+                                return tg;
+                            }
+                        }.visitTag(plugin, ctx);
+                        plugins = addOrUpdateChild(plugins, plugin, getCursor());
+                    }
+                }
+                return autoFormat(plugins, ctx);
+            }
+        };
+    }
+}
---
+++ rewrite-maven/src/test/java/org/openrewrite/maven/AddAnnotationProcessorTest.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright 2024 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.maven;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.maven.Assertions.pomXml;
+
+class AddAnnotationProcessorTest implements RewriteTest {
+
+    @DocumentExample
+    @Test
+    void addCommentToMavenDependency() {
+        rewriteRun(
+          spec -> spec.recipe(new AddAnnotationProcessor(
+            "org.projectlombok",
+            "lombok-mapstruct-binding"
+          )),
+          pomXml(
+            """
+              <project>
+                  <modelVersion>4.0.0</modelVersion>
+              
+                  <groupId>com.mycompany.app</groupId>
+                  <artifactId>my-app</artifactId>
+                  <version>1</version>
+              
+                  <build>
+                      <plugins>
+                          <plugin>
+                              <groupId>org.apache.maven.plugins</groupId>
+                              <artifactId>maven-compiler-plugin</artifactId>
+                              <version>3.5.1</version>
+                              <configuration>
+                                  <source>9</source>
+                                  <target>9</target>
+                                  <annotationProcessorPaths>
+                                      <path>
+                                          <groupId>org.mapstruct</groupId>
+                                          <artifactId>mapstruct-processor</artifactId>
+                                          <version>${version.mapstruct}</version>
+                                      </path>
+                                      <path>
+                                          <groupId>org.projectlombok</groupId>
+                                          <artifactId>lombok</artifactId>
+                                          <version>${version.lombok}</version>
+                                      </path>
+                                  </annotationProcessorPaths>
+                              </configuration>
+                          </plugin>
+                      </plugins>
+                  </build>
+              </project>
+              """,
+            """
+              <project>
+                  <modelVersion>4.0.0</modelVersion>
+              
+                  <groupId>com.mycompany.app</groupId>
+                  <artifactId>my-app</artifactId>
+                  <version>1</version>
+              
+                  <build>
+                      <plugins>
+                          <plugin>
+                              <groupId>org.apache.maven.plugins</groupId>
+                              <artifactId>maven-compiler-plugin</artifactId>
+                              <version>3.5.1</version>
+                              <configuration>
+                                  <source>9</source>
+                                  <target>9</target>
+                                  <annotationProcessorPaths>
+                                      <path>
+                                          <groupId>org.mapstruct</groupId>
+                                          <artifactId>mapstruct-processor</artifactId>
+                                          <version>${version.mapstruct}</version>
+                                      </path>
+                                      <path>
+                                          <groupId>org.projectlombok</groupId>
+                                          <artifactId>lombok</artifactId>
+                                          <version>${version.lombok}</version>
+                                      </path>
+                                      <path>
+                                          <groupId>org.projectlombok</groupId>
+                                          <artifactId>lombok-mapstruct-binding</artifactId>
+                                          <version>${version.mapstruct-lombok}</version>
+                                      </path>
+                                  </annotationProcessorPaths>
+                              </configuration>
+                          </plugin>
+                      </plugins>
+                  </build>
+              </project>
+              """
+          )
+        );
+    }
+
+}
