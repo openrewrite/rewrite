713ced29e3f92f9acbe2ac32e438eae093af72f4
Tim te Beek <tim@moderne.io>
2024-11-25 12:48:07 +0100

    Minor polish to clarify expected usage pattern

--- rewrite-properties/src/main/java/org/openrewrite/properties/trait/PropertiesReference.java
+++ rewrite-properties/src/main/java/org/openrewrite/properties/trait/PropertiesReference.java
@@ -16,10 +16,8 @@
 package org.openrewrite.properties.trait;
 
 import lombok.Value;
-import org.jspecify.annotations.NonNull;
 import org.jspecify.annotations.Nullable;
 import org.openrewrite.Cursor;
-import org.openrewrite.Incubating;
 import org.openrewrite.SourceFile;
 import org.openrewrite.properties.tree.Properties;
 import org.openrewrite.trait.Reference;
@@ -27,21 +25,21 @@ import org.openrewrite.trait.SimpleTraitMatcher;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.function.Predicate;
 import java.util.regex.Pattern;
 
-@Incubating(since = "8.40.3")
 @Value
 public class PropertiesReference implements Reference {
     Cursor cursor;
     Kind kind;
 
     @Override
-    public @NonNull Kind getKind() {
+    public Kind getKind() {
         return kind;
     }
 
     @Override
-    public @NonNull String getValue() {
+    public String getValue() {
         if (getTree() instanceof Properties.Entry) {
             return ((Properties.Entry) getTree()).getValue().getText();
         }
@@ -54,12 +52,14 @@ public class PropertiesReference implements Reference {
     }
 
     public static class Matcher extends SimpleTraitMatcher<PropertiesReference> {
-        private static final Pattern javaFullyQualifiedTypePattern = Pattern.compile("\\p{javaJavaIdentifierStart}\\p{javaJavaIdentifierPart}*\\.\\p{javaJavaIdentifierStart}\\p{javaJavaIdentifierPart}*(?:\\.\\p{javaJavaIdentifierStart}\\p{javaJavaIdentifierPart}*)*");
+        private static final Predicate<String> javaFullyQualifiedTypeMatcher = Pattern.compile(
+                "\\p{javaJavaIdentifierStart}\\p{javaJavaIdentifierPart}*\\.\\p{javaJavaIdentifierStart}\\p{javaJavaIdentifierPart}*(?:\\.\\p{javaJavaIdentifierStart}\\p{javaJavaIdentifierPart}*)*").asPredicate();
 
         @Override
         protected @Nullable PropertiesReference test(Cursor cursor) {
             Object value = cursor.getValue();
-            if (value instanceof Properties.Entry && javaFullyQualifiedTypePattern.matcher(((Properties.Entry) value).getValue().getText()).matches()) {
+            if (value instanceof Properties.Entry &&
+                javaFullyQualifiedTypeMatcher.test(((Properties.Entry) value).getValue().getText())) {
                 return new PropertiesReference(cursor, determineKind(((Properties.Entry) value).getValue().getText()));
             }
             return null;
@@ -72,10 +72,10 @@ public class PropertiesReference implements Reference {
 
     @SuppressWarnings("unused")
     public static class Provider implements Reference.Provider {
-        private static final Pattern applicationPropertiesPattern = Pattern.compile("^.*application(\\.\\w*)?\\.properties$");
+        private static final Predicate<String> applicationPropertiesMatcher = Pattern.compile("^application(-\\w+)?\\.properties$").asPredicate();
 
         @Override
-        public @NonNull Set<Reference> getReferences(SourceFile sourceFile) {
+        public Set<Reference> getReferences(SourceFile sourceFile) {
             Set<Reference> references = new HashSet<>();
             new Matcher().asVisitor(reference -> {
                 references.add(reference);
@@ -86,8 +86,7 @@ public class PropertiesReference implements Reference {
 
         @Override
         public boolean isAcceptable(SourceFile sourceFile) {
-            return sourceFile instanceof Properties.File &&
-                   applicationPropertiesPattern.matcher(sourceFile.getSourcePath().toString()).matches();
+            return sourceFile instanceof Properties.File && applicationPropertiesMatcher.test(sourceFile.getSourcePath().getFileName().toString());
         }
     }
 }
