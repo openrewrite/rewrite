f0b1d51c5efe74958a16c7f2ccf3a64e230cb43f
Laurens Westerlaken <laurens.westerlaken@jdriven.com>
2024-12-03 17:39:52 +0100

    Add `NamingService` with Java implementation (#4725)
    * Add interface and Java implementation that is also returned when a NamingService is requested from a JavaSourceFile
    
    * Update rewrite-core/src/main/java/org/openrewrite/internal/NamingService.java
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Adjust interface name and move check from `MethodNameCasing` to NamingService
    
    * Add test to verify Service implementation and fix the logic
    
    * Update rewrite-java/src/test/java/org/openrewrite/java/service/JavaNamingServiceTest.java
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Add newline at eof
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ rewrite-java/src/test/java/org/openrewrite/java/service/JavaNamingServiceTest.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2024 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.service;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class JavaNamingServiceTest {
+
+    @ParameterizedTest
+    @CsvSource(textBlock = """
+      foo_bar,fooBar
+      foo$bar,fooBar
+      foo_bar$,fooBar
+      foo$bar$,fooBar
+      """)
+    void changeMethodName(String before, String after) {
+        String actual = new JavaNamingService().standardizeMethodName(before);
+        assertThat(actual).isEqualTo(after);
+    }
+}
