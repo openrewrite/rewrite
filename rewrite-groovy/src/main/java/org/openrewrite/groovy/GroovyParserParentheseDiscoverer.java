e0f2efa5367693b5a5c37e7440c4cc1ad4c07de3
lingenj <jacob.van.lingen@moderne.io>
2024-12-04 15:10:55 +0100

    Add comments to explain code

--- rewrite-groovy/src/main/java/org/openrewrite/groovy/GroovyParserParentheseDiscoverer.java
+++ rewrite-groovy/src/main/java/org/openrewrite/groovy/GroovyParserParentheseDiscoverer.java
@@ -1,6 +1,20 @@
+/*
+ * Copyright 2024 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.openrewrite.groovy;
 
-import org.codehaus.groovy.ast.expr.CastExpression;
 import org.codehaus.groovy.ast.expr.MethodCallExpression;
 import org.jspecify.annotations.Nullable;
 import org.openrewrite.internal.StringUtils;
@@ -9,18 +23,14 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class GroovyParserParentheseDiscoverer {
-    // Matches the first method invocation arguments including leading and trailing parenthesis
-    // Eg: ((((a.invoke~~>("arg")))))<~~
+    // Matches a code block including leading and trailing parenthesis
+    // Eg: ((((a.invoke~~>("arg")))))<~~ or ~~>(((((( "" as String )))))<~~.toString())
     private static final Pattern PARENTHESES_GROUP = Pattern.compile(".*?(\\(+[^()]+\\)+).*", Pattern.DOTALL);
     // Matches when string consist of multiple parenthese blocks
     // Eg: `((a.invoke())` does not match, `((a.invoke() && b.invoke())` does match
     private static final Pattern HAS_SUB_PARENTHESIS = Pattern.compile("\\(+[^)]+\\)+[^)]+\\).*", Pattern.DOTALL);
 
     public static @Nullable Integer getInsideParenthesesLevel(MethodCallExpression node, String source) {
-        if (node.getObjectExpression() instanceof CastExpression) {
-            return null;
-        }
-
         // start with a (` character with optional whitespace
         if (source.matches("(?s)^\\s*\\(.*")) {
             // grab the source code until method and closing parenthesis
@@ -37,12 +47,16 @@ public class GroovyParserParentheseDiscoverer {
 
     private static int getInsideParenthesesLevelForMethodCalls(String source) {
         String s = source;
+        // lookup for first code block with parenthesis
         Matcher m = PARENTHESES_GROUP.matcher(s);
+        // check if source matches any code block with parenthesis + check if there are still inner parenthesis
         while (m.matches() && HAS_SUB_PARENTHESIS.matcher(s).find()) {
             int parenthesis = lowestParentheseLevel(m.group(1));
             String part = m.group(1).replaceAll("\\(", "").replaceAll("\\)", "");
             String regex = StringUtils.repeat("\\(", parenthesis) + part + StringUtils.repeat("\\)", parenthesis);
+            // remove parentheses and arguments in source code
             s = s.replaceAll(regex, "");
+            // move to next possible code block with parenthesis
             m = PARENTHESES_GROUP.matcher(s);
         }
 
