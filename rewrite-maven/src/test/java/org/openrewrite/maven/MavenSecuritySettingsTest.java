4c45440a28894a78579939ca65833e9832f57765
Tim te Beek <tim@moderne.io>
2024-12-08 12:36:03 +0100

    Polish MavenSettingsSecurityTest

--- rewrite-maven/src/test/java/org/openrewrite/maven/MavenSecuritySettingsTest.java
+++ rewrite-maven/src/test/java/org/openrewrite/maven/MavenSecuritySettingsTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 the original author or authors.
+ * Copyright 2024 the original author or authors.
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,23 +24,25 @@ import org.openrewrite.InMemoryExecutionContext;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class MavenSettingsSecurityTest {
-    @TempDir Path tempDir;
+
+    private static final String MASTER_PASS_ENCRYPTED = "FyoLIiN2Fx8HpT8O0aBsTn2/s3pYmtLRRCpoWPzhN4A="; // "master"
+    private static final String USER_PASS_ENCRYPTED = "ERozWEamSJoHRBT+wVx51V2Emr9PazZR9txMntZPlJc="; // "testpass"
+    private static final String USER_PASS_DECRYPTED = "testpass";
 
     private String originalUserHome;
-    private final String MASTER_PASS_ENCRYPTED = "FyoLIiN2Fx8HpT8O0aBsTn2/s3pYmtLRRCpoWPzhN4A=";
-//    private final String MASTER_PASS_DECRYPTED = "master";
-    private final String USER_PASS_ENCRYPTED = "ERozWEamSJoHRBT+wVx51V2Emr9PazZR9txMntZPlJc=";
-    private final String USER_PASS_DECRYPTED = "testpass";
+
+    @TempDir
+    Path tempDir;
 
     @BeforeEach
-    void setUp() {
+    void setUp() throws IOException {
         originalUserHome = System.getProperty("user.home");
         System.setProperty("user.home", tempDir.toString());
+        Files.createDirectories(tempDir.resolve(".m2"));
     }
 
     @AfterEach
@@ -51,240 +53,191 @@ class MavenSettingsSecurityTest {
     @Test
     void decryptCredentials() throws IOException {
         // Create settings-security.xml with master password
-        Path securitySettingsPath = tempDir.resolve(".m2/settings-security.xml");
-        Files.createDirectories(securitySettingsPath.getParent());
-        Files.write(
-          securitySettingsPath, (
-            //language=xml
-            """
-              <?xml version="1.0" encoding="UTF-8"?>
-              <settingsSecurity>
-                  <master>{%s}</master>
-              </settingsSecurity>
-              """.formatted(MASTER_PASS_ENCRYPTED)
-          ).getBytes(), StandardOpenOption.CREATE
-        );
+        Files.writeString(tempDir.resolve(".m2/settings-security.xml"),
+          //language=xml
+          """
+            <?xml version="1.0" encoding="UTF-8"?>
+            <settingsSecurity>
+                <master>{%s}</master>
+            </settingsSecurity>
+            """.formatted(MASTER_PASS_ENCRYPTED));
 
         // Create settings.xml with encrypted password
-        Path settingsPath = tempDir.resolve(".m2/settings.xml");
-        Files.write(
-          settingsPath, (
-            //language=xml
-            """
-              <?xml version="1.0" encoding="UTF-8"?>
-              <settings>
-                  <servers>
-                      <server>
-                          <id>test-server</id>
-                          <username>admin</username>
-                          <password>{%s}</password>
-                      </server>
-                  </servers>
-              </settings>
-              """.formatted(USER_PASS_ENCRYPTED)
-          ).getBytes(), StandardOpenOption.CREATE
-        );
+        Files.writeString(tempDir.resolve(".m2/settings.xml"),
+          //language=xml
+          """
+            <?xml version="1.0" encoding="UTF-8"?>
+            <settings>
+                <servers>
+                    <server>
+                        <id>test-server</id>
+                        <username>admin</username>
+                        <password>{%s}</password>
+                    </server>
+                </servers>
+            </settings>
+            """.formatted(USER_PASS_ENCRYPTED));
 
+        // Use the public API to read settings
         var ctx = new InMemoryExecutionContext(t -> {
             throw new RuntimeException(t);
         });
-
-        // Use the public API to read settings
-        MavenSecuritySettings securitySettings = MavenSecuritySettings.readMavenSecuritySettingsFromDisk(ctx);
         MavenSettings settings = MavenSettings.readMavenSettingsFromDisk(ctx);
-
-        assertThat(securitySettings).isNotNull();
-        assertThat(securitySettings.getMaster()).isNotNull();
-
-        assertThat(settings).isNotNull();
-        assertThat(settings.getServers()).isNotNull();
-        assertThat(settings.getServers().getServers()).hasSize(1).first().satisfies(server -> {
-            assertThat(server.getId()).isEqualTo("test-server");
-            assertThat(server.getUsername()).isEqualTo("admin");
-            assertThat(server.getPassword()).isEqualTo(USER_PASS_DECRYPTED);
-        });
+        assert settings != null && settings.getServers() != null;
+        assertThat(settings.getServers().getServers())
+          .hasSize(1)
+          .first()
+          .satisfies(server -> {
+              assertThat(server.getId()).isEqualTo("test-server");
+              assertThat(server.getUsername()).isEqualTo("admin");
+              assertThat(server.getPassword()).isEqualTo(USER_PASS_DECRYPTED);
+          });
     }
 
     @Test
     void handleInvalidEncryptedPassword() throws IOException {
         // Create settings-security.xml with master password
-        Path securitySettingsPath = tempDir.resolve(".m2/settings-security.xml");
-        Files.createDirectories(securitySettingsPath.getParent());
-        Files.write(
-          securitySettingsPath, (
-            //language=xml
-            """
-              <?xml version="1.0" encoding="UTF-8"?>
-              <settingsSecurity>
-                  <master>{jSMOWnoPFgsHVpMvz5VrIt5kRbzGpI8u+9EF1iFQyJQ=}</master>
-              </settingsSecurity>
-              """
-          ).getBytes(), StandardOpenOption.CREATE
-        );
+        Files.writeString(tempDir.resolve(".m2/settings-security.xml"),
+          //language=xml
+          """
+            <?xml version="1.0" encoding="UTF-8"?>
+            <settingsSecurity>
+                <master>{jSMOWnoPFgsHVpMvz5VrIt5kRbzGpI8u+9EF1iFQyJQ=}</master>
+            </settingsSecurity>
+            """);
 
         // Create settings.xml with invalid encrypted password
-        Path settingsPath = tempDir.resolve(".m2/settings.xml");
-        Files.write(
-          settingsPath, (
-            //language=xml
-            """
-              <?xml version="1.0" encoding="UTF-8"?>
-              <settings>
-                  <servers>
-                      <server>
-                          <id>test-server</id>
-                          <username>admin</username>
-                          <password>{invalid_format}</password>
-                      </server>
-                  </servers>
-              </settings>
-              """
-          ).getBytes(), StandardOpenOption.CREATE
-        );
+        Files.writeString(tempDir.resolve(".m2/settings.xml"),
+          //language=xml
+          """
+            <?xml version="1.0" encoding="UTF-8"?>
+            <settings>
+                <servers>
+                    <server>
+                        <id>test-server</id>
+                        <username>admin</username>
+                        <password>{invalid_format}</password>
+                    </server>
+                </servers>
+            </settings>
+            """);
 
         var ctx = new InMemoryExecutionContext(t -> {
             throw new RuntimeException(t);
         });
-
         MavenSettings settings = MavenSettings.readMavenSettingsFromDisk(ctx);
-
-        assertThat(settings).isNotNull();
-        assertThat(settings.getServers().getServers()).hasSize(1).first().satisfies(server -> {
-            assertThat(server.getPassword()).isEqualTo("{invalid_format}");
-        });
+        assert settings != null && settings.getServers() != null;
+        assertThat(settings.getServers().getServers()).hasSize(1)
+          .first()
+          .satisfies(server -> assertThat(server.getPassword()).isEqualTo("{invalid_format}"));
     }
 
     @Test
-    void noSecuritySettings() throws IOException {
+    void noSecuritySettingsNoDecryption() throws IOException {
         // Only create settings.xml without settings-security.xml
-        Path settingsPath = tempDir.resolve(".m2/settings.xml");
-        Files.createDirectories(settingsPath.getParent());
-        Files.write(
-          settingsPath, (
-            //language=xml
-            """
-              <?xml version="1.0" encoding="UTF-8"?>
-              <settings>
-                  <servers>
-                      <server>
-                          <id>test-server</id>
-                          <username>admin</username>
-                          <password>{encrypted_password}</password>
-                      </server>
-                  </servers>
-              </settings>
-              """
-          ).getBytes(), StandardOpenOption.CREATE
-        );
+        Files.writeString(tempDir.resolve(".m2/settings.xml"),
+          //language=xml
+          """
+            <?xml version="1.0" encoding="UTF-8"?>
+            <settings>
+                <servers>
+                    <server>
+                        <id>test-server</id>
+                        <username>admin</username>
+                        <password>{encrypted_password}</password>
+                    </server>
+                </servers>
+            </settings>
+            """);
 
         var ctx = new InMemoryExecutionContext(t -> {
             throw new RuntimeException(t);
         });
-
         MavenSettings settings = MavenSettings.readMavenSettingsFromDisk(ctx);
-
-        assertThat(settings).isNotNull();
-        assertThat(settings.getServers().getServers()).hasSize(1).first().satisfies(server -> {
-            assertThat(server.getPassword()).isEqualTo("{encrypted_password}");
-        });
+        assert settings != null && settings.getServers() != null;
+        assertThat(settings.getServers().getServers())
+          .hasSize(1)
+          .first()
+          .satisfies(server -> assertThat(server.getPassword()).isEqualTo("{encrypted_password}"));
     }
 
     @Test
     void decryptPasswordWithComments() throws IOException {
         // Create settings-security.xml with master password
-        Path securitySettingsPath = tempDir.resolve(".m2/settings-security.xml");
-        Files.createDirectories(securitySettingsPath.getParent());
-        Files.write(
-          securitySettingsPath, (
-            //language=xml
-            """
-              <?xml version="1.0" encoding="UTF-8"?>
-              <settingsSecurity>
-                  <master>{%s}</master>
-              </settingsSecurity>
-              """.formatted(MASTER_PASS_ENCRYPTED)
-          ).getBytes(), StandardOpenOption.CREATE
-        );
+        Files.writeString(tempDir.resolve(".m2/settings-security.xml"),
+          //language=xml
+          """
+            <?xml version="1.0" encoding="UTF-8"?>
+            <settingsSecurity>
+                <master>{%s}</master>
+            </settingsSecurity>
+            """.formatted(MASTER_PASS_ENCRYPTED));
 
         // Create settings.xml with password containing comments
-        Path settingsPath = tempDir.resolve(".m2/settings.xml");
-        Files.write(
-          settingsPath, (
-            //language=xml
-            """
-              <?xml version="1.0" encoding="UTF-8"?>
-              <settings>
-                  <servers>
-                      <server>
-                          <id>test-server</id>
-                          <username>admin</username>
-                          <password>Oleg reset this password on 2009-03-11, expires on 2009-04-11 {%s}</password>
-                      </server>
-                  </servers>
-              </settings>
-              """.formatted(USER_PASS_ENCRYPTED)
-          ).getBytes(), StandardOpenOption.CREATE
-        );
+        Files.writeString(tempDir.resolve(".m2/settings.xml"),
+          //language=xml
+          """
+            <?xml version="1.0" encoding="UTF-8"?>
+            <settings>
+                <servers>
+                    <server>
+                        <id>test-server</id>
+                        <username>admin</username>
+                        <password>Oleg reset this password on 2009-03-11, expires on 2009-04-11 {%s}</password>
+                    </server>
+                </servers>
+            </settings>
+            """.formatted(USER_PASS_ENCRYPTED));
 
         var ctx = new InMemoryExecutionContext(t -> {
             throw new RuntimeException(t);
         });
-
         MavenSettings settings = MavenSettings.readMavenSettingsFromDisk(ctx);
-
-        assertThat(settings).isNotNull();
-        assertThat(settings.getServers().getServers()).hasSize(1).first().satisfies(server -> {
-            assertThat(server.getPassword()).isEqualTo(USER_PASS_DECRYPTED);
-        });
+        assert settings != null && settings.getServers() != null;
+        assertThat(settings.getServers().getServers())
+          .hasSize(1)
+          .first()
+          .satisfies(server -> assertThat(server.getPassword()).isEqualTo(USER_PASS_DECRYPTED));
     }
 
     @Test
     void invalidMasterPasswordButValidPasswordFormat() throws IOException {
         // Create settings-security.xml with invalid master password
-        Path securitySettingsPath = tempDir.resolve(".m2/settings-security.xml");
-        Files.createDirectories(securitySettingsPath.getParent());
-        Files.write(
-          securitySettingsPath, (
-            //language=xml
-            """
-              <?xml version="1.0" encoding="UTF-8"?>
-              <settingsSecurity>
-                  <master>{invalid_master_password}</master>
-              </settingsSecurity>
-              """
-          ).getBytes(), StandardOpenOption.CREATE
-        );
+        Files.writeString(tempDir.resolve(".m2/settings-security.xml"),
+          //language=xml
+          """
+            <?xml version="1.0" encoding="UTF-8"?>
+            <settingsSecurity>
+                <master>{invalid_master_password}</master>
+            </settingsSecurity>
+            """);
 
         // Create settings.xml with valid encrypted password
-        Path settingsPath = tempDir.resolve(".m2/settings.xml");
-        Files.write(
-          settingsPath, (
-            //language=xml
-            """
-              <?xml version="1.0" encoding="UTF-8"?>
-              <settings>
-                  <servers>
-                      <server>
-                          <id>test-server</id>
-                          <username>admin</username>
-                          <password>{%s}</password>
-                      </server>
-                  </servers>
-              </settings>
-              """.formatted(USER_PASS_ENCRYPTED)
-          ).getBytes(), StandardOpenOption.CREATE
-        );
+        Files.writeString(tempDir.resolve(".m2/settings.xml"),
+          //language=xml
+          """
+            <?xml version="1.0" encoding="UTF-8"?>
+            <settings>
+                <servers>
+                    <server>
+                        <id>test-server</id>
+                        <username>admin</username>
+                        <password>{%s}</password>
+                    </server>
+                </servers>
+            </settings>
+            """.formatted(USER_PASS_ENCRYPTED));
 
         var ctx = new InMemoryExecutionContext(t -> {
             throw new RuntimeException(t);
         });
-
         MavenSettings settings = MavenSettings.readMavenSettingsFromDisk(ctx);
-
-        assertThat(settings).isNotNull();
-        assertThat(settings.getServers().getServers()).hasSize(1).first().satisfies(server -> {
+        assert settings != null && settings.getServers() != null;
+        assertThat(settings.getServers().getServers())
+          .hasSize(1)
+          .first()
+          .satisfies(server ->
             // Password should remain in encrypted form since master password is invalid
-            assertThat(server.getPassword()).isEqualTo("{%s}".formatted(USER_PASS_ENCRYPTED));
-        });
+            assertThat(server.getPassword()).isEqualTo("{%s}".formatted(USER_PASS_ENCRYPTED)));
     }
 }
