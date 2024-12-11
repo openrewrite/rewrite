openrewrite/rewrite

commit e536ed273fda87ce6857b06f200488130b3ef5f3

`SemanticallyEqual` should not require non-null `J.Identifier#fieldType`

rewrite-java/src/main/java/org/openrewrite/java/tree/J.java
@@ -1979,21 +1979,26 @@ public interface J extends Tree {
         }
 
         public boolean isFullyQualifiedClassReference(String className) {
-            return isFullyQualifiedClassReference(this, className);
-        }
-
-        private boolean isFullyQualifiedClassReference(J.FieldAccess fieldAccess, String className) {
             if (!className.contains(".")) {
                 return false;
             }
-            if (!fieldAccess.getName().getSimpleName().equals(className.substring(className.lastIndexOf('.') + 1))) {
+            return isFullyQualifiedClassReference(this, className, className.length());
+        }
+
+        private boolean isFullyQualifiedClassReference(J.FieldAccess fieldAccess, String className, int prevDotIndex) {
+            int dotIndex = className.lastIndexOf('.', prevDotIndex - 1);
+            if (dotIndex < 0) {
+                return false;
+            }
+            String simpleName = fieldAccess.getName().getSimpleName();
+            if (!simpleName.regionMatches(0, className, dotIndex + 1, simpleName.length())) {
                 return false;
             }
             if (fieldAccess.getTarget() instanceof J.FieldAccess) {
-                return isFullyQualifiedClassReference((J.FieldAccess) fieldAccess.getTarget(), className.substring(0, className.lastIndexOf('.')));
+                return isFullyQualifiedClassReference((J.FieldAccess) fieldAccess.getTarget(), className, dotIndex);
             }
             if (fieldAccess.getTarget() instanceof Identifier) {
-                return ((Identifier) fieldAccess.getTarget()).getSimpleName().equals(className.substring(0, className.lastIndexOf('.')));
+                return ((Identifier) fieldAccess.getTarget()).getSimpleName().equals(className.substring(0, dotIndex));
             }
             return false;
         }

rewrite-java-test/src/test/java/org/openrewrite/java/cleanup/SimplifyBooleanExpressionVisitorTest.java
@@ -22,6 +22,7 @@ import org.openrewrite.DocumentExample;
 import org.openrewrite.Issue;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
+import org.openrewrite.test.TypeValidation;
 
 import static org.openrewrite.java.Assertions.java;
 import static org.openrewrite.test.RewriteTest.toRecipe;
@@ -55,6 +56,21 @@ class SimplifyBooleanExpressionVisitorTest implements RewriteTest {
                       if (a) {}
                       if (a) {}
                   }
+              }
+              """
+          )
+        );
+    }
+
+    @DocumentExample
+    @Test
+    void foo() {
+        rewriteRun(
+          spec -> spec.typeValidationOptions(TypeValidation.builder().identifiers(false).build()),
+          java(
+            """
+              public class A {
+                  boolean a = null instanceof Unknown || null instanceof java.lang.Unknown;
               }
               """
           )

rewrite-java/src/main/java/org/openrewrite/java/search/SemanticallyEqual.java
@@ -681,42 +681,49 @@ public class SemanticallyEqual {
         @Override
         public J.Identifier visitIdentifier(J.Identifier identifier, J j) {
             if (isEqual.get()) {
-                Map<String, String> scope = variableScope.peek();
                 if (j instanceof J.FieldAccess) {
                     J.FieldAccess field = (J.FieldAccess) j;
                     Expression fieldTarget = field.getTarget();
 
                     // Consider implicit-this "a" equivalent to "this.a"
                     // Definitely does not account for every possible edge case around "this", but handles the common case
-                    if(fieldTarget instanceof J.Identifier && "this".equals(((J.Identifier) fieldTarget).getSimpleName())) {
+                    if (fieldTarget instanceof J.Identifier && "this".equals(((J.Identifier) fieldTarget).getSimpleName())) {
                         visit(identifier, field.getName());
                         return identifier;
                     }
 
                     // Identifier name and type aren't the same as the field access they are obviously different
-                    if(!identifier.getSimpleName().equals(field.getSimpleName()) || !TypeUtils.isOfType(identifier.getFieldType(), field.getName().getFieldType())) {
+                    if (!identifier.getSimpleName().equals(field.getSimpleName()) || !TypeUtils.isOfType(identifier.getFieldType(), field.getName().getFieldType())) {
                         isEqual.set(false);
                         return identifier;
                     }
 
-                    String identifierTypeFqn = identifier.getType() instanceof JavaType.FullyQualified ? ((JavaType.FullyQualified)identifier.getType()).getFullyQualifiedName() : "";
+                    // Either both are variables or both are not (e.g. types)
+                    if ((identifier.getFieldType() == null) ^ (field.getName().getFieldType() == null)) {
+                        isEqual.set(false);
+                        return identifier;
+                    }
+
+                    String identifierTypeFqn = identifier.getType() instanceof JavaType.FullyQualified ? ((JavaType.FullyQualified) identifier.getType()).getFullyQualifiedName() : "";
                     // If the field access is a static field access, the identifier must be a class reference
                     // e.g.: Pattern is semantically equal to java.util.regex.Pattern
                     if (field.isFullyQualifiedClassReference(identifierTypeFqn)) {
                         return identifier;
                     }
 
-                    // Similarly, might be comparing a statically imported field to a fully qualified
-                    // e.g. java.util.regex.Pattern.CASE_INSENSITIVE is semantically equal to CASE_INSENSITIVE when the latter is a static import of the former
-                    JavaType identifierOwner = identifier.getFieldType().getOwner();
-                    String identifierOwnerFqn = identifierOwner instanceof JavaType.FullyQualified ? ((JavaType.FullyQualified) identifierOwner).getFullyQualifiedName() : "";
-                    if(field.getTarget() instanceof J.FieldAccess && ((J.FieldAccess) field.getTarget()).isFullyQualifiedClassReference(identifierOwnerFqn)) {
-                        return identifier;
-                    }
+                    if (identifier.getFieldType() != null) {
+                        // Similarly, might be comparing a statically imported field to a fully qualified
+                        // e.g. java.util.regex.Pattern.CASE_INSENSITIVE is semantically equal to CASE_INSENSITIVE when the latter is a static import of the former
+                        JavaType identifierOwner = identifier.getFieldType().getOwner();
+                        String identifierOwnerFqn = identifierOwner instanceof JavaType.FullyQualified ? ((JavaType.FullyQualified) identifierOwner).getFullyQualifiedName() : "";
+                        if (field.getTarget() instanceof J.FieldAccess && ((J.FieldAccess) field.getTarget()).isFullyQualifiedClassReference(identifierOwnerFqn)) {
+                            return identifier;
+                        }
 
-                    // Identifier is statically imported field name "CASE_INSENSITIVE", field is field  access "Pattern.CASE_INSENSITIVE"
-                    if (identifierOwner instanceof JavaType.FullyQualified && ((JavaType.FullyQualified) identifierOwner).getClassName().equals(fieldTarget.toString()) && TypeUtils.isOfType(identifier.getFieldType().getOwner(), fieldTarget.getType())) {
-                        return identifier;
+                        // Identifier is statically imported field name "CASE_INSENSITIVE", field is field  access "Pattern.CASE_INSENSITIVE"
+                        if (identifierOwner instanceof JavaType.FullyQualified && ((JavaType.FullyQualified) identifierOwner).getClassName().equals(fieldTarget.toString()) && TypeUtils.isOfType(identifier.getFieldType().getOwner(), fieldTarget.getType())) {
+                            return identifier;
+                        }
                     }
 
                     isEqual.set(false);
@@ -729,6 +736,7 @@ public class SemanticallyEqual {
 
                 J.Identifier compareTo = (J.Identifier) j;
                 if (identifier.getFieldType() != null) {
+                    Map<String, String> scope = variableScope.peek();
                     if (scope != null && scope.containsKey(identifier.getSimpleName()) && scope.get(identifier.getSimpleName()).equals(compareTo.getSimpleName())) {
                         return identifier;
                     }

commit e28e4bdbe454c5a5c458eddf81a399c8b1893981

Correct `J.FieldAccess.isFullyQualifiedClassReference()`
The commit e536ed273fda87ce6857b06f200488130b3ef5f3 introduced a regression here.

Fixes: #4773

rewrite-java/src/main/java/org/openrewrite/java/tree/J.java
@@ -1991,7 +1991,7 @@ public interface J extends Tree {
                 return false;
             }
             String simpleName = fieldAccess.getName().getSimpleName();
-            if (!simpleName.regionMatches(0, className, dotIndex + 1, simpleName.length())) {
+            if (!simpleName.regionMatches(0, className, dotIndex + 1, className.length())) {
                 return false;
             }
             if (fieldAccess.getTarget() instanceof J.FieldAccess) {

rewrite-java-test/src/test/java/org/openrewrite/java/ChangeTypeTest.java
@@ -25,6 +25,7 @@ import org.openrewrite.java.tree.TypeUtils;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
 import org.openrewrite.test.SourceSpec;
+import org.openrewrite.test.TypeValidation;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -2072,10 +2073,12 @@ class ChangeTypeTest implements RewriteTest {
           properties(
             """
               a.property=java.lang.String
+              c.property=java.lang.StringBuilder
               b.property=String
               """,
             """
               a.property=java.lang.Integer
+              c.property=java.lang.StringBuilder
               b.property=String
               """, spec -> spec.path("application.properties"))
         );
@@ -2101,6 +2104,56 @@ class ChangeTypeTest implements RewriteTest {
                   d: String
               """,
             spec -> spec.path("application.yaml")
+          )
+        );
+    }
+
+    @Test
+    void testRecipe() {
+        // language=java
+        rewriteRun(
+          spec -> spec.typeValidationOptions(TypeValidation.all())
+            .recipe(new ChangeType("org.codehaus.jackson.annotate.JsonIgnoreProperties", "com.fasterxml.jackson.annotation.JsonIgnoreProperties", false))
+            .parser(JavaParser.fromJavaVersion()
+              .dependsOn(
+                """
+                  package org.codehaus.jackson.annotate;
+                  public @interface JsonIgnoreProperties {
+                      boolean ignoreUnknown() default false;
+                  }
+                  """,
+                """
+                  package org.codehaus.jackson.annotate;
+                  public @interface JsonIgnore {
+                  }
+                  """
+              )
+            ),
+          java(
+            """
+              import org.codehaus.jackson.annotate.JsonIgnore;
+              import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+              
+              @JsonIgnoreProperties(ignoreUnknown = true)
+              public class myClass {
+                  @JsonIgnore
+                  public boolean isDirty() {
+                      return false;
+                  }
+              }
+              """,
+            """
+              import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+              import org.codehaus.jackson.annotate.JsonIgnore;
+              
+              @JsonIgnoreProperties(ignoreUnknown = true)
+              public class myClass {
+                  @JsonIgnore
+                  public boolean isDirty() {
+                      return false;
+                  }
+              }
+              """
           )
         );
     }