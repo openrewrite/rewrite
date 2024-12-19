6bbf0cbd387243afa81d7bee8ed53eed2b731210
lingenj <jacob.van.lingen@moderne.io>
2024-12-19 11:27:30 +0100

    revert lost

--- rewrite-properties/src/main/java/org/openrewrite/properties/trait/PropertiesReference.java
+++ rewrite-properties/src/main/java/org/openrewrite/properties/trait/PropertiesReference.java
@@ -18,7 +18,9 @@ package org.openrewrite.properties.trait;
 import lombok.Value;
 import org.jspecify.annotations.Nullable;
 import org.openrewrite.Cursor;
+import org.openrewrite.ExecutionContext;
 import org.openrewrite.SourceFile;
+import org.openrewrite.Tree;
 import org.openrewrite.properties.tree.Properties;
 import org.openrewrite.trait.Reference;
 import org.openrewrite.trait.SimpleTraitMatcher;
@@ -47,6 +49,17 @@ public class PropertiesReference implements Reference {
     @Override
     public boolean supportsRename() {
         return true;
+    }
+
+    @Override
+    public Tree rename(Renamer renamer, Cursor cursor, ExecutionContext ctx) {
+        Tree tree = cursor.getValue();
+        if (tree instanceof Properties.Entry) {
+            Properties.Entry entry = (Properties.Entry) tree;
+            String newValueText = renamer.rename(this);
+            return entry.withValue(entry.getValue().withText(newValueText));
+        }
+        return tree;
     }
 
     @SuppressWarnings("unused")
