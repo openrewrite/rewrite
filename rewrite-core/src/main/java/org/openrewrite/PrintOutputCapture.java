66dc881a0d7e45992dfc0b4ef06bd492c77f0cda
Peter Streef <p.streef@gmail.com>
2024-12-19 09:49:22 +0100

    Update names and add comments

--- rewrite-core/src/main/java/org/openrewrite/PrintOutputCapture.java
+++ rewrite-core/src/main/java/org/openrewrite/PrintOutputCapture.java
@@ -75,11 +75,31 @@ public class PrintOutputCapture<P> implements Cloneable {
         @Incubating(since = "8.41.4")
         @RequiredArgsConstructor
         enum MarkerMode {
+            /**
+             * Does not print any markers.
+             */
             NONE(MarkerPrinter.NONE),
+            /**
+             * Prints a squiggly line arrow in front of the marked element wrapped in a comment.
+             * /*~~>* /Thing thing
+             */
             DEFAULT(MarkerPrinter.DEFAULT),
+            /**
+             * Prints a squiggly line arrow in front of the marked element wrapped in a comment.
+             * Possibly adding verbose information, depending on the marker
+             * /*~~>(this is some verbose information)* /Thing thing
+             */
             VERBOSE(MarkerPrinter.VERBOSE),
-            FENCED(MarkerPrinter.FENCED),
-            SEARCH_ONLY(MarkerPrinter.SEARCH_ONLY);
+            /**
+             * Prints a squiggly arrow in front and after the marked element. It only includes {@link SearchResult}
+             * /*~~>* /Thing thing /**~~>* /
+             */
+            SEARCH_ONLY(MarkerPrinter.SEARCH_ONLY),
+            /**
+             * Prints a fenced marker ID in front and after the marked element. It only includes {@link Markup} and {@link SearchResult}
+             * /*{{3e2a36bb-7c16-4b03-bdde-bffda08838e7}}* /Thing thing /*{{3e2a36bb-7c16-4b03-bdde-bffda08838e7}}* /
+             */
+            FENCED_MARKUP_AND_SEARCH(MarkerPrinter.FENCED_MARKUP_AND_SEARCH);
             @Getter
             private final MarkerPrinter printer;
         }
@@ -101,7 +121,7 @@ public class PrintOutputCapture<P> implements Cloneable {
             }
         };
 
-        MarkerPrinter FENCED = new MarkerPrinter() {
+        MarkerPrinter FENCED_MARKUP_AND_SEARCH = new MarkerPrinter() {
             @Override
             public String beforeSyntax(Marker marker, Cursor cursor, UnaryOperator<String> commentWrapper) {
                 return marker instanceof SearchResult || marker instanceof Markup ? "{{" + marker.getId() + "}}" : "";
