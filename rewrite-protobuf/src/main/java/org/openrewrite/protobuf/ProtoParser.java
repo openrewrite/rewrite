ebb2ee6adada1eb2deeba1fb2b3d60f2cf6b06aa
lingenj <jacob.van.lingen@moderne.io>
2024-12-23 09:50:10 +0100

    Make SourceFileWithReferences `abstract`, so we can define `getReferences` once

--- rewrite-protobuf/src/main/java/org/openrewrite/protobuf/ProtoParser.java
+++ rewrite-protobuf/src/main/java/org/openrewrite/protobuf/ProtoParser.java
@@ -66,7 +66,6 @@ public class ProtoParser implements Parser {
                                     input.getFileAttributes(),
                                     null,
                                     sourceStr,
-                                    null,
                                     null
                             );
                         }
