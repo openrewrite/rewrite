048fb7480b079ae56cbd8e54cfa30350f4d67062
Greg Oledzki <greg.oledzki@moderne.io>
2025-01-02 13:52:31 +0100

    Allowing for expressions on LHS of objectelem

--- rewrite-hcl/src/main/java/org/openrewrite/hcl/internal/grammar/JsonPathParser.java
+++ rewrite-hcl/src/main/java/org/openrewrite/hcl/internal/grammar/JsonPathParser.java
@@ -1505,7 +1505,8 @@ public class JsonPathParser extends Parser {
 		return _localctx;
 	}
 
-	public boolean sempred(RuleContext _localctx, int ruleIndex, int predIndex) {
+    @Override
+    public boolean sempred(RuleContext _localctx, int ruleIndex, int predIndex) {
 		switch (ruleIndex) {
 		case 7:
 			return binaryExpression_sempred((BinaryExpressionContext)_localctx, predIndex);
