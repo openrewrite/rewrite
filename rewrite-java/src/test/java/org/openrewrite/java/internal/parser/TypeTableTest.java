c7d46ae57b722d5db214b652d6e522244d5f926a
Jonathan Schnéider <jkschneider@gmail.com>
2025-01-21 15:53:24 -0500

    Update rewrite-java/src/test/java/org/openrewrite/java/internal/parser/TypeTableTest.java
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- rewrite-java/src/test/java/org/openrewrite/java/internal/parser/TypeTableTest.java
+++ rewrite-java/src/test/java/org/openrewrite/java/internal/parser/TypeTableTest.java
@@ -116,7 +116,9 @@ public class TypeTableTest implements RewriteTest {
 
     private String humanReadableByteCount(double bytes) {
         int unit = 1024;
-        if (bytes < unit || Double.isNaN(bytes)) return decimalOrNan(bytes) + " B";
+        if (bytes < unit || Double.isNaN(bytes)) {
+            return decimalOrNan(bytes) + " B";
+        }
         int exp = (int) (Math.log(bytes) / Math.log(unit));
         String pre = "KMGTPE".charAt(exp - 1) + "i";
         return decimalOrNan(bytes / Math.pow(unit, exp)) + " " + pre + "B";
