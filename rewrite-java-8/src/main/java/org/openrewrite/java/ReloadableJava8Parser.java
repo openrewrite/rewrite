d6b9f5fb763399b6732f00666dee57db6576920a
Tim te Beek <tim@moderne.io>
2025-02-05 13:01:18 +0100

    Enable Lombok by default
    Using local edge release jar

--- rewrite-java-8/src/main/java/org/openrewrite/java/ReloadableJava8Parser.java
+++ rewrite-java-8/src/main/java/org/openrewrite/java/ReloadableJava8Parser.java
@@ -112,8 +112,7 @@ class ReloadableJava8Parser implements JavaParser {
         Options.instance(context).put("should-stop.ifError", "GENERATE");
 
         LOMBOK:
-        if (System.getenv().getOrDefault("REWRITE_LOMBOK", System.getProperty("rewrite.lombok")) != null &&
-                classpath != null && classpath.stream().anyMatch(it -> it.toString().contains("lombok"))) {
+        if (classpath != null && classpath.stream().anyMatch(it -> it.toString().contains("lombok"))) {
             Processor lombokProcessor = null;
             try {
                 // https://projectlombok.org/contributing/lombok-execution-path
