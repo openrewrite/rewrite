3704153d0ca1d80d45eff859328cfd798a2ad2ce
Tim te Beek <tim@moderne.io>
2025-02-05 13:57:50 +0100

    Restore feature flags in LombokTest for Java 8

--- rewrite-java-tck/src/main/java/org/openrewrite/java/tree/LombokTest.java
+++ rewrite-java-tck/src/main/java/org/openrewrite/java/tree/LombokTest.java
@@ -15,6 +15,8 @@
  */
 package org.openrewrite.java.tree;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.openrewrite.java.JavaParser;
@@ -33,6 +35,17 @@ import static org.openrewrite.java.Assertions.java;
 
 @SuppressWarnings({"CaughtExceptionImmediatelyRethrown", "LombokGetterMayBeUsed", "LombokSetterMayBeUsed", "DefaultAnnotationParam", "NotNullFieldNotInitialized", "ProtectedMemberInFinalClass", "WriteOnlyObject", "ConcatenationWithEmptyString"})
 class LombokTest implements RewriteTest {
+
+    @BeforeAll
+    static void setUp() {
+        // Only needed for Java 8, until enabled by default there
+        System.setProperty("rewrite.lombok", "true");
+    }
+
+    @AfterAll
+    static void tearDown() {
+        System.clearProperty("rewrite.lombok");
+    }
 
     @Override
     public void defaults(RecipeSpec spec) {
