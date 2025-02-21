235bf357041f91bc08ea73602f2146c44d70daa8
Gabriel de Aguiar Féo <gfeo@gradle.com>
2025-02-21 18:46:30 +0000

    Apply reproducible-builds plugin to ensure stable JAR

--- rewrite-java-tck/build.gradle.kts
+++ rewrite-java-tck/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("org.openrewrite.build.language-library")
     id("org.openrewrite.build.java8-text-blocks")
+    // Prevent cache misses due to metadata differences inside the JAR
+    id("org.gradlex.reproducible-builds") version "1.0"
 }
 
 dependencies {
