96679be8f92cc47da1427ced1074e6e0ed7e2ec4
Laurens Westerlaken <laurens.westerlaken@jdriven.com>
2025-02-24 19:36:31 +0100

    Parse properties from `maven.config` file (#5070)
    * Add simple testcase
    
    * Add a little more advanced testcase
    
    * Add assertions to verify revision is properly parsed
    
    * Add `upgradeVersionWithConfigFile` test
    
    * Simplify test slightly
    
    * Add `mavenPartlyManagedInParent` test
    
    * Revert test (only focus on parser)
    
    * Revert test (only focus on parser)
    
    * improvement
    
    * Add `upgradeVersionWithGroupIdAndArtifactIdDefinedAsMavenConfigProperty` test
    
    * Fix parsing of the `maven.config` for profiles and properties
    
    * Format
    
    * Cleanup tests that are no longer important
    
    * Remove redundant things and format
    
    * Make the test utility more versatile
    
    * Make backwards compatible
    
    * Apply feedback
    
    ---------
    
    Co-authored-by: lingenj <jacob.van.lingen@moderne.io>

--- rewrite-maven/src/test/java/org/openrewrite/maven/MavenParserTest.java
+++ rewrite-maven/src/test/java/org/openrewrite/maven/MavenParserTest.java
@@ -1017,7 +1017,7 @@ class MavenParserTest implements RewriteTest {
             assertThat(maven.getMarkers().findFirst(MavenResolutionResult.class).orElseThrow().getDependencies().get(Scope.Compile))
               .hasSize(1)
               .matches(deps -> deps.get(0).getGroupId().equals("com.foo") &&
-                deps.get(0).getArtifactId().equals("bar"));
+                               deps.get(0).getArtifactId().equals("bar"));
             mockRepo.shutdown();
         }
     }
@@ -1260,7 +1260,7 @@ class MavenParserTest implements RewriteTest {
 
         @Issue("https://github.com/openrewrite/rewrite/issues/4269")
         @DisplayName("activeByDefault=true profiles from a POM should be active " +
-          "unless there is another active profile _from the same POM file_")
+                     "unless there is another active profile _from the same POM file_")
         @Test
         void activeByDefaultWithoutPomLocalActiveProfile() {
             rewriteRun(
@@ -1387,7 +1387,7 @@ class MavenParserTest implements RewriteTest {
 
         @Issue("https://github.com/openrewrite/rewrite/issues/4269")
         @DisplayName("activeByDefault=true profiles from a POM should not be active" +
-          " if there is another active profile _from the same POM file_")
+                     " if there is another active profile _from the same POM file_")
         @Test
         void activeByDefaultWithPomLocalActiveProfile() {
             expectMavenDownloadingException("active-profile-1");
@@ -1395,7 +1395,7 @@ class MavenParserTest implements RewriteTest {
 
         @Issue("https://github.com/openrewrite/rewrite/issues/4269")
         @DisplayName("activeByDefault=true profiles from a POM should not be active" +
-          " if deactivated from the command-line")
+                     " if deactivated from the command-line")
         @Test
         @Disabled
         void activeByDefaultWithProfileDisabled() {
@@ -1744,7 +1744,7 @@ class MavenParserTest implements RewriteTest {
                 assertThat(pomXml.getMarkers().findFirst(MavenResolutionResult.class).orElseThrow().getDependencies().get(Scope.Compile))
                   .hasSize(7)
                   .matches(deps -> deps.get(0).getArtifactId().equals("guava") &&
-                    deps.get(0).getVersion().equals("29.0-jre"))
+                                   deps.get(0).getVersion().equals("29.0-jre"))
               )
             )
           )
@@ -1818,9 +1818,9 @@ class MavenParserTest implements RewriteTest {
                       .getDependencies().get(Scope.Compile);
                     assertThat(compileDependencies).hasSize(2);
                     assertThat(compileDependencies).anyMatch(it -> it.getArtifactId().equals("b") &&
-                      it.getVersion().equals("0.1.0-SNAPSHOT"));
+                                                                   it.getVersion().equals("0.1.0-SNAPSHOT"));
                     assertThat(compileDependencies).anyMatch(it -> it.getArtifactId().equals("d") &&
-                      it.getVersion().equals("0.1.0-SNAPSHOT"));
+                                                                   it.getVersion().equals("0.1.0-SNAPSHOT"));
                 })
               ),
               mavenProject("b-parent",
@@ -1961,7 +1961,7 @@ class MavenParserTest implements RewriteTest {
                   var compileDependencies = pomXml.getMarkers().findFirst(MavenResolutionResult.class).orElseThrow()
                     .getDependencies().get(Scope.Compile);
                   assertThat(compileDependencies).anyMatch(it -> it.getArtifactId().equals("junit") &&
-                    it.getVersion().equals("4.11"));
+                                                                 it.getVersion().equals("4.11"));
                   assertThat(compileDependencies).noneMatch(it -> it.getArtifactId().equals("hamcrest-core"));
               })
             )
@@ -3707,4 +3707,75 @@ class MavenParserTest implements RewriteTest {
           )
         );
     }
+
+    @Test
+    void propertyFromMavenConfig() {
+        rewriteRun(
+          pomXml(
+            """
+              <project>
+                <groupId>com.mycompany.app</groupId>
+                <artifactId>parent</artifactId>
+                <version>${revision}</version>
+              </project>
+              """,
+            spec -> spec.afterRecipe(p -> {
+                  var results = p.getMarkers().findFirst(MavenResolutionResult.class).orElseThrow();
+                  assertThat(results.getPom().getVersion()).isEqualTo("${revision}");
+                  assertThat(results.getPom().getProperties().get("revision")).isEqualTo("1.0.0");
+              }
+            ),
+            MavenParser.builder().mavenConfig(
+              """
+                -Drevision=1.0.0
+                """
+            )
+          )
+        );
+    }
+
+    @Test
+    void profilesFromMavenConfig() {
+        rewriteRun(
+          pomXml(
+            """
+              <project>
+                <groupId>com.mycompany.app</groupId>
+                <artifactId>parent</artifactId>
+                <version>1.0.0</version>
+              </project>
+              """,
+            spec -> spec.afterRecipe(p -> {
+                  var results = p.getMarkers().findFirst(MavenResolutionResult.class).orElseThrow();
+                  assertThat(results.getPom().getActiveProfiles()).contains("a", "b", "c");
+              }
+            ),
+            MavenParser.builder().mavenConfig(
+              """
+                -P a,b,c
+                """
+            )
+          )
+        );
+    }
+
+    @Test
+    void emptyMavenConfig() {
+        rewriteRun(
+          pomXml(
+            """
+              <project>
+                <groupId>com.mycompany.app</groupId>
+                <artifactId>parent</artifactId>
+                <version>1.0.0</version>
+              </project>
+              """,
+            MavenParser.builder().mavenConfig(
+              """
+                """
+            )
+          )
+        );
+    }
+
 }
