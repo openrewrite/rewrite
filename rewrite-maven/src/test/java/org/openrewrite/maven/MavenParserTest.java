8eddda28ddfa42778710d26b7b1718c5de76deef
Sam Snyder <sam@moderne.io>
2025-02-24 12:13:55 -0800

    Add disabled test case reproducing an issue

--- rewrite-maven/src/test/java/org/openrewrite/maven/MavenParserTest.java
+++ rewrite-maven/src/test/java/org/openrewrite/maven/MavenParserTest.java
@@ -3778,4 +3778,55 @@ class MavenParserTest implements RewriteTest {
         );
     }
 
+    /**
+     * Maven successfully resolves this pom, but warns that transitive dependencies are invalid. e.g.:
+     * [WARNING] The POM for com.sun.xml.stream.buffer:streambuffer:jar:0.6 is invalid, transitive dependencies (if any) will not be available, enable debug logging for more details
+     * [WARNING] The POM for org.jvnet.staxex:stax-ex:jar:1.0 is invalid, transitive dependencies (if any) will not be available, enable debug logging for more details
+     * [WARNING] The POM for com.sun.xsom:xsom:jar:20070323 is invalid, transitive dependencies (if any) will not be available, enable debug logging for more details
+     * Looking inside streambuffer's pom it is missing version numbers for its "activation:activation" dependency.
+     * Maven lists these as the dependencies of this pom and we should match its behavior:
+     *   org.jvnet.jax-ws-commons:jaxws-json:jar:1.1:compile
+     *   +- com.sun.xml.ws:jaxws-rt:jar:2.1.2-alpha-20070426:compile
+     *   |  +- javax.xml.ws:jaxws-api:jar:2.1:compile
+     *   |  +- com.sun.xml.messaging.saaj:saaj-impl:jar:1.3:compile
+     *   |  |  \- javax.xml.soap:saaj-api:jar:1.3:compile
+     *   |  +- com.sun.xml.stream:sjsxp:jar:1.0:compile
+     *   |  |  \- javax.xml.stream:stax-api:jar:1.0:compile
+     *   |  \- org.jvnet.staxex:stax-ex:jar:1.0:compile
+     *   +- com.sun.xml.bind:jaxb-impl:jar:2.1.3:compile
+     *   |  \- javax.xml.bind:jaxb-api:jar:2.1:compile
+     *   |     \- javax.activation:activation:jar:1.1:compile
+     *   +- com.sun.xml.stream.buffer:streambuffer:jar:0.6:compile
+     *   +- com.sun.xsom:xsom:jar:20070323:compile
+     *   +- org.codehaus.jettison:jettison:jar:1.0-beta-1:compile
+     *   |  +- junit:junit:jar:3.8.1:compile
+     *   |  \- stax:stax-api:jar:1.0.1:compile
+     *   +- velocity:velocity:jar:1.5:compile
+     *   |  +- commons-collections:commons-collections:jar:3.1:compile
+     *   |  +- commons-lang:commons-lang:jar:2.1:compile
+     *   |  \- oro:oro:jar:2.0.8:compile
+     *   \- com.sun.xml:relaxngDatatype:jar:1.0:compile
+     */
+    @Disabled
+    @Test
+    void invalidTransitives() {
+        rewriteRun(
+          pomXml(
+            """
+              <project>
+                <groupId>com.mycompany.app</groupId>
+                <artifactId>app</artifactId>
+                <version>1.0.0</version>
+                  <dependencies>
+                    <dependency>
+                      <groupId>org.jvnet.jax-ws-commons</groupId>
+                      <artifactId>jaxws-json</artifactId>
+                      <version>1.1</version>
+                    </dependency>
+                  </dependencies>
+              </project>
+              """
+          )
+        );
+    }
 }
