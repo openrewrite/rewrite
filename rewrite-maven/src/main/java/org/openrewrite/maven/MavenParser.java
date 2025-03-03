90913d437663bc47ba0b318c6ab3806227a5761e
Laurens Westerlaken <laurens.westerlaken@jdriven.com>
2025-03-03 14:09:34 +0100

    Remove no longer necessary

--- rewrite-maven/src/main/java/org/openrewrite/maven/MavenParser.java
+++ rewrite-maven/src/main/java/org/openrewrite/maven/MavenParser.java
@@ -77,9 +77,6 @@ public class MavenParser implements Parser {
                 if (pom.getProperties().isEmpty()) {
                     pom = pom.withProperties(new LinkedHashMap<>());
                 }
-                String baseDir = pomPath.toAbsolutePath().getParent().toString();
-                pom.getProperties().put("project.basedir", baseDir);
-                pom.getProperties().put("basedir", baseDir);
                 pom.getProperties().putAll(properties);
 
                 SourceFile sourceFile = new MavenXmlParser()
