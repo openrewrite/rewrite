671f030fac30e92af576ccd275a52a8c444e1239
Greg Oledzki <greg.oledzki@moderne.io>
2025-03-10 12:10:15 +0100

    Handling enums with no values as J.EnumValueSet (#5147)
    * Handling enums with no values as J.EnumValueSet
    
    * Inline variable
    
    Co-authored-by: Jacob van Lingen <jacob.van.lingen@moderne.io>
    
    * Inline variable
    
    ---------
    
    Co-authored-by: Jacob van Lingen <jacob.van.lingen@moderne.io>

--- rewrite-java-21/src/main/java/org/openrewrite/java/isolated/ReloadableJava21ParserVisitor.java
+++ rewrite-java-21/src/main/java/org/openrewrite/java/isolated/ReloadableJava21ParserVisitor.java
@@ -515,6 +515,10 @@ public class ReloadableJava21ParserVisitor extends TreePathScanner<J, Space> {
                     ),
                     EMPTY
             );
+        } else if (kind.getType() == J.ClassDeclaration.Kind.Type.Enum) {
+            if (positionOfNext(";", null) >= 0) {
+                enumSet = padRight(new J.EnumValueSet(randomId(), sourceBefore(";"), Markers.EMPTY, emptyList(), true), EMPTY);
+            }
         }
 
         List<Tree> membersMultiVariablesSeparated = new ArrayList<>(node.getMembers().size());
