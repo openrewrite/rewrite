16dcce183fc0f3dd1f728f10052cda8a31eab035
Greg Oledzki <greg.oledzki@moderne.io>
2025-04-08 09:48:02 +0200

    get() goes private

--- rewrite-maven/src/main/java/org/openrewrite/maven/MavenHomeDirectory.java
+++ rewrite-maven/src/main/java/org/openrewrite/maven/MavenHomeDirectory.java
@@ -19,7 +19,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class MavenHomeDirectory {
-    public static Path get() {
+    private static Path get() {
         String m2Home = System.getenv("M2_HOME");
         if (m2Home != null) {
             return Paths.get(m2Home);
