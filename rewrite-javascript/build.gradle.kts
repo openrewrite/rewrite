d06428214d16a0d75c2540bc3f2d135db7de349e
Jonathan Schneider <jkschneider@gmail.com>
2025-04-08 09:17:28 -0400

    [wip] build

--- rewrite-javascript/build.gradle.kts
+++ rewrite-javascript/build.gradle.kts
@@ -1,6 +1,9 @@
+import org.eclipse.jgit.api.Git
+
 plugins {
     id("org.openrewrite.build.language-library")
     id("org.openrewrite.build.moderne-source-available-license")
+    id("com.netflix.nebula.integtest-standalone")
     id("com.github.node-gradle.node") version "latest.release"
 }
 
@@ -18,7 +21,43 @@ dependencies {
     testImplementation("io.moderne:jsonrpc:latest.release")
 }
 
-tasks.named("publishToMavenLocal") {
+//tasks.named("snapshot") {
+// npm run build
+//}
 
+// snapshot
+// npm version ${project.version.replace('SNAPSHOT', System.currentTimeMillis())}
+// npm publish --tag next
+
+// to later install this snapshot: npm install @openrewrite/rewrite@next
+
+//tasks.named("final") {
+// npm version ${project.version}
+// npm publish
+// `--tag latest` is implicitly the case if not specified
+// git reset package.json package.lock
+//}
+
+val npmTest = tasks.named("npm_test")
+
+tasks.check {
+    dependsOn(
+        tasks.named("npmInstall"),
+        npmTest
+    )
 }
 
+tasks.named("integrationTest") {
+    dependsOn(tasks.named("npmInstall"))
+}
+
+class NpmPublish : DefaultTask() {
+    override fun dependsOn(vararg paths: Any?): Task {
+        return super.dependsOn(*paths)
+    }
+
+    @TaskAction
+    fun publish() {
+        val git = Git.open(project.rootDir)
+    }
+}
