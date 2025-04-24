459390fcf5aa6317c4c17b91dfa727fae31849e6
Tim te Beek <tim@moderne.io>
2025-04-24 14:24:28 +0200

    Rename test class to match

--- rewrite-maven/src/test/java/org/openrewrite/maven/plugin/DependencyPluginGoalResolveSourcesTest.java
+++ rewrite-maven/src/test/java/org/openrewrite/maven/plugin/DependencyPluginGoalResolveSourcesTest.java
@@ -22,7 +22,7 @@ import org.openrewrite.test.RewriteTest;
 import static org.openrewrite.java.Assertions.mavenProject;
 import static org.openrewrite.maven.Assertions.pomXml;
 
-class DependencyPluginSourcesToResolveSourcesTest implements RewriteTest {
+class DependencyPluginGoalResolveSourcesTest implements RewriteTest {
 
     @Override
     public void defaults(RecipeSpec spec) {
