39893a845f4f06e25d75dab810b74a350c7acbb9
Jente Sondervorst <jentesondervorst@gmail.com>
2025-04-28 22:37:16 +0200

    Do not bump transitive dependencies if there is a direct dependency also
    Do not bump transitive dependencies if there is a direct dependency also
    
    Fixes #5355

--- rewrite-gradle/src/test/java/org/openrewrite/gradle/UpgradeTransitiveDependencyVersionTest.java
+++ rewrite-gradle/src/test/java/org/openrewrite/gradle/UpgradeTransitiveDependencyVersionTest.java
@@ -679,6 +679,25 @@ class UpgradeTransitiveDependencyVersionTest implements RewriteTest {
     }
 
     @Test
+    void noChangesIfDependencyIsAlsoPresentOnProject() {
+        rewriteRun(
+          buildGradle(
+            """
+              plugins {
+                id 'java'
+              }
+              repositories { mavenCentral() }
+              
+              dependencies {
+                  implementation('com.fasterxml.jackson.core:jackson-core:2.12.0')
+                  implementation('com.fasterxml.jackson.core:jackson-databind:2.12.0')
+              }
+              """
+          )
+        );
+    }
+
+    @Test
     void kotlinDslAddConstraint() {
         rewriteRun(
           buildGradleKts(
