b4a8da7d8445a9eb42dd70001183b26717bfcc2d
Knut Wannheden <knut@moderne.io>
2025-04-29 11:32:25 +0200

    JS parser

--- rewrite-javascript/rewrite/src/java/markers.ts
+++ rewrite-javascript/rewrite/src/java/markers.ts
@@ -2,11 +2,14 @@ import {Marker} from "../markers";
 import {Space} from "./tree";
 
 export interface Semicolon extends Marker {
+    kind: "org.openrewrite.java.marker.Semicolon"
 }
 
 export interface TrailingComma extends Marker {
+    kind: "org.openrewrite.java.marker.TrailingComma"
     readonly suffix: Space;
 }
 
 export interface OmitParentheses extends Marker {
+    kind: "org.openrewrite.java.marker.OmitParentheses"
 }
