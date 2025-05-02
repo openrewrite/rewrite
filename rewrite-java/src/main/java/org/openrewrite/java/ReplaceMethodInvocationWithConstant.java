347204abf13743e2766901e7c338c91add09ad8a
Merlin Bögershausen <merlin.boegershausen@rwth-aachen.de>
2025-05-02 12:56:10 +0200

    Add a recipe to replace `J.MethodInvocation`s with a constant value

---
+++ rewrite-java/src/main/java/org/openrewrite/java/ReplaceMethodInvocationWithConstant.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2025 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java;
+
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+import org.openrewrite.*;
+import org.openrewrite.java.search.UsesMethod;
+import org.openrewrite.java.trait.MethodAccess;
+import org.openrewrite.java.trait.Traits;
+import org.openrewrite.java.tree.CoordinateBuilder;
+import org.openrewrite.java.tree.Expression;
+import org.openrewrite.trait.VisitFunction;
+
+@Value
+@EqualsAndHashCode(callSuper = false)
+public class ReplaceMethodInvocationWithConstant extends Recipe {
+
+    @Option(displayName = "Method pattern",
+            description = "A pattern to match method invocations to replace. " + MethodMatcher.METHOD_PATTERN_DESCRIPTION,
+            example = "java.lang.StringBuilder append(java.lang.String)")
+    String methodPattern;
+
+    @Option(displayName = "Replacement",
+            description = "The constant to replace the method invocation with.",
+            example = "null")
+    String replacement;
+
+    @Override
+    public String getDisplayName() {
+        return "Replace method invocation with constant";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Replace all method invocations matching the method pattern with the specified constant.";
+    }
+
+    @Override
+    public TreeVisitor<?, ExecutionContext> getVisitor() {
+        return Preconditions.check(new UsesMethod<>(methodPattern),
+                Traits.methodAccess(methodPattern).asVisitor(replaceWith(replacement)));
+    }
+
+    private VisitFunction<MethodAccess> replaceWith(String replacement) {
+        return new VisitFunction<MethodAccess>() {
+            final JavaTemplate replacementTemplate = JavaTemplate.builder(replacement).build();
+
+            @Override
+            public Tree visit(MethodAccess ma) {
+                return replacementTemplate.apply(ma.getCursor(), coordinateOf(ma).replace());
+            }
+
+            CoordinateBuilder.Expression coordinateOf(MethodAccess ma) {
+                return ((Expression) ma.getCursor().getValue()).getCoordinates();
+            }
+        };
+    }
+
+
+}
