e38b7951ef0dbf08a97047221165413dc0f26cb6
lingenj <jacob.van.lingen@moderne.io>
2025-05-06 09:36:44 +0200

    Polish

--- rewrite-yaml/src/main/java/org/openrewrite/yaml/UnfoldProperties.java
+++ rewrite-yaml/src/main/java/org/openrewrite/yaml/UnfoldProperties.java
@@ -21,7 +21,10 @@ import org.jspecify.annotations.Nullable;
 import org.openrewrite.*;
 import org.openrewrite.yaml.tree.Yaml;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
