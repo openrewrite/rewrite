9369ba92ab4896ed1d9aea04eafdb41cfae37821
Merlin Bögershausen <merlin.boegershausen@rwth-aachen.de>
2025-05-06 14:10:09 +0200

    add additional test case

--- rewrite-java/src/test/java/org/openrewrite/java/style/CheckstyleConfigLoaderTest.java
+++ rewrite-java/src/test/java/org/openrewrite/java/style/CheckstyleConfigLoaderTest.java
@@ -386,6 +386,27 @@ class CheckstyleConfigLoaderTest {
     }
 
     @Test
+    void unusedImportsWithoutExpliciteJavadocProcessing() throws CheckstyleException {
+        var checkstyle = loadCheckstyleConfig("""
+            <!DOCTYPE module PUBLIC
+                "-//Checkstyle//DTD Checkstyle Configuration 1.2//EN"
+                "https://checkstyle.org/dtds/configuration_1_2.dtd">
+            <module name="Checker">
+                <module name="TreeWalker">
+                    <module name="UnusedImports">
+                        <property name="processJavadoc" value="false"/>
+                    </module>
+                </module>
+            </module>
+        """, emptyMap());
+
+        assertThat(checkstyle.getStyles()).hasSize(1).allSatisfy(style -> {
+            assertThat(style).isInstanceOf(UnusedImportsStyle.class);
+            assertThat(((UnusedImportsStyle) style).isProcessJavadoc()).isFalse();
+        });
+    }
+
+    @Test
     void unusedImportsWithoutJavadocProcessing() throws CheckstyleException {
         var checkstyle = loadCheckstyleConfig("""
             <!DOCTYPE module PUBLIC
