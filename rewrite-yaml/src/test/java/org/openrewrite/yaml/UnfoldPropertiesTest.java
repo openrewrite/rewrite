dce93af8c0e73aefc0539b084480b19dfb0aa753
Jacob van Lingen <jacob.van.lingen@moderne.io>
2025-05-06 13:37:57 +0200

    Improve UnfoldProperties recipe (#5312)
    * Add new tests to UnfoldPropertiesTest to show the missing pieces
    
    * Polish test (not two features in one test)
    
    * Implement `exclusionWithSubSetOfKey` and `exclusionWithRegex`
    
    * Implement `exclusionWithParentAndRegex`
    
    * Implement `mergeDuplicatedSections`
    
    * Implement `mergeDuplicatedSections`
    
    * Polish
    
    * Polish
    
    * Polish
    
    * Replace custom syntax with JSONPath Plus syntax
    
    * Replace custom syntax with JSONPath Plus syntax
    
    * Polish
    
    * Polish
    
    * Polish
    
    * Polish
    
    * Polish
    
    * Polish

--- rewrite-yaml/src/test/java/org/openrewrite/yaml/UnfoldPropertiesTest.java
+++ rewrite-yaml/src/test/java/org/openrewrite/yaml/UnfoldPropertiesTest.java
@@ -16,6 +16,7 @@
 package org.openrewrite.yaml;
 
 import org.junit.jupiter.api.Test;
+import org.junitpioneer.jupiter.ExpectedToFail;
 import org.openrewrite.DocumentExample;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
@@ -34,8 +35,13 @@ class UnfoldPropertiesTest implements RewriteTest {
     @Test
     void unfold() {
         rewriteRun(
+          spec -> spec.recipe(new UnfoldProperties(List.of("$..[logging.level][?(@property.match(/.*/))]", "$..[enable.process.files]"))),
           yaml(
             """
+              spring.application.name: my-app
+              logging.level:
+                root: INFO
+                org.springframework.web: DEBUG
               management:
                 metrics.enable.process.files: true
                 endpoint.health:
@@ -43,11 +49,16 @@ class UnfoldPropertiesTest implements RewriteTest {
                   show-details: always
               """,
             """
+              spring:
+                application:
+                  name: my-app
+              logging:
+                level:
+                  root: INFO
+                  org.springframework.web: DEBUG
               management:
                 metrics:
-                  enable:
-                    process:
-                      files: true
+                  enable.process.files: true
                 endpoint:
                   health:
                     show-components: always
@@ -73,7 +84,8 @@ class UnfoldPropertiesTest implements RewriteTest {
                       # Comment 2
                       7.8.10:
                         # Comment 3
-                        11.12: false
+                        11.12: false # Comment 4
+              # Comment 5
               add.more: value
               """,
             """
@@ -95,7 +107,8 @@ class UnfoldPropertiesTest implements RewriteTest {
                               10:
                                 # Comment 3
                                 11:
-                                  12: false
+                                  12: false # Comment 4
+              # Comment 5
               add:
                 more: value
               """
@@ -126,14 +139,196 @@ class UnfoldPropertiesTest implements RewriteTest {
     @Test
     void exclusions() {
         rewriteRun(
-          spec -> spec.recipe(new UnfoldProperties(List.of("metrics.enable.process.files", "endpoint.health"))),
+          spec -> spec.recipe(new UnfoldProperties(List.of(
+            "$..['metrics.enable.process.files']",
+            "$..[\"endpoint.health\"]",
+            "$..[show.details]",
+            "$.management.endpoint.health[show.controllers]",
+            "$.management.endpoint.health.show.views"
+          ))),
           yaml(
             """
               management:
                 metrics.enable.process.files: true
                 endpoint.health:
                   show-components: always
-                  show-details: always
+                  show.details: always
+                  show.controllers: always
+                  show.views: always
+              """
+          )
+        );
+    }
+
+    @Test
+    void exclusionWithSubSetOfKey() {
+        rewriteRun(
+          spec -> spec.recipe(new UnfoldProperties(List.of("$..['com.service']"))),
+          yaml(
+            """
+              logging.level.com.service.A: DEBUG
+              logging.level.com.service.B: DEBUG
+              """,
+            """
+              logging:
+                level:
+                  com.service:
+                    A: DEBUG
+                    B: DEBUG
+              """
+          )
+        );
+    }
+
+    @Test
+    void exclusionWithRegex() {
+        rewriteRun(
+          spec -> spec.recipe(new UnfoldProperties(List.of("$..[?(@property.match(/some.*/))]"))),
+          yaml(
+            """
+              A.B:
+                com.some.service: DEBUG
+              """,
+            """
+             A:
+               B:
+                 com:
+                   some.service: DEBUG
+             """
+          )
+        );
+    }
+
+    @Test
+    void exclusionWithParentAndRegex() {
+        rewriteRun(
+          spec -> spec.recipe(new UnfoldProperties(List.of("$..[logging.level][?(@property.match(/^com.*/))]"))),
+          yaml(
+            """
+              first:
+                logging.level:
+                  com.company.extern.service: DEBUG
+                  com.another.package: INFO
+              some.things.else: value
+              """,
+            """
+              first:
+                logging:
+                  level:
+                    com.company.extern.service: DEBUG
+                    com.another.package: INFO
+              some:
+                things:
+                  else: value
+              """
+          )
+        );
+    }
+
+    @Test
+    void mergeDuplicatedSections() {
+        rewriteRun(
+          yaml(
+            """
+              logging.level:
+                com.first.package: INFO
+              some.thing: else
+              logging.level.com.second.package: DEBUG
+              other: thing
+              logging.level.com.third.package: DEBUG
+              logging.level.com:
+                fourth.package: DEBUG
+              """,
+            """
+              logging:
+                level:
+                  com:
+                    first:
+                      package: INFO
+                    second:
+                      package: DEBUG
+                    third:
+                      package: DEBUG
+                    fourth:
+                      package: DEBUG
+              some:
+                thing: else
+              other: thing
+              """
+          )
+        );
+    }
+
+    @Test
+    // TODO implement handling of comments, remove this test and enable the `mergeDuplicatedSectionsWitComments` test
+    void mergeDuplicatedSectionsCommentsAreIgnored() {
+        rewriteRun(
+          yaml(
+            """
+              logging.level:
+                 # unaltered comment
+                com.first.package: INFO
+              logging.level.com.second.package: DEBUG # comment 1
+              logging.level.com.third.package: DEBUG # comment 2
+              """,
+            """
+              logging:
+                level:
+                  # unaltered comment
+                  com:
+                    first:
+                      package: INFO
+              logging:
+                level:
+                  com:
+                    second:
+                      package: DEBUG # comment 1
+              logging:
+                level:
+                  com:
+                    third:
+                      package: DEBUG # comment 2
+              """
+          )
+        );
+    }
+
+    @Test
+    @ExpectedToFail("Comments are not supported yet")
+    void mergeDuplicatedSectionsWitComments() {
+        rewriteRun(
+          yaml(
+            """
+              # comment 1
+              logging.level:
+                # comment 4
+                com.first.package: INFO # comment 5
+              some.thing: else
+              # comment 2
+              logging.level.com.second.package: DEBUG  # comment 6
+              other: thing
+              # comment 3
+              logging.level.com.third.package: DEBUG  # comment 7
+              logging.level.com:
+                fourth.package: DEBUG  # comment 8
+              """,
+            """
+              # comment 1
+              # comment 2
+              # comment 3
+              logging:
+                level:
+                  # comment 4
+                  com:
+                    first:
+                      package: INFO # comment 5
+                    second:
+                      package: DEBUG # comment 6
+                    third:
+                      package: DEBUG # comment 7
+              some:
+                thing: else
+              other: thing
               """
           )
         );
