e6e4a9ab895114a154e1bfce712d5bf5a591a2fe
Francisco Modesto <fran.bcn@gmail.com>
2025-05-09 06:43:05 +0100

    fixes

--- rewrite-java-tck/src/main/java/org/openrewrite/java/tree/TypeUtilsTest.java
+++ rewrite-java-tck/src/main/java/org/openrewrite/java/tree/TypeUtilsTest.java
@@ -15,7 +15,6 @@
  */
 package org.openrewrite.java.tree;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.openrewrite.ExecutionContext;
 import org.openrewrite.InMemoryExecutionContext;
@@ -337,7 +336,7 @@ class TypeUtilsTest implements RewriteTest {
     }
 
     @Test
-    void isAssignableToGenericTypeVariable1() {
+    void isAssignableToGenericTypeVariable() {
         rewriteRun(
           java(
             """
