2eab3c8c02babb309cbcb705e5f1633a9ad4647c
Jonathan Schneider <jkschneider@gmail.com>
2025-05-09 07:17:17 +0200

    Fix licenses

--- rewrite-javascript/rewrite/test/javascript/parser/intersection.test.ts
+++ rewrite-javascript/rewrite/test/javascript/parser/intersection.test.ts
@@ -1,11 +1,11 @@
 /*
- * Copyright 2023 the original author or authors.
+ * Copyright 2025 the original author or authors.
  * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Moderne Source Available License (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  * <p>
- * https://www.apache.org/licenses/LICENSE-2.0
+ * https://docs.moderne.io/licensing/moderne-source-available-license
  * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -29,6 +29,7 @@ describe('intersection type mapping', () => {
             //language=typescript
             typescript('let c: & true & 1 & "foo"')
         ));
+    // noinspection TypeScriptUnresolvedReference
     test('union which starts with & ', () =>
         spec.rewriteRun(
             //language=typescript
--- rewrite-javascript/rewrite/test/javascript/parser/type-literal.test.ts
+++ rewrite-javascript/rewrite/test/javascript/parser/type-literal.test.ts
@@ -1,19 +1,18 @@
 /*
- * Copyright 2023 the original author or authors.
+ * Copyright 2025 the original author or authors.
  * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Moderne Source Available License (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  * <p>
- * https://www.apache.org/licenses/LICENSE-2.0
+ * https://docs.moderne.io/licensing/moderne-source-available-license
  * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
-import {RecipeSpec} from "../../../src/test";
+ */import {RecipeSpec} from "../../../src/test";
 import {typescript} from "../../../src/javascript";
 
 describe('type literal mapping', () => {
