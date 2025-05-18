1dff8f7625ae38ea421632285114a24a5283c31c
Francisco Modesto <fran.bcn@gmail.com>
2025-05-18 19:47:18 +0100

    Test modifiers

--- rewrite-java-test/src/test/java/org/openrewrite/java/JavaTemplateGenericsTest.java
+++ rewrite-java-test/src/test/java/org/openrewrite/java/JavaTemplateGenericsTest.java
@@ -16,7 +16,6 @@
 package org.openrewrite.java;
 
 import org.junit.jupiter.api.Test;
-import org.junitpioneer.jupiter.ExpectedToFail;
 import org.openrewrite.ExecutionContext;
 import org.openrewrite.java.tree.Expression;
 import org.openrewrite.java.tree.J;
@@ -156,6 +155,50 @@ class JavaTemplateGenericsTest implements RewriteTest {
               class Test {
                   void test() {
                       /*~~>*/Assertions.assertThat(ImmutableSet.of(1)).size().isLessThan(2).returnToIterable();
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void methodModifiersMismatch() {
+        rewriteRun(
+          spec -> spec.recipe(toRecipe(() -> new JavaIsoVisitor<>() {
+              final JavaTemplate template = JavaTemplate.builder("#{stream:any(java.util.stream.Stream<T>)}.filter(#{map:any(java.util.Map<K, V>)}::containsKey).map(#{map}::get)")
+                .genericTypes("T", "K", "V")
+                .build();
+
+              @Override
+              public J.MethodInvocation visitMethodInvocation(J.MethodInvocation method, ExecutionContext executionContext) {
+                  return template.matches(getCursor()) ? SearchResult.found(method) : super.visitMethodInvocation(method, executionContext);
+              }
+          })),
+          java(
+            """
+              import com.google.common.collect.ImmutableMap;
+              
+              import java.util.function.Function;
+              import java.util.function.Predicate;
+              import java.util.stream.Stream;
+              
+              class Test {
+                  Stream<Integer> test() {
+                      return Stream.of("foo").filter(ImmutableMap.of(1, 2)::containsKey).map(ImmutableMap.of(1, 2)::get);
+                  }
+              }
+              """,
+            """
+              import com.google.common.collect.ImmutableMap;
+              
+              import java.util.function.Function;
+              import java.util.function.Predicate;
+              import java.util.stream.Stream;
+              
+              class Test {
+                  Stream<Integer> test() {
+                      return /*~~>*/Stream.of("foo").filter(ImmutableMap.of(1, 2)::containsKey).map(ImmutableMap.of(1, 2)::get);
                   }
               }
               """
