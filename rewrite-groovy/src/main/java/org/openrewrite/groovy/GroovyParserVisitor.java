94f604a1a78fd627b9f308ce7a624ed419513725
JohannisK <johan.kragt@moderne.io>
2025-05-22 16:02:41 +0200

    Fix for idempotency exception when build gradle contains shell commands with escape characters (#5476)
    * Added tests showing the issue
    
    * Added check to skip escaped '$' signs in build.groovy files
    
    * Added support for escaped $ in single quoted strings, added tests showing correct parsing, added test showing correct behavior

--- rewrite-groovy/src/main/java/org/openrewrite/groovy/GroovyParserVisitor.java
+++ rewrite-groovy/src/main/java/org/openrewrite/groovy/GroovyParserVisitor.java
@@ -1602,6 +1602,9 @@ public class GroovyParserVisitor {
                     String value = sourceSubstring(cursor, delimiter.close);
                     // There could be a closer GString before the end of the closing delimiter, so shorten the string if needs be
                     int indexNextSign = source.indexOf("$", cursor);
+                    while (indexNextSign > 0 && source.charAt(indexNextSign - 1) == '\\') {
+                        indexNextSign = source.indexOf("$", indexNextSign + 1);
+                    }
                     if (indexNextSign != -1 && indexNextSign < (cursor + value.length())) {
                         value = source.substring(cursor, indexNextSign);
                     }
