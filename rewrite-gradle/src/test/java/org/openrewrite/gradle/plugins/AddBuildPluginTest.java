3bec246555d8a99d8ef93093e13645e66c475957
Andrii Rodionov <andrii@moderne.io>
2025-05-26 14:11:33 +0200

    Plugin management support for Gradle Kotlin DSL

--- rewrite-gradle/src/test/java/org/openrewrite/gradle/plugins/AddBuildPluginTest.java
+++ rewrite-gradle/src/test/java/org/openrewrite/gradle/plugins/AddBuildPluginTest.java
@@ -23,6 +23,7 @@ import org.openrewrite.test.RewriteTest;
 
 import static org.openrewrite.Tree.randomId;
 import static org.openrewrite.gradle.Assertions.buildGradle;
+import static org.openrewrite.gradle.Assertions.buildGradleKts;
 import static org.openrewrite.gradle.toolingapi.Assertions.withToolingApi;
 
 class AddBuildPluginTest implements RewriteTest {
@@ -68,6 +69,26 @@ class AddBuildPluginTest implements RewriteTest {
     }
 
     @Test
+    void addPluginWithoutVersionToExistingBlockKts() {
+        rewriteRun(
+          spec -> spec.recipe(new AddBuildPlugin("java-library", null, null, null, null)),
+          buildGradleKts(
+            """
+              plugins {
+                  id("java")
+              }
+              """,
+            """
+              plugins {
+                  id("java")
+                  id("java-library")
+              }
+              """
+          )
+        );
+    }
+
+    @Test
     void addThirdPartyPluginWithoutVersion() {
         rewriteRun(
           spec -> spec.recipe(new AddBuildPlugin("org.openrewrite.rewrite", null, null, null, null)),
@@ -88,6 +109,26 @@ class AddBuildPluginTest implements RewriteTest {
     }
 
     @Test
+    void addThirdPartyPluginWithoutVersionKts() {
+        rewriteRun(
+          spec -> spec.recipe(new AddBuildPlugin("org.openrewrite.rewrite", null, null, null, null)),
+          buildGradleKts(
+            """
+              plugins {
+                  id("java")
+              }
+              """,
+            """
+              plugins {
+                  id("java")
+                  id("org.openrewrite.rewrite")
+              }
+              """
+          )
+        );
+    }
+
+    @Test
     void addPluginWithVersionToNewBlock() {
         rewriteRun(
           spec -> spec.recipe(new AddBuildPlugin("com.jfrog.bintray", "1.0", null, null, null)),
@@ -96,6 +137,21 @@ class AddBuildPluginTest implements RewriteTest {
             """
               plugins {
                   id 'com.jfrog.bintray' version '1.0'
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void addPluginWithVersionToNewBlockKts() {
+        rewriteRun(
+          spec -> spec.recipe(new AddBuildPlugin("com.jfrog.bintray", "1.0", null, null, null)),
+          buildGradleKts(
+            "",
+            """
+              plugins {
+                   id("com.jfrog.bintray") version "1.0"
               }
               """
           )
@@ -123,6 +179,26 @@ class AddBuildPluginTest implements RewriteTest {
     }
 
     @Test
+    void addPluginWithVersionToExistingBlockKts() {
+        rewriteRun(
+          spec -> spec.recipe(new AddBuildPlugin("com.jfrog.bintray", "1.0", null, null, null)),
+          buildGradleKts(
+            """
+              plugins {
+                  id("java")
+              }
+              """,
+            """
+              plugins {
+                  id("java")
+                  id("com.jfrog.bintray") version "1.0"
+              }
+              """
+          )
+        );
+    }
+
+    @Test
     void addPluginAfterBuildscriptBlock() {
         rewriteRun(
           spec -> spec.recipe(new AddBuildPlugin("com.jfrog.bintray", "1.0", null, null, null)),
@@ -140,6 +216,29 @@ class AddBuildPluginTest implements RewriteTest {
               plugins {
                   id 'com.jfrog.bintray' version '1.0'
               }
+              """
+          )
+        );
+    }
+
+    @Test
+    void addPluginAfterBuildscriptBlockKts() {
+        rewriteRun(
+          spec -> spec.recipe(new AddBuildPlugin("com.jfrog.bintray", "1.0", null, null, null)),
+          buildGradleKts(
+            """
+              import java.util.List
+              buildscript {
+              }
+              """,
+            """
+              import java.util.List
+              buildscript {
+              }
+
+              plugins {
+                   id("com.jfrog.bintray") version "1.0"
+               }
               """
           )
         );
@@ -210,6 +309,38 @@ class AddBuildPluginTest implements RewriteTest {
     }
 
     @Test
+    void addPluginToNewBlockWithLicenseHeaderAndCommentKts() {
+        rewriteRun(
+          spec -> {
+              spec.recipe(new AddBuildPlugin("com.jfrog.bintray", "1.0", null, null, null));
+              spec.allSources(s -> s.markers(new BuildTool(randomId(), BuildTool.Type.Gradle, "7.6.1")));
+          },
+          buildGradleKts(
+            """
+              /*
+               * License header
+               */
+              // Comment
+              dependencies {
+              }
+              """,
+                """
+              /*
+               * License header
+               */
+              plugins {
+                   id("com.jfrog.bintray") version "1.0"
+              }
+
+              // Comment
+              dependencies {
+              }
+              """
+          )
+        );
+    }
+
+    @Test
     void addPluginToNewBlockWithOnlyLicenseHeader() {
         rewriteRun(
           spec -> {
@@ -249,6 +380,26 @@ class AddBuildPluginTest implements RewriteTest {
             plugins {
                 id "java"
                 id "com.jfrog.bintray" version "1.0" apply false
+            }
+            """
+          )
+        );
+    }
+
+    @Test
+    void addPluginApplyFalseKts() {
+        rewriteRun(
+          spec -> spec.recipe(new AddBuildPlugin("com.jfrog.bintray", "1.0", null, false, null)),
+          buildGradleKts(
+            """
+            plugins {
+                id("java")
+            }
+            """,
+            """
+            plugins {
+                id("java")
+                id("com.jfrog.bintray") version "1.0" apply false
             }
             """
           )
