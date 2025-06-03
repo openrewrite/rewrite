504c65169e2f045af96298a4bd28604fa130fdd6
Merlin Bögershausen <merlin.boegershausen@rwth-aachen.de>
2025-06-03 11:27:28 +0200

    move test to LombokTest and simplify

--- rewrite-java-21/src/test/java/org/openrewrite/java/Java21ParserTest.java
+++ rewrite-java-21/src/test/java/org/openrewrite/java/Java21ParserTest.java
@@ -15,26 +15,13 @@
  */
 package org.openrewrite.java;
 
-import org.intellij.lang.annotations.Language;
 import org.junit.jupiter.api.Test;
 import org.openrewrite.InMemoryExecutionContext;
-import org.openrewrite.Parser;
-import org.openrewrite.SourceFile;
-import org.openrewrite.java.internal.JavaTypeCache;
-import org.openrewrite.java.isolated.ReloadableJava21Parser;
 import org.openrewrite.test.RewriteTest;
-import org.openrewrite.tree.ParseError;
 
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Stream;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 class Java21ParserTest implements RewriteTest {
 
@@ -42,37 +29,5 @@ class Java21ParserTest implements RewriteTest {
     void shouldLoadResourceFromClasspath() throws IOException {
         Files.deleteIfExists(Paths.get(System.getProperty("user.home"), ".rewrite", "classpath", "jackson-annotations-2.17.1.jar"));
         rewriteRun(spec -> spec.parser(JavaParser.fromJavaVersion().classpathFromResources(new InMemoryExecutionContext(), "jackson-annotations")));
-    }
-
-    @Test
-    void stringIndexOutOfBoundsException() {
-        @Language("java")
-        String src = """
-          package nl.issue.test;
-          
-          import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-          import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
-          
-          import lombok.Builder;
-          
-          @Builder
-          public class Clazz {
-          
-              private final String accountID;
-              private final String documentNumber;
-          
-              @JsonPOJOBuilder(withPrefix = "")
-              public static final class ClazzBuilder {}
-          }
-          """;
-        InMemoryExecutionContext ctx = new InMemoryExecutionContext();
-        ReloadableJava21Parser build = ReloadableJava21Parser.builder()
-          .classpath("lombok", "jackson")
-          .build();
-        Stream<SourceFile> sourceFileStream = build.parseInputs(List.of(Parser.Input.fromString(src)), null, ctx);
-        Optional<SourceFile> optionalSourceFile = sourceFileStream.findFirst();
-        assertThat(optionalSourceFile).isPresent();
-        SourceFile sourceFile = optionalSourceFile.get();
-        assertThat(sourceFile).isNotInstanceOf(ParseError.class);
     }
 }
