47b1122de7f0f95eefa9afc7ab07525625e1e88e
Jente Sondervorst <jentesondervorst@gmail.com>
2025-06-03 16:30:12 +0200

    Removed licensed module from tests

--- rewrite-core/src/test/java/org/openrewrite/config/EnvironmentTest.java
+++ rewrite-core/src/test/java/org/openrewrite/config/EnvironmentTest.java
@@ -26,8 +26,7 @@ class EnvironmentTest {
         var env = Environment.builder().scanYamlResources().build();
         var licenses = env.getRequiredLicenses();
         assertThat(licenses).containsExactlyInAnyOrder(
-          new License("Apache License Version 2.0", "https://www.apache.org/licenses/LICENSE-2.0"),
-          new License("Moderne Source Available License", "https://docs.moderne.io/licensing/moderne-source-available-license")
+          new License("Apache License Version 2.0", "https://www.apache.org/licenses/LICENSE-2.0")
         );
     }
 
@@ -37,8 +36,7 @@ class EnvironmentTest {
         var env = Environment.builder().scanRuntimeClasspath().build();
         var licenses = env.getRequiredLicenses();
         assertThat(licenses).containsExactlyInAnyOrder(
-          new License("Apache License Version 2.0", "https://www.apache.org/licenses/LICENSE-2.0"),
-          new License("Moderne Source Available License", "https://docs.moderne.io/licensing/moderne-source-available-license")
+          new License("Apache License Version 2.0", "https://www.apache.org/licenses/LICENSE-2.0")
         );
     }
 }
