867f81726d41c175382915a6f0157b7ba89f80e5
Johan Kragt <johan.kragt@moderne.io>
2025-06-30 16:15:44 +0200

    Added comment explaining the condition and action

--- rewrite-java/src/main/java/org/openrewrite/java/AddOrUpdateAnnotationAttribute.java
+++ rewrite-java/src/main/java/org/openrewrite/java/AddOrUpdateAnnotationAttribute.java
@@ -251,6 +251,7 @@ public class AddOrUpdateAnnotationAttribute extends Recipe {
                                 }
                                 return ((J.Literal) it).withValue(newAttributeValue).withValueSource(newAttributeValue);
                             } else if (oldAttributeValue == null) {
+                                // Without an oldAttributeValue and an attributeName not matching `value` we want to add an extra argument to the annotation.
                                 // Make the attribute name explicit, before we add the new value below
                                 //noinspection ConstantConditions
                                 return ((J.Annotation) JavaTemplate.builder("value = #{}")
