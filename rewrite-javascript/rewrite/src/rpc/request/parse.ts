fdfce692c354fdf82f0304044f10d2e7e9094c04
Knut Wannheden <knut@moderne.io>
2025-07-01 15:36:05 +0200

    Slightly optimize `parse.ts`

--- rewrite-javascript/rewrite/src/rpc/request/parse.ts
+++ rewrite-javascript/rewrite/src/rpc/request/parse.ts
@@ -16,7 +16,7 @@
 import * as rpc from "vscode-jsonrpc/node";
 import {ExecutionContext} from "../../execution";
 import {UUID} from "node:crypto";
-import {Parser, ParserInput, Parsers} from "../../parser";
+import {ParserInput, Parsers} from "../../parser";
 import {SourceFile} from "../../tree";
 import {LRUCache} from "lru-cache";
 
@@ -31,42 +31,43 @@ export class Parse {
         const ongoingRequests = new Map<string, AsyncGenerator<SourceFile>>();
 
         connection.onRequest(new rpc.RequestType<Parse, UUID[], Error>("Parse"), async (request) => {
-            let parser: Parser | undefined = Parsers.createParser("javascript", {
-                ctx: new ExecutionContext(),
-                relativeTo: request.relativeTo
-            });
+            const requestId = request.id;
 
-            if (parser) {
-                const requestId = request.id;
+            // Check if we already have an ongoing request for this ID
+            let generator = ongoingRequests.get(requestId);
 
-                // Check if we already have an ongoing request for this ID
-                let generator = ongoingRequests.get(requestId);
-                if (!generator) {
-                    generator = parser.parse(...request.inputs);
-                    ongoingRequests.set(requestId, generator);
+            if (!generator) {
+                let parser = Parsers.createParser("javascript", {
+                    ctx: new ExecutionContext(),
+                    relativeTo: request.relativeTo
+                });
+
+                if (!parser) {
+                    return [];
                 }
-
-                const batch: string[] = [];
-                const batchSize = 10;
-
-                for (let i = 0; i < batchSize; i++) {
-                    const result = await generator.next();
-
-                    if (result.done) {
-                        // Generator is exhausted, clean up
-                        ongoingRequests.delete(requestId);
-                        break;
-                    } else {
-                        // Store the generated object and add its ID to batch
-                        const sourceFile = result.value;
-                        localObjects.set(sourceFile.id.toString(), sourceFile);
-                        batch.push(sourceFile.id);
-                    }
-                }
-
-                return batch;
+                generator = parser.parse(...request.inputs);
+                ongoingRequests.set(requestId, generator);
             }
-            return [];
+
+            const batch: string[] = [];
+            const batchSize = 10;
+
+            for (let i = 0; i < batchSize; i++) {
+                const result = await generator.next();
+
+                if (result.done) {
+                    // Generator is exhausted, clean up
+                    ongoingRequests.delete(requestId);
+                    break;
+                } else {
+                    // Store the generated object and add its ID to batch
+                    const sourceFile = result.value;
+                    localObjects.set(sourceFile.id.toString(), sourceFile);
+                    batch.push(sourceFile.id);
+                }
+            }
+
+            return batch;
         });
     }
 }
