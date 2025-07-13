f5c2f757ed6438404fa431424a5fd04e5c1847e5
Vincent Potucek <vpotucek@me.com>
2025-07-13 14:59:15 +0200

    RemoveUnusedImports

--- rewrite-java-test/src/test/java/org/openrewrite/java/RemoveUnusedImportsTest.java
+++ rewrite-java-test/src/test/java/org/openrewrite/java/RemoveUnusedImportsTest.java
@@ -200,38 +200,15 @@ class RemoveUnusedImportsTest implements RewriteTest {
               /**
                * The declaration of an annotation type.
                * This is a {@linkplain Node#isFindBoundary() find boundary} for tree traversal methods.
-               *
-               * <p>Note that in contrast to interface types, no {@linkplain ASTExtendsList extends clause}
-               * is permitted, and an annotation type cannot be generic.
-               *
-               * <pre class="grammar">
-               *
-               * AnnotationTypeDeclaration ::= {@link ASTModifierList ModifierList}
-               *                               "@" "interface"
-               *                               &lt;IDENTIFIER&gt;
-               *                               {@link ASTAnnotationTypeBody AnnotationTypeBody}
-               *
                * </pre>
                *
                */
-              public final class ASTAnnotationTypeDeclaration extends AbstractTypeDeclaration {
+              public final class ASTAnnotationTypeDeclaration {
               
               
                   ASTAnnotationTypeDeclaration(int id) {
-                      super(id);
                   }
               
-              
-                  @Override
-                  protected <P, R> R acceptVisitor(JavaVisitor<? super P, ? extends R> visitor, P data) {
-                      return visitor.visit(this, data);
-                  }
-              
-              
-                  @Override
-                  public boolean isInterface() {
-                      return true;
-                  }
               }
               """
           )
