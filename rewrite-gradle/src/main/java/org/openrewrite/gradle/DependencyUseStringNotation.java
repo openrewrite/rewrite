89fdfebd3f62264479ca158c2ba66b3e32ff751b
aet2505 <aet2505@gmail.com>
2025-07-17 23:41:44 +0100

    Improve construction of gradle string notation (#5748)
    * Improve construction of gradle string notation
    
    All elements apart from name are considered optional in gradle string notation.
    Enhance the conversion logic to handle null elements
    
    * Centralise the toStringNotation logic into Dependency object
    
    Additionally, update string build logic to remove extra unnecessary colons
    Gradle parse the ext value first and the parses the remaining object notation. As such ext can be removed from previous null checks
    
    * Include License as suggested by bot
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Assume bot wants new line at end of file
    
    * Update groupId to be nullable
    
    * Remove duplicate test
    
    * Polish
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Jonathan Schnéider <jkschneider@gmail.com>

--- rewrite-gradle/src/main/java/org/openrewrite/gradle/DependencyUseStringNotation.java
+++ rewrite-gradle/src/main/java/org/openrewrite/gradle/DependencyUseStringNotation.java
@@ -20,6 +20,7 @@ import org.openrewrite.ExecutionContext;
 import org.openrewrite.Preconditions;
 import org.openrewrite.Recipe;
 import org.openrewrite.TreeVisitor;
+import org.openrewrite.gradle.internal.Dependency;
 import org.openrewrite.gradle.trait.GradleDependency;
 import org.openrewrite.groovy.tree.G;
 import org.openrewrite.java.JavaVisitor;
@@ -148,27 +149,16 @@ public class DependencyUseStringNotation extends Recipe {
             }
 
             private J.@Nullable Literal toLiteral(Space prefix, Markers markers, Map<String, Expression> mapNotation) {
-                if (mapNotation.containsKey("group") && mapNotation.containsKey("name")) {
-                    String stringNotation = "";
-
+                // Name is the only required key in a dependency map.
+                if (mapNotation.containsKey("name")) {
                     String group = coerceToStringNotation(mapNotation.get("group"));
-                    if (group != null) {
-                        stringNotation += group;
-                    }
-
                     String name = coerceToStringNotation(mapNotation.get("name"));
-                    if (name != null) {
-                        stringNotation += ":" + name;
-                    }
-
                     String version = coerceToStringNotation(mapNotation.get("version"));
-                    if (version != null) {
-                        stringNotation += ":" + version;
-                        String classifier = coerceToStringNotation(mapNotation.get("classifier"));
-                        if (classifier != null) {
-                            stringNotation += ":" + classifier;
-                        }
-                    }
+                    String classifier = coerceToStringNotation(mapNotation.get("classifier"));
+                    String extension = coerceToStringNotation(mapNotation.get("ext"));
+
+                    Dependency dependency = new Dependency(group, name, version, classifier, extension);
+                    String stringNotation = dependency.toStringNotation();
 
                     return new J.Literal(randomId(), prefix, markers, stringNotation, "\"" + stringNotation + "\"", Collections.emptyList(), JavaType.Primitive.String);
                 }
