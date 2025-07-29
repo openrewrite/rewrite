e2bcbc0a5f688dc768cab8785c825cf58d7feed1
lingenj <jacob.van.lingen@moderne.io>
2025-07-29 16:56:24 +0200

    Impl

--- rewrite-yaml/src/main/java/org/openrewrite/yaml/CoalescePropertiesVisitor.java
+++ rewrite-yaml/src/main/java/org/openrewrite/yaml/CoalescePropertiesVisitor.java
@@ -18,10 +18,10 @@ package org.openrewrite.yaml;
 
 import org.jspecify.annotations.Nullable;
 import org.openrewrite.Cursor;
+import org.openrewrite.internal.ListUtils;
 import org.openrewrite.yaml.search.FindIndentYamlVisitor;
 import org.openrewrite.yaml.tree.Yaml;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.Collections.emptyList;
@@ -29,16 +29,12 @@ import static java.util.stream.Collectors.toList;
 
 public class CoalescePropertiesVisitor<P> extends YamlIsoVisitor<P> {
     private final FindIndentYamlVisitor<P> findIndent = new FindIndentYamlVisitor<>();
-    private final List<String> exclusions;
-    private final List<String> applyTo;
-    private List<JsonPathMatcher> exclusionMatchers;
-    private List<JsonPathMatcher> applyToMatchers;
+    private final List<JsonPathMatcher> exclusionMatchers;
+    private final List<JsonPathMatcher> applyToMatchers;
 
     public CoalescePropertiesVisitor(@Nullable final List<String> exclusions, @Nullable final List<String> applyTo) {
-        this.exclusions = exclusions == null ? emptyList() : exclusions;
-        this.applyTo = applyTo == null ? emptyList() : applyTo;
-        exclusionMatchers = this.exclusions.stream().map(JsonPathMatcher::new).collect(toList());
-        applyToMatchers = this.applyTo.stream().map(JsonPathMatcher::new).collect(toList());
+        exclusionMatchers = exclusions == null ? emptyList() : exclusions.stream().map(JsonPathMatcher::new).collect(toList());
+        applyToMatchers = applyTo == null ? emptyList() : applyTo.stream().map(JsonPathMatcher::new).collect(toList());
     }
 
     @Override
@@ -51,99 +47,53 @@ public class CoalescePropertiesVisitor<P> extends YamlIsoVisitor<P> {
     }
 
     @Override
-    public Yaml.Mapping.Entry visitMappingEntry(Yaml.Mapping.Entry entry, P p) {
-        int levels = 0;
-        Cursor c = getCursor();
-        while (c != null && !c.isRoot()) {
-            Cursor current = c;
-            boolean foundMatch = exclusionMatchers.stream().anyMatch(it -> it.matches(current));
-            if (foundMatch) {
-                for (int i = 0; i <= levels; i++) {
-                    getCursor().getParent(i).putMessage("COALESCE_COLLAPSING", false);
-                }
-                break;
-            } else {
-                c = c.getParent();
-                levels++;
-            }
-        }
-        return e;
-    }
-
-    @Override
     public Yaml.Mapping visitMapping(Yaml.Mapping mapping, P p) {
         Yaml.Mapping m = super.visitMapping(mapping, p);
 
-        boolean allowCoalesce = getCursor().getMessage("COALESCE_COLLAPSING", true);
-        if (!allowCoalesce) {
-            return m;
-        }
-        boolean changed = false;
-        List<Yaml.Mapping.Entry> entries = new ArrayList<>();
-
-        for (Yaml.Mapping.Entry entry : m.getEntries()) {
+        return m.withEntries(ListUtils.map(m.getEntries(), entry -> {
             if (entry.getValue() instanceof Yaml.Mapping) {
                 Yaml.Mapping valueMapping = (Yaml.Mapping) entry.getValue();
-                if (valueMapping.getEntries().size() == 1) { //&& !matchesExclusion(entry) && matchesApplyTo(entry)) {
+                if (valueMapping.getEntries().size() == 1) {
                     Yaml.Mapping.Entry subEntry = valueMapping.getEntries().iterator().next();
-                    if (!subEntry.getPrefix().contains("#")) {// && !matchesExclusion(subEntry) && matchesApplyTo(subEntry)) {
-                        Yaml.Scalar coalescedKey = ((Yaml.Scalar) entry.getKey()).withValue(entry.getKey().getValue() + "." + subEntry.getKey().getValue());
-
-                        entries.add(entry.withKey(coalescedKey)
-                                .withValue(subEntry.getValue()));
-
+                    if (!subEntry.getPrefix().contains("#") && !isExcluded(entry, subEntry) && isApplied(entry)) {
                         int indentToUse = findIndent.getMostCommonIndent() > 0 ? findIndent.getMostCommonIndent() : 4;
                         doAfterVisit(new ShiftFormatLeftVisitor<>(subEntry.getValue(), indentToUse));
 
-                        changed = true;
-                    } else {
-                        entries.add(entry);
+                        Yaml.Scalar coalescedKey = ((Yaml.Scalar) entry.getKey()).withValue(entry.getKey().getValue() + "." + subEntry.getKey().getValue());
+                        return entry.withKey(coalescedKey).withValue(subEntry.getValue());
                     }
-                } else {
-                    entries.add(entry);
                 }
-            } else {
-                entries.add(entry);
             }
-        }
-
-        if (changed) {
-            m = m.withEntries(entries);
-        }
-
-        return m;
+            return entry;
+        }));
     }
 
-    private boolean matchesExclusion(Yaml.Mapping.Entry entry) {
-        boolean foundMatch = false;
+    private boolean isExcluded(Yaml.Mapping.Entry entry, Yaml.Mapping.Entry subEntry) {
+        if (exclusionMatchers.isEmpty()) {
+            return false;
+        }
         Cursor c = new Cursor(getCursor(), entry);
-        while (c != null && !c.isRoot()) {
-            Cursor current = c;
-            foundMatch = exclusionMatchers.stream().anyMatch(it -> it.matches(current));
-            if (foundMatch) {
-                return foundMatch;
-            } else {
-                c = c.getParent();
-            }
-        }
-        return foundMatch;
+        Cursor c2 = new Cursor(c, subEntry);
+        return match(c, exclusionMatchers) || exclusionMatchers.stream().anyMatch(it -> it.matches(c2));
     }
 
-    private boolean matchesApplyTo(Yaml.Mapping.Entry entry) {
+    private boolean isApplied(Yaml.Mapping.Entry entry) {
         if (applyToMatchers.isEmpty()) {
             return true;
         }
-        boolean foundMatch = false;
         Cursor c = new Cursor(getCursor(), entry);
+        return match(c, applyToMatchers);
+    }
+
+    private boolean match(Cursor c, List<JsonPathMatcher> matchers) {
         while (c != null && !c.isRoot()) {
             Cursor current = c;
-            foundMatch = applyToMatchers.stream().anyMatch(it -> it.matches(current));
-            if (foundMatch) {
-                return foundMatch;
+            if (matchers.stream().anyMatch(it -> it.matches(current))) {
+                return true;
             } else {
                 c = c.getParent();
             }
         }
-        return foundMatch;
+        return false;
     }
 }
