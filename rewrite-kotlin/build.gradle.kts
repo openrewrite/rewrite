0e5e51a8f797c1edcb60112d6dbcd0fcdaa8003d
Jonathan Schneider <jkschneider@gmail.com>
2025-08-02 14:07:13 -0400

    Remove JUnit dependency pinning

--- rewrite-kotlin/build.gradle.kts
+++ rewrite-kotlin/build.gradle.kts
@@ -6,27 +6,19 @@ plugins {
 }
 
 val kotlinVersion = "1.9.25"
-val junitVersion = "5.13.4"
 
 dependencies {
     compileOnly(project(":rewrite-core"))
     compileOnly(project(":rewrite-test"))
-//    compileOnly("com.google.code.findbugs:jsr305:latest.release")
 
     implementation(project(":rewrite-java"))
 
     implementation(platform(kotlin("bom", kotlinVersion)))
     implementation(kotlin("compiler-embeddable"))
-//    implementation("org.jetbrains.kotlin:kotlin-scripting-compiler-embeddable:${kotlinVersion}")
-
     implementation(kotlin("stdlib"))
 
-    testImplementation("org.assertj:assertj-core:latest.release")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:$junitVersion")
     testImplementation("org.junit-pioneer:junit-pioneer:latest.release")
     testImplementation(project(":rewrite-test"))
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
     testRuntimeOnly(project(":rewrite-java-21"))
     testRuntimeOnly("org.antlr:antlr4-runtime:4.13.2")
 
