c81b3400e660dc42d9fb3e7fa97c4e3bdcfc413c
lingenj <jacob.van.lingen@moderne.io>
2025-08-05 17:43:27 +0200

    RemoveImport should compare supertypes as well

--- rewrite-java/src/main/java/org/openrewrite/java/RemoveImport.java
+++ rewrite-java/src/main/java/org/openrewrite/java/RemoveImport.java
@@ -147,7 +147,7 @@ public class RemoveImport<P> extends JavaIsoVisitor<P> {
                             spaceForNextImport.set(import_.getPrefix());
                             return null;
                         } else if (!isPackageAlwaysFolded(importLayoutStyle.getPackagesToFold(), import_) &&
-                                methodsAndFieldsUsed.size() + otherMethodsAndFieldsInTypeUsed.size() < importLayoutStyle.getNameCountToUseStarImport()) {
+                                   methodsAndFieldsUsed.size() + otherMethodsAndFieldsInTypeUsed.size() < importLayoutStyle.getNameCountToUseStarImport()) {
                             methodsAndFieldsUsed.addAll(otherMethodsAndFieldsInTypeUsed);
                             return unfoldStarImport(import_, methodsAndFieldsUsed);
                         }
