a99362a047087b89508cb31048b207493b90f138
Laurens Westerlaken <laurens.westerlaken@jdriven.com>
2025-08-08 13:54:28 +0200

    Adjust predicate instead of internal gradle AddDependency logic

--- rewrite-gradle/src/main/java/org/openrewrite/gradle/internal/AddDependencyVisitor.java
+++ rewrite-gradle/src/main/java/org/openrewrite/gradle/internal/AddDependencyVisitor.java
@@ -115,19 +115,6 @@ public class AddDependencyVisitor extends JavaIsoVisitor<ExecutionContext> {
     public J.Block visitBlock(J.Block block, ExecutionContext ctx) {
         J.Block b = super.visitBlock(block, ctx);
         if (b == block && insertPredicate != null && insertPredicate.test(getCursor())) {
-            // If we're visiting a gradle kotlin file but we're not visiting the highest level block we should not add a dependencies block.
-            if (getCursor().firstEnclosing(K.CompilationUnit.class) != null &&
-                !(getCursor().getParentOrThrow().getValue() instanceof K.CompilationUnit)) {
-                try {
-                    getCursor().dropParentUntil(value -> value instanceof J.MethodInvocation &&
-                                                         ("testing".equals(((J.MethodInvocation) value).getSimpleName()) ||
-                                                         "sourceSets ".equals(((J.MethodInvocation) value).getSimpleName()) ||
-                                                         "configurations ".equals(((J.MethodInvocation) value).getSimpleName()))
-                            );
-                } catch (IllegalStateException e) {
-                    return b;
-                }
-            }
             if (!hasDependenciesBlock(b.getStatements())) {
                 Cursor parent = getCursor().dropParentUntil(value -> value instanceof J.MethodInvocation || value == Cursor.ROOT_VALUE);
                 Space prefix = b.getStatements().isEmpty() ? Space.format("\n") : Space.format("\n\n");
