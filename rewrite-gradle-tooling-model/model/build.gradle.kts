5cda76be2c9514cd050bca210429a26ca9b3baf6
Sam Snyder <sam@moderne.io>
2025-08-19 14:50:54 -0700

    Standardize build

--- rewrite-gradle-tooling-model/model/build.gradle.kts
+++ rewrite-gradle-tooling-model/model/build.gradle.kts
@@ -1,7 +1,5 @@
 plugins {
-    id("java-library")
-    id("org.openrewrite.build.metadata")
-    id("org.openrewrite.build.publish")
+    id("org.openrewrite.build.language-library")
     id("org.openrewrite.build.java8-text-blocks")
 }
 
@@ -11,18 +9,11 @@ plugins {
 // This is all messy and non-idiomatic from Gradle's standpoint, so some better way would be ideal
 evaluationDependsOn(":rewrite-gradle-tooling-model:plugin")
 
-// It is intentional that these are declared explicitly
-// org.openrewrite.gradle.RewriteDependencyRepositoriesPlugin in rewrite-build-gradle-plugin disallows snapshot repositories during release builds
-// Uniquely amongst our repositories, this repository is supposed to be able to release built against rewrite-core snapshots
-repositories {
-    mavenLocal()
-    maven {
-        url = uri("https://central.sonatype.com/repository/maven-snapshots/")
-    }
-    mavenCentral()
-}
-
 dependencies {
+    constraints {
+        // last version which supports java 8, which testGradle4 runs on
+        testImplementation("org.assertj:assertj-core:3.27.4!!")
+    }
     implementation(gradleApi())
 
     // NOTE: this is latest.integration because we need to be able to release
@@ -39,10 +30,6 @@ dependencies {
     compileOnly(project(":rewrite-properties"))
     compileOnly(project(":rewrite-toml"))
 
-    compileOnly("org.projectlombok:lombok:latest.release")
-    testCompileOnly("org.projectlombok:lombok:latest.release")
-    annotationProcessor("org.projectlombok:lombok:latest.release")
-    testAnnotationProcessor("org.projectlombok:lombok:latest.release")
 
     testImplementation(project(":rewrite-test"))
     testImplementation(project(":rewrite-gradle")) {
@@ -50,11 +37,10 @@ dependencies {
     }
     testImplementation(project(":rewrite-core"))
     testImplementation(project(":rewrite-maven"))
-    testImplementation(platform("com.fasterxml.jackson:jackson-bom:2.17.+"))
     testImplementation("com.fasterxml.jackson.core:jackson-core")
     testImplementation("com.fasterxml.jackson.core:jackson-databind")
     testImplementation("com.fasterxml.jackson.dataformat:jackson-dataformat-smile")
-    // last version which supports java 8, which testGradle4 runs on
+
     testImplementation("org.assertj:assertj-core:3.+")
     testImplementation(platform("org.junit:junit-bom:5.13.3"))
     testImplementation("org.junit.jupiter:junit-jupiter-api")
@@ -64,8 +50,6 @@ dependencies {
 
 tasks.named<JavaCompile>("compileJava").configure {
     options.release.set(8)
-    options.compilerArgs.add("-parameters")
-    options.encoding = "UTF-8"
 }
 
 val testGradle4 = tasks.register<Test>("testGradle4") {
--- rewrite-gradle-tooling-model/plugin/build.gradle.kts
+++ rewrite-gradle-tooling-model/plugin/build.gradle.kts
@@ -1,20 +1,10 @@
 plugins {
-    id("java-library")
-    id("org.openrewrite.build.metadata")
-    id("org.openrewrite.build.publish")
+    id("org.openrewrite.build.language-library")
     id("org.openrewrite.build.java8-text-blocks")
 }
 
 group = "org.openrewrite.gradle.tooling"
 description = "A model for extracting semantic information out of Gradle build files necessary for refactoring them."
-
-repositories {
-    mavenLocal()
-    maven {
-        url = uri("https://central.sonatype.com/repository/maven-snapshots/")
-    }
-    mavenCentral()
-}
 
 dependencies {
     implementation(project(":rewrite-gradle-tooling-model:model"))
