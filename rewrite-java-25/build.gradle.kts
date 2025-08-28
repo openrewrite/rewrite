78e0070f99ad71926cfa8e549d468abb88bee134
Laurens Westerlaken <laurens.westerlaken@jdriven.com>
2025-08-28 17:00:50 +0200

    Dynamically switch toolchain

--- rewrite-java-25/build.gradle.kts
+++ rewrite-java-25/build.gradle.kts
@@ -27,7 +27,14 @@ dependencies {
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(25))
+        // Use Java 25 if available, otherwise use 21
+        val javaVersion = System.getProperty("java.version")
+        val toolchainVersion = if (javaVersion?.startsWith("25") == true) {
+            25
+        } else {
+            21
+        }
+        languageVersion.set(JavaLanguageVersion.of(toolchainVersion))
     }
 }
 
