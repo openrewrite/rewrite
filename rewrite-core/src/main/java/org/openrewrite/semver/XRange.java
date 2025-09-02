1a1c8e018e8b3834ad86dbd6af8bf4a968466dd9
Shannon Pamperl <shannon@moderne.io>
2025-09-02 15:09:43 -0500

    Enable comparison of x ranges within an existing XRange

--- rewrite-core/src/main/java/org/openrewrite/semver/XRange.java
+++ rewrite-core/src/main/java/org/openrewrite/semver/XRange.java
@@ -21,12 +21,14 @@ import org.openrewrite.Validated;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static java.util.Objects.requireNonNull;
+
 /**
  * Any of X, x, or * may be used to "stand in" for one of the numeric values in the [major, minor, patch] tuple.
  * <a href="https://github.com/npm/node-semver#x-ranges-12x-1x-12-">X-Ranges</a>.
  */
 public class XRange extends LatestRelease {
-    private static final Pattern X_RANGE_PATTERN = Pattern.compile("([*xX]|\\d+)(?:\\.([*xX]|\\d+)(?:\\.([*xX]|\\d+))?(?:\\.([*xX]|\\d+))?)?");
+    private static final Pattern X_RANGE_PATTERN = Pattern.compile("[*xX]|\\d+\\.([*xX]|\\d+\\.([*xX]|\\d+\\.([*xX]|\\d+\\.[*xX])))");
 
     private final String major;
     private final String minor;
@@ -76,27 +78,121 @@ public class XRange extends LatestRelease {
 
     public static Validated<XRange> build(String pattern, @Nullable String metadataPattern) {
         Matcher matcher = X_RANGE_PATTERN.matcher(pattern);
-        if (!matcher.matches() || !(pattern.contains("x") || pattern.contains("X") || pattern.contains("*"))) {
+        if (!matcher.matches()) {
             return Validated.invalid("xRange", pattern, "not an x-range");
         }
 
-        String major = normalizeWildcard(matcher.group(1));
-        String minor = normalizeWildcard(matcher.group(2) == null ? "0" : matcher.group(2));
-        String patch = normalizeWildcard(matcher.group(3) == null ? "0" : matcher.group(3));
-        String micro = normalizeWildcard(matcher.group(4) == null ? "0" : matcher.group(4));
-
-        if ("*".equals(major) && (matcher.group(2) != null || matcher.group(3) != null || matcher.group(4) != null)) {
-            return Validated.invalid("xRange", pattern, "not an x-range: nothing can follow a wildcard");
-        } else if ("*".equals(minor) && (matcher.group(3) != null || matcher.group(4) != null)) {
-            return Validated.invalid("xRange", pattern, "not an x-range: nothing can follow a wildcard");
-        } else if ("*".equals(patch) && matcher.group(4) != null) {
-            return Validated.invalid("xRange", pattern, "not an x-range: nothing can follow a wildcard");
-        }
+        String[] parts = pattern.split("\\.");
+        String major = normalizeWildcard(parts[0]);
+        String minor = normalizeWildcard(parts.length < 2 ? "*" : parts[1]);
+        String patch = normalizeWildcard(parts.length < 3 ? "*" : parts[2]);
+        String micro = normalizeWildcard(parts.length < 4 ? "*" : parts[3]);
 
         return Validated.valid("xRange", new XRange(major, minor, patch, micro, metadataPattern));
     }
 
     private static String normalizeWildcard(String part) {
         return "*".equals(part) || "x".equals(part) || "X".equals(part) ? "*" : part;
+    }
+
+    @Override
+    public int compare(@Nullable String currentVersion, String v1, String v2) {
+        boolean v1Matches = X_RANGE_PATTERN.matcher(v1).matches();
+        boolean v2Matches = X_RANGE_PATTERN.matcher(v2).matches();
+        if (v1Matches && v2Matches) {
+            XRange xrangeV1 = requireNonNull(build(v1, null).getValue());
+            XRange xrangeV2 = requireNonNull(build(v2, null).getValue());
+
+            if ("*".equals(xrangeV1.major) && "*".equals(xrangeV2.major)) {
+                return 0;
+            } else if ("*".equals(xrangeV1.major)) {
+                return 1;
+            } else if ("*".equals(xrangeV2.major)) {
+                return -1;
+            }
+
+            int compare = xrangeV1.major.compareTo(xrangeV2.major);
+            if (compare != 0) {
+                return compare;
+            }
+
+            if ("*".equals(xrangeV1.minor) && "*".equals(xrangeV2.minor)) {
+                return 0;
+            } else if ("*".equals(xrangeV1.minor)) {
+                return 1;
+            } else if ("*".equals(xrangeV2.minor)) {
+                return -1;
+            }
+
+            compare =  xrangeV1.minor.compareTo(xrangeV2.minor);
+            if (compare != 0) {
+                return compare;
+            }
+
+            if ("*".equals(xrangeV1.patch) && "*".equals(xrangeV2.patch)) {
+                return 0;
+            } else if ("*".equals(xrangeV1.patch)) {
+                return 1;
+            } else if ("*".equals(xrangeV2.patch)) {
+                return -1;
+            }
+
+            // Micro is guaranteed to be the same, so we can stop here
+            return xrangeV1.patch.compareTo(xrangeV2.patch);
+        } else if (v1Matches) {
+            XRange xrangeV1 = requireNonNull(build(v1, null).getValue());
+            if ("*".equals(xrangeV1.major)) {
+                return 0;
+            }
+
+            Matcher gav = VersionComparator.RELEASE_PATTERN.matcher(normalizeVersion(v2));
+            gav.matches();
+
+            if (!xrangeV1.major.equals(gav.group(1))) {
+                return xrangeV1.major.compareTo(gav.group(1));
+            }
+
+            if ("*".equals(xrangeV1.minor)) {
+                return 0;
+            } else if (gav.group(2) == null || !xrangeV1.minor.equals(gav.group(2))) {
+                return xrangeV1.minor.compareTo(gav.group(2));
+            }
+
+            if ("*".equals(xrangeV1.patch)) {
+                return 0;
+            } else if (gav.group(3) == null || !xrangeV1.patch.equals(gav.group(3))) {
+                return xrangeV1.patch.compareTo(gav.group(3));
+            }
+
+            return 0;
+        } else if (v2Matches) {
+            XRange xrangeV2 = requireNonNull(build(v2, null).getValue());
+            if ("*".equals(xrangeV2.major)) {
+                return 0;
+            }
+
+            Matcher gav = VersionComparator.RELEASE_PATTERN.matcher(normalizeVersion(v1));
+            gav.matches();
+
+            if (!gav.group(1).equals(xrangeV2.major)) {
+                return gav.group(1).compareTo(xrangeV2.major);
+            }
+
+            if ("*".equals(xrangeV2.minor)) {
+                return 0;
+            } else if (gav.group(2) == null || !gav.group(2).equals(xrangeV2.minor)) {
+                return gav.group(2).compareTo(xrangeV2.minor);
+            }
+
+            if ("*".equals(xrangeV2.patch)) {
+                return 0;
+            } else if (gav.group(3) == null || !gav.group(3).equals(xrangeV2.patch)) {
+                return gav.group(3).compareTo(xrangeV2.patch);
+            }
+
+            return 0;
+        }
+
+        return super.compare(currentVersion, v1, v2);
     }
 }
