f1aa64c2b6052af25226556301f9d0676f5913be
Shannon Pamperl <shanman190@gmail.com>
2025-09-05 14:49:46 -0500

    Introduce support for pattern matching within the `Semver` type classes (#5991)
    * Fix incorrect upper bound for caret range
    
    * Enable comparison of caret ranges within an existing CaretRange
    
    * Enable comparison of hyphen ranges with an existing HyphenRange
    
    * Enable comparison of set ranges within an existing SetRange
    
    * Enable comparison of tilde ranges within an existing TildeRange
    
    * Enable comparison of x ranges within an existing XRange
    
    * Polish
    
    * Second fix special case of caret range for pre-1.0 versions
    
    * Properly sort `latest.snapshot`, `latest.minor`, and `latest.patch`
    
    * Also handle `latest.major`
    
    * Rollback special case caret range test

--- rewrite-core/src/main/java/org/openrewrite/semver/XRange.java
+++ rewrite-core/src/main/java/org/openrewrite/semver/XRange.java
@@ -21,6 +21,9 @@ import org.openrewrite.Validated;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static java.util.Objects.requireNonNull;
+import static org.openrewrite.semver.Semver.isVersion;
+
 /**
  * Any of X, x, or * may be used to "stand in" for one of the numeric values in the [major, minor, patch] tuple.
  * <a href="https://github.com/npm/node-semver#x-ranges-12x-1x-12-">X-Ranges</a>.
@@ -98,5 +101,114 @@ public class XRange extends LatestRelease {
 
     private static String normalizeWildcard(String part) {
         return "*".equals(part) || "x".equals(part) || "X".equals(part) ? "*" : part;
+    }
+
+    @Override
+    public int compare(@Nullable String currentVersion, String v1, String v2) {
+        Validated<XRange> maybeXRangeV1 = build(v1, null);
+        Validated<XRange> maybeXRangeV2 = build(v2, null);
+        if (maybeXRangeV1.isValid() && maybeXRangeV2.isValid()) {
+            XRange xrangeV1 = requireNonNull(maybeXRangeV1.getValue());
+            XRange xrangeV2 = requireNonNull(maybeXRangeV2.getValue());
+
+            if ("*".equals(xrangeV1.major) && "*".equals(xrangeV2.major)) {
+                return 0;
+            } else if ("*".equals(xrangeV1.major)) {
+                return 1;
+            } else if ("*".equals(xrangeV2.major)) {
+                return -1;
+            }
+
+            int compare = xrangeV1.major.compareTo(xrangeV2.major);
+            if (compare != 0) {
+                return compare;
+            }
+
+            if ("*".equals(xrangeV1.minor) && "*".equals(xrangeV2.minor)) {
+                return 0;
+            } else if ("*".equals(xrangeV1.minor)) {
+                return 1;
+            } else if ("*".equals(xrangeV2.minor)) {
+                return -1;
+            }
+
+            compare =  xrangeV1.minor.compareTo(xrangeV2.minor);
+            if (compare != 0) {
+                return compare;
+            }
+
+            if ("*".equals(xrangeV1.patch) && "*".equals(xrangeV2.patch)) {
+                return 0;
+            } else if ("*".equals(xrangeV1.patch)) {
+                return 1;
+            } else if ("*".equals(xrangeV2.patch)) {
+                return -1;
+            }
+
+            // Micro is guaranteed to be the same, so we can stop here
+            return xrangeV1.patch.compareTo(xrangeV2.patch);
+        } else if (maybeXRangeV1.isValid()) {
+            if (!isVersion(v2)) {
+                return 1;
+            }
+
+            XRange xrangeV1 = requireNonNull(maybeXRangeV1.getValue());
+            if ("*".equals(xrangeV1.major)) {
+                return 0;
+            }
+
+            Matcher gav = VersionComparator.RELEASE_PATTERN.matcher(normalizeVersion(v2));
+            gav.matches();
+
+            if (!xrangeV1.major.equals(gav.group(1))) {
+                return xrangeV1.major.compareTo(gav.group(1));
+            }
+
+            if ("*".equals(xrangeV1.minor)) {
+                return 0;
+            } else if (gav.group(2) == null || !xrangeV1.minor.equals(gav.group(2))) {
+                return xrangeV1.minor.compareTo(gav.group(2));
+            }
+
+            if ("*".equals(xrangeV1.patch)) {
+                return 0;
+            } else if (gav.group(3) == null || !xrangeV1.patch.equals(gav.group(3))) {
+                return xrangeV1.patch.compareTo(gav.group(3));
+            }
+
+            return 0;
+        } else if (maybeXRangeV2.isValid()) {
+            if (!isVersion(v1)) {
+                return -1;
+            }
+
+            XRange xrangeV2 = requireNonNull(maybeXRangeV2.getValue());
+            if ("*".equals(xrangeV2.major)) {
+                return 0;
+            }
+
+            Matcher gav = VersionComparator.RELEASE_PATTERN.matcher(normalizeVersion(v1));
+            gav.matches();
+
+            if (!gav.group(1).equals(xrangeV2.major)) {
+                return gav.group(1).compareTo(xrangeV2.major);
+            }
+
+            if ("*".equals(xrangeV2.minor)) {
+                return 0;
+            } else if (gav.group(2) == null || !gav.group(2).equals(xrangeV2.minor)) {
+                return gav.group(2).compareTo(xrangeV2.minor);
+            }
+
+            if ("*".equals(xrangeV2.patch)) {
+                return 0;
+            } else if (gav.group(3) == null || !gav.group(3).equals(xrangeV2.patch)) {
+                return gav.group(3).compareTo(xrangeV2.patch);
+            }
+
+            return 0;
+        }
+
+        return super.compare(currentVersion, v1, v2);
     }
 }
