87c9a83575ec5e29eacadb0f365d771fe6fc4c1d
Shannon Pamperl <shannon@moderne.io>
2025-09-10 17:04:53 -0500

    Handle new elvis operator in unix and windows Gradle script templates

--- rewrite-gradle/src/main/java/org/openrewrite/gradle/UpdateGradleWrapper.java
+++ rewrite-gradle/src/main/java/org/openrewrite/gradle/UpdateGradleWrapper.java
@@ -405,7 +405,15 @@ public class UpdateGradleWrapper extends ScanningRecipe<UpdateGradleWrapper.Grad
         Map<String, String> binding = new HashMap<>();
         String defaultJvmOpts = defaultJvmOpts(gradleWrapper);
         binding.put("defaultJvmOpts", StringUtils.isNotEmpty(defaultJvmOpts) ? "'" + defaultJvmOpts + "'" : "");
-        binding.put("classpath", "$APP_HOME/gradle/wrapper/gradle-wrapper.jar");
+        if (requireNonNull(Semver.validate("[8.14,)", null).getValue()).compare(null, gradleWrapper.getVersion(), "8.14") >= 0) {
+            binding.put("classpath", "\"\\\\\\\\\\\"\\\\\\\\\\\"\"");
+            binding.put("entryPointArgs", "-jar \"$APP_HOME/gradle/wrapper/gradle-wrapper.jar\"");
+            binding.put("mainClassName", "");
+        } else {
+            binding.put("classpath", "$APP_HOME/gradle/wrapper/gradle-wrapper.jar");
+            binding.put("entryPointArgs", "");
+            binding.put("mainClassName", "org.gradle.wrapper.GradleWrapperMain");
+        }
 
         String gradlewTemplate = StringUtils.readFully(gradleWrapper.gradlew().getInputStream(ctx));
         return renderTemplate(gradlewTemplate, binding, "\n");
@@ -414,7 +422,15 @@ public class UpdateGradleWrapper extends ScanningRecipe<UpdateGradleWrapper.Grad
     private String batchScript(GradleWrapper gradleWrapper, ExecutionContext ctx) {
         Map<String, String> binding = new HashMap<>();
         binding.put("defaultJvmOpts", defaultJvmOpts(gradleWrapper));
-        binding.put("classpath", "%APP_HOME%\\gradle\\wrapper\\gradle-wrapper.jar");
+        if (requireNonNull(Semver.validate("[8.14,)", null).getValue()).compare(null, gradleWrapper.getVersion(), "8.14") >= 0) {
+            binding.put("classpath", "");
+            binding.put("mainClassName", "");
+            binding.put("entryPointArgs", "-jar \"%APP_HOME%\\gradle\\wrapper\\gradle-wrapper.jar\"");
+        } else {
+            binding.put("classpath", "%APP_HOME%\\gradle\\wrapper\\gradle-wrapper.jar");
+            binding.put("mainClassName", "org.gradle.wrapper.GradleWrapperMain");
+            binding.put("entryPointArgs", "");
+        }
 
         String gradlewBatTemplate = StringUtils.readFully(gradleWrapper.gradlewBat().getInputStream(ctx));
         return renderTemplate(gradlewBatTemplate, binding, "\r\n");
@@ -437,7 +453,7 @@ public class UpdateGradleWrapper extends ScanningRecipe<UpdateGradleWrapper.Grad
         binding.put("applicationName", "Gradle");
         binding.put("optsEnvironmentVar", "GRADLE_OPTS");
         binding.put("exitEnvironmentVar", "GRADLE_EXIT_CONSOLE");
-        binding.put("mainClassName", "org.gradle.wrapper.GradleWrapperMain");
+        binding.put("moduleEntryPoint", "");
         binding.put("appNameSystemProperty", "org.gradle.appname");
         binding.put("appHomeRelativePath", "");
         binding.put("modulePath", "");
@@ -447,6 +463,7 @@ public class UpdateGradleWrapper extends ScanningRecipe<UpdateGradleWrapper.Grad
             script = script.replace("${" + variable.getKey() + "}", variable.getValue())
                     .replace("$" + variable.getKey(), variable.getValue());
         }
+        script = script.replace("${mainClassName ?: entryPointArgs}", StringUtils.isNotEmpty(binding.get("mainClassName")) ? binding.get("mainClassName") : binding.get("entryPointArgs"));
 
         script = script.replaceAll("(?sm)<% /\\*.*?\\*/ %>", "");
         script = script.replaceAll("(?sm)<% if \\( mainClassName\\.startsWith\\('--module '\\) \\) \\{.*?} %>", "");
