c8b77a9e8741782ecf0b281b000a79972ed0a867
Jente Sondervorst <jentesondervorst@gmail.com>
2025-10-20 15:06:17 +0200

    comments

--- rewrite-java/src/main/java/org/openrewrite/java/format/MinimumViableSpacingVisitor.java
+++ rewrite-java/src/main/java/org/openrewrite/java/format/MinimumViableSpacingVisitor.java
@@ -178,7 +178,7 @@ public class MinimumViableSpacingVisitor<P> extends JavaIsoVisitor<P> {
             }
         }
 
-        m = m.withParameters(ListUtils.map(m.getParameters(), (ix, param) -> {
+        return m.withParameters(ListUtils.map(m.getParameters(), (ix, param) -> {
             if (param.getPrefix().getLastWhitespace().contains("\n")) {
                 return param;
             }
@@ -187,8 +187,6 @@ public class MinimumViableSpacingVisitor<P> extends JavaIsoVisitor<P> {
             }
             return param.withPrefix(param.getPrefix().withWhitespace(" "));
         }));
-
-        return m;
     }
 
     @Override
