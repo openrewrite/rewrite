f18f1efebc9d8cf09d4161bf0cc558e65176ab09
Tim te Beek <tim@moderne.io>
2025-10-28 00:50:54 +0100

    Remove unused import
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- rewrite-docker/src/test/java/org/openrewrite/docker/ChangeBaseImageTest.java
+++ rewrite-docker/src/test/java/org/openrewrite/docker/ChangeBaseImageTest.java
@@ -15,7 +15,6 @@
  */
 package org.openrewrite.docker;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.openrewrite.DocumentExample;
 import org.openrewrite.test.RecipeSpec;
