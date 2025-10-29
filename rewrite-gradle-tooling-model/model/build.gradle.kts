db03dbf842dde1b330eb0eaf2199a3c87df6ad6a
Tim te Beek <tim@moderne.io>
2025-10-29 19:54:33 +0100

    Drop launcher from rewrite-test, as it's now set in the build plugin (#6220)
    * Drop launcher from rewrite-test, as it's now set in the build plugin
    
    This makes for a lighter `rewrite-test` when used by Maven projects.
    
    * Have `rewrite-javascript` depend on `rewrite-java-21` for toolchain default
    
    * Use `rewrite-java-21` in more places to align with toolchain usage
    
    * Fix a dangling reference in rewrite-javascript integration tests
    
    * Drop the one test that required rewrite-java-2x

--- rewrite-gradle-tooling-model/model/build.gradle.kts
+++ rewrite-gradle-tooling-model/model/build.gradle.kts
@@ -40,11 +40,6 @@ dependencies {
     testImplementation("com.fasterxml.jackson.core:jackson-databind")
     testImplementation("com.fasterxml.jackson.dataformat:jackson-dataformat-smile")
 
-    testImplementation("org.assertj:assertj-core:3.+")
-    testImplementation(platform("org.junit:junit-bom:5.13.3"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api")
-    testImplementation("org.junit.jupiter:junit-jupiter-params")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
     "pluginLocalTestClasspath"(project(":rewrite-gradle-tooling-model:plugin"))
 }
 
