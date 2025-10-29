6bfbf60631af1a83080026b2e3213b993b7252c7
Tim te Beek <tim@moderne.io>
2025-10-29 14:55:06 +0100

    `--build-file` is only supported up to Gradle 8

--- rewrite-gradle-tooling-model/model/src/main/java/org/openrewrite/gradle/toolingapi/OpenRewriteModelBuilder.java
+++ rewrite-gradle-tooling-model/model/src/main/java/org/openrewrite/gradle/toolingapi/OpenRewriteModelBuilder.java
@@ -77,12 +77,14 @@ public class OpenRewriteModelBuilder {
      */
     public static OpenRewriteModel forProjectDirectory(File projectDir, @Nullable File buildFile, @Nullable String initScript) throws IOException {
         DefaultGradleConnector connector = (DefaultGradleConnector) GradleConnector.newConnector();
-        if (System.getProperty("org.openrewrite.test.gradleVersion") != null) {
-            connector.useGradleVersion(System.getProperty("org.openrewrite.test.gradleVersion"));
+        String gradleVersion = System.getProperty("org.openrewrite.test.gradleVersion");
+        if (gradleVersion != null) {
+            connector.useGradleVersion(gradleVersion);
         } else if (Files.exists(projectDir.toPath().resolve("gradle/wrapper/gradle-wrapper.properties"))) {
             connector.useBuildDistribution();
         } else {
-            connector.useGradleVersion("9.1.0");
+            gradleVersion = "9.1.0";
+            connector.useGradleVersion(gradleVersion);
         }
         connector
                 // Uncomment to hit breakpoints inside OpenRewriteModelBuilder in unit tests
@@ -90,7 +92,7 @@ public class OpenRewriteModelBuilder {
                 // .embedded(true)
                 .forProjectDirectory(projectDir);
         List<String> arguments = new ArrayList<>();
-        if (buildFile != null && buildFile.exists()) {
+        if (buildFile != null && buildFile.exists() && (gradleVersion == null || !gradleVersion.startsWith("9"))) {
             arguments.add("-b");
             arguments.add(buildFile.getAbsolutePath());
         }
