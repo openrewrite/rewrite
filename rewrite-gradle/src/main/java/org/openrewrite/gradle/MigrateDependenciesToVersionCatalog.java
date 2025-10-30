315c620ea1734a9059887b8b6424bde41e102cc0
Benjamin Muschko <benjamin.muschko@gmail.com>
2025-10-30 09:20:06 -0600

    Update rewrite-gradle/src/main/java/org/openrewrite/gradle/MigrateDependenciesToVersionCatalog.java
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- rewrite-gradle/src/main/java/org/openrewrite/gradle/MigrateDependenciesToVersionCatalog.java
+++ rewrite-gradle/src/main/java/org/openrewrite/gradle/MigrateDependenciesToVersionCatalog.java
@@ -639,7 +639,7 @@ public class MigrateDependenciesToVersionCatalog extends ScanningRecipe<MigrateD
                                         Tree.randomId(),
                                         Space.EMPTY,
                                         Markers.EMPTY,
-                                        Collections.emptyList(),
+                                        emptyList(),
                                         "libs",
                                         null,
                                         null
