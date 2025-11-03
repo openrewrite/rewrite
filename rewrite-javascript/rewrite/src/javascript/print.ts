1e60d38ff3dcd4494b6938a95aef77e122ddaa56
Greg Oledzki <greg.oledzki@moderne.io>
2025-11-03 12:18:36 +0100

    Fix missing beforeSyntax, afterSyntax

--- rewrite-javascript/rewrite/src/javascript/print.ts
+++ rewrite-javascript/rewrite/src/javascript/print.ts
@@ -75,16 +75,16 @@ export class JavaScriptPrinter extends JavaScriptVisitor<PrintOutputCapture> {
     }
 
     override async visitExpressionStatement(statement: JS.ExpressionStatement, p: PrintOutputCapture): Promise<J | undefined> {
-        await this.visitSpace(statement.prefix, p);
-        await this.visitMarkers(statement.markers, p);
+        await this.beforeSyntax(statement, p);
         await this.visit(statement.expression, p);
+        await this.afterSyntax(statement, p);
         return statement;
     }
 
     override async visitStatementExpression(statementExpression: JS.StatementExpression, p: PrintOutputCapture): Promise<J | undefined> {
-        await this.visitSpace(statementExpression.prefix, p);
-        await this.visitMarkers(statementExpression.markers, p);
+        await this.beforeSyntax(statementExpression, p);
         await this.visit(statementExpression.statement, p);
+        await this.afterSyntax(statementExpression, p);
         return statementExpression;
     }
 
