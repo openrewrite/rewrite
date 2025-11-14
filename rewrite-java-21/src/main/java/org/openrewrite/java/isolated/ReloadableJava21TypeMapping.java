11591470e29c4d5cca9b433414221500499bcb6c
Tim te Beek <tim@moderne.io>
2025-11-14 13:37:28 +0100

    Adjust usages of `unsafeSet`

--- rewrite-java-21/src/main/java/org/openrewrite/java/isolated/ReloadableJava21TypeMapping.java
+++ rewrite-java-21/src/main/java/org/openrewrite/java/isolated/ReloadableJava21TypeMapping.java
@@ -87,15 +87,13 @@ class ReloadableJava21TypeMapping implements JavaTypeMapping<Tree> {
             TypeMirror bound = bounds.get(i);
             types[i] = type((Type) bound);
         }
-        intersection.unsafeSet(types);
-        return intersection;
+        return intersection.unsafeSet(types);
     }
 
     private JavaType array(Type type, String signature) {
         JavaType.Array arr = new JavaType.Array(null, null, null);
         typeCache.put(signature, arr);
-        arr.unsafeSet(type(((Type.ArrayType) type).elemtype), null);
-        return arr;
+        return arr.unsafeSet(type(((Type.ArrayType) type).elemtype), null);
     }
 
     /**
@@ -160,24 +158,6 @@ class ReloadableJava21TypeMapping implements JavaTypeMapping<Tree> {
         return types.toArray(new JavaType.FullyQualified[0]);
     }
 
-    private JavaType.@Nullable FullyQualified[] mapAnnotations(Type type) {
-        if (!type.isAnnotated()) {
-            return null;
-        }
-        List<JavaType.FullyQualified> annotations = new ArrayList<>();
-        for (TypeMetadata metadata : type.getMetadata()) {
-            if (metadata instanceof TypeMetadata.Annotations) {
-                for (Attribute.Compound annotation : ((TypeMetadata.Annotations) metadata).annotationBuffer().toArray(Attribute.Compound[]::new)) {
-                    JavaType.FullyQualified fq = TypeUtils.asFullyQualified(type(annotation.type));
-                    if (fq != null) {
-                        annotations.add(fq);
-                    }
-                }
-            }
-        }
-        return annotations.toArray(new JavaType.FullyQualified[0]);
-    }
-
     private JavaType.GenericTypeVariable generic(Type.WildcardType wildcard, String signature) {
         JavaType.GenericTypeVariable gtv = new JavaType.GenericTypeVariable(null, "?", INVARIANT, null);
         typeCache.put(signature, gtv);
@@ -206,8 +186,7 @@ class ReloadableJava21TypeMapping implements JavaTypeMapping<Tree> {
             bounds = null;
         }
 
-        gtv.unsafeSet(gtv.getName(), variance, bounds);
-        return gtv;
+        return gtv.unsafeSet(gtv.getName(), variance, bounds);
     }
 
     private JavaType generic(Type.TypeVar type, String signature) {
@@ -240,8 +219,7 @@ class ReloadableJava21TypeMapping implements JavaTypeMapping<Tree> {
             }
         }
 
-        gtv.unsafeSet(gtv.getName(), bounds == null ? INVARIANT : COVARIANT, bounds);
-        return gtv;
+        return gtv.unsafeSet(gtv.getName(), bounds == null ? INVARIANT : COVARIANT, bounds);
     }
 
     private JavaType.FullyQualified classType(Type.ClassType classType, String signature) {
@@ -323,7 +301,7 @@ class ReloadableJava21TypeMapping implements JavaTypeMapping<Tree> {
                     typeParameters.add(type(tParam));
                 }
             }
-            clazz.unsafeSet(typeParameters, supertype, owner, listAnnotations(sym), interfaces, fields, methods);
+            clazz = clazz.unsafeSet(typeParameters, supertype, owner, listAnnotations(sym), interfaces, fields, methods);
         }
 
         if (classType.typarams_field != null && classType.typarams_field.length() > 0) {
@@ -337,7 +315,7 @@ class ReloadableJava21TypeMapping implements JavaTypeMapping<Tree> {
                     typeParameters.add(type(tParam));
                 }
 
-                pt.unsafeSet(clazz, typeParameters);
+                return pt.unsafeSet(clazz, typeParameters);
             }
             return pt;
         }
@@ -459,8 +437,7 @@ class ReloadableJava21TypeMapping implements JavaTypeMapping<Tree> {
             assert resolvedOwner != null;
         }
 
-        variable.unsafeSet(resolvedOwner, type(symbol.type), listAnnotations(symbol));
-        return variable;
+        return variable.unsafeSet(resolvedOwner, type(symbol.type), listAnnotations(symbol));
     }
 
     /**
@@ -564,10 +541,9 @@ class ReloadableJava21TypeMapping implements JavaTypeMapping<Tree> {
 
         assert returnType != null;
 
-        method.unsafeSet(resolvedDeclaringType,
+        return method.unsafeSet(resolvedDeclaringType,
                 methodSymbol.isConstructor() ? resolvedDeclaringType : returnType,
                 parameterTypes, exceptionTypes, listAnnotations(methodSymbol));
-        return method;
     }
 
     /**
@@ -693,10 +669,9 @@ class ReloadableJava21TypeMapping implements JavaTypeMapping<Tree> {
                 throw new UnsupportedOperationException("Unexpected method signature type" + signatureType.getClass().getName());
             }
 
-            method.unsafeSet(resolvedDeclaringType,
+            return method.unsafeSet(resolvedDeclaringType,
                     methodSymbol.isConstructor() ? resolvedDeclaringType : returnType,
                     parameterTypes, exceptionTypes, listAnnotations(methodSymbol));
-            return method;
         }
 
         return null;
