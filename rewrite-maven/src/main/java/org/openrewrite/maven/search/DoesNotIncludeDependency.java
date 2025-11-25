cfb5a04e8dfb0692bbac63991fe77308dde79faf
Tim te Beek <timtebeek@gmail.com>
2025-11-25 15:13:57 +0100

    Apply suggestions from code review

--- rewrite-maven/src/main/java/org/openrewrite/maven/search/DoesNotIncludeDependency.java
+++ rewrite-maven/src/main/java/org/openrewrite/maven/search/DoesNotIncludeDependency.java
@@ -56,7 +56,7 @@ public class DoesNotIncludeDependency extends Recipe {
 
     @Option(displayName = "Version",
             description = "Match only dependencies with the specified version. " +
-                    "Node-style [version selectors](https://docs.openrewrite.org/reference/dependency-version-selectors) may be used." +
+                    "Node-style [version selectors](https://docs.openrewrite.org/reference/dependency-version-selectors) may be used. " +
                     "All versions are searched by default.",
             example = "1.x",
             required = false)
