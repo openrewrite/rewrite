76c4531df6050c85d1f593a7b396aba4a3bc31ba
Tim te Beek <tim@moderne.io>
2025-11-25 13:01:58 +0000

    Drop `com.sun.tools.javac.code.@Nullable` (#6336)
    - As seen on https://github.com/openrewrite/rewrite/pull/5337/

--- rewrite-java-21/src/main/java/org/openrewrite/java/isolated/ReloadableJava21TypeMapping.java
+++ rewrite-java-21/src/main/java/org/openrewrite/java/isolated/ReloadableJava21TypeMapping.java
@@ -45,7 +45,7 @@ class ReloadableJava21TypeMapping implements JavaTypeMapping<Tree> {
 
     private final JavaTypeCache typeCache;
 
-    public JavaType type(com.sun.tools.javac.code.@Nullable Type type) {
+    public JavaType type(@Nullable Type type) {
         if (type == null || type instanceof Type.ErrorType || type instanceof Type.PackageType || type instanceof Type.UnknownType ||
                 type instanceof NullType) {
             return JavaType.Class.Unknown.getInstance();
@@ -448,7 +448,7 @@ class ReloadableJava21TypeMapping implements JavaTypeMapping<Tree> {
      * @param symbol     The method symbol.
      * @return Method type attribution.
      */
-    public JavaType.@Nullable Method methodInvocationType(com.sun.tools.javac.code.@Nullable Type selectType, @Nullable Symbol symbol) {
+    public JavaType.@Nullable Method methodInvocationType(@Nullable Type selectType, @Nullable Symbol symbol) {
         if (selectType instanceof Type.ErrorType) {
             try {
                 // Ugly reflection solution, because AttrRecover$RecoveryErrorType is private inner class
