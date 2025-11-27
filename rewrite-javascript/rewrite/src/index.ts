86a128df0ea6617d1de441f8be2c1840a9ea9ae9
Knut Wannheden <knut@moderne.io>
2025-11-27 19:46:58 +0100

    JavaScript: Add `SimplifyObjectBindingProperties` recipe

--- rewrite-javascript/rewrite/src/index.ts
+++ rewrite-javascript/rewrite/src/index.ts
@@ -38,11 +38,14 @@ export async function activate(registry: RecipeRegistry): Promise<void> {
     const {OrderImports} = await import("./recipe/index.js");
     const {ModernizeOctalEscapeSequences, ModernizeOctalLiterals, RemoveDuplicateObjectKeys} = await import("./javascript/migrate/es6/index.js");
     const {ExportAssignmentToExportDefault} = await import("./javascript/migrate/typescript/index.js");
+    const {SimplifyObjectBindingProperties} = await import("./javascript/cleanup/index.js");
+
     registry.register(ExportAssignmentToExportDefault);
     registry.register(OrderImports);
     registry.register(ModernizeOctalEscapeSequences);
     registry.register(ModernizeOctalLiterals);
     registry.register(RemoveDuplicateObjectKeys);
+    registry.register(SimplifyObjectBindingProperties);
 }
 
 RpcCodecs.registerCodec(MarkersKind.ParseExceptionResult, {
---
+++ rewrite-javascript/rewrite/test/javascript/cleanup/simplify-object-binding-properties.test.ts
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2025 the original author or authors.
+ * <p>
+ * Licensed under the Moderne Source Available License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://docs.moderne.io/licensing/moderne-source-available-license
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {RecipeSpec} from "../../../src/test";
+import {javascript} from "../../../src/javascript";
+import {SimplifyObjectBindingProperties} from "../../../src/javascript/cleanup";
+
+describe("SimplifyObjectBindingProperties", () => {
+    const spec = new RecipeSpec();
+    spec.recipe = new SimplifyObjectBindingProperties();
+
+    test("simplifies { x: x } to { x }", () => spec.rewriteRun(
+        //language=javascript
+        javascript(
+            `const { x: x } = obj;`,
+            `const { x } = obj;`
+        )
+    ));
+
+    test("simplifies multiple properties", () => spec.rewriteRun(
+        //language=javascript
+        javascript(
+            `const { foo: foo, bar: bar } = obj;`,
+            `const { foo, bar } = obj;`
+        )
+    ));
+
+    test("simplifies with rest element", () => spec.rewriteRun(
+        //language=javascript
+        javascript(
+            `const { ref: ref, ...props } = obj;`,
+            `const { ref, ...props } = obj;`
+        )
+    ));
+
+    test("does not change when names differ", () => spec.rewriteRun(
+        //language=javascript
+        javascript(
+            `const { x: y } = obj;`
+        )
+    ));
+
+    test("handles mixed properties", () => spec.rewriteRun(
+        //language=javascript
+        javascript(
+            `const { foo: foo, bar: baz, qux: qux } = obj;`,
+            `const { foo, bar: baz, qux } = obj;`
+        )
+    ));
+
+    test("preserves already shorthand properties", () => spec.rewriteRun(
+        //language=javascript
+        javascript(
+            `const { x } = obj;`
+        )
+    ));
+
+    test("handles nested destructuring", () => spec.rewriteRun(
+        //language=javascript
+        javascript(
+            `const { outer: { inner: inner } } = obj;`,
+            `const { outer: { inner } } = obj;`
+        )
+    ));
+
+    test("handles function parameters", () => spec.rewriteRun(
+        //language=javascript
+        javascript(
+            `function foo({ x: x, y: y }) { return x + y; }`,
+            `function foo({ x, y }) { return x + y; }`
+        )
+    ));
+
+    test("handles arrow function parameters", () => spec.rewriteRun(
+        //language=javascript
+        javascript(
+            `const fn = ({ a: a }) => a;`,
+            `const fn = ({ a }) => a;`
+        )
+    ));
+});
