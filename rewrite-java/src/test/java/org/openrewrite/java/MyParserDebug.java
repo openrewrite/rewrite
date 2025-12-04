09d3d15c13110b970051e810d93866c0c26597de
Tim te Beek <timtebeek@gmail.com>
2025-12-04 12:53:31 +0100

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- rewrite-java/src/test/java/org/openrewrite/java/MyParserDebug.java
+++ rewrite-java/src/test/java/org/openrewrite/java/MyParserDebug.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2025 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.openrewrite.java;
 
 import org.junit.jupiter.api.Test;
@@ -5,7 +20,8 @@ import org.openrewrite.SourceFile;
 import org.openrewrite.java.tree.J;
 import org.openrewrite.tree.ParseError;
 import java.util.List;
-import java.util.stream.Collectors;
+import static java.util.stream.Collectors.toList;
+
 import org.openrewrite.java.TreeVisitingPrinter;
 
 
@@ -25,7 +41,7 @@ class MyParserDebug {
         """;
 
         List<SourceFile> sourceFiles = parser.parse(sourceCode)
-          .collect(Collectors.toList());
+          .collect(toList());
 
         SourceFile result = sourceFiles.get(0);
 
