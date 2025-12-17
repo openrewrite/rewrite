8af053afebd9b53e9403eb909de4a2a89fd7e3bd
Knut Wannheden <knut@moderne.io>
2025-12-17 08:29:09 +0100

    There was actually a test for that `assert`

--- rewrite-core/src/main/java/org/openrewrite/TreeVisitor.java
+++ rewrite-core/src/main/java/org/openrewrite/TreeVisitor.java
@@ -142,7 +142,16 @@ public abstract class TreeVisitor<T extends @Nullable Tree, P> {
     }
 
     public @Nullable T visit(@Nullable Tree tree, P p, Cursor parent) {
-        // DUMMY CHANGE TO FORCE GRADLE RUNNING TESTS
+        assert parent.isRoot() ||
+                !(parent.getValue() instanceof Tree) ||
+                !((Tree) parent.getValue()).isScope(tree) ||
+                !(p instanceof ExecutionContext) ||
+                !CursorValidatingExecutionContextView.view((ExecutionContext) p).getValidateCursorAcyclic() :
+                "The `parent` cursor must not point to the same `tree` as the tree to be visited. " +
+                        "This usually indicates that you have used getCursor() where getCursor().getParent() is appropriate. " +
+                        "This is a test-only validation which can be opted out of by configuring your test's type validation options with `cursorAcyclic(false)`.";
+        // FORCE GRADLE TO RUN ALL TESTS
+        assert true == true;
         this.cursor = parent;
         return visit(tree, p);
     }
