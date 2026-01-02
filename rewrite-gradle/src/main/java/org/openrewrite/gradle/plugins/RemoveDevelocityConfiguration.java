4c85a22845621abe2cc72ba82fcc8c638a6c225b
Divyanshu Bargali <divyanshubargali@gmail.com>
2026-01-02 19:44:14 +0530

    Fix Recipe to remove build cache configuration for Develocity (#6465)
    * Add failing tests for Develocity build cache connector configurations
    
    * Add option to remove buildCache configuration in RemoveDevelocityConfiguration
    
    * Update rewrite-gradle/src/test/java/org/openrewrite/gradle/plugins/RemoveDevelocityTest.java
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update rewrite-gradle/src/test/java/org/openrewrite/gradle/plugins/RemoveDevelocityTest.java
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update rewrite-gradle/src/main/java/org/openrewrite/gradle/plugins/RemoveDevelocityConfiguration.java
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Refactor RemoveDevelocityConfiguration to enhance functionality and remove buildCache configurations
    
    * Apply suggestions
    
    * Inline nested recipe to just using the visitor; update tests to run top level
    
    * Condense logic
    
    * Apply automated suggestions
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Tim te Beek <tim@moderne.io>

--- rewrite-gradle/src/main/java/org/openrewrite/gradle/plugins/RemoveDevelocityConfiguration.java
+++ rewrite-gradle/src/main/java/org/openrewrite/gradle/plugins/RemoveDevelocityConfiguration.java
@@ -15,13 +15,28 @@
  */
 package org.openrewrite.gradle.plugins;
 
+import org.jspecify.annotations.Nullable;
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.Preconditions;
 import org.openrewrite.Recipe;
+import org.openrewrite.TreeVisitor;
 import org.openrewrite.gradle.RemoveExtension;
+import org.openrewrite.groovy.GroovyIsoVisitor;
+import org.openrewrite.internal.ListUtils;
+import org.openrewrite.java.MethodMatcher;
+import org.openrewrite.java.search.UsesMethod;
+import org.openrewrite.java.tree.J;
+import org.openrewrite.java.tree.Statement;
 
 import java.util.Arrays;
 import java.util.List;
 
+import static java.util.Collections.singletonList;
+
 public class RemoveDevelocityConfiguration extends Recipe {
+
+    protected static final MethodMatcher BUILD_CACHE_MATCHER = new MethodMatcher("*..* buildCache(..)");
+
     @Override
     public String getDisplayName() {
         return "Remove Develocity configuration";
@@ -29,7 +44,45 @@ public class RemoveDevelocityConfiguration extends Recipe {
 
     @Override
     public String getDescription() {
-        return "Remove Develocity configuration from a Gradle build.";
+        return "Remove the Develocity Gradle plugin and associated configuration.";
+    }
+
+    @Override
+    public TreeVisitor<?, ExecutionContext> getVisitor() {
+        return Preconditions.check(
+                new UsesMethod<>(BUILD_CACHE_MATCHER),
+                new GroovyIsoVisitor<ExecutionContext>() {
+                    @Override
+                    public J.@Nullable MethodInvocation visitMethodInvocation(J.MethodInvocation method, ExecutionContext ctx) {
+                        J.MethodInvocation m = super.visitMethodInvocation(method, ctx);
+
+                        if (BUILD_CACHE_MATCHER.matches(m) &&
+                                m.getArguments().size() == 1 &&
+                                m.getArguments().get(0) instanceof J.Lambda) {
+                            J.Lambda lambda = (J.Lambda) m.getArguments().get(0);
+                            J.Block body = (J.Block) lambda.getBody();
+
+                            List<Statement> filteredStatements = ListUtils.filter(body.getStatements(),
+                                    stmt -> {
+                                        if (stmt instanceof J.Return && ((J.Return) stmt).getExpression() instanceof J.MethodInvocation) {
+                                            // Unpack J.Return wrapping last statement in closure
+                                            stmt = (J.MethodInvocation) ((J.Return) stmt).getExpression();
+                                        }
+                                        if (!(stmt instanceof J.MethodInvocation)) {
+                                            return true;
+                                        }
+                                        // Remove only 'remote' method invocations
+                                        return !"remote".equals(((J.MethodInvocation) stmt).getSimpleName());
+                                    });
+                            if (filteredStatements.isEmpty()) {
+                                return null;
+                            }
+                            return m.withArguments(singletonList(lambda.withBody(body.withStatements(filteredStatements))));
+                        }
+
+                        return m;
+                    }
+                });
     }
 
     @Override
