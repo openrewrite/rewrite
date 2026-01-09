18ef4479fa19ef44056fbe46ac8d4cb0a63f036a
Steve Elliott <steve@moderne.io>
2026-01-09 15:36:56 -0500

    Altering check for `AddDependency` to compare versions differently when direct vs indirect dependency. (#6505)
    * If transitive, actually just allow regardless of version, as per old behaviour.

--- rewrite-maven/src/main/java/org/openrewrite/maven/AddDependency.java
+++ rewrite-maven/src/main/java/org/openrewrite/maven/AddDependency.java
@@ -30,6 +30,7 @@ import org.openrewrite.maven.tree.ResolvedDependency;
 import org.openrewrite.maven.tree.ResolvedGroupArtifactVersion;
 import org.openrewrite.maven.tree.Scope;
 import org.openrewrite.semver.Semver;
+import org.openrewrite.semver.VersionComparator;
 import org.openrewrite.xml.tree.Xml;
 
 import java.util.*;
@@ -219,6 +220,8 @@ public class AddDependency extends ScanningRecipe<AddDependency.Scanned> {
                     return maven;
                 }
 
+                VersionComparator vc = requireNonNull(Semver.validate(version, versionPattern).getValue());
+
                 // If the dependency is already in compile scope it will be available everywhere, no need to continue
                 Map<Scope, List<ResolvedDependency>> dependencies = getResolutionResult().getDependencies();
                 if (dependencies.get(Scope.Compile) != null) {
@@ -226,7 +229,8 @@ public class AddDependency extends ScanningRecipe<AddDependency.Scanned> {
                         if (hasAcceptableTransitivity(d, acc) &&
                             groupId.equals(d.getGroupId()) &&
                             artifactId.equals(d.getArtifactId()) &&
-                            version.equals(d.getVersion())
+                            (d.isTransitive() ||
+                                    (d.isDirect() && version.equals(d.getVersion())))
                         ) {
                             return maven;
                         }
