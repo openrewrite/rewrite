380090eb111deb2bfafc9cc25c5a66972fc714f2
Merlin Bögershausen <merlin.boegershausen@rwth-aachen.de>
2026-01-13 16:25:29 +0100

    add recipe to change an XML tags attribute.

---
+++ rewrite-xml/src/main/java/org/openrewrite/xml/ChangeTagAttributeKey.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2026 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.xml;
+
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+import org.jspecify.annotations.Nullable;
+import org.openrewrite.*;
+import org.openrewrite.xml.tree.Xml;
+
+import static org.openrewrite.internal.ListUtils.map;
+
+@Value
+@EqualsAndHashCode(callSuper = false)
+public class ChangeTagAttributeKey extends Recipe {
+
+    String displayName = "Change XML attribute key";
+    String description = "Change an attributes key on XML elements matching the given XPath expression.";
+
+    @Option(displayName = "Element XPath",
+            description = "XPath expression to match elements.",
+            example = "//a4j:ajax")
+    String elementXPath;
+
+    @Option(displayName = "Old attribute name",
+            description = "The current name of the attribute to rename.",
+            example = "reRender")
+    String oldAttributeName;
+
+    @Option(displayName = "New attribute name",
+            description = "The new name for the attribute.",
+            example = "render")
+    String newAttributeName;
+
+    @Option(displayName = "File matcher",
+            description = "Glob pattern for files to process.",
+            example = "**/*.xhtml",
+            required = false)
+    @Nullable
+    String fileMatcher;
+
+    @Override
+    public TreeVisitor<?, ExecutionContext> getVisitor() {
+        XPathMatcher xPathMatcher = new XPathMatcher(elementXPath);
+        return Preconditions.check(new FindSourceFiles(fileMatcher), new XmlIsoVisitor<ExecutionContext>() {
+            @Override
+            public Xml.Tag visitTag(Xml.Tag tag, ExecutionContext ctx) {
+                Xml.Tag t = super.visitTag(tag, ctx);
+                if (!xPathMatcher.matches(getCursor())) {
+                    return t;
+                }
+
+                return t.withAttributes(map(t.getAttributes(),
+                        attr -> oldAttributeName.equals(attr.getKeyAsString()) ?
+                                attr.withKey(attr.getKey().withName(newAttributeName)) : attr));
+            }
+        });
+    }
+}
---
+++ rewrite-xml/src/test/java/org/openrewrite/xml/ChangeTagAttributeKeyTest.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2026 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.xml;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.xml.Assertions.xml;
+
+class ChangeTagAttributeKeyTest implements RewriteTest {
+
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.recipe(new ChangeTagAttributeKey(
+          "//a4j:ajax",
+          "reRender",
+          "render",
+          "**/*.xhtml"
+        ));
+    }
+
+    @DocumentExample
+    @Test
+    void renameReRenderToRender() {
+        rewriteRun(
+          //language=xml
+          xml(
+            """
+              <?xml version="1.0" encoding="UTF-8"?>
+              <html xmlns="http://www.w3.org/1999/xhtml"
+                    xmlns:a4j="http://richfaces.org/a4j">
+                  <body>
+                      <a4j:ajax reRender="form:output" event="change"/>
+                  </body>
+              </html>
+              """,
+            """
+              <?xml version="1.0" encoding="UTF-8"?>
+              <html xmlns="http://www.w3.org/1999/xhtml"
+                    xmlns:a4j="http://richfaces.org/a4j">
+                  <body>
+                      <a4j:ajax render="form:output" event="change"/>
+                  </body>
+              </html>
+              """,
+            spec -> spec.path("test.xhtml")
+          )
+        );
+    }
+
+    @Test
+    void noChangeWhenAttributeNotPresent() {
+        rewriteRun(
+          //language=xml
+          xml(
+            """
+              <?xml version="1.0" encoding="UTF-8"?>
+              <html xmlns="http://www.w3.org/1999/xhtml"
+                    xmlns:a4j="http://richfaces.org/a4j">
+                  <body>
+                      <a4j:ajax render="form:output" event="change"/>
+                  </body>
+              </html>
+              """,
+            spec -> spec.path("test.xhtml")
+          )
+        );
+    }
+
+    @Test
+    void noChangeWhenElementDoesNotMatch() {
+        rewriteRun(
+          //language=xml
+          xml(
+            """
+              <?xml version="1.0" encoding="UTF-8"?>
+              <html xmlns="http://www.w3.org/1999/xhtml"
+                    xmlns:h="http://java.sun.com/jsf/html">
+                  <body>
+                      <h:inputText reRender="form:output"/>
+                  </body>
+              </html>
+              """,
+            spec -> spec.path("test.xhtml")
+          )
+        );
+    }
+}
