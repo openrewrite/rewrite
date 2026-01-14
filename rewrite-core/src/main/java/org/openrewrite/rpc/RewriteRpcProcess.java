560e77f77b96a39453e6597ee0c0462618a0f7a6
Knut Wannheden <knut@moderne.io>
2026-01-14 11:41:02 +0100

    Revert accidental change

--- rewrite-core/src/main/java/org/openrewrite/rpc/RewriteRpcProcess.java
+++ rewrite-core/src/main/java/org/openrewrite/rpc/RewriteRpcProcess.java
@@ -21,7 +21,9 @@ import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import io.moderne.jsonrpc.JsonRpc;
+import io.moderne.jsonrpc.formatter.JsonMessageFormatter;
 import io.moderne.jsonrpc.handler.HeaderDelimitedMessageHandler;
 import io.moderne.jsonrpc.handler.MessageHandler;
 import io.moderne.jsonrpc.handler.TraceMessageHandler;
@@ -128,7 +130,11 @@ public class RewriteRpcProcess extends Thread {
             }
         }
 
-        MessageHandler handler = new HeaderDelimitedMessageHandler(
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(Path.class, new PathSerializer());
+        module.addDeserializer(Path.class, new PathDeserializer());
+        JsonMessageFormatter formatter = new JsonMessageFormatter(module);
+        MessageHandler handler = new HeaderDelimitedMessageHandler(formatter,
                 process.getInputStream(), process.getOutputStream());
         if (trace) {
             handler = new TraceMessageHandler("client", handler);
