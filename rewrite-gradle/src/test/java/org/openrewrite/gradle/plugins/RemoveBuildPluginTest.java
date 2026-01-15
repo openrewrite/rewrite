1a461bf012cb108899e9fb76287e9e2dfc344666
Divyanshu Bargali <divyanshubargali@gmail.com>
2026-01-15 12:44:26 +0530

    Refactor RemovePluginVisitor to support Kotlin DSL and enhance plugin removal logic

--- rewrite-gradle/src/test/java/org/openrewrite/gradle/plugins/RemoveBuildPluginTest.java
+++ rewrite-gradle/src/test/java/org/openrewrite/gradle/plugins/RemoveBuildPluginTest.java
@@ -21,6 +21,8 @@ import org.openrewrite.test.RewriteTest;
 
 import static org.openrewrite.gradle.Assertions.buildGradle;
 import static org.openrewrite.gradle.Assertions.settingsGradle;
+import static org.openrewrite.gradle.Assertions.buildGradleKts;
+import static org.openrewrite.gradle.Assertions.settingsGradleKts;
 
 class RemoveBuildPluginTest implements RewriteTest {
     @DocumentExample
@@ -125,6 +127,158 @@ class RemoveBuildPluginTest implements RewriteTest {
                   classpath "org.openrewrite:plugin:6.1.6"
                 }
               }
+              """
+          )
+        );
+    }
+
+    @Test
+    void removePluginKts() {
+        rewriteRun(
+          spec -> spec.recipe(new RemoveBuildPlugin("com.jfrog.bintray")),
+          buildGradleKts(
+            """
+              plugins {
+                  id("com.jfrog.bintray") version "1.0"
+              }
+              """,
+            ""
+          )
+        );
+    }
+
+    @Test
+    void removePluginWithoutVersionKts() {
+        rewriteRun(
+          spec -> spec.recipe(new RemoveBuildPlugin("java")),
+          buildGradleKts(
+            """
+              plugins {
+                  id("java")
+              }
+              """,
+            ""
+          )
+        );
+    }
+
+    @Test
+    void removeUnappliedPluginKts() {
+        rewriteRun(
+          spec -> spec.recipe(new RemoveBuildPlugin("com.jfrog.bintray")),
+          buildGradleKts(
+            """
+              plugins {
+                  id("com.jfrog.bintray") version "1.0" apply false
+              }
+              """,
+            ""
+          )
+        );
+    }
+
+    @Test
+    void removeUnappliedPluginWithoutVersionKts() {
+        rewriteRun(
+          spec -> spec.recipe(new RemoveBuildPlugin("com.jfrog.bintray")),
+          buildGradleKts(
+            """
+              plugins {
+                  id("com.jfrog.bintray") apply false
+              }
+              """,
+            ""
+          ),
+          settingsGradleKts(
+            """
+              pluginManagement {
+                  plugins {
+                      id("com.jfrog.bintray") version "1.0"
+                  }
+              }
+              
+              rootProject.name = "example"
+              """
+          )
+        );
+    }
+
+    @Test
+    void applySyntaxKts() {
+        rewriteRun(
+          spec -> spec.recipe(new RemoveBuildPlugin("org.openrewrite.rewrite")),
+          buildGradleKts(
+            """
+              buildscript {
+                  repositories {
+                      maven {
+                          url = uri("https://plugins.gradle.org/m2/")
+                      }
+                  }
+                  dependencies {
+                      classpath("org.openrewrite:plugin:6.1.6")
+                  }
+              }
+              
+              apply(plugin = "org.openrewrite.rewrite")
+              """,
+            """
+              buildscript {
+                  repositories {
+                      maven {
+                          url = uri("https://plugins.gradle.org/m2/")
+                      }
+                  }
+                  dependencies {
+                      classpath("org.openrewrite:plugin:6.1.6")
+                  }
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void removeMultipleOccurrencesKts() {
+        rewriteRun(
+          spec -> spec.recipe(new RemoveBuildPlugin("com.example.plugin")),
+          buildGradleKts(
+            """
+              plugins {
+                  id("com.example.plugin")
+                  id("com.example.plugin") version "1.0"
+                  id("com.example.plugin") version "1.0" apply false
+                  id("other.plugin") version "2.0"
+              }
+              """,
+            """
+              plugins {
+                  id("other.plugin") version "2.0"
+              }
+              """
+          )
+        );
+    }
+
+    @Test
+    void removePluginLeavesOthersKts() {
+        rewriteRun(
+          spec -> spec.recipe(new RemoveBuildPlugin("com.gradle.develocity")),
+          buildGradleKts(
+            """
+              plugins {
+                  id("com.gradle.develocity") version "3.17"
+                  id("org.springframework.boot") version "3.2.0"
+              }
+              
+              apply(plugin = "java")
+              """,
+            """
+              plugins {
+                  id("org.springframework.boot") version "3.2.0"
+              }
+              
+              apply(plugin = "java")
               """
           )
         );
