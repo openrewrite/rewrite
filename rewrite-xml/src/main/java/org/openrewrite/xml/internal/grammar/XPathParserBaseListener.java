bbac9bf11d58590ab293859bbd22dae72b248bae
Tim te Beek <tim@moderne.io>
2026-01-30 02:41:54 +0100

    Minimize diff

--- rewrite-xml/src/main/java/org/openrewrite/xml/internal/grammar/XPathParserBaseListener.java
+++ rewrite-xml/src/main/java/org/openrewrite/xml/internal/grammar/XPathParserBaseListener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2026 the original author or authors.
+ * Copyright 2025 the original author or authors.
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-// Generated from /Users/tim/conductor/workspaces/moderne-product-management/jerusalem/working-set-xmlparser-jsp-issues/rewrite/rewrite-xml/src/main/antlr/XPathParser.g4 by ANTLR 4.13.2
+// Generated from /Users/knut/git/openrewrite/rewrite/rewrite-xml/src/main/antlr/XPathParser.g4 by ANTLR 4.13.2
 package org.openrewrite.xml.internal.grammar;
 
 import org.antlr.v4.runtime.ParserRuleContext;
