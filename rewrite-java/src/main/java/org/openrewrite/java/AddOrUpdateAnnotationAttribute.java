70f0dc5cfb79fe27a7ea6896594a8120cd5f4f43
Jente Sondervorst <jentesondervorst@gmail.com>
2026-02-12 13:47:13 +0100

    Remove unused ShortenFullyQualifiedTypeReferences

--- rewrite-java/src/main/java/org/openrewrite/java/AddOrUpdateAnnotationAttribute.java
+++ rewrite-java/src/main/java/org/openrewrite/java/AddOrUpdateAnnotationAttribute.java
@@ -152,9 +152,6 @@ public class AddOrUpdateAnnotationAttribute extends Recipe {
 
                 if (original != a) {
                     doAfterVisit(new SimplifySingleElementAnnotation().getVisitor());
-                    if (isFullyQualifiedClass()) {
-                        doAfterVisit(new ShortenFullyQualifiedTypeReferences().getVisitor());
-                    }
                 }
                 return maybeAutoFormat(original, a, ctx);
             }
