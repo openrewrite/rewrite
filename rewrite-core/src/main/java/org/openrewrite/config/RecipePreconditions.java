3267732952a367a7e89abc087a5270665f061ada
Sam Snyder <sam@moderne.io>
2026-02-19 17:36:04 -0800

    Javadoc screw off

--- rewrite-core/src/main/java/org/openrewrite/config/RecipePreconditions.java
+++ rewrite-core/src/main/java/org/openrewrite/config/RecipePreconditions.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2026 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.openrewrite.config;
 
 import org.openrewrite.Recipe;
@@ -9,7 +24,6 @@ import java.util.List;
  * This is purely informational and not taken into consideration by recipe execution.
  * Imperative recipes interact with preconditions as implementation details of their visitor(s).
  * There is no reason for an imperative recipe to implement this, it will not affect the behavior of the recipe in any way.
- *
  */
 public interface RecipePreconditions {
     List<Recipe> getPreconditions();
