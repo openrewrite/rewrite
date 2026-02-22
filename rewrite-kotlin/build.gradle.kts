adf0613bd6ec2d2074cbecc4f2bdc91765210a8a
Tim te Beek <tim@moderne.io>
2026-02-22 17:38:05 +0100

    Move all `kotlinx` recipes to rewrite-migrate-kotlin (#6791)
    * Update kotlinx-coroutines to 1.10.2 and kotlinx-serialization to 1.8.0
    
    - Upgrade kotlinx-coroutines from 1.8.1 to 1.10.2 (49 recipes, up from 37)
    - Upgrade kotlinx-serialization from 1.6.3 to 1.8.0
    - Fix DeprecatedMethodScanner to include extension function receiver types in method patterns
    - Regenerate type table with updated library versions
    
    * Move all kotlinx recipes to rewrite-migrate-kotlin

--- rewrite-kotlin/build.gradle.kts
+++ rewrite-kotlin/build.gradle.kts
@@ -16,10 +16,6 @@ dependencies {
     implementation(kotlin("compiler-embeddable", kotlinVersion))
     implementation(kotlin("stdlib", kotlinVersion))
 
-    testImplementation("org.jetbrains.kotlin:kotlin-metadata-jvm:2.1.0") {
-        exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib")
-    }
-
     testImplementation("org.junit-pioneer:junit-pioneer:latest.release")
     testImplementation(project(":rewrite-test"))
     testRuntimeOnly(project(":rewrite-java-21"))
@@ -29,17 +25,6 @@ dependencies {
     testImplementation("com.github.ajalt.clikt:clikt:3.5.0")
     testImplementation("com.squareup:javapoet:1.13.0")
     testImplementation("com.google.testing.compile:compile-testing:0.+")
-
-    // Kotlin libraries for KotlinDeprecationRecipeGenerator
-    testRuntimeOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
-    testRuntimeOnly("org.jetbrains.kotlinx:kotlinx-serialization-core:1.6.3")
-}
-
-recipeDependencies {
-    // Kotlin libraries with @Deprecated(replaceWith=ReplaceWith(...)) annotations
-    // Use the JVM variant artifact names since Kotlin multiplatform resolves to these
-    testParserClasspath("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.8.1")
-    testParserClasspath("org.jetbrains.kotlinx:kotlinx-serialization-core-jvm:1.6.3")
 }
 
 java {
@@ -57,27 +42,5 @@ kotlin {
 tasks.named<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>("compileTestKotlin") {
     compilerOptions {
         jvmTarget.set(JvmTarget.JVM_21)
-    }
-}
-
-tasks.withType<Test> {
-    maxHeapSize = "6g"
-    javaLauncher = javaToolchains.launcherFor {
-        languageVersion = JavaLanguageVersion.of(21)
-    }
-}
-
-
-tasks {
-    val generateKotlinDeprecatedReplaceWithRecipes by registering(JavaExec::class) {
-        group = "generate"
-        description = "Generate recipes from Kotlin `@Deprecated` annotations using `ReplaceWith`."
-        mainClass = "org.openrewrite.kotlin.replace.KotlinDeprecatedRecipeGenerator"
-        classpath = sourceSets.getByName("test").runtimeClasspath
-        args(
-            "org.jetbrains.kotlinx:kotlinx-coroutines-core",
-            "org.jetbrains.kotlinx:kotlinx-serialization-core"
-        )
-        finalizedBy("licenseFormat")
     }
 }
