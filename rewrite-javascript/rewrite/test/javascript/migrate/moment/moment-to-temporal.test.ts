c0e5186c4fa561fc0efc9bb487a6c18fda6581e7
Kyle <scullykns@gmail.com>
2026-03-12 10:56:33 -0700

    fix: remove @jest/globals imports after vitest migration

--- rewrite-javascript/rewrite/test/javascript/migrate/moment/moment-to-temporal.test.ts
+++ rewrite-javascript/rewrite/test/javascript/migrate/moment/moment-to-temporal.test.ts
@@ -15,7 +15,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import {describe} from "@jest/globals";
 import {RecipeSpec} from "../../../../src/test";
 import {MomentToTemporal} from "../../../../src/javascript/migrate/moment/moment-to-temporal";
 import {typescript} from "../../../../src/javascript";
