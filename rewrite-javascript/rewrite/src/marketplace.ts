519bcbaa25b2146c15b06e58ed5ba6c11cd79d46
Greg Oledzki <greg.oledzki@moderne.io>
2026-03-18 11:18:19 +0100

    When failing to install a recipe, let the user know which recipe

--- rewrite-javascript/rewrite/src/marketplace.ts
+++ rewrite-javascript/rewrite/src/marketplace.ts
@@ -87,7 +87,7 @@ export namespace RecipeMarketplace {
                         const recipeInst = new recipe({});
                         this.recipes.set(await recipeInst.descriptor(), recipe);
                     } catch (e) {
-                        throw new Error(`Failed to install recipe. Ensure the constructor can be called without any arguments.`);
+                        throw new Error(`Failed to install recipe '${recipe.name}'. Ensure the constructor can be called without any arguments.`, {cause: e});
                     }
                 } else {
                     this.recipes.set(recipe, undefined);
