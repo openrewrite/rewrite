4361ba6c5f48a2f0364e7fa491495c2ff64c1f31
Greg Oledzki <greg.oledzki@moderne.io>
2026-04-28 12:56:05 +0200

    Don't depend on java-tck JAR (#7489)

--- rewrite-java-11/build.gradle.kts
+++ rewrite-java-11/build.gradle.kts
@@ -5,7 +5,15 @@ plugins {
 
 val javaTck = configurations.create("javaTck") {
     isTransitive = false
+    isCanBeConsumed = false
+    isCanBeResolved = true
 }
+
+val javaTckClasses = javaTck.incoming.artifactView {
+    attributes {
+        attribute(LibraryElements.LIBRARY_ELEMENTS_ATTRIBUTE, objects.named(LibraryElements.CLASSES))
+    }
+}.files
 
 dependencies {
     api(project(":rewrite-core"))
@@ -84,7 +92,7 @@ testing {
                 all {
                     testTask.configure {
                         useJUnitPlatform()
-                        testClassesDirs += files(javaTck.files.map { zipTree(it) })
+                        testClassesDirs += javaTckClasses
                         jvmArgs = listOf("-XX:+UnlockDiagnosticVMOptions", "-XX:+ShowHiddenFrames")
                         shouldRunAfter(test)
                     }
