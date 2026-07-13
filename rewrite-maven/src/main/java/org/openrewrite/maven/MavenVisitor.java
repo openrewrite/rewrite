73950c19590f1dd098370a81eca6ccca10a28af9
Tim te Beek <tim@moderne.io>
2026-07-13 12:09:17 +0200

    Drop redundant PROFILE_PLUGIN_DEPENDENCY_MATCHER; //plugins already covers profiles

--- rewrite-maven/src/main/java/org/openrewrite/maven/MavenVisitor.java
+++ rewrite-maven/src/main/java/org/openrewrite/maven/MavenVisitor.java
@@ -39,7 +39,6 @@ public class MavenVisitor<P> extends XmlVisitor<P> {
     static final XPathMatcher DEPENDENCY_MATCHER = new XPathMatcher("/project/dependencies/dependency");
     static final XPathMatcher PROFILE_DEPENDENCY_MATCHER = new XPathMatcher("/project/profiles/profile/dependencies/dependency");
     static final XPathMatcher PLUGIN_DEPENDENCY_MATCHER = new XPathMatcher("//plugins/plugin/dependencies/dependency");
-    static final XPathMatcher PROFILE_PLUGIN_DEPENDENCY_MATCHER = new XPathMatcher("/project/profiles/profile/build/plugins/plugin/dependencies/dependency");
     static final XPathMatcher MANAGED_DEPENDENCY_MATCHER = new XPathMatcher("/project/dependencyManagement/dependencies/dependency");
     static final XPathMatcher PROFILE_MANAGED_DEPENDENCY_MATCHER = new XPathMatcher("/project/profiles/profile/dependencyManagement/dependencies/dependency");
     static final XPathMatcher PROPERTY_MATCHER = new XPathMatcher("/project/properties/*");
@@ -164,8 +163,7 @@ public class MavenVisitor<P> extends XmlVisitor<P> {
     }
 
     public boolean isPluginDependencyTag() {
-        return isTag("dependency") &&
-                (PLUGIN_DEPENDENCY_MATCHER.matches(getCursor()) || PROFILE_PLUGIN_DEPENDENCY_MATCHER.matches(getCursor()));
+        return isTag("dependency") && PLUGIN_DEPENDENCY_MATCHER.matches(getCursor());
     }
 
     public boolean isPluginDependencyTag(String groupId, String artifactId) {
