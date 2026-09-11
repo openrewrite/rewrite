a8aeb3dfef61fa93ca66832adab9b9a7fe7dac02
Greg Oledzki <greg.oledzki@moderne.io>
2026-09-11 13:52:34 +0200

    JavaScript: downgrade commander to v10

---
+++ rewrite-javascript/rewrite/test/commander-version.test.ts
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2026 the original author or authors.
+ * <p>
+ * Licensed under the Moderne Source Available License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://docs.moderne.io/licensing/moderne-source-available-license
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as semver from "semver";
+import packageJson from "../package.json";
+
+describe("commander version", () => {
+    test("declared range cannot resolve to 11 or later, so the RPC server stays runnable on Node 14", () => {
+        // given
+        const range = packageJson.dependencies.commander;
+
+        // when
+        const allowsEleven = semver.intersects(range, ">=11.0.0");
+
+        // then
+        expect(semver.validRange(range)).not.toBeNull();
+        expect(allowsEleven).toBe(false);
+    });
+});
